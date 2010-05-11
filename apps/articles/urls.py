4221998c62017767ee019184913a20990cd53e9c
eloy <ezuniga@schipul.com>
2010-05-11 18:09:05 -0500

    Building articles module.

--- apps/articles/urls.py
+++ apps/articles/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',                  
-    url(r'^$', 'articles.views.articles', name="articles"),
-#    url(r'^add/$', 'articles.views.add', name="article_add"),
-#    url(r'^edit/(\d+)/$', 'articles.views.edit', name="article_edit"),
-#    url(r'^delete/(\d+)/$', 'articles.views.delete', name="article_delete"),     
-#    url(r'^([\d\w\-\/]+)/$', 'articles.views.details', name="article_details"),
+    url(r'^$', 'articles.views.index', name="articles"),
+    url(r'^search/$', 'articles.views.search', name="article.search"),
+    url(r'^view/$', 'articles.views.view', name="article.view"),
+    url(r'^print-view/$', 'articles.views.print_view', name="article.print_view"),
+    url(r'^edit/$', 'articles.views.edit', name="article.edit"),
+    url(r'^delete/$', 'articles.views.delete', name="article.delete"),
 )
--- apps/articles/views.py
+++ apps/articles/views.py
@@ -1,13 +1,30 @@
 # django
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 # local
-#from articles.models import Article
+from articles.models import Article
 
+def index(request, template_name="articles/articles.html"):
+    return HttpResponseRedirect(reverse('article.search'))
 
-# Create your views here.
-def articles(request, template_name="articles/articles.html"):
+def search(request, template_name="articles/search.html"):
+    return render_to_response(template_name, {}, 
+        context_instance=RequestContext(request))
 
+def view(request, template_name="articles/view.html"):
+    return render_to_response(template_name, {}, 
+        context_instance=RequestContext(request))
+
+def print_view(request, template_name="articles/print-view.html"):
+    return render_to_response(template_name, {}, 
+        context_instance=RequestContext(request))
+
+def edit(request, template_name="articles/edit.html"):
+    return render_to_response(template_name, {}, 
+        context_instance=RequestContext(request))
+
+def delete(request, template_name="articles/delete.html"):
     return render_to_response(template_name, {}, 
         context_instance=RequestContext(request))
