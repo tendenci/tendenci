3ca9dc8254a4a4f660c44fe6f7f2452f2e1efb2d
eloy <ezuniga@schipul.com>
2010-05-13 18:30:33 -0500

    Articles module. Made articles-edit page. It works and is linked, but has thin styles applied.

--- apps/articles/views.py
+++ apps/articles/views.py
@@ -5,6 +5,7 @@ from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from articles.models import Article
+from articles.forms import ArticleForm
 
 def index(request, id=None, template_name="articles/view.html"):
     if not id: return HttpResponseRedirect(reverse('article.search'))
@@ -25,7 +26,14 @@ def print_view(request, id, template_name="articles/print-view.html"):
 @login_required
 def edit(request, id, template_name="articles/edit.html"):
     article = get_object_or_404(Article, pk=id)
-    return render_to_response(template_name, {'article': article}, 
+    form = ArticleForm(instance=article)
+
+    if request.method == "POST":
+        form = ArticleForm(request.POST, request.user, instance=article)
+        if form.is_valid():
+            article = form.save()
+
+    return render_to_response(template_name, {'article': article, 'form':form}, 
         context_instance=RequestContext(request))
 
 @login_required
