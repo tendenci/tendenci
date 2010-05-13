52046dee3b596cd9af7a1a07b33a581c701b8332
eloy <ezuniga@schipul.com>
2010-05-13 16:05:59 -0500

    Articles module.  Building out the base pages. View, edit, delete.  Added a little bit of JS for delete confirmation prompt and pagination.

--- apps/articles/urls.py
+++ apps/articles/urls.py
@@ -2,10 +2,9 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',                  
     url(r'^$', 'articles.views.index', name="articles"),
+    url(r'^(?P<id>\d+)/$', 'articles.views.index', name="article"),
     url(r'^search/$', 'articles.views.search', name="article.search"),
-    # TODO: use slugs instead of view/<id>
-    url(r'^view/$', 'articles.views.view', name="article.view"),
-    url(r'^print-view/$', 'articles.views.print_view', name="article.print_view"),
-    url(r'^edit/$', 'articles.views.edit', name="article.edit"),
-    url(r'^delete/$', 'articles.views.delete', name="article.delete"),
+    url(r'^print-view/(?P<id>\d+)/$', 'articles.views.print_view', name="article.print_view"),
+    url(r'^edit/(?P<id>\d+)/$', 'articles.views.edit', name="article.edit"),
+    url(r'^delete/(?P<id>\d+)/$', 'articles.views.delete', name="article.delete"),
 )
--- settings.py
+++ settings.py
@@ -72,6 +72,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'pagination.middleware.PaginationMiddleware',
 )
 
 ROOT_URLCONF = 'Tendenci50.urls'
@@ -83,12 +84,20 @@ TEMPLATE_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.auth",
+    "django.core.context_processors.request",
+)
+
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
+
+    # plugin package
+    'pagination',
 
     # tendenci application
     'profiles',
