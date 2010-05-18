4169dec6e6f642d6f1e9a304d8857fc8d8806743
glenbot <gzangirolami@schipul.com>
2010-05-18 12:58:14 -0500

    Updated the article forms and seperated them into two classes. One for add and one for edit (mainly for owner field and other customizations). Added an articles add page. Added permission checks to add, edit, delete, view to articles. Articles add now assigns item level permissions to the user that creates or edits.

--- apps/articles/views.py
+++ apps/articles/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
 
 from articles.models import Article
-from articles.forms import ArticleForm
+from articles.forms import ArticleForm, ArticleEditForm
 from articles.permissions import ArticlePermission
 from base.auth import Authorize
 
@@ -14,13 +14,13 @@ from base.auth import Authorize
 def index(request, id=None, template_name="articles/view.html"):
     if not id: return HttpResponseRedirect(reverse('article.search'))
     article = get_object_or_404(Article, pk=id)
-    auth_check = Authorize(request.user, ArticlePermission, article)
+    auth = Authorize(request.user, ArticlePermission, article)
     
-    if auth_check.view():
+    if auth.view(): # TODO : maybe make this a decorator
         return render_to_response(template_name, {'article': article}, 
             context_instance=RequestContext(request))
     else:
-        raise Http404
+        raise Http404 # TODO : change where this goes
 
 def search(request, template_name="articles/search.html"):
     articles = Article.objects.all()
@@ -33,28 +33,65 @@ def print_view(request, id, template_name="articles/print-view.html"):
         context_instance=RequestContext(request))
 
 @login_required
-def edit(request, id, template_name="articles/edit.html"):
+def edit(request, id, form_class=ArticleEditForm, template_name="articles/edit.html"):
     article = get_object_or_404(Article, pk=id)
-    form = ArticleForm(instance=article)
-
-    auth_check = Authorize(request.user, ArticlePermission, article)
-    if auth_check.edit():     
+    auth = Authorize(request.user, ArticlePermission, article)
+    
+    if auth.edit(): # TODO : maybe make this a decorator    
         if request.method == "POST":
-            form = ArticleForm(request.POST, request.user, instance=article)
+            form = form_class(request.user, request.POST, instance=article)
             if form.is_valid():
-                article = form.save()
+                article = form.save(commit=False)
+                article.save()
+                
+                # assign permissions
+                auth.assign(content_object=article)   
+                
+                return HttpResponseRedirect(reverse('article', args=[article.pk]))             
+        else:
+            form = form_class(request.user, instance=article)
 
         return render_to_response(template_name, {'article': article, 'form':form}, 
             context_instance=RequestContext(request))
     else:
-        raise Http404
+        raise Http404 # TODO : change where this goes
 
+@login_required
+def add(request, form_class=ArticleForm, template_name="articles/add.html"):
+    auth = Authorize(request.user, ArticlePermission)
+    
+    if auth.add(): # TODO : maybe make this a decorator   
+        if request.method == "POST":
+            form = form_class(request.user, request.POST)
+            if form.is_valid():
+                article = form.save(commit=False)
+                
+                # set up the user information
+                article.creator = request.user
+                article.creator_username = request.user.username
+                article.owner = request.user
+                article.owner_username = request.user.username
+                
+                article.save()
+                
+                # assign permissions
+                auth.assign(content_object=article)
+                
+                return HttpResponseRedirect(reverse('article', args=[article.pk]))
+        else:
+            form = form_class(request.user)
+           
+        return render_to_response(template_name, {'form':form}, 
+            context_instance=RequestContext(request))
+    else:
+        raise Http404 # TODO : change where this goes
+    
 @login_required
 def delete(request, id, template_name="articles/delete.html"):
     article = get_object_or_404(Article, pk=id)
 
-    auth_check = Authorize(request.user, ArticlePermission, article)
-    if auth_check.delete():     
+    auth = Authorize(request.user, ArticlePermission, article)
+    if auth.delete(): # TODO : make this a decorator    
         if request.method == "POST":
             article.delete()
             return HttpResponseRedirect(reverse('article.search'))
@@ -62,4 +99,4 @@ def delete(request, id, template_name="articles/delete.html"):
         return render_to_response(template_name, {'article': article}, 
             context_instance=RequestContext(request))
     else:
-        raise Http404
+        raise Http404 # TODO : change where this goes
