6a03b4a0d5c7973293a80623e672e0d119073272
glenbot <gzangirolami@schipul.com>
2010-05-25 13:55:00 -0500

    Added group id to the perms model

--- apps/perms/models.py
+++ apps/perms/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 from perms.managers import ObjectPermissionManager
 
 class ObjectPermission(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, null=True)
+    group = models.ForeignKey(Group, null=True)
     content_type = models.ForeignKey(ContentType)
     codename = models.CharField(max_length=255)
     object_id = models.IntegerField()
