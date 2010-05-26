89c0c20a8b96eae756bf2d4b5a2d2e6ed4ef19ba
glenbot <gzangirolami@schipul.com>
2010-05-26 13:14:27 -0500

    Updates settings model to add choices for input_type, data_type, scope and scope_category. Added site_settings to the django admin.

---
+++ apps/site_settings/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from site_settings.models import Setting
+
+admin.site.register(Setting)
--- apps/site_settings/models.py
+++ apps/site_settings/models.py
@@ -1,21 +1,43 @@
 from django.db import models
 
+SCOPE_CHOICES = (
+    ('module', 'Module'),
+    ('site', 'Site'),
+)
+
+SCOPE_CATEGORY_CHOICES = (
+    ('global','Global'),
+    ('articles','Articles'),
+    ('news','News'),
+)
+
+INPUT_TYPE_CHOICES = (
+    ('text','Text'),
+    ('select','Select'),         
+)
+
+DATA_TYPE_CHOICES = (
+    ('string','string'),
+    ('boolean','boolean'),
+    ('integer','int'),         
+)
+
 class Setting(models.Model):
     name = models.CharField(max_length=50)
     label = models.CharField(max_length=255)
     description = models.TextField()
-    data_type = models.CharField(max_length=10)
-    value = models.TextField()
-    default_value = models.TextField()
-    input_type = models.CharField(max_length=25)
-    input_value = models.CharField(max_length=255)
-    client_editable = models.BooleanField()
-    store = models.BooleanField()
+    data_type = models.CharField(max_length=10, choices=DATA_TYPE_CHOICES)
+    value = models.TextField(blank=True)
+    default_value = models.TextField(blank=True)
+    input_type = models.CharField(max_length=25, choices=INPUT_TYPE_CHOICES)
+    input_value = models.CharField(max_length=255, blank=True)
+    client_editable = models.BooleanField(default=True)
+    store = models.BooleanField(default=True)
     update_dt = models.DateTimeField(auto_now=True, null=True)
-    updated_by = models.CharField(max_length=50)
-    scope = models.CharField(max_length=50)
-    scope_category = models.CharField(max_length=50)
-    parent_id = models.IntegerField()
+    updated_by = models.CharField(max_length=50, blank=True)
+    scope = models.CharField(max_length=50, choices=SCOPE_CHOICES)
+    scope_category = models.CharField(max_length=50, choices=SCOPE_CATEGORY_CHOICES)
+    parent_id = models.IntegerField(blank=True, default=0)
 
     def get_absolute_url(self):
         return ("setting.permalink", 
