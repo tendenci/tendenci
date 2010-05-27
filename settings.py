6917884153e9e2d485d67dc9df16e9df5b758958
eloy <ezuniga@schipul.com>
2010-05-27 13:35:09 -0500

    Removed MediaUploadMiddleware.

--- settings.py
+++ settings.py
@@ -89,7 +89,6 @@ TEMPLATE_LOADERS = (
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
-#    'swfupload.middleware.MediaUploadMiddleware',
     'swfupload.middleware.SWFUploadMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
