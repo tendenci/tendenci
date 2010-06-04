f170ea579e42c088d2e06c358416303f0e3c026b
glenbot <gzangirolami@schipul.com>
2010-06-04 15:24:14 -0500

    Added permissions to stories and updates all the templates to use site setting, permission, and inherit from the correct base.

---
+++ apps/stories/templatetags/story_tags.py
@@ -0,0 +1,18 @@
+from django.template import Library
+
+register = Library()
+
+@register.inclusion_tag("stories/options.html", takes_context=True)
+def stories_options(context, user, story):
+    context.update({
+        "story": story,
+        "user": user
+    })
+    return context
+
+@register.inclusion_tag("stories/nav.html", takes_context=True)
+def stories_nav(context, user):
+    context.update({
+        "user": user
+    })
+    return context
