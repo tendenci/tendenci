c2e5f9e99a319c82272d98f1ef2632d2cb7a8454
eloy <ezuniga@schipul.com>
2010-06-04 17:48:18 -0500

    Updating permissions on files module.

--- apps/files/forms.py
+++ apps/files/forms.py
@@ -1,11 +1,13 @@
 from django import forms
-
 from files.models import File
-from perms.forms import AuditingBaseForm
 
-class FileForm(AuditingBaseForm):
+class FileForm(forms.ModelForm):
     class Meta:
         model = File
         fields = (
         'file',
         )
+
+    def __init__(self, user=None, *args, **kwargs): 
+        self.user = user
+        super(FileForm, self).__init__(user, *args, **kwargs)
