0548531adb7402d151b73dda20dae8120cc39333
jenny <jqian@schipul.com>
2010-06-06 14:49:34 -0500

    Updated avatar change page to take a user id.

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -4,8 +4,24 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, Http404
-
 from django.contrib.auth.models import User
+
+from avatar.models import Avatar, avatar_file_path
+from avatar.forms import PrimaryAvatarForm
+from django.utils.translation import ugettext as _
+from django.db.models import get_app
+from django.core.exceptions import ImproperlyConfigured
+from django.conf import settings
+
+try:
+    notification = get_app('notification')
+except ImproperlyConfigured:
+    notification = None
+    
+friends = False
+if 'friends' in settings.INSTALLED_APPS:
+    friends = True
+    from friends.models import Friendship
 
 from profiles.models import Profile
 from profiles.forms import ProfileForm, ProfileEditForm, UserForm, UserEditForm, UserPermissionForm
@@ -57,7 +73,7 @@ def add(request, form_class=ProfileForm, form_class2=UserForm, template_name="pr
             new_user.user_permissions = form2.cleaned_data['user_permissions']
             new_user.save()
             
-            return HttpResponseRedirect(reverse('profile', args=[new_user.pk]))
+            return HttpResponseRedirect(reverse('profile', args=[new_user.username]))
     else:
         form = form_class()
         form2 = form_class2()
@@ -173,4 +189,76 @@ def edit_user_groups(request, id, template_name="profiles/edit_groups.html"):
         #print request.POST 
     return render_to_response(template_name, {'user_this':user_edit, 'profile':profile, 'groups':groups, 'groups_joined':groups_joined}, 
         context_instance=RequestContext(request))
+ 
+def _get_next(request):
+    """
+    The part that's the least straightforward about views in this module is how they 
+    determine their redirects after they have finished computation.
+
+    In short, they will try and determine the next place to go in the following order:
+
+    1. If there is a variable named ``next`` in the *POST* parameters, the view will
+    redirect to that variable's value.
+    2. If there is a variable named ``next`` in the *GET* parameters, the view will
+    redirect to that variable's value.
+    3. If Django can determine the previous page from the HTTP headers, the view will
+    redirect to that previous page.
+    """
+    next = request.POST.get('next', request.GET.get('next', request.META.get('HTTP_REFERER', None)))
+    if not next:
+        next = request.path
+    return next
+   
+@login_required
+def change_avatar(request, id, extra_context={}, next_override=None):
+    user_edit = get_object_or_404(User, pk=id)
+    avatars = Avatar.objects.filter(user=user_edit).order_by('-primary')
+    if avatars.count() > 0:
+        avatar = avatars[0]
+        kwargs = {'initial': {'choice': avatar.id}}
+    else:
+        avatar = None
+        kwargs = {}
+    primary_avatar_form = PrimaryAvatarForm(request.POST or None, user=user_edit, **kwargs)
+    if request.method == "POST":
+        updated = False
+        if 'avatar' in request.FILES:
+            path = avatar_file_path(user=user_edit, 
+                filename=request.FILES['avatar'].name)
+            avatar = Avatar(
+                user = user_edit,
+                primary = True,
+                avatar = path,
+            )
+            new_file = avatar.avatar.storage.save(path, request.FILES['avatar'])
+            avatar.save()
+            updated = True
+            request.user.message_set.create(
+                message=_("Successfully uploaded a new avatar."))
+        if 'choice' in request.POST and primary_avatar_form.is_valid():
+            avatar = Avatar.objects.get(id=
+                primary_avatar_form.cleaned_data['choice'])
+            avatar.primary = True
+            avatar.save()
+            updated = True
+            request.user.message_set.create(
+                message=_("Successfully updated your avatar."))
+        if updated and notification:
+            notification.send([request.user], "avatar_updated", {"user": user_edit, "avatar": avatar})
+            if friends:
+                notification.send((x['friend'] for x in Friendship.objects.friends_for_user(user_edit)), "avatar_friend_updated", {"user": user_edit, "avatar": avatar})
+        return HttpResponseRedirect(reverse('profile', args=[user_edit.username]))
+        #return HttpResponseRedirect(next_override or _get_next(request))
+    return render_to_response(
+        'profiles/change_avatar.html',
+        extra_context,
+        context_instance = RequestContext(
+            request,
+            {'user_this': user_edit,
+              'avatar': avatar, 
+              'avatars': avatars,
+              'primary_avatar_form': primary_avatar_form,
+              'next': next_override or _get_next(request), }
+        )
+    )
 
