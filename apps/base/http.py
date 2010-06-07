c09d32bd2b880bbd6a00ade5612d923151ca8458
glenbot <gzangirolami@schipul.com>
2010-06-06 20:59:35 -0500

    Added Http403Middleware that checks for Http403 exceptions in views and renders a HttpResponseForbidden template with request context intact. Updated pages and site_settings to use this new method. The old method is backwards compatible.

--- apps/base/http.py
+++ apps/base/http.py
@@ -9,18 +9,13 @@ def render_to_403(*args, **kwargs):
     Returns a HttpResponseForbidden whose content is filled with the result of calling
     django.template.loader.render_to_string() with the passed arguments.
     """
-    response = None
-    
     if not isinstance(args,list):
         args = []
         args.append('403.html')
         
     httpresponse_kwargs = {'mimetype': kwargs.pop('mimetype', None)}
     
-    try:
-        response = HttpResponseForbidden(loader.render_to_string(*args, **kwargs), **httpresponse_kwargs)
-    except:
-        raise Http403
+    response = HttpResponseForbidden(loader.render_to_string(*args, **kwargs), **httpresponse_kwargs)
         
     return response
         
