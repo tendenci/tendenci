7da0e8cb16f4263c69b52270de48a1f183aa4231
jenny <jqian@schipul.com>
2010-06-13 16:28:38 -0500

    Added upload in stories. Updated permission check for stories.

--- apps/stories/views.py
+++ apps/stories/views.py
@@ -1,22 +1,44 @@
+import os.path
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.conf import settings
 
 from base.http import render_to_403
 from stories.models import Story
-from stories.forms import StoryForm
+from stories.forms import StoryForm, UploadStoryImageForm
 from perms.models import ObjectPermission
+
 
 def index(request, id=None, template_name="stories/view.html"):
     if not id: return HttpResponseRedirect(reverse('story.search'))
     story = get_object_or_404(Story, pk=id)
-    if request.user.has_perm('stories.view_story', story):
-        return render_to_response(template_name, {'story': story}, 
-            context_instance=RequestContext(request))
-    else:
-        return render_to_403()
+    
+    if not story.allow_view_by(request.user): return render_to_403()
+    
+    return render_to_response(template_name, {'story': story}, 
+        context_instance=RequestContext(request))
+    
+# get the latest image name
+def get_latest_image_name(images_path):
+    latest_image = ""
+    if os.path.isdir(images_path):
+        image_list = os.listdir(images_path)
+        
+        if image_list <> []:
+            image_full_path_list = [images_path+'/'+image_name for image_name in image_list]
+            
+            latest_image = image_list[0]
+            mtime = os.path.getmtime(image_full_path_list[0])
+            
+            for i in range(1, len(image_full_path_list)):
+                if mtime < os.path.getmtime(image_full_path_list[i]):
+                    latest_image = image_list[i]
+                    mtime = os.path.getmtime(image_full_path_list[i])
+    return latest_image
+        
     
 
 def search(request, template_name="stories/search.html"):
@@ -33,76 +55,122 @@ def search(request, template_name="stories/search.html"):
     
 @login_required   
 def add(request, form_class=StoryForm, template_name="stories/add.html"):
-    if request.user.has_perm('stories.add_story'):
-        if request.method == "POST":
-            form = form_class(request.user, request.POST)
-            if form.is_valid():           
-                story = form.save(commit=False)
-                # set up the user information
-                story.creator = request.user
-                story.creator_username = request.user.username
-                story.owner = request.user
-                story.owner_username = request.user.username
-                story.save()
-                
-                # assign permissions for selected users
-                user_perms = form.cleaned_data['user_perms']
-                if user_perms:
-                    ObjectPermission.objects.assign(user_perms, story)
-                
-                # assign creator permissions
-                ObjectPermission.objects.assign(story.creator, story) 
-                
-                return HttpResponseRedirect(reverse('story', args=[story.pk]))
-        else:
-            form = form_class(request.user)
-        
-        return render_to_response(template_name, {'form':form}, 
-            context_instance=RequestContext(request))
+    # permission check
+    story = Story()
+    if not story.allow_add_by(request.user): return render_to_403()
+    
+    if request.method == "POST":
+        form = form_class(request.user, request.POST)
+        if form.is_valid():           
+            story = form.save(commit=False)
+            # set up the user information
+            story.allow_anonymous_view = 1
+            story.creator = request.user
+            story.creator_username = request.user.username
+            story.owner = request.user
+            story.owner_username = request.user.username
+            story.save()
+            
+            # assign permissions for selected users
+            user_perms = form.cleaned_data['user_perms']
+            if user_perms:
+                ObjectPermission.objects.assign(user_perms, story)
+            
+            # assign creator permissions
+            ObjectPermission.objects.assign(story.creator, story) 
+            
+            return HttpResponseRedirect(reverse('story', args=[story.pk]))
     else:
-        return render_to_403()
+        form = form_class(request.user)
+    
+    return render_to_response(template_name, {'form':form}, 
+        context_instance=RequestContext(request))
     
 @login_required
 def edit(request, id, form_class=StoryForm, template_name="stories/edit.html"):
     story = get_object_or_404(Story, pk=id)
+    # permission check
+    if not story.allow_edit_by(request.user): return render_to_403()
+    # temporarily - need to use file module to store the image
+    imagepath = os.path.join(settings.MEDIA_ROOT, 'stories/'+str(story.id))
+    image_name = story.get_latest_image_name(imagepath)
 
-    if request.user.has_perm('stories.change_story', story):    
-        if request.method == "POST":
-            form = form_class(request.user, request.POST, instance=story)
-            if form.is_valid():
-                story = form.save(commit=False)
-                story.save()
-
-                # remove all permissions on the object
-                ObjectPermission.objects.remove_all(story)
-                
-                # assign new permissions
-                user_perms = form.cleaned_data['user_perms']
-                if user_perms:
-                    ObjectPermission.objects.assign(user_perms, story)               
- 
-                # assign creator permissions
-                ObjectPermission.objects.assign(story.creator, story) 
-                                                              
-                return HttpResponseRedirect(reverse('story', args=[story.pk]))             
-        else:
-            form = form_class(request.user, instance=story)
-
-        return render_to_response(template_name, {'story': story, 'form':form}, 
-            context_instance=RequestContext(request))
+    if request.method == "POST":
+        form = form_class(request.user, request.POST, instance=story)
+        if form.is_valid():
+            story = form.save(commit=False)
+            story.save()
+    
+            # remove all permissions on the object
+            ObjectPermission.objects.remove_all(story)
+               
+            # assign new permissions
+            user_perms = form.cleaned_data['user_perms']
+            if user_perms:
+                ObjectPermission.objects.assign(user_perms, story)               
+    
+            # assign creator permissions
+            ObjectPermission.objects.assign(story.creator, story) 
+                                                             
+            return HttpResponseRedirect(reverse('story', args=[story.pk]))             
     else:
-        return render_to_403()
+        form = form_class(request.user, instance=story)
+    
+    return render_to_response(template_name, {'story': story, 'form':form, 'image_name':image_name}, 
+        context_instance=RequestContext(request))
+
 
 @login_required
 def delete(request, id, template_name="stories/delete.html"):
     story = get_object_or_404(Story, pk=id)
+    # permission check
+    if not story.allow_edit_by(request.user): return render_to_403()
 
     if request.user.has_perm('stories.delete_stories'):   
         if request.method == "POST":
+            # delete files first
+            imagepath = os.path.join(settings.MEDIA_ROOT, 'stories/'+str(story.id))
+            if os.path.isdir(imagepath):
+                for file in os.listdir(imagepath):
+                    os.remove(os.path.join(imagepath+'/' + file))
+                os.rmdir(imagepath)
+            
             story.delete()
+            
+            
             return HttpResponseRedirect(reverse('story.search'))
     
         return render_to_response(template_name, {'story': story}, 
             context_instance=RequestContext(request))
     else:
         return render_to_403()
+ 
+@login_required   
+def upload(request, id, form_class=UploadStoryImageForm, 
+                template_name="stories/upload.html"):
+    story = get_object_or_404(Story, pk=id)
+    # permission check
+    if not story.allow_edit_by(request.user): return render_to_403()
+    
+    if request.method == 'POST':
+        form = form_class(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data['file']
+            imagename = data.name
+            imagepath = os.path.join(settings.MEDIA_ROOT, 'stories/'+str(story.id))
+            if not os.path.isdir(imagepath):
+                os.makedirs(imagepath)
+            fd = open(imagepath + '/' + imagename, 'wb+')
+            for chunk in data.chunks():
+                fd.write(chunk)
+            fd.close()
+            #filelog(mode='wb+', filename=imagename, path=imagepath)
+            return HttpResponseRedirect(reverse('story', args=[story.pk]))
+    else:
+        form = form_class()
+    return render_to_response(template_name, {'form':form, 'story': story}, 
+            context_instance=RequestContext(request))
+    
+            
+            
+            
