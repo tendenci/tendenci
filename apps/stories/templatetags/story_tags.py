c5fdb10ef14048f3850c95a99d564ef84f8ac0ae
jenny <jqian@schipul.com>
2010-06-13 16:28:38 -0500

    Added upload in stories. Updated permission check for stories.

--- apps/stories/templatetags/story_tags.py
+++ apps/stories/templatetags/story_tags.py
@@ -5,16 +5,18 @@ register = Library()
 @register.inclusion_tag("stories/options.html", takes_context=True)
 def stories_options(context, user, story):
     context.update({
-        "story": story,
-        "user": user
+        "story_this": story,
+        "user_current": user,
     })
     return context
 
 @register.inclusion_tag("stories/nav.html", takes_context=True)
-def stories_nav(context, user):
+def stories_nav(context, user, story=None):
     context.update({
-        "user": user
+        "story_this": story,
+        "user_current": user,
     })
+    print context["story_this"]
     return context
 
 @register.inclusion_tag("stories/search-form.html", takes_context=True)
