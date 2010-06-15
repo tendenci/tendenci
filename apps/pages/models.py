88a2cf6e57c42999e97a257594a84e34365f6216
eloy <ezuniga@schipul.com>
2010-06-15 12:08:27 -0500

    Adding tags to modules; articles, news, pages, and photos.  The tag field is available in the forms.  The tags are currently not being displayed on the front-end pages.  Having trouble with django-tagging template tags.

--- apps/pages/models.py
+++ apps/pages/models.py
@@ -1,34 +1,26 @@
+import uuid
 from django.db import models
-
+from tagging.fields import TagField
 from perms.models import AuditingBaseModel
 from pages.managers import PageManager
 
 class Page(AuditingBaseModel):
-
-    # TODO: make unique=True (dependent on migration script)
-    guid = models.TextField(blank=True)
-
+    guid = models.CharField(max_length=200, default=uuid.uuid1)
     title = models.CharField(max_length=500, blank=True)
     content = models.TextField(blank=True)
-
-    # meta information
-    page_title = models.TextField(blank=True)
-    meta_keywords = models.TextField(blank=True)
-    meta_description = models.TextField(blank=True)
-
+    page_title = models.TextField(blank=True) # meta info (maybe meta_title)
+    meta_keywords = models.TextField(blank=True) # meta info
+    meta_description = models.TextField(blank=True) # meta info
     update_dt = models.DateTimeField(auto_now=True)
     create_dt = models.DateTimeField(auto_now_add=True)
-
     view_contact_form = models.BooleanField()
     design_notes = models.TextField(blank=True)
-
     syndicate = models.BooleanField()
     displaypagetemplate = models.CharField(max_length=50, blank=True)
-
     metacanonical = models.TextField(blank=True)
-
+    tags = TagField(blank=True)
     objects = PageManager()
-    
+
     class Meta:
         permissions = (("view_page","Can view page"),)
 
