3d663972378bed801e45699dd79229b3b2ce34ee
jennyq <jqian@schipul.com>
2010-06-17 16:11:05 -0500

    Added authorize.net response process and thankyou page.

--- apps/payments/authorizenet/urls.py
+++ apps/payments/authorizenet/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('payments.authorizenet.views',
      url(r'^thankyou/(?P<payment_id>\d+)/$', 'sim_thank_you', name="authorizenet.sim_thank_you"),
+     #url(r'^thankyou/(?P<payment_id>\d+)/', 'sim_thank_you', name="authorizenet.sim_thank_you"),
 )
 
