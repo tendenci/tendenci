573c06c10ee3a9315152336afd41f548022f1778
glenbot <gzangirolami@schipul.com>
2010-06-24 10:47:43 -0500

    Updated some field names in the entity model

--- apps/entities/models.py
+++ apps/entities/models.py
@@ -7,9 +7,8 @@ from perms.models import AuditingBaseModel
 class Entity(AuditingBaseModel):
     guid = models.CharField(max_length=40, default=uuid.uuid1)
     entity_name = models.CharField(_('Name'), max_length=200, blank=True)
-    entitytype = models.CharField(_('Type'), max_length=200, blank=True)
-    entityownerid = models.IntegerField(_('Owner ID'), default=0)
-    entityparentid = models.IntegerField(_('Parent ID'), default=0)
+    entity_type = models.CharField(_('Type'), max_length=200, blank=True)
+    entity_parent_id = models.IntegerField(_('Parent ID'), default=0)
       
     # contact info
     contact_name = models.CharField(_('Contact Name'), max_length=200, blank=True)
@@ -17,12 +16,9 @@ class Entity(AuditingBaseModel):
     fax = models.CharField(max_length=50, blank=True)
     email = models.CharField(max_length=120, blank=True)
     website = models.CharField(max_length=300, blank=True)
-
     summary = models.TextField(blank=True)
-
     notes = models.TextField(blank=True)
     admin_notes = models.TextField(_('Admin Notes'), blank=True)
-
 
     class Meta:
         permissions = (("view_entity","Can view entity"),)
