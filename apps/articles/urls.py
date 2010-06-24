56a1ce65778732a4c4035b57d4768e084800c746
eloy <ezuniga@schipul.com>
2010-06-24 11:07:36 -0500

    Added meta-edit form to articles module.

--- apps/articles/urls.py
+++ apps/articles/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls.defaults import patterns, url
 from articles.feeds import LatestEntriesFeed
 
-urlpatterns = patterns('',                  
-    url(r'^$', 'articles.views.index', name="articles"),
-    url(r'^(?P<id>\d+)/$', 'articles.views.index', name="article"),
-    url(r'^search/$', 'articles.views.search', name="article.search"),
-    url(r'^print-view/(?P<id>\d+)/$', 'articles.views.print_view', name="article.print_view"),
-    url(r'^add/$', 'articles.views.add', name="article.add"),
-    url(r'^edit/(?P<id>\d+)/$', 'articles.views.edit', name="article.edit"),
-    url(r'^delete/(?P<id>\d+)/$', 'articles.views.delete', name="article.delete"),
+urlpatterns = patterns('articles',                  
+    url(r'^$', 'views.index', name="articles"),
+    url(r'^(?P<id>\d+)/$', 'views.index', name="article"),
+    url(r'^search/$', 'views.search', name="article.search"),
+    url(r'^print-view/(?P<id>\d+)/$', 'views.print_view', name="article.print_view"),
+    url(r'^add/$', 'views.add', name="article.add"),
+    url(r'^edit/(?P<id>\d+)/$', 'views.edit', name="article.edit"),
+    url(r'^edit/meta/(?P<id>\d+)/$', 'views.edit_meta', name="article.edit.meta"),
+    url(r'^delete/(?P<id>\d+)/$', 'views.delete', name="article.delete"),
     url(r'^feed/$', LatestEntriesFeed(), name='article.feed'),
 )
