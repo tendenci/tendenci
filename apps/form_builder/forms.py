5315eba125b544a18208bc89ff206c76786b263d
eloy <ezuniga@schipul.com>
2010-06-27 17:51:58 -0500

    Updated contacts module.  Added the 'address' field and labled url to "URL."

--- apps/form_builder/forms.py
+++ apps/form_builder/forms.py
@@ -1,12 +1,14 @@
 from django import forms
 from contacts.models import Contact
-from captcha.fields import CaptchaField 
+from captcha.fields import CaptchaField
+from django.utils.translation import ugettext_lazy as _ 
 
 class ContactForm(forms.Form):
 
     first_name = forms.CharField(max_length=100)
     last_name = forms.CharField(max_length=100, required=False)
 
+    address = forms.CharField(max_length=50, required=False)
     city = forms.CharField(max_length=50, required=False)
     state = forms.CharField(max_length=50, required=False)
     zipcode = forms.CharField(max_length=10, required=False)
@@ -14,7 +16,7 @@ class ContactForm(forms.Form):
 
     phone = forms.CharField(max_length=20, required=False)
     email = forms.EmailField(max_length=100)
-    url = forms.URLField(max_length=200, required=False)
+    url = forms.URLField(label=_('URL'), max_length=200, required=False)
 
     message = forms.CharField(widget=forms.Textarea)
     #captcha = CaptchaField()
