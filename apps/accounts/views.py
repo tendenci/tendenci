9e12cb63661ace5a8a621b911e2013627aad6730
glenbot <gzangirolami@schipul.com>
2010-06-30 11:05:35 -0500

    Added eventlogs to login and registration.

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -5,7 +5,10 @@ from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
 #from django.contrib.auth.models import User
+
+from registration.forms import RegistrationForm
 from forms import LoginForm
+from event_logs.models import EventLog
 
 def login(request, form_class=LoginForm, template_name="account/login.html"):
     if request.method == "POST":
@@ -21,6 +24,15 @@ def login(request, form_class=LoginForm, template_name="account/login.html"):
         
         form = form_class(request.POST)
         if form.login(request):
+            log_defaults = {
+                'event_id' : 125200,
+                'event_data': '%s (%d) logged in by form' % (request.user._meta.object_name, request.user.pk),
+                'description': '%s logged in' % request.user._meta.object_name,
+                'user': request.user,
+                'request': request,
+                'instance': request.user,
+            }
+            EventLog.objects.log(**log_defaults)
             return HttpResponseRedirect(redirect_to)
     else:
         form = form_class()
@@ -28,3 +40,105 @@ def login(request, form_class=LoginForm, template_name="account/login.html"):
     return render_to_response(template_name, {
         "form": form
     }, context_instance=RequestContext(request))
+    
+    
+def register(request, success_url=None,
+             form_class=RegistrationForm, profile_callback=None,
+             template_name='registration/registration_form.html',
+             extra_context=None):
+    """
+    Allow a new user to register an account.
+    
+    Following successful registration, issue a redirect; by default,
+    this will be whatever URL corresponds to the named URL pattern
+    ``registration_complete``, which will be
+    ``/accounts/register/complete/`` if using the included URLConf. To
+    change this, point that named pattern at another URL, or pass your
+    preferred URL as the keyword argument ``success_url``.
+    
+    By default, ``registration.forms.RegistrationForm`` will be used
+    as the registration form; to change this, pass a different form
+    class as the ``form_class`` keyword argument. The form class you
+    specify must have a method ``save`` which will create and return
+    the new ``User``, and that method must accept the keyword argument
+    ``profile_callback`` (see below).
+    
+    To enable creation of a site-specific user profile object for the
+    new user, pass a function which will create the profile object as
+    the keyword argument ``profile_callback``. See
+    ``RegistrationManager.create_inactive_user`` in the file
+    ``models.py`` for details on how to write this function.
+    
+    By default, use the template
+    ``registration/registration_form.html``; to change this, pass the
+    name of a template as the keyword argument ``template_name``.
+    
+    **Required arguments**
+    
+    None.
+    
+    **Optional arguments**
+    
+    ``form_class``
+        The form class to use for registration.
+    
+    ``extra_context``
+        A dictionary of variables to add to the template context. Any
+        callable object in this dictionary will be called to produce
+        the end result which appears in the context.
+    
+    ``profile_callback``
+        A function which will be used to create a site-specific
+        profile instance for the new ``User``.
+    
+    ``success_url``
+        The URL to redirect to on successful registration.
+    
+    ``template_name``
+        A custom template to use.
+    
+    **Context:**
+    
+    ``form``
+        The registration form.
+    
+    Any extra variables supplied in the ``extra_context`` argument
+    (see above).
+    
+    **Template:**
+    
+    registration/registration_form.html or ``template_name`` keyword
+    argument.
+    
+    """
+    if request.method == 'POST':
+        form = form_class(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            new_user = form.save(profile_callback=profile_callback)
+            # success_url needs to be dynamically generated here; setting a
+            # a default value using reverse() will cause circular-import
+            # problems with the default URLConf for this application, which
+            # imports this file.
+ 
+            log_defaults = {
+                'event_id' : 121000,
+                'event_data': '%s (%d) self added by form' % (new_user._meta.object_name, new_user.pk),
+                'description': '%s self added' % new_user._meta.object_name,
+                'user': new_user,
+                'request': request,
+                'instance': new_user,
+            }
+            EventLog.objects.log(**log_defaults)
+                   
+            return HttpResponseRedirect(success_url or reverse('registration_complete'))
+    else:
+        form = form_class()
+    
+    if extra_context is None:
+        extra_context = {}
+    context = RequestContext(request)
+    for key, value in extra_context.items():
+        context[key] = callable(value) and value() or value
+    return render_to_response(template_name,
+                              { 'form': form },
+                              context_instance=context)
