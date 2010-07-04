f52e220ebdeb4c7cc9110ff22e61d30c9a3337ca
jennyq <jqian@schipul.com>
2010-07-04 12:46:50 -0500

    Added the functionality to distribute the newsletter instantly if the number of emails to be sent is less than the threshold (50). Updated the default time zone to 'US/Central'.

--- apps/actions/urls.py
+++ apps/actions/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('actions.views',
     url(r'^step4/(?P<action_id>\d+)/$', 'step4', name="action.step4"),
     url(r'^step5/(?P<action_id>\d+)/$', 'step5', name="action.step5"),
     url(r'^confirm/(?P<action_id>\d+)/$', 'confirm', name="action.confirm"),
+    url(r'^send/(?P<action_id>\d+)/$', 'send', name="action.send"),
     url(r'^view/(?P<action_id>\d+)/$', 'view', name="action.view"),
 )
