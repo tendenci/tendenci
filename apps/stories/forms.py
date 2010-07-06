f0e874a03fbf93a256ab4ce5cfbad50a0baf4d95
glenbot <gzangirolami@schipul.com>
2010-07-06 11:52:29 -0500

    Added date/time picker to articles, news, and stories.

--- apps/stories/forms.py
+++ apps/stories/forms.py
@@ -1,10 +1,20 @@
+from datetime import datetime
+
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+
 from stories.models import Story
 from perms.forms import TendenciBaseForm
+from base.fields import SplitDateTimeField
 
 class StoryForm(TendenciBaseForm):
     fullstorylink = forms.CharField(label=_("Full Story Link"), required=False, max_length=300)
+
+    start_dt = SplitDateTimeField(label=_('Start Date/Time'),
+                                    initial=datetime.now())
+
+    end_dt = SplitDateTimeField(label=_('End Date/Time'),
+                                    initial=datetime.now())
     class Meta:
         model = Story
         fields = (
