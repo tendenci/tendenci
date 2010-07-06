8cc1b64d6f4bca97509cf231ddafb7c6dd28aac8
glenbot <gzangirolami@schipul.com>
2010-07-06 11:52:05 -0500

    Created base split date/time field and widget.

--- apps/base/fields.py
+++ apps/base/fields.py
@@ -1,6 +1,11 @@
-from base import forms
+from time import strptime, strftime
+
+from django.forms import fields
 from django.db.models import CharField
 from django.utils.translation import ugettext_lazy as _
+
+from base import forms
+from base.widgets import SplitDateTimeWidget
 
 class SlugField(CharField):
     """
@@ -23,3 +28,34 @@ class SlugField(CharField):
         defaults.update(kwargs)
         return super(SlugField, self).formfield(**defaults)
 
+class SplitDateTimeField(fields.MultiValueField):
+    """
+        Custom split date time widget
+        Modified version of http://www.copiesofcopies.org/webl/?p=81
+    """
+    widget = SplitDateTimeWidget
+
+    def __init__(self, *args, **kwargs):
+        """
+        Have to pass a list of field types to the constructor, else we
+        won't get any data to our compress method.
+        """
+        all_fields = (
+            fields.CharField(max_length=10),
+            fields.CharField(max_length=8),
+            )
+        super(SplitDateTimeField, self).__init__(all_fields, *args, **kwargs)
+
+    def compress(self, data_list):
+        """
+        Takes the values from the MultiWidget and passes them as a
+        list to this function. This function needs to compress the
+        list into a single object to save.
+        """
+        if data_list:
+            if not (data_list[0] and data_list[1]):
+                raise forms.ValidationError("Field is missing data.")
+            input_time = strptime("%s" % (data_list[1]), "%I:%M %p")
+            datetime_string = "%s %s" % (data_list[0], strftime('%H:%M', input_time))
+            return datetime_string
+        return None
