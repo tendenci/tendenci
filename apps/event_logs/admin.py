72f9e59747727276f2bb2b4ae493ac56be835778
vitaily <ppr.vitaly@gmail.com>
2010-07-12 08:46:29 +0300

    user activity, event summary, admins reports

--- apps/event_logs/admin.py
+++ apps/event_logs/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from event_logs.models import EventLog
 
-admin.site.register(EventLog)
+class EventLogAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'content_type', 'object_id', 'source', 
+                    'event_id', 'event_name', 'event_type', 'category']
+    list_filter = ['source', 'event_id', 'event_name', 'event_type', 'category', 'content_type']
+    
+    date_hierarchy = 'create_dt'
+
+admin.site.register(EventLog, EventLogAdmin)
