dd42463824d14c5694b92afe0fe37a406571f47b
jennyq <jqian@schipul.com>
2010-08-01 22:25:47 -0500

    Updated the user imports to handle the potential timeout for the large number of user records.

--- apps/imports/urls.py
+++ apps/imports/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('imports.views',
     url(r'^users/upload/add/$', 'user_upload_add', name="import.user_upload_add"),
     url(r'^users/upload/preview/$', 'user_upload_preview', name="import.user_upload_preview"),
     url(r'^users/upload/process/$', 'user_upload_process', name="import.user_upload_process"),
+    url(r'^users/upload/subprocess/$', 'user_upload_subprocess', name="import.user_upload_subprocess"),
     url(r'^users/upload/formats/$', 'download_user_upload_template_xls', name="import.download_user_formats"),
 )
