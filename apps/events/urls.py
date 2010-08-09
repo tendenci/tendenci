280b02dee9922fe9a6e21698a0d27087d4c6b56d
eloyz <eloyz.email@gmail.com>
2010-08-09 13:22:11 -0500

    Events is very basic at this point. You can create an event. You can enabled registration and set 1 price option for different time periods [early, regular, and late registration].  There are 3 views (e.g. search, month, and day view). You can register yourself [must be logged in]. You cannot register other people or a group of people.  Currently the event system is not tied into the standard TendenciBaseModel, it doesn't not have permissions setup, and it is not being logged within eventlogs.

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -12,8 +12,17 @@ urlpatterns = patterns('events',
     url(r'^feed/$', LatestEntriesFeed(), name='event.feed'),
     url(r'^(?P<id>\d+)/$', 'views.index', name="event"),
 
+    # month-view / day-view
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$', 'views.month_view', name='event.month'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', 'views.day_view', name='event.day'),
+
+    # register for event
     url(r'^(?P<event_id>\d+)/register/$', 'views.register', name='event.register'),
     url(r'^(?P<event_id>\d+)/register/confirm/$', 'views.register_confirm', name='event.register.confirm'),
+
+    # registrants (search/view); admin-only
+    url(r'^(?P<event_id>\d+)/registrants/search/$', 'views.registrant_search', name="event.registrant.search"),
+    url(r'^registrants/(?P<id>\d+)/$', 'views.registrant_details', name="event.registrant"),
 
     url(r'^edit/place/(?P<id>\d+)/$', 'views.edit_place', name="event.edit.place"),
     url(r'^edit/sponsors/(?P<id>\d+)/$', 'views.edit_sponsor', name="event.edit.sponsor"),
