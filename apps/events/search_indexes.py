069b5c670e562340b00b8cb06b25a7ab4e516250
eloyz <eloyz.email@gmail.com>
2010-08-17 10:22:21 -0500

    Permission updates. Making sure everyone can see events if they're made available.

--- apps/events/search_indexes.py
+++ apps/events/search_indexes.py
@@ -9,18 +9,27 @@ class EventIndex(indexes.RealTimeSearchIndex):
     text = indexes.CharField(document=True, use_template=True)
     title = indexes.CharField(model_attr='title')
     description = indexes.CharField(model_attr='description')
+
+    # authority fields
+    allow_anonymous_view = indexes.BooleanField(model_attr='allow_anonymous_view')
+    allow_user_view = indexes.BooleanField(model_attr='allow_user_view')
+    allow_member_view = indexes.BooleanField(model_attr='allow_member_view')
+    allow_anonymous_edit = indexes.BooleanField(model_attr='allow_anonymous_edit')
+    allow_user_edit = indexes.BooleanField(model_attr='allow_user_edit')
+    allow_member_edit = indexes.BooleanField(model_attr='allow_member_edit')
+    creator = indexes.CharField(model_attr='creator')
+    creator_username = indexes.CharField(model_attr='creator_username')
+    owner = indexes.CharField(model_attr='owner')
+    owner_username = indexes.CharField(model_attr='owner_username')
+    status = indexes.IntegerField(model_attr='status')
+    status_detail = indexes.CharField(model_attr='status_detail')
     
     who_can_view = indexes.CharField()
 
     def prepare_who_can_view(self, obj):
         users = ObjectPermission.objects.who_has_perm('events.view_event', obj)
-        user_list = []
-        if users:
-            for user in users:
-                user_list.append(user.username)
-            return ','.join(user_list)
-        else: 
-            return ''
+        if not users: users = []
+        return ','.join([user.name for user in users])
     
     def prepare_description(self, obj):
         description = obj.description
@@ -36,14 +45,9 @@ class RegistrantIndex(indexes.RealTimeSearchIndex):
     who_can_view = indexes.CharField()
 
     def prepare_who_can_view(self, obj):
-        users = ObjectPermission.objects.who_has_perm('events.view_event', obj)
-        user_list = []
-        if users:
-            for user in users:
-                user_list.append(user.username)
-            return ','.join(user_list)
-        else: 
-            return ''
+        users = ObjectPermission.objects.who_has_perm('registrants.view_registrant', obj)
+        if not users: users = []
+        return ','.join([user.name for user in users])
     
 site.register(Event, EventIndex)
 site.register(Registrant, RegistrantIndex)
