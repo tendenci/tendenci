ceaf912961a60129ae350d8929d86b0974dc4f39
glenbot <glenbot@gmail.com>
2010-08-20 11:06:47 -0500

    Updates to helpfiles. Renamed helpfile to help_files for consistecy. Updated the admin interface. Updated templates and styles.

--- apps/helpfiles/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('helpfiles.views',
-    url(r'^$', 'home', name='helpfiles'),
-    url(r'^topic/(?P<id>\d+)/$', 'topic', name='helpfiles-topic'),
-    url(r'^(?P<id>\d+)/$', 'details', name='helpfiles-details'),
-    url(r'^search/$', 'search', name='helpfiles-search'),
-    url(r'^request/$', 'request_new', name='helpfiles-new'),
-)
