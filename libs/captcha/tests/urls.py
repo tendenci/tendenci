bafc9e6344e1db34137c22e28a15d75db7f01f4f
jenny <jqian@schipul.com>
2010-09-03 11:11:20 -0500

    Somehow the libs/ folder got merged into my local milestone-5.0.2. It should be in milestone-5.0.2-libs. Deleted from milestone-5.0.2.

--- libs/captcha/tests/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import *
-urlpatterns = patterns('',
-    url(r'test/$','captcha.tests.views.test',name='captcha-test'),
-    url(r'test2/$','captcha.tests.views.test_custom_error_message',name='captcha-test-custom-error-message'),
-    url(r'',include('captcha.urls')),
-)
