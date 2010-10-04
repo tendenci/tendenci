abe7dd9d2f61da3917c8261c5f6655830cc612f5
eloyz <eloyz.email@gmail.com>
2010-10-04 16:11:11 -0500

    Articles template tag created.

--- apps/articles/templatetags/article_tags.py
+++ apps/articles/templatetags/article_tags.py
@@ -1,4 +1,5 @@
-from django.template import Library
+from django.template import Node, Library, TemplateSyntaxError, Variable
+from articles.models import Article
 
 register = Library()
 
@@ -21,3 +22,64 @@ def article_nav(context, user, article=None):
 @register.inclusion_tag("articles/search-form.html", takes_context=True)
 def article_search(context):
     return context
+
+class ListArticlesNode(Node):
+    
+    def __init__(self, context_var, *args, **kwargs):
+
+        self.limit = 3
+        self.user = None
+
+        if "limit" in kwargs:
+            self.limit = Variable(kwargs["limit"])
+
+        if "user" in kwargs:
+            self.user = Variable(kwargs["user"])
+
+        self.context_var = context_var
+
+    def render(self, context):
+
+        if self.user:
+            self.user = self.user.resolve(context)
+
+        if hasattr(self.limit, "resolve"):
+            self.limit = self.limit.resolve(context)
+
+        articles = Article.objects.search(user=self.user)
+        articles = [article.object for article in articles[:self.limit]]
+        context[self.context_var] = articles
+        return ""
+
+@register.tag
+def list_articles(parser, token):
+    """
+    Example:
+        {% list_articles as articles [user=user limit=3] %}
+        {% for article in articles %}
+            {{ article }}
+        {% endfor %}
+
+    """
+    args, kwargs = [], {}
+    bits = token.split_contents()
+    context_var = bits[2]
+
+    print bits
+
+    for bit in bits:
+        if "limit=" in bit:
+            kwargs["limit"] = bit.split("=")[1]
+        if "user=" in bit:
+            kwargs["user"] = bit.split("=")[1]
+
+    if len(bits) < 3:
+        message = "'%s' tag requires more than 3" % bits[0]
+        raise TemplateSyntaxError(message)
+
+    if bits[1] != "as":
+        message = "'%s' second argument must be 'as" % bits[0]
+        raise TemplateSyntaxError(message)
+
+    return ListArticlesNode(context_var, *args, **kwargs)
+
