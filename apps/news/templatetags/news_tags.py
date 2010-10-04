ab12bb84122d7b092dd8255afbee7ceab98c7964
eloyz <eloyz.email@gmail.com>
2010-10-04 16:19:14 -0500

    News template tag created.

--- apps/news/templatetags/news_tags.py
+++ apps/news/templatetags/news_tags.py
@@ -1,4 +1,5 @@
-from django.template import Library
+from django.template import Node, Library, TemplateSyntaxError, Variable
+from news.models import News
 
 register = Library()
 
@@ -21,3 +22,63 @@ def news_nav(context, user, news=None):
 @register.inclusion_tag("news/search-form.html", takes_context=True)
 def news_search(context):
     return context
+
+class ListNewsNode(Node):
+    
+    def __init__(self, context_var, *args, **kwargs):
+
+        self.limit = 3
+        self.user = None
+
+        if "limit" in kwargs:
+            self.limit = Variable(kwargs["limit"])
+
+        if "user" in kwargs:
+            self.user = Variable(kwargs["user"])
+
+        self.context_var = context_var
+
+    def render(self, context):
+
+        if self.user:
+            self.user = self.user.resolve(context)
+
+        if hasattr(self.limit, "resolve"):
+            self.limit = self.limit.resolve(context)
+
+        news = News.objects.search(user=self.user)
+        news = [news_item.object for news_item in news[:self.limit]]
+        context[self.context_var] = news
+        return ""
+
+@register.tag
+def list_news(parser, token):
+    """
+    Example:
+        {% list_articles as articles [user=user limit=3] %}
+        {% for article in articles %}
+            {{ article }}
+        {% endfor %}
+
+    """
+    args, kwargs = [], {}
+    bits = token.split_contents()
+    context_var = bits[2]
+
+    print bits
+
+    for bit in bits:
+        if "limit=" in bit:
+            kwargs["limit"] = bit.split("=")[1]
+        if "user=" in bit:
+            kwargs["user"] = bit.split("=")[1]
+
+    if len(bits) < 3:
+        message = "'%s' tag requires more than 3" % bits[0]
+        raise TemplateSyntaxError(message)
+
+    if bits[1] != "as":
+        message = "'%s' second argument must be 'as" % bits[0]
+        raise TemplateSyntaxError(message)
+
+    return ListNewsNode(context_var, *args, **kwargs)
