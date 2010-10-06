99d607bddd37e20fc3351d81410e16084827eab9
eloyz <eloyz.email@gmail.com>
2010-10-06 16:09:47 -0500

    Added url pattern that allows linking to calendar with type selected.

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -20,6 +20,7 @@ urlpatterns = patterns('events',
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', 'views.day_view', name='event.day'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$', 'views.month_view', name='event.month'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<type>[\w\-\/]+)/$', 'views.month_view', name='event.month'),
+    url(r'^(?P<type>[\w\-\/]+)/$', 'views.month_view', name='event.month'),
 
     url(r'^types/$', 'views.types', name='event.types'),
 
