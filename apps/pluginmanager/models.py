6d9ac97d5acc24d1e5ddd5ec55b785bea5cb05d4
jenny <jqian@schipul.com>
2010-10-12 11:19:26 -0500

    Merged milestone-5.0.2-plugins to milestone-5.0.4. Moved donations to the plugins for testing. Fixed the anonymous donation error.

---
+++ apps/pluginmanager/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+class PluginApp(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    package = models.CharField(max_length=255)
+    is_installed = models.BooleanField(default=False, blank=True)
+    is_enabled = models.BooleanField(default=False, blank=True)
+    
+    def __unicode__(self):
+        return self.title
+
+
+###
+# Signals
+from django.core.management import call_command
+post_save = models.signals.post_save
+post_delete = models.signals.post_delete
+
+def _update_apps():
+    db2json()
+    from django.conf import settings
+    from django.db.models.loading import cache as app_cache
+    from pluginmanager import plugin_apps
+    settings.INSTALLED_APPS = plugin_apps(settings.DEFAULT_INSTALLED_APPS)
+    app_cache.loaded = False # clear cache
+    call_command('syncdb', interactive=False, migrate_all=True)
+    call_command('touch_settings')
+
+def post_save_pluginapp(sender, **kwargs):
+    _update_apps()
+post_save.connect(post_save_pluginapp, sender=PluginApp)
+
+def post_del_pluginapp(sender, **kwargs):
+    _update_apps()
+post_delete.connect(post_del_pluginapp, sender=PluginApp)
+
+###
+# Tools
+from django.utils import simplejson
+import os
+
+def db2json():
+    path = os.path.abspath(os.path.dirname(__file__))
+    plugins = list(PluginApp.objects.all().values('id','title','package','is_enabled','is_installed'))
+    data = simplejson.dumps(plugins, indent=1)
+    f = open(os.path.join(path, 'plugins.json'), 'w')
+    f.write(data)
+    f.close()
