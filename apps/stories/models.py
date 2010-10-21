99e2e9b908b1081b0686c0459498ecfac3c4c0bd
glenbot <glenbot@gmail.com>
2010-10-21 11:24:47 -0500

    Updates stories to better use new permissions code. Added photo upload field. Updated forms, views, model and templates.

--- apps/stories/models.py
+++ apps/stories/models.py
@@ -1,4 +1,7 @@
-import os.path, uuid
+import os.path
+import uuid
+import re
+
 from django.db import models
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
@@ -7,19 +10,25 @@ from perms.models import TendenciBaseModel
 from stories.managers import StoryManager
 from perms.utils import is_admin
 from entities.models import Entity
-from tinymce import models as tinymce_models
+
+def file_directory(instance, filename):
+    filename = re.sub(r'[^a-zA-Z0-9._]+', '-', filename)
+    return 'stories/%s' % (filename)
 
 # Create your models here.
 class Story(TendenciBaseModel):
     guid = models.CharField(max_length=40)
     title = models.CharField(max_length=200, blank=True)
-    content = tinymce_models.HTMLField()
+    content = models.TextField(blank=True)
     syndicate = models.BooleanField(_('Include in RSS feed'))
     full_story_link = models.CharField(_('Full Story Link'), max_length=300, blank=True)
     start_dt = models.DateTimeField(_('Start Date/Time'), null=True, blank=True)
     end_dt = models.DateTimeField(_('End Date/Time'), null=True, blank=True)
     ncsortorder = models.IntegerField(null=True, blank=True)
     entity = models.ForeignKey(Entity,null=True)
+    photo = models.FileField(max_length=260, upload_to=file_directory, 
+                             help_text=_('Photo that represents this story.'), 
+                             null=True, blank=True)
     
     objects = StoryManager()
     
@@ -39,68 +48,3 @@ class Story(TendenciBaseModel):
 
     def __unicode__(self):
         return self.title
-    
-    # if this story allows view by user2_compare
-    def allow_view_by(self, user2_compare):
-        boo = False
-       
-        if is_admin(user2_compare):
-            boo = True
-        else:
-            if self.creator == user2_compare or self.owner == user2_compare:
-                if self.status == 1:
-                    boo = True
-            else:
-                if self.status==1 and self.status_detail.lower()=='active':
-                    boo = True
-            
-        return boo
-    
-    def allow_add_by(self, user2_compare):
-        if is_admin(user2_compare):
-            return True
-        else:
-            if user2_compare.has_perm('stories.add_story'):
-                return True
-        return False
-    
-    # if this story allows edit by user2_compare
-    def allow_edit_by(self, user2_compare):
-        if is_admin(user2_compare):
-            return True
-        else:
-            if self.creator == user2_compare or self.owner == user2_compare:
-                if self.status == 1:
-                    return True
-            else:
-                if user2_compare.has_perm('stories.change_story', self):
-                    return True
-        return False
-    
-    def get_image_url(self):
-        imagepath = os.path.join(settings.MEDIA_ROOT, 'stories/'+str(self.id))
-        image_name = self.get_latest_image_name(imagepath)
-        if image_name <> "":
-            image_url = os.path.join(settings.MEDIA_URL, 'stories/'+str(self.id) +'/' + image_name)
-        else:
-            image_url = ""
-        return image_url
-    
-    # get the latest image name
-    def get_latest_image_name(self, images_path):
-        latest_image = ""
-        if os.path.isdir(images_path):
-            image_list = os.listdir(images_path)
-            
-            if image_list <> []:
-                image_full_path_list = [images_path+'/'+image_name for image_name in image_list]
-                
-                latest_image = image_list[0]
-                mtime = os.path.getmtime(image_full_path_list[0])
-                
-                for i in range(1, len(image_full_path_list)):
-                    if mtime < os.path.getmtime(image_full_path_list[i]):
-                        latest_image = image_list[i]
-                        mtime = os.path.getmtime(image_full_path_list[i])
-        return latest_image
-        
