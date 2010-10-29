44c4ef0fa82bc8b2d10218d74371cc98d689f9c6
glenbot <glenbot@gmail.com>
2010-10-29 13:35:34 -0500

    Updated pages admin to use pre-populated admin field

--- site_media/static/js/apps/slug-pages.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	var slugit = new Slugify({
-		input_element : 'input#id_title',
-		slug_element : 'input#id_slug',
-		submit_element : 'input[type="submit"]',
-		help: false,
-		limit : 150
-	});
-});
