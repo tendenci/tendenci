8c24342e7f37c7fe311e402e4e94194c62a4504a
jmoswalt <jmoswalt@gmail.com>
2010-11-03 08:50:08 -0500

    Updating events view template to parse HTML from imported events. Fixing pluginsmanager to also touch the wsgi file

--- apps/pluginmanager/management/commands/touch_settings.py
+++ apps/pluginmanager/management/commands/touch_settings.py
@@ -7,6 +7,7 @@ class Command(BaseCommand):
     @commit_on_success
     def handle(self, *args, **kwargs):
         path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../../../settings.py')
+        wsgi = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../../../deploy/deploy.wsgi')
         print path
         os.system('touch '+path)
-
+        os.system('touch '+wsgi)
