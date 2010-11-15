f645b410f4f40b10b009e1c629d3127a6d084473
jenny <jqian@schipul.com>
2010-11-15 14:16:43 -0600

    Updated is_admin function. Created the command admin_converter to convert all admins on the site to use the new criteria (that is is_superuser=False and is_staff=True).

--- apps/profiles/utils.py
+++ apps/profiles/utils.py
@@ -22,10 +22,43 @@ def profile_edit_admin_notify(request, old_user, old_profile, profile, **kwargs)
     except:
         pass
     
-    
+# return admin auth group as a list    
 def get_admin_auth_group(name="Admin"):
     from django.contrib.auth.models import Group as Auth_Group
     
     auth_groups = Auth_Group.objects.filter(name=name)
     
     return auth_groups
+
+def user_add_remove_admin_auth_group(user, auth_group=None):
+    """
+        if user is admin and not on admin auth group, add them.
+        if user is not admin but on admin auth group, remove them
+    """
+    if user.is_staff and (not user.is_superuser):   # they are admin
+        if auth_group:
+            auth_group_list = [auth_group]
+        else:
+            auth_group_list = get_admin_auth_group()
+        
+        if auth_group_list:
+            if not user.id: # new user
+                user.groups = auth_group_list
+                user.save()
+                
+            else:           # existing user
+                add_to_auth_group = True
+                user_edit_auth_groups = user.groups.all()
+                if user_edit_auth_groups:
+                    if user_edit_auth_groups[0] == auth_group_list[0]:
+                        add_to_auth_group = False
+                        
+                if add_to_auth_group:
+                    user.groups = [auth_group_list[0]]
+                    user.save()
+                    
+    else:
+        if user.id:
+            user.groups = []
+            user.save()
+        
