76620bc8ab623eba0fa7f9701472e6f0ad0e1674
glenbot <glenbot@gmail.com>
2010-11-16 16:17:44 -0600

    Moved forms model manager from models.py to managers.py in forms model due to a circular import from search_indexes.py causing haystack to duece

---
+++ apps/forms_builder/forms/managers.py
@@ -0,0 +1,42 @@
+from django.db.models import Manager
+from django.contrib.auth.models import User
+
+from haystack.query import SearchQuerySet
+from perms.utils import is_admin
+
+class FormManager(Manager):
+    """
+    Only show published forms for non-staff users.
+    """
+    def published(self, for_user=None):
+        if for_user is not None and for_user.is_staff:
+            return self.all()
+        return self.filter(status_detail='published')
+
+    def search(self, query=None, *args, **kwargs):
+        """
+            Uses haystack to query news.
+            Returns a SearchQuerySet
+        """
+        sqs = SearchQuerySet()
+        user = kwargs.get('user', None)
+
+        # check to see if there is impersonation
+        if hasattr(user,'impersonated_user'):
+            if isinstance(user.impersonated_user, User):
+                user = user.impersonated_user
+
+        is_an_admin = is_admin(user)
+
+        if query:
+            sqs = sqs.auto_query(sqs.query.clean(query))
+            if user:
+                if not is_an_admin:
+                    return []
+        else:
+            sqs = sqs.all()
+            if user:
+                if not is_an_admin:
+                    return []
+
+        return sqs.models(self.model).order_by('-create_dt')
