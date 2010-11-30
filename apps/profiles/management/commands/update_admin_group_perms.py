fba682845e985977f3ba6c03e20200dc4216988e
jennyq <jqian@schipul.com>
2010-11-30 13:51:29 -0600

    Updated the pluginmanager to assign permissions to admin auth group once a new plugin is added. Registered a post_syncdb signal to the memberships models to assign permissions to the admin auth group.

--- apps/profiles/management/commands/update_admin_group_perms.py
+++ apps/profiles/management/commands/update_admin_group_perms.py
@@ -4,9 +4,9 @@ from django.contrib.contenttypes.models import ContentType
 from django.conf import settings
 
 class Command(BaseCommand):
-    # create an admin auth group, and assign all permissions 
+    # create an admin auth group if not exists, and assign all permissions 
     # (except 4 auth permission the auth_user, auth_groups..) to this group
-    # command to run: python manage.py make_admin_group
+    # command to run: python manage.py update_admin_group_perms
     def handle(self, *args, **options):
         out = ''
         if hasattr(settings, 'ADMIN_AUTH_GROUP_NAME'):
@@ -29,6 +29,6 @@ class Command(BaseCommand):
         auth_group.save()
         
         #self.stdout.write('Successfully added all permissions to group "Admin".')
-        out += 'Successfully added all permissions to group "%s".' % name
+        out += 'Successfully added/updated all permissions to group "%s".' % name
         print out
 
