5b272374cea35d0d89228575d70907dcc5c63d2f
jennyq <jqian@schipul.com>
2010-11-30 11:33:26 -0600

    Updated admin_converter command and add/remove admin auth group function to use the new setting 'ADMIN_AUTH_GROUP_NAME'.

--- apps/profiles/utils.py
+++ apps/profiles/utils.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from site_settings.utils import get_setting
 
 def profile_edit_admin_notify(request, old_user, old_profile, profile, **kwargs):
@@ -26,9 +27,13 @@ def profile_edit_admin_notify(request, old_user, old_profile, profile, **kwargs)
 def get_admin_auth_group(name="Admin"):
     from django.contrib.auth.models import Group as Auth_Group
     
-    auth_groups = Auth_Group.objects.filter(name=name)
+    try:
+        auth_group = Auth_Group.objects.get(name=name)
+    except Auth_Group.DoesNotExist:
+        auth_group = Auth_Group(name=name)
+        auth_group.save()
     
-    return auth_groups
+    return auth_group
 
 def user_add_remove_admin_auth_group(user, auth_group=None):
     """
@@ -36,26 +41,32 @@ def user_add_remove_admin_auth_group(user, auth_group=None):
         if user is not admin but on admin auth group, remove them
     """
     if user.is_staff and (not user.is_superuser):   # they are admin
-        if auth_group:
-            auth_group_list = [auth_group]
-        else:
-            auth_group_list = get_admin_auth_group()
-        
-        if auth_group_list:
-            if not user.id: # new user
-                user.groups = auth_group_list
+        if not auth_group:
+            if hasattr(settings, 'ADMIN_AUTH_GROUP_NAME'):
+                auth_group_name = settings.ADMIN_AUTH_GROUP_NAME
+            else:
+                auth_group_name = 'Admin'
+            auth_group = get_admin_auth_group(name=auth_group_name)
+            
+      
+        if not user.id: # new user
+            user.groups = [auth_group]
+            user.save()
+            
+        else:           # existing user
+            group_updated = False
+            user_edit_auth_groups = user.groups.all()
+            if user_edit_auth_groups:
+                if auth_group not in user_edit_auth_groups:
+                    user_edit_auth_groups.append(auth_group)
+                    user.groups = user_edit_auth_groups
+                    group_updated = True
+            else:
+                user.groups = [auth_group]
+                group_updated = True
+                    
+            if group_updated:
                 user.save()
-                
-            else:           # existing user
-                add_to_auth_group = True
-                user_edit_auth_groups = user.groups.all()
-                if user_edit_auth_groups:
-                    if user_edit_auth_groups[0] == auth_group_list[0]:
-                        add_to_auth_group = False
-                        
-                if add_to_auth_group:
-                    user.groups = [auth_group_list[0]]
-                    user.save()
                     
     else:
         if user.id:
