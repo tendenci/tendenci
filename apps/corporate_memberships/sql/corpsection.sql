7884abfcd492c49fceb6bf4064415b6537c5e01a
jennyq <jqian@schipul.com>
2010-12-26 21:06:13 -0600

    Updated corpapp: 1) hided the Save and add another and Save buttons on add. 2) disabled the Required and Visible checkboxes for the mandatory required fields. 3) hided some fields for the payment_method and corporate_membership_type.

--- apps/corporate_memberships/sql/corpsection.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `corporate_memberships_corpsection` (`label`, `description`, `admin_only`, `css_class`) VALUES ('Company Details','',0,'');
-INSERT INTO `corporate_memberships_corpsection` (`label`, `description`, `admin_only`, `css_class`) VALUES ('Membership Details','',0,'');
-INSERT INTO `corporate_memberships_corpsection` (`label`, `description`, `admin_only`, `css_class`) VALUES ('Admin Only','',1,'');
-INSERT INTO `corporate_memberships_corpsection` (`label`, `description`, `admin_only`, `css_class`) VALUES ('Payment Details','',0,'');
-INSERT INTO `corporate_memberships_corpsection` (`label`, `description`, `admin_only`, `css_class`) VALUES ('Representatives','',1,'');
