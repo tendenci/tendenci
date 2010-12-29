be845016d4c802effa4e65ff9ea659ad46a80472
Eloy Zuniga Jr <eloyz.email@gmail.com>
2010-12-29 13:42:21 -0600

    Updated drag/drop collapse/expand feature on membership application on Django admin site.

--- site_media/static/js/admin/dynamic-inlines-with-sort.js
+++ site_media/static/js/admin/dynamic-inlines-with-sort.js
@@ -2,7 +2,7 @@ jQuery(function($) {
     $('div.inline-group').sortable({
         containment: 'parent',
         items: 'div.inline-related',
-        handle: 'h3:first',
+        handle: 'h3:first, .collapse-label',
         update: function() {
             $(this).find('div.inline-related').each(function(i) {
                 if ($(this).find('input[id$=label]').val()) {
@@ -13,6 +13,7 @@ jQuery(function($) {
     });
     $('div.inline-related h3').css('cursor', 'move');
     $('div.inline-related').find('input[id$=position]').parent('div').hide();
+
 
     //    text
     //    paragraph
@@ -31,22 +32,22 @@ jQuery(function($) {
     //    description
     //    horizontal-rule
 
-        // description field function
+    // function: toggle label field
     var manage_label_field = function(){
     	var selected_value = $(this).find(":selected").val();
     	var selected_text = $(this).find(":selected").text();
     	var fieldset = $(this).parents("fieldset");
 
-    	// toggle description field
+    	// toggle label field
     	if (selected_value == "description"){
             fieldset.find(".label").hide();
     	}
     	else {
     		fieldset.find(".label").show();
     	}
-    }
+    };
 
-    // description field function
+    // function: toggle description field
     var manage_description_field = function(){
     	var selected_value = $(this).find(":selected").val();
     	var selected_text = $(this).find(":selected").text();
@@ -59,9 +60,9 @@ jQuery(function($) {
     	else {
     		fieldset.find(".description").hide();
     	}
-    }
+    };
 
-    // choice field function
+    // function: toggle choice field
     var manage_choice_field = function(){
     	var selected_value = $(this).find(":selected").val();
     	var selected_text = $(this).find(":selected").text();
@@ -74,12 +75,54 @@ jQuery(function($) {
     	else {
     		fieldset.find(".choices").hide();
     	}
+    };
+
+    // function; hide all fields (display label)
+    var hide_fields = function(){
+
+        $(this).children().hide();
+
+        var last_field = $(this).find("fieldset").children().last();
+        last_field.prepend('<div class="done-btn">I am Done Here. Collapse.</div>') // done link
+
+        var label = $(this).find("label").next().val(); // get label
+        $(this).prepend('<div class="collapse-label">' + label + '</div>'); // show label
+    };
+
+    // function: expand_field_options
+    var expand_field_options = function(){
+
+//        var sorting = $(this).parents(".inline-group").find(".ui-sortable-placeholder");
+//        console.log(sorting)
+//        if(sorting) return false;
+
+        $(this).siblings().show(); // show original markup
+        $(this).hide();
     }
 
-    // type-drop-down initial; type-drop-down change-event binding
+    // function: collapse field options
+    var collapse_field_options = function(){
+        var parent = $(this).parents(".dynamic-fields");
+        parent.children().hide(); // hide original markup
+        parent.find(".collapse-label").show(); // show label
+    }
+
+    /* called immediately; hiding and event-binding
+    --------------------------------------------------------------------------------- */
+
+    // bind change event to dropdown
     $("div.inline-group .field_type select")
+        // each() handles initial calls; live() binds the change-event to f()
         .each(manage_label_field).live('change', manage_label_field)
         .each(manage_description_field).live('change', manage_description_field)
         .each(manage_choice_field).live('change', manage_choice_field);
+
+    // hide dynamic field fields
+    $(".dynamic-fields").each(hide_fields)
+
+    // bind click event to label
+    $(".collapse-label").live("click", expand_field_options);
+    // bind click event to done-button
+    $(".done-btn").live("click", collapse_field_options);
 
 });
