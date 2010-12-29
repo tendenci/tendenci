74a7983fa6d25299c9e5c1f5ecc261be7e94c9c4
jennyq <jqian@schipul.com>
2010-12-29 15:29:12 -0600

    Renamed and added fields in CorpApp. Added wysiwyg to the description and confirmation_text for the CorpApp admin. Started the corporate membership add page.

--- apps/corporate_memberships/urls.py
+++ apps/corporate_memberships/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, url, include
 
 urlpatterns = patterns('corporate_memberships.views',                  
-    url(r"(?P<slug>.*)/preview/$", "preview", name="corp_memb.preview"),
+    url(r"(?P<slug>.*)/add/$", "add", name="corp_memb.add"),
 )
