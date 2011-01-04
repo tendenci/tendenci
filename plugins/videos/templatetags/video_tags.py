847fc02545470e46648bc88605e8e5e93300ad88
jmoswalt <jmoswalt@gmail.com>
2011-01-04 13:18:48 -0600

    Added tags to admin and detail view, updated template tags to pull context on videos.

--- plugins/videos/templatetags/video_tags.py
+++ plugins/videos/templatetags/video_tags.py
@@ -9,7 +9,7 @@ class ListVideosNode(Node):
 
         self.limit = 3
         self.user = None
-        self.tags = []
+        self.tags = ''
         self.q = []
         self.context_var = context_var
 
@@ -18,13 +18,20 @@ class ListVideosNode(Node):
         if "user" in kwargs:
             self.user = Variable(kwargs["user"])
         if "tags" in kwargs:
-            self.tags = kwargs["tags"]
+            self.tags_string = kwargs['tags']
+            self.tags = Variable(kwargs["tags"])
         if "q" in kwargs:
             self.q = kwargs["q"]
 
     def render(self, context):
         query = ''
 
+        self.tags = self.tags.resolve(context)
+        if self.tags:
+            self.tags = self.tags.split(',')
+        if not self.tags:
+            self.tags = self.tags_string.split(',')
+            
         if self.user:
             self.user = self.user.resolve(context)
 
@@ -41,6 +48,7 @@ class ListVideosNode(Node):
 
 
         videos = Video.objects.search(user=self.user, query=query)
+        videos = videos.order_by('create_dt')
         videos = [video.object for video in videos[:self.limit]]
         context[self.context_var] = videos
         return ""
@@ -65,16 +73,16 @@ def list_videos(parser, token):
         if "user=" in bit:
             kwargs["user"] = bit.split("=")[1]
         if "tags=" in bit:
-            kwargs["tags"] = bit.split("=")[1].replace('"','').split(',')
+            kwargs["tags"] = bit.split("=")[1].replace('"','')
         if "q=" in bit:
             kwargs["q"] = bit.split("=")[1].replace('"','').split(',')
 
     if len(bits) < 3:
-        message = "'%s' tag requires more than 3" % bits[0]
+        message = "'%s' tag requires more than 2" % bits[0]
         raise TemplateSyntaxError(message)
 
     if bits[1] != "as":
-        message = "'%s' second argument must be 'as" % bits[0]
+        message = "'%s' second argument must be 'as'" % bits[0]
         raise TemplateSyntaxError(message)
 
     return ListVideosNode(context_var, *args, **kwargs)
