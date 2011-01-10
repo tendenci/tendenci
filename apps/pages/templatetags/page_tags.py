3652eac48f345b38a29ace4d20375882e2bec6a0
glenbot <glenbot@gmail.com>
2011-01-10 16:58:41 -0600

    Major updates to the template tag for pages. Needed an overhaul to allow for context resolution to happen within loops

--- apps/pages/templatetags/page_tags.py
+++ apps/pages/templatetags/page_tags.py
@@ -1,4 +1,5 @@
 from django.template import Node, Library, TemplateSyntaxError, Variable
+from django.contrib.auth.models import AnonymousUser
 from pages.models import Page
 from datetime import datetime
 
@@ -26,56 +27,59 @@ def page_search(context):
 
 
 class ListPageNode(Node):
-
     def __init__(self, context_var, *args, **kwargs):
-
-        self.limit = 3
-        self.user = None
-        self.tags = ''
-        self.q = []
         self.context_var = context_var
-
-        if "limit" in kwargs:
-            self.limit = Variable(kwargs["limit"])
-        if "user" in kwargs:
-            self.user = Variable(kwargs["user"])
-        if "tags" in kwargs:
-            self.tags_string = kwargs['tags']
-            self.tags = Variable(kwargs["tags"])
-        if "q" in kwargs:
-            self.q = kwargs["q"]
+        self.kwargs = kwargs
 
     def render(self, context):
-        query = ''
+        tags = u''
+        query = u''
+        user = AnonymousUser()
+        limit = 3
 
-        try:
-            self.tags = self.tags.resolve(context)
-        except:
-            self.tags = self.tags_string
-        if self.tags:
-            self.tags = self.tags.split(',')
-        if not self.tags:
-            self.tags = self.tags_string.split(',')
-            
-        if self.user:
-            self.user = self.user.resolve(context)
+        if 'tags' in self.kwargs:
+            try:
+                tags = Variable(self.kwargs['tags'])
+                tags = unicode(tags.resolve(context))
+            except:
+                tags = self.kwargs['tags'] # context string
+            tags = tags.split(',')
 
-        if hasattr(self.limit, "resolve"):
-            self.limit = self.limit.resolve(context)
+        if 'user' in self.kwargs:
+            try:
+                user = Variable(self.kwargs['user'])
+                user = user.resolve(context)
+            except:
+                pass # use user default
+        else:
+            # check the context for an already existing user
+            if 'user' in context:
+                user = context['user']
 
-        for tag in self.tags:
+        if 'limit' in self.kwargs:
+            try:
+                limit = Variable(self.kwargs['limit'])
+                limit = limit.resolve(context)
+            except:
+                pass # use limit default
+
+        if 'query' in self.kwargs:
+            try:
+                query = Variable(self.kwargs['query'])
+                query = query.resolve(context)
+            except:
+                query = self.kwargs['query'] # context string
+
+        # process tags
+        for tag in tags:
             tag = tag.strip()
             query = '%s "tag:%s"' % (query, tag)
 
-        for q_item in self.q:
-            q_item = q_item.strip()
-            query = '%s "%s"' % (query, q_item)
-
-        print query
-        pages = Page.objects.search(user=self.user, query=query)
+        # get the list of pages
+        pages = Page.objects.search(user=user, query=query)
         pages = pages.order_by('create_dt')
+        pages = [page.object for page in pages[:limit]]
 
-        pages = [page.object for page in pages[:self.limit]]
         context[self.context_var] = pages
         return ""
 
@@ -93,6 +97,14 @@ def list_pages(parser, token):
     bits = token.split_contents()
     context_var = bits[2]
 
+    if len(bits) < 3:
+        message = "'%s' tag requires at least 2 parameters" % bits[0]
+        raise TemplateSyntaxError(message)
+
+    if bits[1] != "as":
+        message = "'%s' second argument must be 'as'" % bits[0]
+        raise TemplateSyntaxError(message)
+    
     for bit in bits:
         if "limit=" in bit:
             kwargs["limit"] = bit.split("=")[1]
@@ -102,13 +114,5 @@ def list_pages(parser, token):
             kwargs["tags"] = bit.split("=")[1].replace('"','')
         if "q=" in bit:
             kwargs["q"] = bit.split("=")[1].replace('"','').split(',')
-
-    if len(bits) < 3:
-        message = "'%s' tag requires at least 2 parameters" % bits[0]
-        raise TemplateSyntaxError(message)
-
-    if bits[1] != "as":
-        message = "'%s' second argument must be 'as'" % bits[0]
-        raise TemplateSyntaxError(message)
 
     return ListPageNode(context_var, *args, **kwargs)
