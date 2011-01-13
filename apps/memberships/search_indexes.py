a83ca6c1412c45f010e14c41ec8f9b8175b93296
eloyz <eloyz.email@gmail.com>
2011-01-13 14:55:43 -0600

    Made membership search_indexing and search_text file. Made membership manager.  Updated URL's with membership details page. Added memberships to Django Admin Site linking to T5 details page. Membership details page is just being started.  Not much there yet, thinking I might just put this information in a tab on the profile page and that should be enough.  Rather than having a profile and membership page which look very very similiar.

--- apps/memberships/search_indexes.py
+++ apps/memberships/search_indexes.py
@@ -1,8 +1,39 @@
 from django.utils.html import strip_tags, strip_entities
 
 from haystack import indexes, site
-from memberships.models import App, AppEntry
+from memberships.models import App, AppEntry, Membership
 from perms.models import ObjectPermission
+
+class MembershipIndex(indexes.RealTimeSearchIndex):
+    text = indexes.CharField(document=True, use_template=True)
+
+    # tendenci_base fields
+    allow_anonymous_view = indexes.BooleanField(model_attr='allow_anonymous_view')
+    allow_user_view = indexes.BooleanField(model_attr='allow_user_view')
+    allow_member_view = indexes.BooleanField(model_attr='allow_member_view')
+    allow_anonymous_edit = indexes.BooleanField(model_attr='allow_anonymous_edit')
+    allow_user_edit = indexes.BooleanField(model_attr='allow_user_edit')
+    allow_member_edit = indexes.BooleanField(model_attr='allow_member_edit')
+    creator = indexes.CharField(model_attr='creator')
+    creator_username = indexes.CharField(model_attr='creator_username')
+    owner = indexes.CharField(model_attr='owner')
+    owner_username = indexes.CharField(model_attr='owner_username')
+    create_dt = indexes.DateTimeField(model_attr='create_dt')
+    update_dt = indexes.DateTimeField(model_attr='update_dt')
+    status = indexes.IntegerField(model_attr='status')
+    status_detail = indexes.CharField(model_attr='status_detail')
+
+    # permission
+    who_can_view = indexes.CharField()
+
+    def prepare_who_can_view(self, obj):
+        users = ObjectPermission.objects.who_has_perm('memberships.view_membership', obj)
+
+        if not users:
+            users = []
+
+        return ','.join([u.username for u in users])
+
 
 class MemberAppIndex(indexes.RealTimeSearchIndex):
     text = indexes.CharField(document=True, use_template=True)
@@ -46,5 +77,6 @@ class MemberAppEntryIndex(indexes.RealTimeSearchIndex):
     text = indexes.CharField(document=True, use_template=True)
     entry_time = indexes.DateTimeField(model_attr='entry_time')
 
+site.register(Membership, MembershipIndex)
 site.register(App, MemberAppIndex)
 site.register(AppEntry, MemberAppEntryIndex)
