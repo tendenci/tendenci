e8c93e4a4494d8d4fdad4b7d3b64ce7232afb118
eloyz <eloyz.email@gmail.com>
2011-01-19 16:42:42 -0600

    Membership application updates. Email sent to 'all admin notice recipients' when an application is approved and disapproved.  The field is found in settings.

--- apps/memberships/management.py
+++ apps/memberships/management.py
@@ -1,10 +1,25 @@
 from django.db.models.signals import post_syncdb
-#from django.contrib.contenttypes.models import ContentType
+from django.utils.translation import ugettext_noop as _
 from perms.utils import update_admin_group_perms
+from memberships import models as membership
+from notification import models as notification
 
 # assign permissions to the admin auth group
 def assign_permissions(app, created_models, verbosity, **kwargs):
     update_admin_group_perms()
 
-from memberships import models as membership
 post_syncdb.connect(assign_permissions, sender=membership)
+
+def create_notice_types(app, created_models, verbosity, **kwargs):
+
+    notification.create_notice_type(
+        'membership_application_approved',
+        _('Membership Application Approved'),
+        _('Membership Application Approved'))
+
+    notification.create_notice_type(
+        'membership_application_disapproved',
+        _('Membership Application Disapproved'),
+        _('Membership Application Disapproved'))
+
+post_syncdb.connect(create_notice_types, sender=notification)
