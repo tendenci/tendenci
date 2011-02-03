82e423f755524f600a688c21b4ebc7bdd6ac0105
Eloy Zuniga Jr <eloyz.email@gmail.com>
2011-02-03 11:02:45 -0600

    Added attributes to case study application.  e.g. 'services', 'technologies'

--- plugins/case_studies/models.py
+++ plugins/case_studies/models.py
@@ -9,10 +9,13 @@ class CaseStudy(TendenciBaseModel):
     client = models.CharField(max_length=75)
     website = models.URLField(max_length=150)
     slug = models.SlugField(max_length=100)
+    url = models.URLField()
     overview = models.TextField(blank=True, null=True)
     execution = models.TextField(blank=True, null=True)
     results = models.TextField(blank=True, null=True)
     tags = TagField(blank=True, help_text=_('Tags separated by commas. E.g Tag1, Tag2, Tag3'))
+    services = models.ManyToManyField('Service')
+    technologies = models.ManyToManyField('Technology')
 
     objects = CaseStudyManager()
 
@@ -27,3 +30,33 @@ class CaseStudy(TendenciBaseModel):
     @models.permalink
     def get_absolute_url(self):
         return ("case_study.view", [self.slug])
+
+
+class Service(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ("case_study.service", [self.pk])
+
+
+class Technology(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ("case_study.technology", [self.pk])
