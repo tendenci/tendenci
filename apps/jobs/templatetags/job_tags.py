f1d5e579084bafc4e4c8e5e5ac84300388313aae
jmoswalt <jmoswalt@gmail.com>
2011-02-09 17:29:17 -0600

    list_jobs template tag created with ordering and random option.

--- apps/jobs/templatetags/job_tags.py
+++ apps/jobs/templatetags/job_tags.py
@@ -1,4 +1,9 @@
-from django.template import Library
+import random
+
+from django.template import Node, Library, TemplateSyntaxError, Variable
+from django.contrib.auth.models import AnonymousUser
+
+from jobs.models import Job
 
 register = Library()
 
@@ -57,3 +62,116 @@ def job_pricing_table(context):
         'show_member_price': show_member_price
     })
     return context
+    
+class ListJobNode(Node):
+    def __init__(self, context_var, *args, **kwargs):
+        self.context_var = context_var
+        self.kwargs = kwargs
+
+    def render(self, context):
+        tags = u''
+        query = u''
+        user = AnonymousUser()
+        limit = 3
+        order = u'-create_dt'
+        randomize = False
+        
+        if 'random' in self.kwargs:
+            randomize = True
+
+        if 'tags' in self.kwargs:
+            try:
+                tags = Variable(self.kwargs['tags'])
+                tags = unicode(tags.resolve(context))
+            except:
+                tags = self.kwargs['tags'] # context string
+            tags = tags.split(',')
+
+        if 'user' in self.kwargs:
+            try:
+                user = Variable(self.kwargs['user'])
+                user = user.resolve(context)
+            except:
+                pass # use user default
+        else:
+            # check the context for an already existing user
+            if 'user' in context:
+                user = context['user']
+
+        if 'limit' in self.kwargs:
+            try:
+                limit = Variable(self.kwargs['limit'])
+                limit = limit.resolve(context)
+            except:
+                pass # use limit default
+
+        if 'query' in self.kwargs:
+            try:
+                query = Variable(self.kwargs['query'])
+                query = query.resolve(context)
+            except:
+                query = self.kwargs['query'] # context string
+
+        if 'order' in self.kwargs:
+            try:
+                order = Variable(self.kwargs['order'])
+                order = order.resolve(context)
+            except:
+                pass # use order default
+        
+        # process tags
+        for tag in tags:
+            tag = tag.strip()
+            query = '%s "tag:%s"' % (query, tag)
+
+        # get the list of jobs
+        jobs = Job.objects.search(user=user, query=query)
+        jobs = jobs.order_by(order)
+        if randomize:
+            if jobs.count() < limit:
+                jobs = [cs.object for cs in jobs]
+            else:
+                jobs = [cs.object for cs in random.sample(jobs, limit)]
+        else:
+            jobs = [cs.object for cs in jobs[:limit]]
+
+        context[self.context_var] = jobs
+        return ""
+
+@register.tag
+def list_jobs(parser, token):
+    """
+    Example:
+        {% list_jobs as jobs_list [user=user limit=3 order"title" q="engineering"] %}
+        {% for job in jobs_list %}
+            {{ job.title }}
+        {% endfor %}
+
+    """
+    args, kwargs = [], {}
+    bits = token.split_contents()
+    context_var = bits[2]
+
+    if len(bits) < 3:
+        message = "'%s' tag requires at least 2 parameters" % bits[0]
+        raise TemplateSyntaxError(message)
+
+    if bits[1] != "as":
+        message = "'%s' second argument must be 'as'" % bits[0]
+        raise TemplateSyntaxError(message)
+
+    for bit in bits:
+        if "limit=" in bit:
+            kwargs["limit"] = bit.split("=")[1]
+        if "user=" in bit:
+            kwargs["user"] = bit.split("=")[1]
+        if "tags=" in bit:
+            kwargs["tags"] = bit.split("=")[1].replace('"','')
+        if "q=" in bit:
+            kwargs["query"] = bit.split("=")[1]
+        if "order=" in bit:
+            kwargs["order"] = bit.split("=")[1]
+        if "random" in bit:
+            kwargs["random"] = True
+
+    return ListJobNode(context_var, *args, **kwargs)
