84717c01b3c077996124e00362bbe95af614e26f
jmoswalt <jmoswalt@gmail.com>
2011-02-10 11:53:23 -0600

    Minor jobs loop update and larger staff updates.

--- plugins/staff/urls.py
+++ plugins/staff/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('staff.views',
     url(r'^staff/feed/$', LatestEntriesFeed(), name='staff.feed'),
     url(r'^staff/(?P<pk>\d+)/photo/(?P<size>\d+x\d+)/$', 'photo', name="staff.photo"),
     url(r'^staff/(?P<slug>[\w\-]+)/$', 'index', name="staff.view"),
+    url(r'^staff/(?P<slug>[\w\-]+)/(?P<cv>cv)/$', 'index', name="staff.cv"),
 )
