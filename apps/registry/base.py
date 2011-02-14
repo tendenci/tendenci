fcb10a1fff4436d7157837a44f86c97c025c9fa0
glenbot <glenbot@gmail.com>
2011-02-14 16:47:28 -0600

    Initital version of app registry. Only pages and staff have been converted. Pushing to test on staging

---
+++ apps/registry/base.py
@@ -0,0 +1,196 @@
+from django.core.urlresolvers import reverse
+from django.utils.functional import lazy
+from django.contrib.admin import site as admin_site
+from django.utils.translation import ugettext_lazy as _
+
+from registry.exceptions import *
+
+lazy_reverse = lazy(reverse, str)
+
+
+class FieldDict(dict):
+    """
+    Custom Field dict that will allow unicode
+    representation based on a models verbose_name
+    """
+    def __unicode__(self):
+        return self['verbose_name']
+
+
+class DeclarativeMetaclass(type):
+    """
+    Metaclass for app registry
+
+    Loops through available fields and assigns them to
+    a FieldDict to be used in templates or python code
+    """
+    def __new__(cls, name, bases, attrs):
+        attrs['fields'] = FieldDict()
+        allowed_fields = [
+            'version',
+            'author',
+            'author_email',
+            'description',
+            'icon',
+            'url',
+            'packages'
+        ]
+
+        if 'app_registry' in attrs['__module__']:
+            for field_name, item in attrs.items():
+                if field_name not in ['fields', '__module__']:
+                    if field_name not in allowed_fields:
+                        exception = 'Registry field %s not allowed. '\
+                                    'They following fields are allowed: %s'
+                        raise FieldNotAllowed(_(exception) % (
+                            field_name,
+                            allowed_fields
+                        ))
+                    field = attrs.pop(field_name)
+                    attrs['fields'][field_name] = field
+
+        return super(DeclarativeMetaclass, cls).__new__(cls, name, bases, attrs)
+
+
+class Registry(object):
+    """
+    Base registry class for core and plugin applications
+
+    Example Registry:
+
+    from registry import site
+    from registry.base import CoreRegistry, lazy_reverse
+    from models import ExmpleModel
+
+    class ExampleRegistry(CoreRegistry):
+        version = '1.0'
+        author = 'Glen Zangirolami'
+        author_email = 'glenbot@gmail.com'
+        description = 'Create staff pages easily with photo, position, bio and more ..'
+
+        url = {
+            'add': lazy_reverse('page.add'),
+            'search': lazy_reverse('page.search'),
+        }
+
+    site.register(ExampleModel, ExampleRegistry)
+    """
+    __metaclass__ = DeclarativeMetaclass
+
+    def __init__(self, model):
+        self.model = model
+        self.has_admin = self._has_admin()
+
+        # provide the model in case extra parameters are needed
+        self.fields['model'] = self.model
+
+        # transfer verbose names for easy access
+        self.fields['verbose_name'] = unicode(self.model._meta.verbose_name)
+        self.fields['verbose_name_plural'] = unicode(self.model._meta.verbose_name_plural)
+
+        # default url patterns
+        if 'url' not in self.fields:
+            self.fields['url'] = self._get_url()
+
+        # default icon path
+        if 'icon' not in self.fields:
+            self.fields['icon'] = self._get_icon()
+
+        if 'packages' in self.fields:
+            if not isinstance(self.fields['packages'], tuple):
+                raise FieldError(_('Registry field packages must be of type tuple'))
+            self._test_packages(self.fields['packages'])
+
+    def _test_packages(self, packages):
+        """
+        Attempts to import the packages listed in the plugin registry
+        Raises an error if the package cannot be imported
+        """
+        for package in packages:
+            try:
+                __import__(package)
+            except:
+                exception = 'Plugin dependency package `%(package)s` could not be imported. ' \
+                            'Try `pip install %(package)s`?'
+                raise PackageImportError(_(exception) % {
+                    'package': package,
+                })
+
+    def _get_icon(self):
+        """
+        Autogenerate the location for the apps icon
+        """
+        # TODO: find a more global area to set this URL path
+        default_plugin_url = 'plugin-media'
+        return '/%s/%s/images/icon.png' % (
+            default_plugin_url,
+            unicode(self.model._meta.verbose_name_plural.lower())
+        )
+
+    def _get_url(self):
+        """
+        Autogenetate the URLs that will exist in the apps urls dict
+
+        Example from template:
+        {{ app.url.add }}
+        {{ app.url.search }}
+
+        Example in python:
+        app['url']['add']
+        app['url']['search']
+        """
+        url = {
+            'add': '',
+            'search': '',
+        }
+        admin_add = 'admin:%s_%s_add' % (
+            self.model._meta.app_label,
+            self.model._meta.module_name,
+        )
+        admin_index = 'admin:%s_%s_changelist' % (
+            self.model._meta.app_label,
+            self.model._meta.module_name,
+        )
+        if self.has_admin:
+            url.update({
+                'add': self._reverse(admin_add),
+                'search': self._reverse(admin_index)
+            })
+        return url
+
+    def _has_admin(self):
+        """
+        Tests for django admin site registration
+        """
+        for model in admin_site._registry.keys():
+            if self.model is model:
+                return True
+        return False
+
+    def _reverse(self, namespace):
+        """
+        Lazy url resolver
+        """
+        return lazy_reverse(namespace)
+
+
+class CoreRegistry(Registry):
+    """
+    Registry for core applications
+    """
+    def __init__(self, model):
+        super(CoreRegistry, self).__init__(model)
+
+        # application type
+        self.fields['app_type'] = 'core'
+
+
+class PluginRegistry(Registry):
+    """
+    Registry for plugin applications
+    """
+    def __init__(self, model):
+        super(PluginRegistry, self).__init__(model)
+
+        # application type
+        self.fields['app_type'] = 'plugin'
---
+++ apps/registry/sites.py
@@ -0,0 +1,45 @@
+import copy
+from registry.exceptions import AlreadyRegistered, NotRegistered
+
+
+class RegistrySite(object):
+    """
+    Encapsulates all the registries that should be available.
+    This follows the django.contrib.admim model
+    """
+    def __init__(self):
+        self._registry = {}
+
+    def register(self, model, registry_class=None):
+        """
+        Registers a model with the site.
+
+        The model should be a Model class, not instances.
+
+        If no registry_class is provided CoreRegistry will be applied
+        to the model.
+        """
+        if not registry_class:
+            from registry.base import CoreRegistry
+            registry_class = CoreRegistry
+
+        if not hasattr(model, '_meta'):
+            raise AttributeError('The model being registered must derive from Model.')
+
+        if model in self._registry:
+            raise AlreadyRegistered('The model %s is already registered' % model.__class__)
+
+        self._registry[model] = registry_class(model)
+
+    def unregister(self, model):
+        """
+        Unregisters a model from the site.
+        """
+        if model not in self._registry:
+            raise NotRegistered('The model %s is not registered' % model.__class__)
+        del(self._registry[model])
+
+    def get_registered_apps(self):
+        return self._registry
+
+site = RegistrySite()
