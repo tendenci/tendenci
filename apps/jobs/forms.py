d6b669dcd611a85c2340e195278fa9e197119317
glenbot <glenbot@gmail.com>
2011-02-22 16:11:46 -0600

    Added functionality for pending jobs.

--- apps/jobs/forms.py
+++ apps/jobs/forms.py
@@ -5,6 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.utils.safestring import mark_safe
 from django.core.urlresolvers import reverse
 
+from captcha.fields import CaptchaField
 from jobs.models import Job
 from perms.utils import is_admin
 from perms.forms import TendenciBaseForm
@@ -14,15 +15,40 @@ from jobs.models import JobPricing
 from jobs.utils import get_duration_choices, get_payment_method_choices
 
 request_duration_defaults = {
-    'help_text': mark_safe('<a href="%s">Add pricing options</a>' % '/jobs/pricing/add/'),                             
+    'help_text': mark_safe('<a href="%s">Add pricing options</a>' % '/jobs/pricing/add/')
 }
+
+STATUS_DETAIL_CHOICES = (
+    ('active', 'Active'),
+    ('inactive', 'Inactive'),
+    ('pending', 'Pending'),
+    ('paid - pending approval', 'Paid - Pending Approval'),
+)
+
+DURATION_CHOICES = (
+    (14, '14 Days from Activation date'),
+    (30, '30 Days from Activation date'),
+    (60, '60 Days from Activation date'),
+    (90, '90 Days from Activation date'),
+    (120, '120 Days from Activation date'),
+    (180, '180 Days from Activation date'),
+    (365, '365 Days from Activation date'),
+)
+
+STATUS_CHOICES = (
+    (1, 'Active'),
+    (0, 'Inactive'),
+)
+
 
 class JobForm(TendenciBaseForm):
 
     description = forms.CharField(required=False,
-        widget=TinyMCE(attrs={'style':'width:100%'}, 
-        mce_attrs={'storme_app_label':Job._meta.app_label, 
-        'storme_model':Job._meta.module_name.lower()}))
+        widget=TinyMCE(attrs={'style': 'width:100%'},
+        mce_attrs={'storme_app_label': Job._meta.app_label,
+        'storme_model': Job._meta.module_name.lower()}))
+
+    captcha = CaptchaField()
 
     activation_dt = SplitDateTimeField(label=_('Activation Date/Time'),
         initial=datetime.now())
@@ -34,14 +60,14 @@ class JobForm(TendenciBaseForm):
         initial=datetime.now())
 
     status_detail = forms.ChoiceField(
-        choices=(('active','Active'),('inactive','Inactive'), ('pending','Pending'),))
-    
+        choices=(('active', 'Active'), ('inactive', 'Inactive'), ('pending', 'Pending'),))
+
     requested_duration = forms.ChoiceField(**request_duration_defaults)
-    
-    list_type = forms.ChoiceField(initial='regular', choices=(('regular','Regular'),
+
+    list_type = forms.ChoiceField(initial='regular', choices=(('regular', 'Regular'),
                                                               ('premium', 'Premium'),))
     payment_method = forms.CharField(error_messages={'required': 'Please select a payment method.'})
-    
+
     class Meta:
         model = Job
         fields = (
@@ -81,7 +107,7 @@ class JobForm(TendenciBaseForm):
         'contact_fax',
         'contact_email',
         'contact_website',
-        'tags',        
+        'tags',
         'allow_anonymous_view',
         'syndicate',
         'status',
@@ -101,15 +127,15 @@ class JobForm(TendenciBaseForm):
                                 'experience',
                                 'education',
                                 'level',
-                                'period',   
+                                'period',
                                 'percent_travel',
                                 'contact_method',
                                 'position_reports_to',
                                 'salary_from',
-                                'salary_to',     
+                                'salary_to',
                                 'is_agency',
                                 'requested_duration',
-                                'activation_dt',                                
+                                'activation_dt',
                                 'expiration_dt',
                                 'post_dt',
                                 'entity'
@@ -138,6 +164,11 @@ class JobForm(TendenciBaseForm):
                                  ],
                         'classes': ['contact'],
                       }),
+                     ('Security Code', {
+                      'fields': ['captcha',
+                                 ],
+                        'classes': ['captcha'],
+                      }),
                       ('Permissions', {
                       'fields': ['allow_anonymous_view',
                                  'user_perms',
@@ -148,55 +179,74 @@ class JobForm(TendenciBaseForm):
                      ('Administrator Only', {
                       'fields': ['syndicate',
                                  'status',
-                                 'status_detail'], 
+                                 'status_detail'],
                       'classes': ['admin-only'],
                     })]
-        
-    def __init__(self, *args, **kwargs): 
+
+    def __init__(self, *args, **kwargs):
         super(JobForm, self).__init__(*args, **kwargs)
         if self.instance.pk:
             self.fields['description'].widget.mce_attrs['app_instance_id'] = self.instance.pk
             if is_admin(self.user):
-                self.fields['status_detail'].choices = (('active','Active'),
-                                                        ('inactive','Inactive'), 
-                                                        ('pending','Pending'),
-                                                        ('paid - pending approval', 'Paid - Pending Approval'),)
+                self.fields['status_detail'].choices = STATUS_DETAIL_CHOICES
         else:
-            self.fields['description'].widget.mce_attrs['app_instance_id'] = 0        
-        
-        if not is_admin(self.user):
-            if 'status' in self.fields: self.fields.pop('status')
-            if 'status_detail' in self.fields: self.fields.pop('status_detail')
-            if 'syndicate' in self.fields: self.fields.pop('syndicate')
-            
-        if self.fields.has_key('payment_method'):
+            self.fields['description'].widget.mce_attrs['app_instance_id'] = 0
+
+        if 'payment_method' in self.fields:
             self.fields['payment_method'].widget = forms.RadioSelect(choices=get_payment_method_choices(self.user))
-        if self.fields.has_key('requested_duration'):
+        if 'requested_duration' in self.fields:
             self.fields['requested_duration'].choices = get_duration_choices()
 
+        # adjust fields depending on user status
+        fields_to_pop = []
+        if not self.user.is_authenticated():
+            fields_to_pop += [
+                'entity',
+                'allow_anonymous_view',
+                'user_perms',
+                'group_perms',
+                'post_dt',
+                'activation_dt',
+                'expiration_dt',
+                'syndicate',
+                'status',
+                'status_detail'
+            ]
+        else:
+            fields_to_pop += [
+               'captcha'
+            ]
+        print is_admin(self.user)
+        if not is_admin(self.user):
+            fields_to_pop += [
+                'entity',
+                'allow_anonymous_view',
+                'user_perms',
+                'group_perms',
+                'post_dt',
+                'activation_dt',
+                'expiration_dt',
+                'syndicate',
+                'status',
+                'status_detail'
+            ]
+        for f in list(set(fields_to_pop)):
+            if f in self.fields:
+                self.fields.pop(f)
 
-DURATION_CHOICES = ((14,'14 Days from Activation date'), 
-                    (30,'30 Days from Activation date'), 
-                    (60,'60 Days from Activation date'), 
-                    (90,'90 Days from Activation date'),
-                    (120,'120 Days from Activation date'),
-                    (180,'180 Days from Activation date'),
-                    (365,'365 Days from Activation date'),
-                    )
-STATUS_CHOICES = ((1, 'Active'),
-                   (0, 'Inactive'),)
-       
-class JobPricingForm(forms.ModelForm): 
+
+class JobPricingForm(forms.ModelForm):
     duration = forms.ChoiceField(initial=14, choices=DURATION_CHOICES)
     status = forms.ChoiceField(initial=1, choices=STATUS_CHOICES, required=False)
+
     class Meta:
         model = JobPricing
-        fields = ('duration',
-                  'regular_price',
-                  'premium_price',
-                  'regular_price_member',
-                  'premium_price_member',
-                  'show_member_pricing',
-                  'status',)
-    
-        
+        fields = (
+            'duration',
+            'regular_price',
+            'premium_price',
+            'regular_price_member',
+            'premium_price_member',
+            'show_member_pricing',
+            'status',
+         )
