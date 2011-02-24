5209761d674560a14921de57be6cbbaa8c4117d2
Herman Polloni <herman@bixly.com>
2011-02-23 22:29:25 -0300

    Improving handling of autoloading of feeds on GlobalFeed, added template for description and title, handling photo rss

--- apps/rss/feeds.py
+++ apps/rss/feeds.py
@@ -16,6 +16,7 @@ from pages.models import Page
 from photos.models import PhotoSet
 from directories.models import Directory
 import feedsmanager
+from django.template.loader import render_to_string
 
 site_url = get_setting('site', 'global', 'siteurl')
 site_display_name = get_setting('site', 'global', 'sitedisplayname')
@@ -31,59 +32,68 @@ class GlobalFeed(Feed):
     link = '%s/rss' % (site_url)
     description = site_description
 
-    def items(self):
-        self.load_feeds_items()
-        items = []
-        for feed in self.feeds_items:
-            items.append(self.feeds_items[feed][:settings.MAX_FEED_ITEMS_PER_APP])
-        return items[:max_items]
-
-    def item_title(self, item):
-        feed = self.get_feed_from_item(item)
-        if not feed is None:
-            return feed.item_title(item)
-        return ''
-
-    def item_description(self, item):
-        feed = self.get_feed_from_item(item)
-        if not feed is None:
-            return feed.item_description(item)
-        return ''
-    
-    def item_pubdate(self, item):
-        feed = self.get_feed_from_item(item)
-        if not feed is None:
-            return feed.item_pubdate(item)
-        return None
-
-    def item_link(self, item):
-        feed = self.get_feed_from_item(item)
-        if not feed is None:
-            return feed.item_link(item)
-        return ''
-    
-    def item_author_name(self, item):
-        feed = self.get_feed_from_item(item)
-        if not feed is None:
-            return feed.item_author_name(item)
-        return ''
-
-    def get_feed_from_item(self, item):
-        for feed in self.feeds_items:
-            if item in self.feeds_items[feed]:
-                return feed
-        return None
+    def __init__(self):
+        Feed.__init__(self)
+        self.all_items = []     # all items for this rss feed
+        self.feed_for_item = {}   # item -> feed cache
+        self.load_feeds_items() # load items
 
     def load_feeds_items(self):
-        if not hasattr(self, 'feeds_items'):
-            self.feeds_items = {}
-        if len(self.feeds_items) == 0:
-            feeds = feedsmanager.get_all_feeds()
-            for feed in feeds:
-                feed_instance = feed()
-                print "Feed found: %s" % feed_instance.title
-                self.feeds_items[feed_instance] = feed_instance.items()
+        """ Load all feeds items """
+        print "creating new feeds_items"
+        feeds = feedsmanager.get_all_feeds()
+        for feed in feeds:
+            feed_instance = feed()
+            print "Feed found: %s" % feed_instance.title
+            item_per_feed_cnt = 0
+            for item in feed_instance.items():
+                print "Item: %s" % feed_instance.item_title(item)
+                self.feed_for_item[item] = feed_instance
+                self.all_items.append(item)
+                item_per_feed_cnt += 1
+                if item_per_feed_cnt >= settings.MAX_FEED_ITEMS_PER_APP:
+                    break
 
+    def items(self):
+        return self.all_items[:max_items]
+
+    def item_title(self, item):
+        feed = self.feed_for_item[item]
+        if hasattr(feed, 'title_template') and not feed.title_template is None:
+            # use the template instead of the method
+            print feed.title_template
+            return render_to_string(feed.title_template, { 'obj' : item })
+        return self.get_attr_item('title', item)
+
+    def item_description(self, item):
+        feed = self.feed_for_item[item]
+        if hasattr(feed, 'description_template') and not feed.description_template is None:
+            # use the template instead
+            print feed.description_template
+            return render_to_string(feed.description_template, { 'obj' : item })
+        return self.get_attr_item('description', item)
+
+    def item_pubdate(self, item):
+        return self.get_attr_item('pubdate', item)
+
+    def item_link(self, item):
+        return self.get_attr_item('link', item)
+    
+    def item_author_name(self, item):
+        return self.get_attr_item('author_name', item)
+
+    def get_attr_item(self, attr, item):
+        """ Makes a call the corresponding item_* method on the feed object """
+        if item in self.feed_for_item:
+            # lookup item on our item->feed cache
+            feed = self.feed_for_item[item]
+            methodname = 'item_' + attr
+            if hasattr(feed, methodname):
+                # check to see if the feed has the method, if so
+                # call it
+                method = getattr(feed, methodname)
+                return method.__func__(feed, item)
+        return None
 
 class MainRSSFeed(Feed):
     #feed_type = Rss201rev2Feed
