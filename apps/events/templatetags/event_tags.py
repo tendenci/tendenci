16e675eed6e5e9d0d313b621afe02fc2b590fdf5
jmoswalt <jmoswalt@gmail.com>
2011-02-25 14:25:41 -0600

    Overwrite of events list tags to allow a custom sort order to filter out past events and only show newest.

--- apps/events/templatetags/event_tags.py
+++ apps/events/templatetags/event_tags.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from django.contrib.humanize.templatetags.humanize import naturalday
 from django.core.urlresolvers import reverse
 from django.template.defaultfilters import floatformat
+from django.db import models
 from django.template import Node, Library, TemplateSyntaxError, Variable
 from django.contrib.auth.models import AnonymousUser
 
@@ -244,6 +245,98 @@ def is_registered_user(parser, token):
 
 class ListEventsNode(ListNode):
     model = Event
+    
+    def __init__(self, context_var, *args, **kwargs):
+        self.context_var = context_var
+        self.kwargs = kwargs
+
+        if not self.model:
+            raise AttributeError(_('Model attribute must be set'))
+        if not issubclass(self.model, models.Model):
+            raise AttributeError(_('Model attribute must derive from Model'))
+        if not hasattr(self.model.objects, 'search'):
+            raise AttributeError(_('Model.objects does not have a search method'))
+
+    def render(self, context):
+        tags = u''
+        query = u''
+        user = AnonymousUser()
+        limit = 3
+        order = u''
+        randomize = False
+
+        if 'random' in self.kwargs:
+            randomize = bool(self.kwargs['random'])
+
+        if 'tags' in self.kwargs:
+            try:
+                tags = Variable(self.kwargs['tags'])
+                tags = unicode(tags.resolve(context))
+            except:
+                tags = self.kwargs['tags']
+
+            tags = tags.replace('"', '')
+            tags = tags.split(',')
+            
+            print tags
+
+        if 'user' in self.kwargs:
+            try:
+                user = Variable(self.kwargs['user'])
+                user = user.resolve(context)
+            except:
+                user = self.kwargs['user']
+        else:
+            # check the context for an already existing user
+            if 'user' in context:
+                user = context['user']
+
+        if 'limit' in self.kwargs:
+            try:
+                limit = Variable(self.kwargs['limit'])
+                limit = limit.resolve(context)
+            except:
+                limit = self.kwargs['limit']
+
+        limit = int(limit)
+
+        if 'query' in self.kwargs:
+            try:
+                query = Variable(self.kwargs['query'])
+                query = query.resolve(context)
+            except:
+                query = self.kwargs['query']  # context string
+
+        if 'order' in self.kwargs:
+            try:
+                order = Variable(self.kwargs['order'])
+                order = order.resolve(context)
+            except:
+                order = self.kwargs['order']
+
+        # process tags
+        for tag in tags:
+            tag = tag.strip()
+            query = '%s "tag:%s"' % (query, tag)
+
+        # get the list of staff
+        items = self.model.objects.search(user=user, query=query)
+
+        # if order is not specified it sorts by relevance
+        if order:
+            if order == "next_upcoming":
+                items = items.filter(start_dt__gt = datetime.now())
+                items = items.order_by("start_dt")
+            else:
+                items = items.order_by(order)
+
+        if randomize:
+            objects = [item.object for item in random.sample(items, items.count())][:limit]
+        else:
+            objects = [item.object for item in items[:limit]]
+
+        context[self.context_var] = objects
+        return ""
 
 
 @register.tag
