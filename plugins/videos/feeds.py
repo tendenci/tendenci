eac906ce013116ad52a5656709dba83ec17d7f70
jmoswalt <jmoswalt@gmail.com>
2011-02-27 14:15:36 -0600

    Updates to RSS feeds to include the proper URL to the piece of content. Tweak to embed forms display. Removed feed import on contributions and files. Fix in stories_tags to recognize kwargs.

--- plugins/videos/feeds.py
+++ plugins/videos/feeds.py
@@ -1,22 +1,22 @@
 from rss.feedsmanager import SubFeed
 from haystack.query import SearchQuerySet
 
-from models import Staff
+from models import Video
 
 class LatestEntriesFeed(SubFeed):
-    title =  'Latest Staff'
-    link =  "/staff/"
-    description =  "Latest Staff"
+    title =  'Latest Videos'
+    link =  "/videos/"
+    description =  "Latest Videos"
 
     def items(self):
-        sqs = SearchQuerySet().models(Staff).order_by('-start_dt')[:20]
+        sqs = SearchQuerySet().models(Video).order_by('-create_dt')[:20]
         return [sq.object for sq in sqs]
 
     def item_title(self, item):
-        return item.name
+        return item.title
 
     def item_description(self, item):
-        return item.biography
+        return item.embed_code()+item.description
 
     def item_link(self, item):
         return item.get_absolute_url()
