d0895c2a3bca9314b1197f78b330e36b4ef69adb
jennyq <jqian@schipul.com>
2011-02-27 18:21:17 -0600

    Updated event multi-registration to perform add and remove a registrant actions using javascript. Removed the broken feed urls in contributions and files.

--- apps/contributions/urls.py
+++ apps/contributions/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import patterns, url
-from contributions.feeds import LatestEntriesFeed
+#from contributions.feeds import LatestEntriesFeed
 
 urlpatterns = patterns('contributions',                  
     url(r'^$', 'views.index', name="contribution"),
     url(r'^(?P<id>\d+)/$', 'views.index', name="contribution"),
     url(r'^search/$', 'views.search', name="contribution.search"),
-    url(r'^feed/$', LatestEntriesFeed(), name='contribution.feed'),
+    #url(r'^feed/$', LatestEntriesFeed(), name='contribution.feed'),
 )
--- apps/files/urls.py
+++ apps/files/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, url
-from files.feeds import LatestEntriesFeed
+#from files.feeds import LatestEntriesFeed
 
 urlpatterns = patterns('files',                  
     url(r'^$', 'views.index', name="files"),
@@ -11,7 +11,7 @@ urlpatterns = patterns('files',
     url(r'^add/$', 'views.add', name="file.add"),
     url(r'^edit/(?P<id>\d+)/$', 'views.edit', name="file.edit"),
     url(r'^delete/(?P<id>\d+)/$', 'views.delete', name="file.delete"),
-    url(r'^feed/$', LatestEntriesFeed(), name='file.feed'),
+    #url(r'^feed/$', LatestEntriesFeed(), name='file.feed'),
     url(r'^tinymce/$', 'views.tinymce', name="file.tinymce"),
     url(r'^tinymce/template/(?P<id>\d+)/$', 'views.tinymce_upload_template'),
     url(r'^swfupload/$', 'views.swfupload', name="file.swfupload"),
