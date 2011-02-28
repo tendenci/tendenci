27adde00922fc4bd45f0a684500cd6b60de5e018
jennyq <jqian@schipul.com>
2011-02-27 18:38:28 -0600

    Removed broken rss urls in contributions and files.

--- apps/contributions/urls.py
+++ apps/contributions/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls.defaults import patterns, url
-#from contributions.feeds import LatestEntriesFeed
 
 urlpatterns = patterns('contributions',                  
     url(r'^$', 'views.index', name="contribution"),
     url(r'^(?P<id>\d+)/$', 'views.index', name="contribution"),
     url(r'^search/$', 'views.search', name="contribution.search"),
-    #url(r'^feed/$', LatestEntriesFeed(), name='contribution.feed'),
 )
