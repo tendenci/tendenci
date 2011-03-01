75bb704eac04f1d2bab98f0460bcd9ace0286136
Herman Polloni <herman@bixly.com>
2011-02-28 23:33:51 -0300

    First version for automatic sitemap

--- apps/pages/feeds.py
+++ apps/pages/feeds.py
@@ -2,6 +2,7 @@ from rss.feedsmanager import SubFeed
 from haystack.query import SearchQuerySet
 from site_settings.utils import get_setting
 from pages.models import Page
+from sitemaps import TendenciSitemap
 
 class LatestEntriesFeed(SubFeed):
     title =  '%s Latest Pages' % get_setting('site','global','sitedisplayname')
@@ -23,3 +24,15 @@ class LatestEntriesFeed(SubFeed):
         
     def item_link(self, item):
         return item.get_absolute_url()
+
+class PageSitemap(TendenciSitemap):
+    """ Sitemap information for pages """
+    changefreq = "yearly"
+    priority = 0.6
+    
+    def items(self):
+        sqs = Page.objects.search().order_by('-update_dt')
+        return [sq.object for sq in sqs]
+    
+    def lastmod(self, obj):
+        return obj.update_dt
