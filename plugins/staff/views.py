a1bc2737f2b8477fc57b5e02ae398362857cb910
glenbot <glenbot@gmail.com>
2011-03-01 22:57:01 -0600

    Removed old photo field code from staff plugin

--- plugins/staff/views.py
+++ plugins/staff/views.py
@@ -41,36 +41,3 @@ def search(request, template_name="staff/search.html"):
 
     return render_to_response(template_name, {'staff':staff},
         context_instance=RequestContext(request))
-        
-def photo(request, pk, size):
-    if not id: raise Http404
-    staff = get_object_or_404(Staff, pk=pk)
-
-    image = None
-    default_image = join(abspath(dirname(__file__)),'media','default.jpg')
-    
-    # get the default size
-    size = tuple([int(size) for size in size.split('x')])
-    if size < (24, 24):
-        size = (24, 24)
-
-    # non-admin can not view the non-active content
-    # status=0 has been taken care of in the has_perm function
-    if (staff.status_detail).lower() <> 'active' and (not is_admin(request.user)):
-        raise Http404
-
-    # get image binary
-    image =  staff.photo
-    if not staff.photo:
-        image = ImageFile(open(default_image,'rb'))
-
-    # set up the response
-    response = HttpResponse(mimetype='image/jpeg')
-    response['Content-Disposition'] = 'filename=%s'% (basename(image.name))
-
-    # save the image to the response
-    image = get_image(image, size, constrain=True, cache=True)
-    image.save(response, "JPEG", quality=100)
-
-    # output the image
-    return response
