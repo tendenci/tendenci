7817ff1c6c88458e43e302ea3fd8462fcca0f6ac
glenbot <glenbot@gmail.com>
2011-03-04 15:27:53 -0600

    Renamed server requirements file and added another boot script for servers

---
+++ scripts/tendenci-boot-server.sh
@@ -0,0 +1,11 @@
+# setup pip environment variables
+export PIP_REQUIRE_VIRTUALENV=true
+export PIP_RESPECT_VIRTUALENV=true
+
+if [ -z "$WORKON_HOME" ]; then
+    export PIP_VIRTUALENV_BASE=$WORKON_HOME
+fi
+
+# run pip
+pip install -r requirements_server.txt
+
