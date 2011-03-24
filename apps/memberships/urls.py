09a91a3cf37f13f56d8cf322c18ba2f6fd579219
Eloy Zuniga Jr <eloyz.email@gmail.com>
2011-03-24 10:07:07 -0500

    Updated MembershipArchive table (south migration not required).  Added membership copy method.  Added renew url pattern and renew method.

--- apps/memberships/urls.py
+++ apps/memberships/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns("memberships.views",
-
-
     # memberships
     url(r"^memberships/(?P<id>\d+)/$", "membership_details", name="membership.details"),
+    url(r'^memberships/renew/(?P<id>\d)+/$', 'membership_renew', name='membership.renew'),
 
     # entries
     url(r"^entries/$", "application_entries", name="membership.application_entries"),
@@ -18,5 +17,3 @@ urlpatterns = patterns("memberships.views",
     url(r"^confirmation/(?P<hash>[\w]+)/$", "application_confirmation", name="membership.application_confirmation"),
     url(r"^(?P<slug>[\w\-]+)/(?P<cmb_id>\d+)?/?$", "application_details", name="membership.application_details"),
     url(r"^(?P<slug>[\w\-]+)/corp-pre/(?P<cmb_id>\d+)?/?$", "application_details_corp_pre", name="membership.application_details_corp_pre"),
-    
-)
