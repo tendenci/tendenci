ce25688914d6c1ddd6f73da19607c2346777bc88
Eloy Zuniga Jr <eloyz.email@gmail.com>
2011-04-08 12:52:48 -0500

    Stash merge. This commit can be ignored.

--- apps/memberships/urls.py
+++ apps/memberships/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls.defaults import *
 
 
-
 urlpatterns = patterns("memberships.views",
     url(r"^$", "membership_index", name="membership.index"),
     url(r"^search/$", "membership_search", name="membership.search"),
@@ -10,7 +9,7 @@ urlpatterns = patterns("memberships.views",
     url(r"^memberships/(?P<id>\d+)/$", "membership_details", name="membership.details"),
     url(r'^memberships/renew/(?P<id>\d)+/$', 'membership_renew', name='membership.renew'),
     url(r"^memberships/import_csv/$", "import_membership_csv", name="import_membership_csv"),
-    
+
     # entries
     url(r"^entries/$", "application_entries", name="membership.application_entries"),
     url(r"^entries/(?P<id>\d+)/$", "application_entries", name="membership.application_entries"),
@@ -23,4 +22,3 @@ urlpatterns = patterns("memberships.views",
     url(r"^confirmation/(?P<hash>[\w]+)/$", "application_confirmation", name="membership.application_confirmation"),
     url(r"^(?P<slug>[\w\-]+)/(?P<cmb_id>\d+)?/?$", "application_details", name="membership.application_details"),
     url(r"^(?P<slug>[\w\-]+)/corp-pre/(?P<cmb_id>\d+)?/?$", "application_details_corp_pre", name="membership.application_details_corp_pre"),
-)
