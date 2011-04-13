f89eaa86f659407dfcb6ecbad26b0108c97d6457
jennyq <jqian@schipul.com>
2011-04-13 10:59:23 -0500

    Update the event management command fill_out_registrant_amount to avoid the string concatenation on the print thus prevent the potential UnicodeEncodeError (we don't want to wrap around the unicode encode function to it to slow down the process).

--- apps/events/management/commands/fill_out_registrant_amount.py
+++ apps/events/management/commands/fill_out_registrant_amount.py
@@ -6,7 +6,7 @@ class Command(BaseCommand):
     """
     This is a one-time script to fill out the amount field in the event registrant table. 
     
-    To run the command: python manage.py fill_out_registrant_amount
+    To run the command: python manage.py fill_out_registrant_amount --verbosity 2
     """
     
     def handle(self, *args, **options):
@@ -19,15 +19,20 @@ class Command(BaseCommand):
         
         if registrations:
             print "Start filling out the amount field for Registrant table:"
+            count = 0
             for i, reg8n in enumerate(registrations):
                 if reg8n.registrant_set.count() == 1:
                     registrant = reg8n.registrant_set.all()[0]
                     if not registrant.amount:
                         registrant.amount = reg8n.invoice.subtotal
                         registrant.save()
+                        count += 1
                         if verbosity >= 2:
-                            print '%d. %s %s (id=%d) - amount: %.2f' % (i+1, registrant.first_name, 
-                                        registrant.last_name, registrant.id, registrant.amount)
+                            try:
+                                print 'id=', registrant.id, registrant.first_name, registrant.last_name, registrant.amount
+                            except:
+                                pass
+            print 'Total updated: %d' % (count)
             print "Done"
         else:
             print "No registrations or registrations with payment on the site."
