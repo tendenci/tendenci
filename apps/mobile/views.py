0bcaecf9c242ca5ce8940ab9eb10978239cfc18f
jmoswalt <jmoswalt@gmail.com>
2011-04-13 16:11:29 -0700

    Updated mobile tag to detect if a site is a mobile browser to limit mobile links and switching to just those browsers. Added option to pass current path to redirect to instead of always going to one path.

--- apps/mobile/views.py
+++ apps/mobile/views.py
@@ -2,9 +2,9 @@ from django.template import RequestContext
 from django.shortcuts import redirect
 from django.conf import settings
 
-def toggle_mobile_mode(request, redirect_url):
+def toggle_mobile_mode(request):
     cookiename = getattr(settings, 'MOBILE_COOKIE_NAME', "tendenci_mobile")
-    response = redirect(redirect_url)
+    response = redirect(request.GET.get('next','/'))
     if request.mobile:
         response.set_cookie(cookiename, "0")
     else:
