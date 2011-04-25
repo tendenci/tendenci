07f95c01eed8957c7686976fdaf95e36d261b19f
Sam Lee <sam@bixly.com>
2011-04-26 04:05:40 +0800

    Found KeyErrors for saving settings when db entries do not match the field names of the forms in site_settings

--- apps/site_settings/forms.py
+++ apps/site_settings/forms.py
@@ -9,14 +9,17 @@ def clean_settings_form(self):
         Cleans data that has been set in the settings form
     """
     for setting in self.settings:
-        field_value = self.cleaned_data[setting.name]
-        if setting.data_type == "boolean":
-            if field_value != 'true' and field_value != 'false':
-                raise forms.ValidationError("'%s' must be true or false" % setting.label)
-        if setting.data_type == "int":
-            if field_value != ' ':
-                if not field_value.isdigit():
-                    raise forms.ValidationError("'%s' must a whole number" % setting.label)  
+        try:
+            field_value = self.cleaned_data[setting.name]
+            if setting.data_type == "boolean":
+                if field_value != 'true' and field_value != 'false':
+                    raise forms.ValidationError("'%s' must be true or false" % setting.label)
+            if setting.data_type == "int":
+                if field_value != ' ':
+                    if not field_value.isdigit():
+                        raise forms.ValidationError("'%s' must a whole number" % setting.label)
+        except KeyError:
+            pass
     return self.cleaned_data
     
 def save_settings_form(self):
@@ -25,20 +28,23 @@ def save_settings_form(self):
         Removes and updates the settings cache
     """
     for setting in self.settings:
-        field_value = self.cleaned_data[setting.name]
-        if setting.value != field_value:
-            # delete the cache for all the settings to reset the context
-            key = [SETTING_PRE_KEY, 'all.settings']
-            key = '.'.join(key)
-            cache.delete(key)
-            
-            # delete and set cache for single key and save the value in the database
-            delete_all_settings_cache()
-            delete_setting_cache(setting.scope, setting.scope_category, setting.name)
-            setting.value = field_value
-            setting.save()
-            cache_setting(setting.scope, setting.scope_category, setting.name,
-              setting)
+        try:
+            field_value = self.cleaned_data[setting.name]
+            if setting.value != field_value:
+                # delete the cache for all the settings to reset the context
+                key = [SETTING_PRE_KEY, 'all.settings']
+                key = '.'.join(key)
+                cache.delete(key)
+                
+                # delete and set cache for single key and save the value in the database
+                delete_all_settings_cache()
+                delete_setting_cache(setting.scope, setting.scope_category, setting.name)
+                setting.value = field_value
+                setting.save()
+                cache_setting(setting.scope, setting.scope_category, setting.name,
+                  setting)
+        except KeyError:
+            pass
             
 
 def build_settings_form(user, settings):
