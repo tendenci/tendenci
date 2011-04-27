f75d0679f82919dd8e289b89ed7a28f0708abc8d
Sam Lee <sam@bixly.com>
2011-04-28 07:57:10 +0800

    deleter for speakers and group pricing. both present in event detail

--- apps/events/models.py
+++ apps/events/models.py
@@ -477,6 +477,9 @@ class GroupRegistrationConfiguration(models.Model):
     
     create_dt = models.DateTimeField(auto_now_add=True)
     update_dt = models.DateTimeField(auto_now=True)
+    
+    def __unicode__(self):
+        return self.group.name
 
 class Payment(models.Model):
     """
