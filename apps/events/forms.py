763b4232c20ccc5832805a3d63ebc7844f44dce7
eloyz <eloyz.email@gmail.com>
2011-05-04 11:52:20 -0500

    Updating "Pricing and Times" label (in more locations) on the events module.

--- apps/events/forms.py
+++ apps/events/forms.py
@@ -281,8 +281,6 @@ class GroupReg8nEditForm(BetterModelForm):
     late_dt = SplitDateTimeField(label=_('Late Date/Time'))
     end_dt = SplitDateTimeField(label=_('End Date/Time'))
     
-    #reg8n_dt_price = Reg8nDtField(label=_('Times and Pricing'), required=False)
-    
     def clean(self):
         
         early_price = self.cleaned_data.get('early_price') or 0
