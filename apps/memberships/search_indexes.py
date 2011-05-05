4d47a9c02435e6fd1d441987033e8ffde8fab1c3
eloyz <eloyz.email@gmail.com>
2011-05-05 11:42:00 -0500

    Updating Memberships Application:  Adding import feature. Duplicate detection and Requirement detection still not available.  The import executes during runtime (which takes a while), worth nothing.

--- apps/memberships/search_indexes.py
+++ apps/memberships/search_indexes.py
@@ -1,7 +1,7 @@
 from django.utils.html import strip_tags, strip_entities
 
 from haystack import indexes, site
-from memberships.models import App, AppEntry, Membership
+from memberships.models import App, AppEntry, Membership, MembershipArchive
 from perms.models import ObjectPermission
 
 class MembershipIndex(indexes.RealTimeSearchIndex):
@@ -34,12 +34,41 @@ class MembershipIndex(indexes.RealTimeSearchIndex):
         return 'update_dt'
 
     def prepare_who_can_view(self, obj):
-        users = ObjectPermission.objects.who_has_perm('memberships.view_membership', obj)
+        users = ObjectPermission.objects.who_has_perm('memberships.view_membership', obj) or []
+        return ','.join(u.username for u in users)
 
-        if not users:
-            users = []
+class MembershipArchiveIndex(indexes.RealTimeSearchIndex):
+    text = indexes.CharField(document=True, use_template=True)
+    corporate_membership_id = indexes.BooleanField(model_attr='corporate_membership_id')
 
-        return ','.join([u.username for u in users])
+    # tendenci_base fields
+    allow_anonymous_view = indexes.BooleanField(model_attr='allow_anonymous_view')
+    allow_user_view = indexes.BooleanField(model_attr='allow_user_view')
+    allow_member_view = indexes.BooleanField(model_attr='allow_member_view')
+    allow_anonymous_edit = indexes.BooleanField(model_attr='allow_anonymous_edit')
+    allow_user_edit = indexes.BooleanField(model_attr='allow_user_edit')
+    allow_member_edit = indexes.BooleanField(model_attr='allow_member_edit')
+    creator = indexes.CharField(model_attr='creator')
+    creator_username = indexes.CharField(model_attr='creator_username')
+    owner = indexes.CharField(model_attr='owner')
+    owner_username = indexes.CharField(model_attr='owner_username')
+    create_dt = indexes.DateTimeField(model_attr='create_dt')
+    update_dt = indexes.DateTimeField(model_attr='update_dt')
+    status = indexes.IntegerField(model_attr='status')
+    status_detail = indexes.CharField(model_attr='status_detail')
+
+    # permission
+    who_can_view = indexes.CharField()
+    
+    #for primary key: needed for exclude list_tags
+    primary_key = indexes.CharField(model_attr='pk')
+
+    def get_updated_field(self):
+        return 'update_dt'
+
+    def prepare_who_can_view(self, obj):
+        users = ObjectPermission.objects.who_has_perm('memberships.view_archived_membership', obj) or []
+        return ','.join(u.username for u in users)
 
 
 class MemberAppIndex(indexes.RealTimeSearchIndex):
@@ -69,16 +98,10 @@ class MemberAppIndex(indexes.RealTimeSearchIndex):
 
     def get_updated_field(self):
         return 'update_dt'
-   
+
     def prepare_who_can_view(self, obj):
-        users = ObjectPermission.objects.who_has_perm('articles.view_article', obj)
-        user_list = []
-        if users:
-            for user in users:
-                user_list.append(user.username)
-            return ','.join(user_list)
-        else: 
-            return ''
+        users = ObjectPermission.objects.who_has_perm('articles.view_article', obj) or []
+        return ','.join(u.username for u in users)
 
     def prepare_description(self, obj):
         description = obj.description
@@ -118,5 +141,7 @@ class MemberAppEntryIndex(indexes.RealTimeSearchIndex):
         return ','.join(u.username for u in users)
 
 site.register(Membership, MembershipIndex)
+site.register(MembershipArchive, MembershipArchiveIndex)
+
 site.register(App, MemberAppIndex)
 site.register(AppEntry, MemberAppEntryIndex)
