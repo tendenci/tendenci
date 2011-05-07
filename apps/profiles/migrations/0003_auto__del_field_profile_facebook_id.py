12f5154f9c909605730219d9d3e6c69289c98845
Sam Lee <sam@bixly.com>
2011-05-07 08:42:11 +0800

    removed unessesary facebook profile field

---
+++ apps/profiles/migrations/0003_auto__del_field_profile_facebook_id.py
@@ -0,0 +1,161 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Deleting field 'Profile.facebook_id'
+        db.delete_column('profiles_profile', 'facebook_id')
+
+
+    def backwards(self, orm):
+        
+        # Adding field 'Profile.facebook_id'
+        db.add_column('profiles_profile', 'facebook_id', self.gf('django.db.models.fields.IntegerField')(null=True, blank=True), keep_default=False)
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'entities.entity': {
+            'Meta': {'object_name': 'Entity'},
+            'admin_notes': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'allow_anonymous_edit': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'allow_anonymous_view': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'allow_member_edit': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'allow_member_view': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'allow_user_edit': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'allow_user_view': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'contact_name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'create_dt': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'entity_creator'", 'to': "orm['auth.User']"}),
+            'creator_username': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'email': ('django.db.models.fields.CharField', [], {'max_length': '120', 'blank': 'True'}),
+            'entity_name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'entity_parent_id': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'entity_type': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'fax': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'guid': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'notes': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'entity_owner'", 'to': "orm['auth.User']"}),
+            'owner_username': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'status': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'status_detail': ('django.db.models.fields.CharField', [], {'default': "'active'", 'max_length': '50'}),
+            'summary': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'update_dt': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'website': ('django.db.models.fields.CharField', [], {'max_length': '300', 'blank': 'True'})
+        },
+        'profiles.profile': {
+            'Meta': {'object_name': 'Profile'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '150', 'blank': 'True'}),
+            'address2': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100', 'blank': 'True'}),
+            'address_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'admin_notes': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'agreed_to_tos': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'allow_anonymous_edit': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'allow_anonymous_view': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'allow_member_edit': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'allow_member_view': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'allow_user_edit': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'allow_user_view': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'company': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'country': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'county': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'create_dt': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'profile_creator'", 'to': "orm['auth.User']"}),
+            'creator_username': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'department': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'direct_mail': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'display_name': ('django.db.models.fields.CharField', [], {'max_length': '120', 'blank': 'True'}),
+            'dob': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'education': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'email': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'email2': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'entity': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['entities.Entity']", 'null': 'True', 'blank': 'True'}),
+            'exported': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'fax': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'first_responder': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'guid': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'hide_address': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'hide_email': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'hide_in_search': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'hide_phone': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'historical_member_number': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'home_phone': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'initials': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'default': "'en-us'", 'max_length': '10'}),
+            'mailing_name': ('django.db.models.fields.CharField', [], {'max_length': '120', 'blank': 'True'}),
+            'member_number': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'mobile_phone': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'notes': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'original_username': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'profile_owner'", 'to': "orm['auth.User']"}),
+            'owner_username': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'phone2': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'pl_id': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
+            'position_assignment': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'position_title': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'referral_source': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'remember_login': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'salutation': ('django.db.models.fields.CharField', [], {'max_length': '15', 'blank': 'True'}),
+            'sex': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'spouse': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'ssn': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'state': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'status': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'status_detail': ('django.db.models.fields.CharField', [], {'default': "'active'", 'max_length': '50'}),
+            'student': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'time_zone': ('timezones.fields.TimeZoneField', [], {'default': "'US/Central'", 'max_length': '100'}),
+            'update_dt': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'url2': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'profile'", 'unique': 'True', 'to': "orm['auth.User']"}),
+            'work_phone': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'zipcode': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['profiles']
