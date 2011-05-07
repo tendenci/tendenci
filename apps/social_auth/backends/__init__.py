c57f90a4d616bd83f182e96b98e14eefd89dd184
Sam Lee <sam@bixly.com>
2011-05-07 08:45:32 +0800

    facebook setting name correction, login with facebook on the login page, logging and default groups on user creation via facebook backend

--- apps/social_auth/backends/__init__.py
+++ apps/social_auth/backends/__init__.py
@@ -34,6 +34,8 @@ from social_auth.models import UserSocialAuth
 from social_auth.store import DjangoOpenIDStore
 from social_auth.signals import pre_update, socialauth_registered
 
+from site_settings.utils import get_setting
+from event_logs.models import EventLog
 
 # OpenID configuration
 OLD_AX_ATTRS = [
@@ -101,6 +103,7 @@ class SocialAuthBackend(ModelBackend):
             return None
 
         response = kwargs.get('response')
+        request = kwargs.get('request')
         details = self.get_user_details(response)
         uid = self.get_user_id(details, response)
         is_new = False
@@ -130,6 +133,50 @@ class SocialAuthBackend(ModelBackend):
                     user = User.objects.create_user(username=username,
                                                     email=email)
                     is_new = True
+                    default_user_groups =[g.strip() for g in (get_setting('module', 'users', 'defaultusergroup')).split(',')]
+                    if default_user_groups:
+                        from user_groups.models import Group, GroupMembership
+                        from django.db.models import Q
+                        for group_name in default_user_groups:
+                            groups = Group.objects.filter(Q(name=group_name) | Q(label=group_name)).filter(allow_self_add=1, status=1, status_detail='active')
+                            if groups:
+                                group = groups[0]
+                            else:
+                                # group doesnot exist, so create the group
+                                group = Group()
+                                group.name  = group_name
+                                group.label = group_name
+                                group.type = 'distribution'
+                                group.show_as_option = 1
+                                group.allow_self_add = 1
+                                group.allow_self_remove = 1
+                                group.creator = user
+                                group.creator_username = user.username
+                                group.owner =  user
+                                group.owner_username = user.username
+                                try:
+                                    group.save()
+                                except:
+                                    group = None
+                                
+                            if group:
+                                gm = GroupMembership()
+                                gm.group = group
+                                gm.member = user
+                                gm.creator_id = user.id
+                                gm.creator_username = user.username
+                                gm.owner_id =  user.id
+                                gm.owner_username = user.username
+                                gm.save()
+                    log_defaults = {
+                        'event_id' : 121000,
+                        'event_data': '%s (%d) self added by form' % (user._meta.object_name, user.pk),
+                        'description': '%s self added' % user._meta.object_name,
+                        'user': user,
+                        'request': request,
+                        'instance': user,
+                    }
+                    EventLog.objects.log(**log_defaults)
             social_user = self.associate_auth(user, uid, response, details)
         else:
             # This account was registered to another user, so we raise an
