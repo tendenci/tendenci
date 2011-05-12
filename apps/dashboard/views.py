4b323219e709d4b6c2ef00fcca0fcf90c8a28ac3
Sam Lee <sam@bixly.com>
2011-05-12 11:41:19 +0800

    Global Setting for profile redirect after login. Only for non-admin

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
+
+from site_settings.models import Setting
+from perms.utils import is_admin
 
 @login_required
 def index(request, template_name="dashboard/index.html"):
+    profile_redirect = Setting.objects.get(scope = 'site', scope_category = 'global', name = 'profile_redirect')
+    if profile_redirect.value != '/dashboard' and not is_admin(request.user):
+        return redirect(profile_redirect.value)
     return render_to_response(template_name, context_instance=RequestContext(request))
