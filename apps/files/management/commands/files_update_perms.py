e295243044c4182daf6736c48f44648285a89ef6
eloyz <eloyz.email@gmail.com>
2011-05-20 09:48:57 -0500

    Uncommenting Files module management command.  Now working because of previous commit.

--- apps/files/management/commands/files_update_perms.py
+++ apps/files/management/commands/files_update_perms.py
@@ -1,12 +1,12 @@
-# from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand
 
-# class Command(BaseCommand):
-#     """
-#     example: python manage.py files_update_perms.py
-#     """
-#     def handle(self, *args, **kwargs):
-#     	from django.db import connection, transaction
-#     	cursor = connection.cursor()
+class Command(BaseCommand):
+    """
+    example: python manage.py files_update_perms.py
+    """
+    def handle(self, *args, **kwargs):
+    	from django.db import connection, transaction
+    	cursor = connection.cursor()
 
-#     	cursor.execute("UPDATE files_file SET allow_anonymous_view = 1 WHERE is_public = 1")
-#     	transaction.commit_unless_managed()
+    	cursor.execute("UPDATE files_file SET allow_anonymous_view = 1 WHERE is_public = 1")
+    	transaction.commit_unless_managed()
