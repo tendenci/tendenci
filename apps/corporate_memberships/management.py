3a6bebbbbb3fba5bb39f6e9f8393437c6d86cd62
jennyq <jqian@schipul.com>
2011-05-30 17:56:26 -0500

    Updated corporate membership renewal interface to have a summary box that displays the number of individuals to be renewed, unit and total price of corporate membership and individual membership, and total amount. Those data are updated instantly whenever the corporate membership type or the individual member selection is changed.

--- apps/corporate_memberships/management.py
+++ apps/corporate_memberships/management.py
@@ -13,12 +13,18 @@ post_syncdb.connect(assign_permissions, sender=corporate_membership)
 from django.conf import settings
 from django.utils.translation import ugettext_noop as _
 
+# corp_memb_renewed - send email notification to dues reps
+# corp_memb_renewal_approved - send email notification to dues reps that the renewal has been approved
 if "notification" in settings.INSTALLED_APPS:
     from notification import models as notification
 
     def create_notice_types(app, created_models, verbosity, **kwargs):
         notification.create_notice_type("corp_memb_added", _("Corporate Membership Added"), 
                                         _("A corporate membership  has been added."))
+        notification.create_notice_type("corp_memb_renewed", _("Corporate Membership Renewed"), 
+                                        _("A corporate membership  has been renewed."))
+        notification.create_notice_type("corp_memb_renewal_approved", _("Corporate Membership Renewal Approved"), 
+                                        _("The corporate membership renewal has been renewed."))
         notification.create_notice_type("corp_memb_edited", _("Corporate Membership Edited"), 
                                         _("A corporate membership  has been edited."))
         notification.create_notice_type("corp_memb_paid", _("Payment Received for Corporate Membership"), 
