6f91cc3f2433887ee35f10a6267c3934306bb0b6
Sam Lee <sam@bixly.com>
2011-06-03 05:06:24 +0800

    notifications for approved payments.

--- apps/payments/authorizenet/views.py
+++ apps/payments/authorizenet/views.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from django.template import RequestContext
 from payments.authorizenet.utils import authorizenet_thankyou_processing
 
+
 def sim_thank_you(request, payment_id, template_name='payments/authorizenet/thankyou.html'):
     payment = authorizenet_thankyou_processing(request, dict(request.POST.items()))
     
