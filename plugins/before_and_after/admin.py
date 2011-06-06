8563b626c379974ee3b4ee808864464e4c23d765
Sam Lee <sam@bixly.com>
2011-06-07 04:10:28 +0800

    admin sortable photoset, get_featured before and after photoset

--- plugins/before_and_after/admin.py
+++ plugins/before_and_after/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
+from django.conf import settings
+
 from before_and_after.models import BeforeAndAfter, Category, \
     Subcategory, PhotoSet
 from before_and_after.forms import BnAForm
 
-class PhotoSetAdmin(admin.TabularInline):
+class PhotoSetAdmin(admin.StackedInline):
     model = PhotoSet
     extra = 1
-    ordering = ("position",)
+    ordering = ("order",)
 
 class BnAAdmin(admin.ModelAdmin):
     form = BnAForm
@@ -24,7 +26,14 @@ class BnAAdmin(admin.ModelAdmin):
                       'fields': ['admin_notes',], 
                       'classes': ['admin-only',],
                     })]
-
+    
+    class Media:
+        js = (
+            '%sjs/admin/sortable_inline/jquery-1.5.1.min.js' % settings.STATIC_URL,
+            '%sjs/admin/sortable_inline/jquery-ui-1.8.13.custom.min.js' % settings.STATIC_URL,
+            '%sjs/admin/sortable_inline/stacked-sort.js' % settings.STATIC_URL,
+        )
+    
 admin.site.register(BeforeAndAfter, BnAAdmin)
 admin.site.register(Category)
 admin.site.register(Subcategory)
