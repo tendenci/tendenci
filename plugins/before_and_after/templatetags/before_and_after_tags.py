73d576a401468b73edd090bac0a8f54f31a56c82
Sam Lee <sam@bixly.com>
2011-06-09 00:45:00 +0800

    moved thumbnail filter to base filters

--- plugins/before_and_after/templatetags/before_and_after_tags.py
+++ plugins/before_and_after/templatetags/before_and_after_tags.py
@@ -1,6 +1,3 @@
-import os
-import Image
-
 from django.template import Node, Library, TemplateSyntaxError, Variable
 from django.contrib.auth.models import AnonymousUser
 
@@ -14,29 +11,4 @@ register = Library()
 def before_and_after_search(context):
     context['form'] = SearchForm()
     return context
-
-def thumbnail(file, size='200x150'):
-    # defining the size
-    x, y = [int(x) for x in size.split('x')]
-    # defining the filename and the miniature filename
-    filehead, filetail = os.path.split(file.path)
-    basename, format = os.path.splitext(filetail)
-    miniature = basename + '_' + size + format
-    filename = file.path
-    miniature_filename = os.path.join(filehead, miniature)
-    filehead, filetail = os.path.split(file.url)
-    miniature_url = filehead + '/' + miniature
-    if os.path.exists(miniature_filename) and os.path.getmtime(filename)>os.path.getmtime(miniature_filename):
-        os.unlink(miniature_filename)
-    # if the image wasn't already resized, resize it
-    if not os.path.exists(miniature_filename):
-        image = Image.open(filename)
-        image.thumbnail([x, y], Image.ANTIALIAS)
-        try:
-            image.save(miniature_filename, image.format, quality=90, optimize=1)
-        except:
-            image.save(miniature_filename, image.format, quality=90)
-
-    return miniature_url
-
-register.filter(thumbnail)
+    
