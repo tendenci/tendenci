0a669f80064bf9640a22a38e1f0716a245b21454
glenbot <glenbot@gmail.com>
2011-06-09 17:34:35 -0500

    Updated eventlogs to not use the search index. All search methods have been changed.

--- apps/event_logs/templatetags/event_log_tags.py
+++ apps/event_logs/templatetags/event_log_tags.py
@@ -2,6 +2,7 @@ from django.template import Library
 
 register = Library()
 
+
 @register.inclusion_tag("event_logs/options.html", takes_context=True)
 def event_log_options(context, user, event_log):
     context.update({
@@ -9,6 +10,7 @@ def event_log_options(context, user, event_log):
         "user": user
     })
     return context
+
 
 @register.inclusion_tag("event_logs/nav.html", takes_context=True)
 def event_log_nav(context, user, event_log=None):
@@ -18,6 +20,10 @@ def event_log_nav(context, user, event_log=None):
     })
     return context
 
+
 @register.inclusion_tag("event_logs/search-form.html", takes_context=True)
-def event_log_search(context):
+def event_log_search(context, search_form):
+    context.update({
+        "search_form": search_form,
+    })
     return context
