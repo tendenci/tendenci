04f771d22fa7b372e1d2ab46cedceff364988165
Sam Lee <sam@bixly.com>
2011-06-13 02:08:03 +0800

    chained select for category search form

--- plugins/before_and_after/urls.py
+++ plugins/before_and_after/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('before_and_after.views',
     url(r'^before-and-after/$', 'index', name="before_and_after"),
     url(r'^before-and-after/search/$', 'search', name="before_and_after.search"),
     url(r'^before-and-after/detail/(?P<id>\d+)/$', 'detail', name="before_and_after.detail"),
+    url(r'^before-and-after/subcategories/$', 'subcategories', name="before_and_after.subcategories"),
 )
