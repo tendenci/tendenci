fd3d37a4d45ae7d5c024161cf6fee021eed3b246
Sam Lee <sam@bixly.com>
2011-06-15 02:57:24 +0800

    removed 'attornies' from git

--- plugins/attornies/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-
-urlpatterns = patterns('attornies.views',
-    url(r'^attornies/$', 'search', name="attornies.search"),
-    url(r'^attornies/(?P<id>\d+)/$', 'detail', name="attornies.detail"),
-)
