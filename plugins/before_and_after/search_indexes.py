279dd147cbd3ed715204b4dc2de687f09969891f
Sam Lee <sam@bixly.com>
2011-06-16 06:44:15 +0800

    created a TendenciBaseSearchIndex. Applied this base index to the plugins search indexes

--- plugins/before_and_after/search_indexes.py
+++ plugins/before_and_after/search_indexes.py
@@ -2,42 +2,13 @@ from haystack import indexes
 from haystack import site
 
 from before_and_after.models import BeforeAndAfter
-from perms.object_perms import ObjectPermission
 
-class BeforeAndAfterIndex(indexes.RealTimeSearchIndex):
-    text = indexes.CharField(document=True, use_template=True)
+from perms.indexes import TendenciBaseSearchIndex
+
+class BeforeAndAfterIndex(TendenciBaseSearchIndex):
     title = indexes.CharField(model_attr='title')
     category = indexes.CharField()
     subcategory = indexes.CharField(null=True)
-    
-    # TendenciBaseModel Fields
-    allow_anonymous_view = indexes.BooleanField(model_attr='allow_anonymous_view')
-    allow_user_view = indexes.BooleanField(model_attr='allow_user_view')
-    allow_member_view = indexes.BooleanField(model_attr='allow_member_view')
-    allow_anonymous_edit = indexes.BooleanField(model_attr='allow_anonymous_edit')
-    allow_user_edit = indexes.BooleanField(model_attr='allow_user_edit')
-    allow_member_edit = indexes.BooleanField(model_attr='allow_member_edit')
-    creator = indexes.CharField(model_attr='creator')
-    creator_username = indexes.CharField(model_attr='creator_username')
-    owner = indexes.CharField(model_attr='owner')
-    owner_username = indexes.CharField(model_attr='owner_username')
-    status = indexes.IntegerField(model_attr='status')
-    status_detail = indexes.CharField(model_attr='status_detail')
-    create_dt = indexes.DateTimeField(model_attr='create_dt', null=True)
-    update_dt = indexes.DateTimeField(model_attr='update_dt', null=True)
-    
-    # PK: needed for exclude list_tags
-    primary_key = indexes.CharField(model_attr='pk')
-    
-    # permission fields
-    users_can_view = indexes.MultiValueField(null=True)
-    groups_can_view = indexes.MultiValueField(null=True)
-    
-    def prepare_users_can_view(self, obj):
-        return ObjectPermission.objects.users_with_perms('before_and_after.view_before_and_after', obj)
-
-    def prepare_groups_can_view(self, obj):
-        return ObjectPermission.objects.groups_with_perms('before_and_after.view_before_and_after', obj)
         
     def prepare_category(self, obj):
         return obj.category.pk
@@ -46,8 +17,5 @@ class BeforeAndAfterIndex(indexes.RealTimeSearchIndex):
         if obj.subcategory:
             return obj.subcategory.pk
         return None
-
-    def get_updated_field(self):
-        return 'update_dt'
 
 site.register(BeforeAndAfter, BeforeAndAfterIndex)
--- plugins/videos/search_indexes.py
+++ plugins/videos/search_indexes.py
@@ -1,58 +1,24 @@
-from datetime import datetime
-
 from haystack import indexes
 from haystack import site
 from django.utils.html import strip_tags, strip_entities
 
+from perms.indexes import TendenciBaseSearchIndex
+
 from models import Video
-from perms.object_perms import ObjectPermission
 
-
-class VideoIndex(indexes.RealTimeSearchIndex):
-    text = indexes.CharField(document=True, use_template=True)
+class VideoIndex(TendenciBaseSearchIndex):
     title = indexes.CharField(model_attr='title')
     description = indexes.CharField(model_attr='description')
     category = indexes.CharField()
-    create_dt = indexes.DateTimeField(model_attr='create_dt')
-
-    # TendenciBaseModel Fields
-    allow_anonymous_view = indexes.BooleanField(model_attr='allow_anonymous_view')
-    allow_user_view = indexes.BooleanField(model_attr='allow_user_view')
-    allow_member_view = indexes.BooleanField(model_attr='allow_member_view')
-    allow_anonymous_edit = indexes.BooleanField(model_attr='allow_anonymous_edit')
-    allow_user_edit = indexes.BooleanField(model_attr='allow_user_edit')
-    allow_member_edit = indexes.BooleanField(model_attr='allow_member_edit')
-    creator = indexes.CharField(model_attr='creator')
-    creator_username = indexes.CharField(model_attr='creator_username')
-    owner = indexes.CharField(model_attr='owner')
-    owner_username = indexes.CharField(model_attr='owner_username')
-    status = indexes.IntegerField(model_attr='status')
-    status_detail = indexes.CharField(model_attr='status_detail')
-
-    # permission fields
-    users_can_view = indexes.MultiValueField()
-    groups_can_view = indexes.MultiValueField()
 
     # RSS fields
     order = indexes.DateTimeField()
-
-    # PK: needed for exclude list_tags
-    primary_key = indexes.CharField(model_attr='pk')
-
-    def get_updated_field(self):
-        return 'update_dt'
 
     def prepare_description(self, obj):
         description = obj.description
         description = strip_tags(description)
         description = strip_entities(description)
         return description
-
-    def prepare_users_can_view(self, obj):
-        return ObjectPermission.objects.users_with_perms('videos.view_video', obj)
-
-    def prepare_groups_can_view(self, obj):
-        return ObjectPermission.objects.groups_with_perms('videos.view_video', obj)
 
     def prepare_order(self, obj):
         return obj.create_dt
