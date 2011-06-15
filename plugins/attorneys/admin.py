f2b370d1b632e2fdfcd179e10fcb3f1883acdf3f
Sam Lee <sam@bixly.com>
2011-06-15 23:20:56 +0800

    migrations for attorneys plugin. remodeled to photo to be inlined in the attorney admin. in effect the model will also be able to support multiple photos for attorneys. if you've already created the attorney tables you'll need to fake the initial migration for attorneys

--- plugins/attorneys/admin.py
+++ plugins/attorneys/admin.py
@@ -4,6 +4,16 @@ from django.conf import settings
 from attorneys.models import Attorney, Photo
 from attorneys.forms import AttorneyForm, PhotoForm
 
+class PhotoInline(admin.StackedInline):
+    model = Photo
+    form = PhotoForm
+    fieldsets = (
+        (None, {'fields': (
+            'file',
+        )}),
+    )
+    extra = 0;
+
 class AttorneyAdmin(admin.ModelAdmin):
     class Meta:
         model = Attorney
@@ -11,6 +21,7 @@ class AttorneyAdmin(admin.ModelAdmin):
     list_display = ["last_name", "first_name", "position", "category"]
     list_filter = ["category"]
     form = AttorneyForm
+    inlines = [PhotoInline,]
     
     fieldsets = (
         (None, {'fields': (
@@ -30,7 +41,6 @@ class AttorneyAdmin(admin.ModelAdmin):
             'education',
             'casework',
             'admissions',
-            'photo',
             'tags',
         )}),
         ('Administrative', {'fields': (
@@ -46,7 +56,17 @@ class AttorneyAdmin(admin.ModelAdmin):
         js = (
             '%sjs/global/tinymce.event_handlers.js' % settings.STATIC_URL,
         )
-        
+    
+    def save_formset(self, request, form, formset, change):
+        instances = formset.save(commit=False)
+        for instance in instances:
+            if not instance.pk:
+                instance.creator = request.user
+                instance.creator_username = request.user.username
+            instance.owner = request.user
+            instance.owner_username = request.user.username
+            instance.save()
+    
     def get_form(self, request, obj=None, **kwargs):
         """
         inject the user in the form.
@@ -54,28 +74,5 @@ class AttorneyAdmin(admin.ModelAdmin):
         form = super(AttorneyAdmin, self).get_form(request, obj, **kwargs)
         form.current_user = request.user
         return form
-        
-class PhotoAdmin(admin.ModelAdmin):
-    class Meta:
-        model = Photo
-        
-    form = PhotoForm
-    list_display = ["name", "file"]
-    
-    fieldsets = (
-        (None, {'fields': (
-            'name',
-            'file',
-        )}),
-    )
-        
-    def get_form(self, request, obj=None, **kwargs):
-        """
-        inject the user in the form.
-        """
-        form = super(PhotoAdmin, self).get_form(request, obj, **kwargs)
-        form.current_user = request.user
-        return form
 
 admin.site.register(Attorney, AttorneyAdmin)
-admin.site.register(Photo, PhotoAdmin)
