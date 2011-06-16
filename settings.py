50cb1ed64aaaeda048f598c221c4bbbe4c27600c
Sam Lee <sam@bixly.com>
2011-06-17 05:12:07 +0800

    csrf middleware enabled, csrf token for styled forms

--- settings.py
+++ settings.py
@@ -105,7 +105,7 @@ TEMPLATE_LOADERS = (
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'swfupload.middleware.SWFUploadMiddleware',
     'swfupload.middleware.MediaUploadMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
