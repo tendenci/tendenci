75ab7ef3908028778087f652f79beb25e64d453d
Sam Lee <sam@bixly.com>
2011-06-17 02:13:26 +0800

    Applied the TendenciBaseSearchIndex changes for all app indexes for children of the TendenciBaseModel

--- apps/resumes/search_indexes.py
+++ apps/resumes/search_indexes.py
@@ -3,53 +3,23 @@ from django.utils.html import strip_tags, strip_entities
 from haystack import indexes
 from haystack import site
 from resumes.models import Resume
-from perms.object_perms import ObjectPermission
+from perms.indexes import TendenciBaseSearchIndex
 
 
-class ResumeIndex(indexes.RealTimeSearchIndex):
-    text = indexes.CharField(document=True, use_template=True)
+class ResumeIndex(TendenciBaseSearchIndex):
     title = indexes.CharField(model_attr='title')
     description = indexes.CharField(model_attr='description')
     activation_dt = indexes.DateTimeField(model_attr='activation_dt', null=True)
     expiration_dt = indexes.DateTimeField(model_attr='expiration_dt', null=True)
-    create_dt = indexes.DateTimeField(model_attr='create_dt')
     syndicate = indexes.BooleanField(model_attr='syndicate')
-
-    # TendenciBaseModel Fields
-    allow_anonymous_view = indexes.BooleanField(model_attr='allow_anonymous_view')
-    allow_user_view = indexes.BooleanField(model_attr='allow_user_view')
-    allow_member_view = indexes.BooleanField(model_attr='allow_member_view')
-    allow_anonymous_edit = indexes.BooleanField(model_attr='allow_anonymous_edit')
-    allow_user_edit = indexes.BooleanField(model_attr='allow_user_edit')
-    allow_member_edit = indexes.BooleanField(model_attr='allow_member_edit')
-    creator = indexes.CharField(model_attr='creator', null=True)
-    creator_username = indexes.CharField(model_attr='creator_username', null=True)
-    owner = indexes.CharField(model_attr='owner', null=True)
-    owner_username = indexes.CharField(model_attr='owner_username', null=True)
-    status = indexes.IntegerField(model_attr='status')
-    status_detail = indexes.CharField(model_attr='status_detail')
-
-    # permission fields
-    users_can_view = indexes.MultiValueField()
-    groups_can_view = indexes.MultiValueField()
 
     # PK: needed for exclude list_tags
     primary_key = indexes.CharField(model_attr='pk')
-
-    def get_updated_field(self):
-        return 'update_dt'
 
     def prepare_description(self, obj):
         description = obj.description
         description = strip_tags(description)
         description = strip_entities(description)
         return description
-
-    def prepare_users_can_view(self, obj):
-        return ObjectPermission.objects.users_with_perms('resumes.view_resume', obj)
-
-    def prepare_groups_can_view(self, obj):
-        return ObjectPermission.objects.groups_with_perms('resumes.view_resume', obj)
-
 
 site.register(Resume, ResumeIndex)
