a9e440074d71064135c0b55f1c99817dc7f29a22
Sam Lee <sam@bixly.com>
2011-06-17 02:14:44 +0800

    Applied the TendenciBaseSearchIndex changes for all app indexes for children of the TendenciBaseModel

--- apps/directories/search_indexes.py
+++ apps/directories/search_indexes.py
@@ -4,35 +4,15 @@ from django.utils.html import strip_tags, strip_entities
 from haystack import indexes
 from haystack import site
 from directories.models import Directory
-from perms.object_perms import ObjectPermission
+from perms.indexes import TendenciBaseSearchIndex
 from categories.models import Category
 
-
-class DirectoryIndex(indexes.RealTimeSearchIndex):
-    text = indexes.CharField(document=True, use_template=True)
+class DirectoryIndex(TendenciBaseSearchIndex):
     headline = indexes.CharField(model_attr='headline', faceted=True)
     body = indexes.CharField(model_attr='body')
     activation_dt = indexes.DateTimeField(model_attr='activation_dt', null=True)
     expiration_dt = indexes.DateTimeField(model_attr='expiration_dt', null=True)
     syndicate = indexes.BooleanField(model_attr='syndicate')
-
-    # TendenciBaseModel Fields
-    allow_anonymous_view = indexes.BooleanField(model_attr='allow_anonymous_view')
-    allow_user_view = indexes.BooleanField(model_attr='allow_user_view')
-    allow_member_view = indexes.BooleanField(model_attr='allow_member_view')
-    allow_anonymous_edit = indexes.BooleanField(model_attr='allow_anonymous_edit')
-    allow_user_edit = indexes.BooleanField(model_attr='allow_user_edit')
-    allow_member_edit = indexes.BooleanField(model_attr='allow_member_edit')
-    creator = indexes.CharField(model_attr='creator')
-    creator_username = indexes.CharField(model_attr='creator_username')
-    owner = indexes.CharField(model_attr='owner')
-    owner_username = indexes.CharField(model_attr='owner_username')
-    status = indexes.IntegerField(model_attr='status')
-    status_detail = indexes.CharField(model_attr='status_detail')
-
-    # permission fields
-    users_can_view = indexes.MultiValueField()
-    groups_can_view = indexes.MultiValueField()
 
     # categories
     category = indexes.CharField()
@@ -42,23 +22,11 @@ class DirectoryIndex(indexes.RealTimeSearchIndex):
     can_syndicate = indexes.BooleanField()
     order = indexes.DateTimeField()
 
-    # PK: needed for exclude list_tags
-    primary_key = indexes.CharField(model_attr='pk')
-
-    def get_updated_field(self):
-        return 'update_dt'
-
     def prepare_body(self, obj):
         body = obj.body
         body = strip_tags(body)
         body = strip_entities(body)
         return body
-
-    def prepare_users_can_view(self, obj):
-        return ObjectPermission.objects.users_with_perms('directories.view_directory', obj)
-
-    def prepare_groups_can_view(self, obj):
-        return ObjectPermission.objects.groups_with_perms('directories.view_directory', obj)
 
     def prepare_category(self, obj):
         category = Category.objects.get_for_object(obj, 'category')
--- apps/memberships/search_indexes.py
+++ apps/memberships/search_indexes.py
@@ -2,111 +2,18 @@ from django.utils.html import strip_tags, strip_entities
 
 from haystack import indexes, site
 from memberships.models import App, AppEntry, Membership, MembershipArchive
-from perms.object_perms import ObjectPermission
+from perms.indexes import TendenciBaseSearchIndex
 
 
-class MembershipIndex(indexes.RealTimeSearchIndex):
-    text = indexes.CharField(document=True, use_template=True)
+class MembershipIndex(TendenciBaseSearchIndex):
     corporate_membership_id = indexes.BooleanField(model_attr='corporate_membership_id')
 
-    # TendenciBaseModel Fields
-    allow_anonymous_view = indexes.BooleanField(model_attr='allow_anonymous_view')
-    allow_user_view = indexes.BooleanField(model_attr='allow_user_view')
-    allow_member_view = indexes.BooleanField(model_attr='allow_member_view')
-    allow_anonymous_edit = indexes.BooleanField(model_attr='allow_anonymous_edit')
-    allow_user_edit = indexes.BooleanField(model_attr='allow_user_edit')
-    allow_member_edit = indexes.BooleanField(model_attr='allow_member_edit')
-    creator = indexes.CharField(model_attr='creator')
-    creator_username = indexes.CharField(model_attr='creator_username')
-    owner = indexes.CharField(model_attr='owner')
-    owner_username = indexes.CharField(model_attr='owner_username')
-    create_dt = indexes.DateTimeField(model_attr='create_dt')
-    update_dt = indexes.DateTimeField(model_attr='update_dt')
-    status = indexes.IntegerField(model_attr='status')
-    status_detail = indexes.CharField(model_attr='status_detail')
-
-    # permission fields
-    users_can_view = indexes.MultiValueField()
-    groups_can_view = indexes.MultiValueField()
-
-    # PK: needed for exclude list_tags
-    primary_key = indexes.CharField(model_attr='pk')
-
-    def get_updated_field(self):
-        return 'update_dt'
-
-    def prepare_users_can_view(self, obj):
-        return ObjectPermission.objects.users_with_perms('memberships.view_membership', obj)
-
-    def prepare_groups_can_view(self, obj):
-        return ObjectPermission.objects.groups_with_perms('memberships.view_membership', obj)
-
-
-class MembershipArchiveIndex(indexes.RealTimeSearchIndex):
-    text = indexes.CharField(document=True, use_template=True)
+class MembershipArchiveIndex(TendenciBaseSearchIndex):
     corporate_membership_id = indexes.BooleanField(model_attr='corporate_membership_id')
 
-    # TendenciBaseModel Fields
-    allow_anonymous_view = indexes.BooleanField(model_attr='allow_anonymous_view')
-    allow_user_view = indexes.BooleanField(model_attr='allow_user_view')
-    allow_member_view = indexes.BooleanField(model_attr='allow_member_view')
-    allow_anonymous_edit = indexes.BooleanField(model_attr='allow_anonymous_edit')
-    allow_user_edit = indexes.BooleanField(model_attr='allow_user_edit')
-    allow_member_edit = indexes.BooleanField(model_attr='allow_member_edit')
-    creator = indexes.CharField(model_attr='creator')
-    creator_username = indexes.CharField(model_attr='creator_username')
-    owner = indexes.CharField(model_attr='owner')
-    owner_username = indexes.CharField(model_attr='owner_username')
-    create_dt = indexes.DateTimeField(model_attr='create_dt')
-    update_dt = indexes.DateTimeField(model_attr='update_dt')
-    status = indexes.IntegerField(model_attr='status')
-    status_detail = indexes.CharField(model_attr='status_detail')
-
-    # permission fields
-    users_can_view = indexes.MultiValueField()
-    groups_can_view = indexes.MultiValueField()
-
-    # PK: needed for exclude list_tags
-    primary_key = indexes.CharField(model_attr='pk')
-
-    def get_updated_field(self):
-        return 'update_dt'
-
-    def prepare_users_can_view(self, obj):
-        return ObjectPermission.objects.users_with_perms('memberships.view_archived_membership', obj)
-
-    def prepare_groups_can_view(self, obj):
-        return ObjectPermission.objects.groups_with_perms('memberships.view_archived_membership', obj)
-
-
-class MemberAppIndex(indexes.RealTimeSearchIndex):
-    text = indexes.CharField(document=True, use_template=True)
+class MemberAppIndex(TendenciBaseSearchIndex):
     name = indexes.CharField(model_attr='name')
     description = indexes.CharField(model_attr='description')
-
-    # TendenciBaseModel Fields
-    allow_anonymous_view = indexes.BooleanField(model_attr='allow_anonymous_view')
-    allow_user_view = indexes.BooleanField(model_attr='allow_user_view')
-    allow_member_view = indexes.BooleanField(model_attr='allow_member_view')
-    allow_anonymous_edit = indexes.BooleanField(model_attr='allow_anonymous_edit')
-    allow_user_edit = indexes.BooleanField(model_attr='allow_user_edit')
-    allow_member_edit = indexes.BooleanField(model_attr='allow_member_edit')
-    creator = indexes.CharField(model_attr='creator')
-    creator_username = indexes.CharField(model_attr='creator_username')
-    owner = indexes.CharField(model_attr='owner')
-    owner_username = indexes.CharField(model_attr='owner_username')
-    status = indexes.IntegerField(model_attr='status')
-    status_detail = indexes.CharField(model_attr='status_detail')
-
-    # permission fields
-    users_can_view = indexes.MultiValueField()
-    groups_can_view = indexes.MultiValueField()
-
-    # PK: needed for exclude list_tags
-    primary_key = indexes.CharField(model_attr='pk')
-
-    def get_updated_field(self):
-        return 'update_dt'
 
     def prepare_description(self, obj):
         description = obj.description
@@ -114,46 +21,12 @@ class MemberAppIndex(indexes.RealTimeSearchIndex):
         description = strip_entities(description)
         return description
 
-    def prepare_users_can_view(self, obj):
-        return ObjectPermission.objects.users_with_perms('memberships.view_app', obj)
 
-    def prepare_groups_can_view(self, obj):
-        return ObjectPermission.objects.groups_with_perms('memberships.view_app', obj)
-
-
-class MemberAppEntryIndex(indexes.RealTimeSearchIndex):
-    text = indexes.CharField(document=True, use_template=True)
+class MemberAppEntryIndex(TendenciBaseSearchIndex):
     entry_time = indexes.DateTimeField(model_attr='entry_time')
-
-    # TendenciBaseModel Fields
-    allow_anonymous_view = indexes.BooleanField(model_attr='allow_anonymous_view')
-    allow_user_view = indexes.BooleanField(model_attr='allow_user_view')
-    allow_member_view = indexes.BooleanField(model_attr='allow_member_view')
-    allow_anonymous_edit = indexes.BooleanField(model_attr='allow_anonymous_edit')
-    allow_user_edit = indexes.BooleanField(model_attr='allow_user_edit')
-    allow_member_edit = indexes.BooleanField(model_attr='allow_member_edit')
-    creator = indexes.CharField(model_attr='creator')
-    creator_username = indexes.CharField(model_attr='creator_username')
-    owner = indexes.CharField(model_attr='owner')
-    owner_username = indexes.CharField(model_attr='owner_username')
-    status = indexes.IntegerField(model_attr='status')
-    status_detail = indexes.CharField(model_attr='status_detail')
-
-    # permission fields
-    users_can_view = indexes.MultiValueField()
-    groups_can_view = indexes.MultiValueField()
-
-    # PK: needed for exclude list_tag
-    primary_key = indexes.CharField(model_attr='pk')
-
+    
     def get_updated_field(self):
         return 'entry_time'
-
-    def prepare_users_can_view(self, obj):
-        return ObjectPermission.objects.users_with_perms('memberships.view_appentry', obj)
-
-    def prepare_groups_can_view(self, obj):
-        return ObjectPermission.objects.groups_with_perms('memberships.view_appentry', obj)
 
 
 site.register(Membership, MembershipIndex)
