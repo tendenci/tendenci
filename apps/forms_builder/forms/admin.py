fdf294e201d014390c9a689ec55c98dd3faf07ee
eloyz <eloyz.email@gmail.com>
2011-06-22 16:10:44 -0500

    Adding safe filter to the membership application.  Changing the textarea fields to WYSIWYG fields inside of the forms application (admin add/edit).

--- apps/forms_builder/forms/admin.py
+++ apps/forms_builder/forms/admin.py
@@ -65,6 +65,7 @@ class FormAdmin(admin.ModelAdmin):
             '%sjs/jquery-1.4.2.min.js' % settings.STATIC_URL,
             '%sjs/jquery_ui_all_custom/jquery-ui-1.8.5.custom.min.js' % settings.STATIC_URL,
             '%sjs/admin/form-fields-inline-ordering.js' % settings.STATIC_URL,
+            '%sjs/global/tinymce.event_handlers.js' % settings.STATIC_URL,
         )
         css = {'all': ['%scss/admin/dynamic-inlines-with-sort.css' % settings.STATIC_URL], }
 
