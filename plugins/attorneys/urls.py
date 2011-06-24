321d6105a3f8205b535abf84598433c1aed7d92c
jmoswalt <jmoswalt@gmail.com>
2011-06-24 15:33:24 -0500

    Attorneys updates for slug and middle initial.

--- plugins/attorneys/urls.py
+++ plugins/attorneys/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('attorneys.views',
+    url(r'^attorneys/$', 'index', name="attorneys"),
     url(r'^attorneys/$', 'search', name="attorneys.search"),
-    url(r'^attorneys/(?P<id>\d+)/$', 'detail', name="attorneys.detail"),
+    url(r'^attorneys/(?P<slug>[\w\-]+)/$', 'detail', name="attorneys.detail"),
 )
