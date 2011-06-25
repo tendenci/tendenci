231daf3ee40389bbc728b8d87b265c2514e737d2
Sam Lee <sam@bixly.com>
2011-06-26 06:11:31 +0800

    category form in page add and edit, updated category form to take into account prefix kwarg

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -5,6 +5,7 @@ from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib import messages
+from django.contrib.contenttypes.models import ContentType
 
 from base.http import Http403
 from base.utils import check_template
@@ -15,6 +16,8 @@ from meta.models import Meta as MetaTags
 from meta.forms import MetaForm
 from perms.utils import update_perms_and_save, get_notice_recipients
 from perms.utils import is_admin, has_perm
+from categories.forms import CategoryForm
+from categories.models import Category
 
 try:
     from notification import models as notification
@@ -87,12 +90,26 @@ def print_view(request, slug, template_name="pages/print-view.html"):
         raise Http403
 
 @login_required
-def edit(request, id, form_class=PageForm, meta_form_class=MetaForm, template_name="pages/edit.html"):
-
+def edit(request, id, form_class=PageForm, meta_form_class=MetaForm, category_form_class=CategoryForm, template_name="pages/edit.html"):
+        
     page = get_object_or_404(Page, pk=id)
-
+    
     if not has_perm(request.user,'pages.change_page',page):
         raise Http403
+    
+    content_type = get_object_or_404(ContentType, app_label='pages',model='page')
+    
+    #setup categories
+    category = Category.objects.get_for_object(page,'category')
+    sub_category = Category.objects.get_for_object(page,'sub_category')
+        
+    initial_category_form_data = {
+        'app_label': 'pages',
+        'model': 'page',
+        'pk': page.pk,
+        'category': getattr(category,'name','0'),
+        'sub_category': getattr(sub_category,'name','0')
+    }
         
     defaults = {
         'title': page.get_title(),
@@ -105,12 +122,36 @@ def edit(request, id, form_class=PageForm, meta_form_class=MetaForm, template_na
     if request.method == "POST":
         form = form_class(request.POST, instance=page, user=request.user, prefix='page')
         metaform = meta_form_class(request.POST, instance=page.meta, prefix='meta')
-        if form.is_valid() and metaform.is_valid():
+        categoryform = category_form_class(content_type, request.POST, initial= initial_category_form_data, prefix='category')
+        if form.is_valid() and metaform.is_valid() and categoryform.is_valid():
             page = form.save(commit=False)
             # update all permissions and save the model
             page = update_perms_and_save(request, form, page)
             
+            #save meta
             meta = metaform.save()
+            page.meta = meta
+            
+            ## update the category of the article
+            category_removed = False
+            category = categoryform.cleaned_data['category']
+            if category != '0': 
+                Category.objects.update(page ,category,'category')
+            else: # remove
+                category_removed = True
+                Category.objects.remove(page ,'category')
+                Category.objects.remove(page ,'sub_category')
+            
+            if not category_removed:
+                # update the sub category of the article
+                sub_category = categoryform.cleaned_data['sub_category']
+                if sub_category != '0': 
+                    Category.objects.update(page, sub_category,'sub_category')
+                else: # remove
+                    Category.objects.remove(page,'sub_category')
+                    
+            #save relationships
+            page.save()
             
             log_defaults = {
                 'event_id' : 582000,
@@ -139,8 +180,15 @@ def edit(request, id, form_class=PageForm, meta_form_class=MetaForm, template_na
     else:
         form = form_class(instance=page, user=request.user, prefix='page')
         metaform = meta_form_class(instance=page.meta, prefix='meta')
+        categoryform = category_form_class(content_type, initial=initial_category_form_data, prefix='category')
     
-    return render_to_response(template_name, {'page': page, 'form':form, 'metaform':metaform}, 
+    return render_to_response(template_name,
+        {
+            'page': page, 
+            'form':form, 
+            'metaform':metaform,
+            'categoryform':categoryform,
+        },
         context_instance=RequestContext(request))
 
 @login_required
@@ -175,57 +223,92 @@ def edit_meta(request, id, form_class=MetaForm, template_name="pages/edit-meta.h
         context_instance=RequestContext(request))
 
 @login_required
-def add(request, form_class=PageForm, meta_form_class=MetaForm, template_name="pages/add.html"):
-
-    if has_perm(request.user,'pages.add_page'):
-        if request.method == "POST":
-            form = form_class(request.POST, user=request.user, prefix='page')
-            metaform = meta_form_class(request.POST, prefix='meta')
-            if form.is_valid() and metaform.is_valid():
-                page = form.save(commit=False)
-                
-                # add all permissions and save the model
-                page = update_perms_and_save(request, form, page)
-                
-                meta = metaform.save()
-                page.meta = meta
-                page.save()
- 
-                log_defaults = {
-                    'event_id' : 581000,
-                    'event_data': '%s (%d) added by %s' % (page._meta.object_name, page.pk, request.user),
-                    'description': '%s added' % page._meta.object_name,
-                    'user': request.user,
-                    'request': request,
-                    'instance': page,
-                }
-                EventLog.objects.log(**log_defaults)
-                
-                messages.add_message(request, messages.INFO, 'Successfully added %s' % page)
-                
-                if not is_admin(request.user):
-                    # send notification to administrators
-                    recipients = get_notice_recipients('module', 'pages', 'pagerecipients')
-                    if recipients:
-                        if notification:
-                            extra_context = {
-                                'object': page,
-                                'request': request,
-                            }
-                            notification.send_emails(recipients,'page_added', extra_context)
-                    
-                return HttpResponseRedirect(reverse('page', args=[page.slug]))
-        else:
-            form = form_class(user=request.user, prefix='page')
-            metaform = meta_form_class(prefix='meta')
-        return render_to_response(template_name, 
-                {
-                    'form':form,
-                    'metaform':metaform,
-                },
-                context_instance=RequestContext(request))
-    else:
+def add(request, form_class=PageForm, meta_form_class=MetaForm, category_form_class=CategoryForm, template_name="pages/add.html"):
+    
+    if not has_perm(request.user,'pages.add_page'):
         raise Http403
+    
+    content_type = get_object_or_404(ContentType, app_label='pages',model='page')
+    
+    if request.method == "POST":
+        form = form_class(request.POST, user=request.user, prefix='page')
+        metaform = meta_form_class(request.POST, prefix='meta')
+        categoryform = category_form_class(content_type, request.POST, prefix='category')
+        if form.is_valid() and metaform.is_valid() and categoryform.is_valid():
+            page = form.save(commit=False)
+            
+            # add all permissions and save the model
+            page = update_perms_and_save(request, form, page)
+            
+            #save meta
+            meta = metaform.save()
+            page.meta = meta
+            
+            #setup categories
+            category = Category.objects.get_for_object(page,'category')
+            sub_category = Category.objects.get_for_object(page,'sub_category')
+            
+            ## update the category of the article
+            category_removed = False
+            category = categoryform.cleaned_data['category']
+            if category != '0': 
+                Category.objects.update(page ,category,'category')
+            else: # remove
+                category_removed = True
+                Category.objects.remove(page ,'category')
+                Category.objects.remove(page ,'sub_category')
+            
+            if not category_removed:
+                # update the sub category of the article
+                sub_category = categoryform.cleaned_data['sub_category']
+                if sub_category != '0': 
+                    Category.objects.update(page, sub_category,'sub_category')
+                else: # remove
+                    Category.objects.remove(page,'sub_category')  
+            
+            #save relationships
+            page.save()
+
+            log_defaults = {
+                'event_id' : 581000,
+                'event_data': '%s (%d) added by %s' % (page._meta.object_name, page.pk, request.user),
+                'description': '%s added' % page._meta.object_name,
+                'user': request.user,
+                'request': request,
+                'instance': page,
+            }
+            EventLog.objects.log(**log_defaults)
+            
+            messages.add_message(request, messages.INFO, 'Successfully added %s' % page)
+            
+            if not is_admin(request.user):
+                # send notification to administrators
+                recipients = get_notice_recipients('module', 'pages', 'pagerecipients')
+                if recipients:
+                    if notification:
+                        extra_context = {
+                            'object': page,
+                            'request': request,
+                        }
+                        notification.send_emails(recipients,'page_added', extra_context)
+                
+            return HttpResponseRedirect(reverse('page', args=[page.slug]))
+    else:
+        initial_category_form_data = {
+            'app_label': 'pages',
+            'model': 'page',
+            'pk': 0, #not used for this view but is required for the form
+        }
+        form = form_class(user=request.user, prefix='page')
+        metaform = meta_form_class(prefix='meta')
+        categoryform = category_form_class(content_type, initial=initial_category_form_data, prefix='category')
+    return render_to_response(template_name, 
+            {
+                'form':form,
+                'metaform':metaform,
+                'categoryform':categoryform,
+            },
+            context_instance=RequestContext(request))
 
 @login_required
 def delete(request, id, template_name="pages/delete.html"):
