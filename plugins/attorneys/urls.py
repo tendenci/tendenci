2345b93e9085481dcd57602833db52a989eb44e1
jmoswalt <jmoswalt@gmail.com>
2011-06-28 14:55:16 -0500

    Updating Eventlog IDs for quotes and making attorneys eventlog and template updates.

--- plugins/attorneys/urls.py
+++ plugins/attorneys/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('attorneys.views',
     url(r'^attorneys/$', 'index', name="attorneys"),
-    url(r'^attorneys/$', 'search', name="attorneys.search"),
+    url(r'^attorneys/search/$', 'search', name="attorneys.search"),
     url(r'^attorneys/(?P<slug>[\w\-]+)/$', 'detail', name="attorneys.detail"),
 )
