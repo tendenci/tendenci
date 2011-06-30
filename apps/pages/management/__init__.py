4f2b43c33a4a237fdb5105aeb4e796d51ab94a15
eloyz <eloyz.email@gmail.com>
2011-06-30 17:50:22 -0500

    Building management command to convert html entity characters to unicode within the pages module.

---
+++ apps/pages/management/__init__.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.utils.translation import ugettext_noop as _
+from django.db.models.signals import post_syncdb
+
+if "notification" in settings.INSTALLED_APPS:
+    from notification import models as notification
+
+    def create_notice_types(app, created_models, verbosity, **kwargs):
+        notification.create_notice_type("page_added", _("Page Added"), _("A page has been added."))
+        notification.create_notice_type("page_edited", _("Page Edited"), _("A page has been edited."))
+        notification.create_notice_type("page_deleted", _("Page Deleted"), _("A page has been deleted"))
+
+    post_syncdb.connect(create_notice_types, sender=notification)
+else:
+    print "Skipping creation of NoticeTypes as notification app not found"
