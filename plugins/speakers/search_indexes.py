0c01a7a21b2ac6a883d46f8a34f6d4e6b06d5627
jmoswalt <jmoswalt@gmail.com>
2011-06-30 10:13:08 -0500

    Renaming order to ordering.

--- plugins/speakers/search_indexes.py
+++ plugins/speakers/search_indexes.py
@@ -10,6 +10,6 @@ class SpeakerIndex(TendenciBaseSearchIndex):
     company = indexes.CharField(model_attr='company', null=True)
     position = indexes.CharField(model_attr='position', null=True)
     track = indexes.CharField(model_attr='track', null=True)
-    order = indexes.CharField(model_attr='order', null=True)
+    ordering = indexes.CharField(model_attr='ordering', null=True)
 
 site.register(Speaker, SpeakerIndex)
