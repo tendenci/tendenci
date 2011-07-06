0a84a3af5b0f63caf3b7ba7136fac14df433d50b
Sam Lee <sam@bixly.com>
2011-07-06 21:50:06 +0800

    temporary id generator for new templates created. updated edit and add views. updated templates. NOTE: This app will NOT work for syncronized server.

--- apps/campaign_monitor/views.py
+++ apps/campaign_monitor/views.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext, TemplateDoesNotExist
 from django.template import Template as DTemplate
 from django.contrib import messages
@@ -10,6 +10,7 @@ from createsend.createsend import BadRequest
 from perms.utils import has_perm
 from campaign_monitor.models import Template
 from campaign_monitor.forms import TemplateForm
+from campaign_monitor.utils import temporary_id
 from site_settings.utils import get_setting
 from base.http import Http403
 
@@ -21,8 +22,8 @@ def template_index(request, template_name='campaign_monitor/templates/index.html
     if not has_perm(request.user, 'campaign_monitor.change_template'):
         raise Http403
     
-    templates = Template.objects.all()
-        
+    templates = Template.objects.all().order_by('id')
+    
     return render_to_response(template_name, {'templates':templates}, 
         context_instance=RequestContext(request))
 
@@ -58,8 +59,9 @@ def template_add(request, form_class=TemplateForm, template_name='campaign_monit
         form = form_class(request.POST, request.FILES)
         if form.is_valid():
             #save template to generate urls
-            template = form.save()
-            
+            template = form.save(commit=False)
+            template.template_id = temporary_id()
+            template.save()
             #set up urls
             site_url = get_setting('site', 'global', 'siteurl')
             html_url = "%s%s"%(site_url, template.get_html_url())
@@ -74,25 +76,32 @@ def template_add(request, form_class=TemplateForm, template_name='campaign_monit
             
             #sync with campaign monitor
             try:
-                t = CST().create(
+                t_id = CST().create(
                         client_id, template.name, 
                         html_url, zip_url, screenshot_url
                     )
             except BadRequest, e:
                 messages.add_message(request, messages.ERROR, 'Bad Request %s: %s' % (e.data.Code, e.data.Message))
+                template.delete()
                 return render_to_response(template_name, {'form':form}, 
                     context_instance=RequestContext(request))
             except Exception, e:
                 messages.add_message(request, messages.ERROR, 'Error: %s' % e)
+                template.delete()
                 return render_to_response(template_name, {'form':form}, 
                     context_instance=RequestContext(request))
-                    
-            messages.add_message(request, messages.INFO, 'Successfully created Template : %s' % t.TemplateID)
-            template.template_id = t.TemplateID
+            
+            #get campaign monitor details
+            t = CST(template_id=t_id).details()
+            template.name = t.Name
+            template.cm_preview_url = t.PreviewURL
+            template.cm_screenshot_url = t.ScreenshotURL
+            template.template_id = t_id
             template.save()
             
-            return render_to_response(template_name, {'form':form}, 
-                    context_instance=RequestContext(request))
+            messages.add_message(request, messages.INFO, 'Successfully created Template : %s' % t_id)
+            
+            return redirect(template)
                     
     else:
         form = form_class()
@@ -116,20 +125,20 @@ def template_edit(request, template_id, form_class=TemplateForm, template_name='
             
             #set up urls
             site_url = get_setting('site', 'global', 'siteurl')
-            html_url = "%s%s"%(site_url, template.get_html_url())
+            html_url = str("%s%s"%(site_url, template.get_html_url()))
             if template.zip_file:
-                zip_url = "%s%s"%(site_url, template.get_zip_url())
+                zip_url = str("%s%s"%(site_url, template.get_zip_url()))
             else:
                 zip_url = ""
             if template.screenshot_file:
-                screenshot_url = "%s%s"%(site_url, template.get_screenshot_url())
+                screenshot_url = str("%s%s"%(site_url, template.get_screenshot_url()))
             else:
                 screenshot_url = ""
             
             #sync with campaign monitor
             try:
                 t = CST(template_id = form.instance.template_id)
-                t.update(template.name, html_url, zip_url, screenshot_url)
+                t.update(str(template.name), html_url, zip_url, screenshot_url)
             except BadRequest, e:
                 messages.add_message(request, messages.ERROR, 'Bad Request %s: %s' % (e.data.Code, e.data.Message))
                 return render_to_response(template_name, {'form':form}, 
@@ -139,10 +148,16 @@ def template_edit(request, template_id, form_class=TemplateForm, template_name='
                 return render_to_response(template_name, {'form':form}, 
                     context_instance=RequestContext(request))
                     
-            messages.add_message(request, messages.INFO, 'Successfully updated Template : %s' % t.TemplateID)
+            #get campaign monitor details
+            t = t.details()
+            template.name = t.Name
+            template.cm_preview_url = t.PreviewURL
+            template.cm_screenshot_url = t.ScreenshotURL
+            template.save()
+                    
+            messages.add_message(request, messages.INFO, 'Successfully updated Template : %s' % template.template_id)
             
-            return render_to_response(template_name, {'form':form}, 
-                    context_instance=RequestContext(request))
+            return redirect(template)
                     
     else:
         form = form_class(instance=template)
