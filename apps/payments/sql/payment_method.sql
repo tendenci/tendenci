37009240dc7d06752e7b9cdbf62654eff730b1d9
eloyz <eloyz.email@gmail.com>
2011-07-06 17:38:04 -0500

    Adding initial sql for Payments application.

---
+++ apps/payments/sql/payment_method.sql
@@ -0,0 +1,11 @@
+INSERT INTO payments_paymentmethod 
+(human_name, machine_name, is_online, admin_only)
+VALUES ('Credit Card', 'credit-card', TRUE, FALSE);
+
+INSERT INTO payments_paymentmethod 
+(human_name, machine_name, is_online, admin_only)
+VALUES ('Check', 'check', FALSE, FALSE);
+
+INSERT INTO payments_paymentmethod 
+(human_name, machine_name, is_online, admin_only)
+VALUES ('Cash', 'cash', FALSE, FALSE);
