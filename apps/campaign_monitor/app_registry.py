5ad2c4a064ab7c5a33483189e791a95f13a988a4
jmoswalt <jmoswalt@gmail.com>
2011-07-07 16:44:39 -0500

    Minor updates to attorneys and uncommented campaign monitor in settings and urls, while then commenting out CM in the app registry file.

--- apps/campaign_monitor/app_registry.py
+++ apps/campaign_monitor/app_registry.py
@@ -3,15 +3,27 @@ from registry.base import CoreRegistry, lazy_reverse
 from models import Template
 
 
-class TemplateRegistry(CoreRegistry):
-    version = '1.0'
-    author = 'Schipul - The Web Marketing Company'
-    author_email = 'programmers@schipul.com'
-    description = 'Create templates via the Campaign Monitor API'
-
-    url = {
-        'add': lazy_reverse('campaign_monitor.template_add'),
-        'search': lazy_reverse('campaign_monitor.template_index'),
-    }
-
-site.register(Template, TemplateRegistry)
+# class TemplateRegistry(CoreRegistry):
+#     version = '1.0'
+#     author = 'Schipul - The Web Marketing Company'
+#     author_email = 'programmers@schipul.com'
+#     description = 'Create templates via the Campaign Monitor API'
+#     
+#     url = {
+#         'add': lazy_reverse('campaign_monitor.template_add'),
+#         'search': lazy_reverse('campaign_monitor.template_index'),
+#     }
+#     
+# class CampaignRegistry(CoreRegistry):
+#     version = '1.0'
+#     author = 'Schipul - The Web Marketing Company'
+#     author_email = 'programmers@schipul.com'
+#     description = 'Create campaigns via the Campaign Monitor API'
+#     
+#     url = {
+#         'add': lazy_reverse('campaign_monitor.campaign_add'),
+#         'search': lazy_reverse('campaign_monitor.campaign_index'),
+#     }
+# 
+# site.register(Template, TemplateRegistry)
+# site.register(Campaign, CampaignRegistry)
