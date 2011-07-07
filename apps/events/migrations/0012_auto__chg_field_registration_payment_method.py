ef52256b58f0948b2a5f4565ddffc9bcb3727701
eloyz <eloyz.email@gmail.com>
2011-07-07 18:49:58 -0500

    Updating event registration migration file with dependency on payments payment method.

--- apps/events/migrations/0012_auto__chg_field_registration_payment_method.py
+++ apps/events/migrations/0012_auto__chg_field_registration_payment_method.py
@@ -6,6 +6,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("payments", "0002_auto__add_field_paymentmethod_admin_only"),
+    )
+
     def forwards(self, orm):
         
         # Changing field 'Registration.payment_method'
