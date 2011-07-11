6b88e6fdc1f9e244a2117903ab5fbdd8100a1e79
Sam Lee <sam@bixly.com>
2011-07-12 04:55:12 +0800

    Display page styles and links matched. Created a display method for group subscription that generates a full name and email string from the subscription form's data.

--- apps/subscribers/models.py
+++ apps/subscribers/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from forms_builder.forms.models import FormEntry
+from forms_builder.forms.models import FormEntry, FieldEntry
 from user_groups.models import Group
 
 # Create your models here.
@@ -12,6 +12,26 @@ class GroupSubscription(models.Model):
     
     def __unicode__(self):
         return "%s - %s" % (self.subscriber.pk , self.group.name)
+        
+    @property
+    def display(self):
+        try:
+            first_name = self.subscriber.fields.get(field__label__iexact="first name").value
+        except FieldEntry.DoesNotExist:
+            first_name = None
+        try:
+            last_name = self.subscriber.fields.get(field__label__iexact="last name").value
+        except FieldEntry.DoesNotExist:
+            last_name = None
+        try:
+            email = self.subscriber.fields.get(field__label__iexact="email").value
+        except FieldEntry.DoesNotExist:
+            email = None
+        name = "%s %s(%s)" % (first_name, last_name, email)
+        print "Name: " + name
+        if first_name or last_name or email:
+            return name
+        return "Info Unavailable"
     
     class Meta:
         unique_together = ('group', 'subscriber',)
