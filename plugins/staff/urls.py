8f4aa4aedfb98b68f4a0403b113042d18ccc8bbb
Sam Lee <sam@bixly.com>
2011-07-12 00:45:55 +0800

    url path setting for staff url

--- plugins/staff/urls.py
+++ plugins/staff/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls.defaults import patterns, url
 from feeds import LatestEntriesFeed
+from site_settings.utils import get_setting
+
+urlpath = get_setting('module', 'staff', 'staff_url')
 
 urlpatterns = patterns('staff.views',
-    url(r'^staff/$', 'index', name="staff"),
-    url(r'^staff/search/$', 'search', name="staff.search"),
-    url(r'^staff/feed/$', LatestEntriesFeed(), name='staff.feed'),
-    url(r'^staff/(?P<slug>[\w\-]+)/$', 'index', name="staff.view"),
-    url(r'^staff/(?P<slug>[\w\-]+)/(?P<cv>cv)/$', 'index', name="staff.cv"),
+    url(r'^%s/$' % urlpath, 'index', name="staff"),
+    url(r'^%s/search/$' % urlpath, 'search', name="staff.search"),
+    url(r'^%s/feed/$' % urlpath, LatestEntriesFeed(), name='staff.feed'),
+    url(r'^%s/(?P<slug>[\w\-]+)/$' % urlpath, 'index', name="staff.view"),
+    url(r'^%s/(?P<slug>[\w\-]+)/(?P<cv>cv)/$' % urlpath, 'index', name="staff.cv"),
 )
