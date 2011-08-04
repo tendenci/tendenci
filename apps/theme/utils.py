a6f98221bd789d8f485138358b322a639823e94d
Sam Lee <sam@bixly.com>
2011-08-05 04:52:13 +0800

    serve all theme related documents

--- apps/theme/utils.py
+++ apps/theme/utils.py
@@ -7,7 +7,7 @@ def get_theme():
     return theme
 
 def get_theme_root():
-    theme = get_setting('module', 'theme_editor', 'theme')
+    theme = get_theme()
     theme_root = os.path.join(settings.THEME_DIR, theme)
     return theme_root
 
