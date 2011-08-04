8a243f1de598aabcd76b9d49179de16dfa8bd14c
Sam Lee <sam@bixly.com>
2011-08-05 06:24:04 +0800

    management command for expired stories

---
+++ apps/stories/management/commands/expire_stories.py
@@ -0,0 +1,16 @@
+import commands
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from datetime import datetime
+
+class Command(BaseCommand):
+    """
+    example: python manage.py expire_stories
+    """
+    def handle(self, *args, **kwargs):
+        from stories.models import Story
+        for story in Story.objects.filter(expires=True, status_detail='active'):
+            if story.end_dt < datetime.now():
+                print "%s has expired." % story.title
+                story.status_detail = 'expired'
+                story.save()
