526c5f2b45777104f923e09f67566134e2474105
Sam Lee <sam@bixly.com>
2011-08-06 04:10:49 +0800

    removing screenshot file in CMTemplate form.

--- apps/campaign_monitor/forms.py
+++ apps/campaign_monitor/forms.py
@@ -13,7 +13,6 @@ class TemplateForm(forms.ModelForm):
         model = Template
         exclude = ["template_id", "create_date", "update_date", "cm_preview_url", "cm_screenshot_url"]
     
-    screenshot_file = forms.FileField(required=False)
     zip_file = forms.FileField(required=False)
     
 class CampaignForm(forms.ModelForm):
