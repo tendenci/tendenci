69a1a280068c632c831d3f720518d2a511a75159
Loren Lugosch <plugosch64@gmail.com>
2011-08-05 13:04:23 -0500

    Forgot to change LANGUAGE_CODE back to en.

--- settings.py
+++ settings.py
@@ -55,7 +55,7 @@ TIME_ZONE = 'US/Central'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'de'
+LANGUAGE_CODE = 'en'
 LANGUAGES = (
     ('en-us', u'English'),
 )
