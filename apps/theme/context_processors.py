3ea80c1ad7824e9bd6eb99942e57719d5f268950
Sam Lee <sam@bixly.com>
2011-08-10 04:57:31 +0800

    initial changes for theme previews.

--- apps/theme/context_processors.py
+++ apps/theme/context_processors.py
@@ -4,4 +4,5 @@ from site_settings.utils import get_setting
 def theme(request):
     contexts = {}
     contexts['THEME_URL'] = '/themes/' + get_setting('module', 'theme_editor', 'theme') + '/'
+    contexts['THEME_BASE'] = request.GET.get('theme', get_setting('module', 'theme_editor', 'theme')) + '/templates/base.html'
     return contexts
