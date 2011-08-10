ea085f3ed5de6b08bc6b19db94b6f691bd952eac
jmoswalt <jmoswalt@gmail.com>
2011-08-10 18:51:24 -0500

    Theme preview and base template updates.

--- apps/theme/context_processors.py
+++ apps/theme/context_processors.py
@@ -1,9 +1,17 @@
 from django.conf import settings
 from site_settings.utils import get_setting
 
+from perms.utils import is_admin
+
 def theme(request):
     contexts = {}
-    theme = request.GET.get('theme', get_setting('module', 'theme_editor', 'theme'))
+    if 'theme' in request.GET and is_admin(request.user):
+        if request.GET.get('theme'):
+            request.session['theme'] = request.GET.get('theme')
+        elif 'theme' in request.session:
+            del request.session['theme']
+
+    theme = request.session.get('theme', get_setting('module', 'theme_editor', 'theme'))
     contexts['THEME'] = theme
     contexts['THEME_URL'] = '/themes/' + theme + '/'
     contexts['ACTIVE_THEME'] = get_setting('module', 'theme_editor', 'theme')
