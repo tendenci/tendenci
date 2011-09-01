656efc0dd3bb2090359d130c89fdee27646b8cc6
jmoswalt <jmoswalt@gmail.com>
2011-09-01 09:49:26 -0500

    Commenting out former developers in the Admins list.

--- settings.py
+++ settings.py
@@ -12,12 +12,12 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    ('Glen Zangirolami', 'gzangirolami@schipul.com'),
+    #('Glen Zangirolami', 'gzangirolami@schipul.com'),
     ('Eloy Zuniga Jr.', 'ezuniga@schipul.com'),
     ('Jenny Qian', 'jqian@schipul.com'),
     ('JMO', 'jmoswalt@schipul.com'),
-    ('Loren Lugosch', 'llugosch@schipul.com'),
-    ('Nabil Bani', 'nbani@schipul.com'),
+    #('Loren Lugosch', 'llugosch@schipul.com'),
+    #('Nabil Bani', 'nbani@schipul.com'),
 )
 
 MANAGERS = ADMINS
