0c60af5e470d8a98281e33ff3de7aa4bdff49463
Sam Lee <sam@bixly.com>
2011-09-03 04:47:29 +0800

    Admin action for building plugins.

--- apps/plugin_builder/admin.py
+++ apps/plugin_builder/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from plugin_builder.models import Plugin, PluginField
 from plugin_builder.forms import PluginForm, PluginFieldForm
+from plugin_builder.utils import build_plugin
 
 class PluginFieldInline(admin.TabularInline):
     model = PluginField
@@ -11,5 +12,14 @@ class PluginAdmin(admin.ModelAdmin):
     inlines = [
         PluginFieldInline,
     ]
+    actions = ['build_plugins']
+    
+    def build_plugins(self, request, queryset):
+        message_bit = ""
+        for plugin in queryset:
+            build_plugin(plugin)
+            message_bit = message_bit + ", %s" % plugin.plural_lower
+        self.message_user(request, "Successfully built the following plugins:%s" % message_bit[1:])
+    build_plugins.short_description = "Build/Rebuild all selected plugins."
 
 admin.site.register(Plugin, PluginAdmin)
