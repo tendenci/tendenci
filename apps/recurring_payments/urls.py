d0e492aca91e139c873f4aabc5475823a807a7c7
Jenny Qian <jqian@schipul.com>
2011-09-09 18:30:16 -0500

    Added recurring payment view page to display recurring payment plan, invoices and transaction history.

--- apps/recurring_payments/urls.py
+++ apps/recurring_payments/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import patterns, url, include
 
-urlpatterns = patterns('',                  
+urlpatterns = patterns('recurring_payments',                  
     (r'^authnet/', include('recurring_payments.authnet.urls')),
+    url(r'^(?P<recurring_payment_id>\d+)/$', 'views.view_account', 
+         name="recurring_payment.view_account"),
 )
