4740fda68a194fdf91b0b5c537f8bb12157da362
Jenny Qian <jqian@schipul.com>
2011-09-15 17:53:00 -0500

    Fixed an attribute error: 'SearchQuerySet' object has no attribute 'objects' in the EventListNode class.

--- apps/events/templatetags/event_tags.py
+++ apps/events/templatetags/event_tags.py
@@ -123,7 +123,7 @@ class EventListNode(Node):
         sqs = Event.objects.search(date_range=(day+bound, day), user=context['user'])
         
         if type:
-            sqs = sqs.objects.filter(type_id=type.pk)
+            sqs = sqs.filter(type_id=type.pk)
         
         sqs = sqs.order_by('start_dt')
         events = [sq.object for sq in sqs]
