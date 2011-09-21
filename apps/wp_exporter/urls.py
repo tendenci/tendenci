9f6d3af4cb9a7a350919b6177f113141c800f1a5
Sam Lee <sam@bixly.com>
2011-09-22 03:48:42 +0800

    filter form and util updates.

--- apps/wp_exporter/urls.py
+++ apps/wp_exporter/urls.py
@@ -2,5 +2,4 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'wp_exporter.views.index', name="wp_exporter"),
-    url(r'^download/$', 'wp_exporter.views.download', name="wp_exporter.download"),
 )
