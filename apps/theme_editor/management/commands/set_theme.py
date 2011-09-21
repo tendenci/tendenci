844b304dda44f77d1a5831d6d73291097058d998
Eloy Zuniga Jr <ezuniga@schipul.com>
2011-09-21 16:23:32 -0500

    Adding management command that can set the website theme.

---
+++ apps/theme_editor/management/commands/set_theme.py
@@ -0,0 +1,29 @@
+import re
+import os
+from django.core.management import call_command
+from django.core.management.base import BaseCommand, CommandError
+from django.db import connections, transaction, DEFAULT_DB_ALIAS
+from StringIO import StringIO
+from django.conf import settings
+
+
+class Command(BaseCommand):
+    """
+    Example: python manage.py set_theme thinksmart
+    """
+
+    def handle(self, theme_name, **options):
+        """
+        Set the website theme via theme name
+        """
+        from site_settings.models import Setting
+
+        try:
+            setting = Setting.objects.filter(
+                name='theme',
+                scope='module',
+                scope_category='theme_editor',
+            ).update(value=theme_name)
+        except:
+            if int(options['verbosity']) > 0:
+                print "We could not update the theme because the setting or theme is not available."
