fe92901e0437385ab3204ee8317b05c0ff539fba
Sam Lee <sam@bixly.com>
2011-09-23 17:38:33 +0800

    email sent after import.

--- apps/wp_importer/tasks.py
+++ apps/wp_importer/tasks.py
@@ -4,6 +4,8 @@ from celery.registry import tasks
 from BeautifulSoup import BeautifulStoneSoup
 from parse_uri import ParseUri
 from wp_importer.utils import get_media, get_posts, get_pages
+from django.core.mail import send_mail
+from django.conf import settings
 
 class WPImportTask(Task):
 
@@ -33,6 +35,10 @@ class WPImportTask(Task):
                 get_posts(item, uri_parser, user)
             elif post_type == 'page' and post_status == 'publish':
                 get_pages(item, uri_parser, user)
+        
+        if user.email:
+            send_mail('Blog Import Success!', 'Your blog has been imported! Thank you for waiting.', settings.DEFAULT_FROM_EMAIL,
+                [user.email], fail_silently=False)
 
 
 tasks.register(WPImportTask)
