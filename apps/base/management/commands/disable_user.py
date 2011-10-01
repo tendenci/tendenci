3582f040a51a405b62d34ac176faddecd58968ab
Jenny Qian <jqian@schipul.com>
2011-10-01 18:12:51 -0500

    Created an API with two functions for recurring payment. One is to add a recurring payment account; the other is to retrieve the token from authorize.net payment gateway for hosted payment page. Updated the disable_user management command to have the email option so we can disable a user based on email instead of username.

--- apps/base/management/commands/disable_user.py
+++ apps/base/management/commands/disable_user.py
@@ -24,40 +24,58 @@ class Command(BaseCommand):
             dest='username',
             default=None,
             help='Username of the user account being disabled'),
+        make_option('--email',
+            action='store',
+            dest='email',
+            default=None,
+            help='Email of the user account being disabled'),
         )
+        
 
     def handle(self, *args, **options):
         from user_groups.models import GroupMembership
 
         verbosity = options['verbosity']
         username = options['username']
+        email = options['email']
 
-        if not username:
-            raise CommandError('downgrade_user: --username parameter is required')
+        if not any([username, email]):
+            raise CommandError('downgrade_user: --username or email parameter is required')
 
         # get the user
-        try:
-            u = User.objects.get(username=username)
-        except ObjectDoesNotExist:
-            print 'User with username (%s) could not be found' % username
+        if username and email:
+            users = User.objects.filter(username=username, email=email)
+            if not users:
+                print 'User with username=%s and email=%s could not be found' % (username, email)
+        elif username:
+            users = User.objects.filter(username=username)
+            if not users:
+                print 'User with username=%s could not be found' % username
+        else:
+            users = User.objects.filter(email=email)
+            if not users:
+                print 'User with email=%s could not be found' % email
+            
+        if not users:
             return
-
-        # Remove the user from all groups
-        u.groups.clear()
-
-        # Remove all user-level permissions
-        u.user_permissions.clear()
-
-        # Reset permission bits
-        u.is_staff = False
-        u.is_superuser = False
-        u.is_active = False
-        u.save()
-
-        # Remove the tendenci group permissions
-        group_memberships = GroupMembership.objects.filter(member=u)
-        for m in group_memberships:
-            m.delete()
-
-        if verbosity >= 2:
-            print 'Done downgrading user (%s).' % u
+            
+        for u in users:
+            # Remove the user from all groups
+            u.groups.clear()
+    
+            # Remove all user-level permissions
+            u.user_permissions.clear()
+    
+            # Reset permission bits
+            u.is_staff = False
+            u.is_superuser = False
+            u.is_active = False
+            u.save()
+    
+            # Remove the tendenci group permissions
+            group_memberships = GroupMembership.objects.filter(member=u)
+            for m in group_memberships:
+                m.delete()
+    
+            if verbosity >= 2:
+                print 'Done downgrading user (%s).' % u
