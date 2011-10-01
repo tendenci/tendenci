8e1480b6547297b50663f56861777c09912aea78
Sam Lee <sam@bixly.com>
2011-10-02 05:36:38 +0800

    formset implementation for nav items. Created ajax functions for retrieving page data.

--- apps/navs/urls.py
+++ apps/navs/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('navs',
     url(r'^detail/(?P<id>\d+)/$', 'views.detail', name="navs.detail"),
     url(r'^edit/(?P<id>\d+)/$', 'views.edit', name="navs.edit"),
     url(r'^edit/(?P<id>\d+)/items/$', 'views.edit_items', name="navs.edit_items"),
-    url(r'^delete/item/$', 'views.delete_item', name="navs.delete_item"),
+    url(r'^page-select/$', 'views.page_select', name="navs.page_select"),
 )
