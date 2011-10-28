33b123ce1e7f892c71e9cb8c46033f670a62a9e0
Sam Lee <sam@bixly.com>
2011-10-29 00:48:35 +0800

    Created BetterModelResource and ModelFormValidation to be able to update and validate settings based on the current setting instance's values. Updated form's dynamic clean method and dynamic field generation.

--- apps/api_tasty/api.py
+++ apps/api_tasty/api.py
@@ -10,16 +10,17 @@ from tastypie.utils.mime import determine_format, build_content_type
 
 from tastypie.authentication import Authentication
 from tastypie.authorization import Authorization
-from tastypie.validation import CleanedDataFormValidation
 from tastypie.resources import ModelResource
 from tastypie.api import Api
 from tastypie import fields
 
 from site_settings.models import Setting
 from discounts.models import Discount
+from api_tasty.resources import BetterModelResource
 from api_tasty.serializers import SafeSerializer
 from api_tasty.forms import SettingForm
 from api_tasty.auth import DeveloperApiKeyAuthentication
+from api_tasty.validation import ModelFormValidation
 
 class SafeApi(Api):
     """
@@ -63,7 +64,18 @@ class SafeApi(Api):
         serialized = serializer.serialize(available_resources, desired_format, options)
         return HttpResponse(content=serialized, content_type=build_content_type(desired_format))
 
-class SettingResource(ModelResource):
+class SettingResource(BetterModelResource):
+    """
+    This resource will clean the given data based on the generated
+    rules of the SettingForm.
+    To access this resource the username and api_key of a developer
+    must be present in request.GET or request.POST
+    for example,
+    /api_tasty/v1/setting/1/?format=json&username=user&api_key=1191910191019101
+    Note that the specification of 'format' is important.
+    curl test example: (assumes you have data.json with correct file contents)
+    curl -H "Content-Type: application/json" -X PUT --data @data.json "http://0.0.0.0:8000/api_tasty/v1/setting/12/?format=json&username=user&api_key=1191910191019101"
+    """
     name = fields.CharField(readonly=True, attribute='name')
     description = fields.CharField(readonly=True, attribute='description')
     
@@ -76,9 +88,9 @@ class SettingResource(ModelResource):
         serializer = SafeSerializer()
         authorization = Authorization()
         authentication = DeveloperApiKeyAuthentication()
-        fields = ['name', 'description', 'value']
+        fields = ['name', 'description', 'value', 'data_type', 'input_value']
         allowed_methods = ['get', 'put']
-        validation = CleanedDataFormValidation(form_class=SettingForm)
+        validation = ModelFormValidation(form_class=SettingForm)
         
 class UserResource(ModelResource):
     class Meta:
