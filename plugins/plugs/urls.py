e159633bd4b35b9d22b821e21b55167ead96f94f
Sam Lee <sam@bixly.com>
2011-11-08 20:17:56 +0800

    removing this test plugin.

--- plugins/plugs/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-from plugs.feeds import LatestEntriesFeed
-
-urlpatterns = patterns('plugs.views',                  
-    url(r'^plugs/$', 'index', name="plugs"),
-    url(r'^plugs/search/$', 'search', name="plugs.search"),
-    url(r'^plugs/feed/$', LatestEntriesFeed(), name='plugs.feed'),
-    url(r'^plugs/(?P<pk>[\d/]+)/$', 'detail', name="plugs.detail"),
-)
