29f4845319a7986ae97f380092047cdc9a8d4903
Sam Lee <sam@bixly.com>
2011-11-11 03:31:24 +0800

    Moving away from session based membership imports, new MembershipImport model to keep track of membership import files.

--- apps/memberships/forms.py
+++ apps/memberships/forms.py
@@ -2,15 +2,13 @@ import sys
 import operator
 from uuid import uuid4
 from captcha.fields import CaptchaField
+from os.path import join
+from datetime import datetime
+
 from django.contrib.auth.models import User, AnonymousUser
 from django.forms.fields import CharField, ChoiceField
 from django.template.defaultfilters import slugify
 from django.forms.widgets import HiddenInput
-
-from haystack.query import SearchQuerySet
-from os.path import join
-from datetime import datetime
-
 from django.http import Http404
 from django import forms
 from django.db.models import Q
@@ -18,21 +16,20 @@ from django.utils.translation import ugettext_lazy as _
 from django.utils.importlib import import_module
 from django.core.files.storage import FileSystemStorage
 
+from haystack.query import SearchQuerySet
 from tinymce.widgets import TinyMCE
 
-from perms.forms import TendenciBaseForm
-from models import MembershipType, Notice, App, AppEntry, AppField, AppFieldEntry
-from fields import TypeExpMethodField, PriceInput, NoticeTimeTypeField
-from memberships.settings import FIELD_MAX_LENGTH, UPLOAD_ROOT
-from memberships.utils import csv_to_dict
-
-from widgets import CustomRadioSelect, TypeExpMethodWidget, NoticeTimeTypeWidget
 from corporate_memberships.models import CorporateMembership, AuthorizedDomain
 from user_groups.models import Group
+from perms.forms import TendenciBaseForm
 
+from memberships.models import MembershipType, Notice, App, AppEntry, AppField, AppFieldEntry
+from memberships.fields import TypeExpMethodField, PriceInput, NoticeTimeTypeField
+from memberships.settings import FIELD_MAX_LENGTH, UPLOAD_ROOT
+from memberships.utils import csv_to_dict, is_import_valid
+from memberships.widgets import CustomRadioSelect, TypeExpMethodWidget, NoticeTimeTypeWidget
 
 fs = FileSystemStorage(location=UPLOAD_ROOT)
-
 
 type_exp_method_fields = (
     'period_type', 'period', 'period_unit', 'rolling_option',
@@ -763,6 +760,82 @@ class AppEntryForm(forms.ModelForm):
             if field_class == "EmailField":
                 return self.cleaned_data["field_%s" % field.id]
         return None
+        
+class UploadForm(forms.Form):
+    """
+    CSV upload form for membership imports
+    """
+    
+    app = forms.ModelChoiceField(label='Application', queryset=App.objects.all())
+    csv = forms.FileField(label="CSV File")
+    
+    
+class ImportMapForm(forms.Form):
+    
+    def __init__(self, *args, **kwargs):
+        memport = kwargs.pop('memport')
+        app = memport.app
+        file_path = os.path.join(settings.MEDIA_ROOT, memport.get_file().file.name)
+        
+        csv = csv_to_dict(file_path)
+        
+        # choices list
+        choices = csv[0].keys()
+        
+        # make tuples; sort tuples (case-insensitive)
+        choice_tuples = [(c,c) for c in csv[0].keys()]
+        
+        choice_tuples.insert(0, ('',''))  # insert blank option
+        choice_tuples = sorted(choice_tuples, key=lambda c: c[0].lower())
+        
+        app_fields = AppField.objects.filter(app=app)
+        
+        native_fields = [
+            'User Name',
+            'Membership Type',
+            'Corp. Membership Name',
+            'Member Number',
+            'Payment Method',
+            'Join Date',
+            'Renew Date',
+            'Expire Date',
+            'Owner',
+            'Creator',
+            'Status',
+            'Status Detail',
+        ]
+
+        for native_field in native_fields:
+            self.fields[slugify(native_field)] = ChoiceField(**{
+                'label': native_field,
+                'choices': choice_tuples,
+                'required': False,
+            })
+
+            # compare required field with choices
+            # if they match; set initial
+            if native_field in choices:
+                self.fields[slugify(native_field)].initial = native_field
+
+        self.fields['user-name'].required = True
+        self.fields['membership-type'].required = True
+
+        for app_field in app_fields:
+            for csv_row in csv:
+            
+                if slugify(app_field.label) == 'membership-type':
+                    continue  # skip membership type
+                
+                self.fields[app_field.label] = ChoiceField(**{
+                    'label':app_field.label,
+                    'choices': choice_tuples,
+                    'required': False,
+                })
+                
+                # compare label with choices
+                # if label matches choice; set initial
+                if app_field.label in choices:
+                    self.fields[app_field.label].initial = app_field.label
 
 class CSVForm(forms.Form):
     """
