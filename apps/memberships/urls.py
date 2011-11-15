fb27b22efcce9c5bf31e9e491e459772d26c880c
Sam Lee <sam@bixly.com>
2011-11-16 07:18:01 +0800

    removing old links

--- apps/memberships/urls.py
+++ apps/memberships/urls.py
@@ -9,15 +9,8 @@ urlpatterns = patterns("memberships.views",
 
     # notices
     (r'^notices/', include('memberships.notices.urls')),
-
-    # old import
-    #url(r"^import/$", "membership_import", name="membership_import"),
-    #url(r"^import/upload-file/$", "membership_import", kwargs={'step':(1,'upload-file')}, name="membership_import_upload_file"),
-    #url(r"^import/map-fields/$", "membership_import", kwargs={'step':(2,'map-fields')}, name="membership_import_map_fields"),
-    #url(r"^import/preview/$", "membership_import", kwargs={'step':(3,'preview')}, name="membership_import_preview"),
-    #url(r"^import/confirm/$", "membership_import", kwargs={'step':(4,'confirm')}, name="membership_import_confirm"),
     
-    # new import
+    # import
     url(r"^import/$", "membership_import_upload", name="membership_import"),
     url(r"^import/upload-file$", "membership_import_upload", name="membership_import_upload_file"),
     url(r"^import/preview/(?P<id>\d+)/$", "membership_import_preview", name="membership_import_preview"),
