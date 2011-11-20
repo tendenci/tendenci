b3af4c5d2930fb7bb6062965b1bf8a95b699ee86
Sam Lee <sam@bixly.com>
2011-11-21 05:13:30 +0800

    test taking flow, take view, completion view, retry utils, migrations for score fields to be decial fields, detail view updated.

---
+++ plugins/courses/utils.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timedelta
+
+from courses.models import CourseAttempt
+
+def can_retry(course, user):
+    """
+    Check if the user has taken this course before.
+    If it is a retake, check if the current time and number of attempts
+    allows her to retake the course.
+    If she/he cannot retry, return the time interval for the next retry.
+    """
+    attempts = CourseAttempt.objects.filter(course=course, user=user).order_by("-create_dt")
+    if attempts:
+        if course.retries == 0 or attempts.count() <= course.retries:
+            #redirect if the user is not yet allowed to retake the course
+            last_try = datetime.now() - attempts[0].create_dt
+            interval = timedelta(hours=course.retry_interval)
+            if last_try < interval:
+                return interval - last_try
+        else:
+            #user has used up all her retries
+            return False
+    return True
+
+def get_passed_attempts(course, user):
+    """
+    Returns the passing attempts of a user for a given course.
+    """
+    passed_attempts = CourseAttempt.objects.filter(
+        course=course,
+        user=user,
+        score__gte=course.passing_score
+    )
+    return passed_attempts
