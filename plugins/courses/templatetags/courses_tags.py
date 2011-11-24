a95ede9a6d75b0e07b60e495ad91b9f2a3c8a97a
Sam Lee <sam@bixly.com>
2011-11-25 05:31:02 +0800

    template tag for determining has_passed, can_retry and retry_time_left on for a given course and user in the templates. used this for the search view.

--- plugins/courses/templatetags/courses_tags.py
+++ plugins/courses/templatetags/courses_tags.py
@@ -1,7 +1,11 @@
 from django.template import Node, Library, TemplateSyntaxError, Variable
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext_lazy as _
+
+from base.template_tags import ListNode, parse_tag_kwargs
 
 from courses.models import Course
-from base.template_tags import ListNode, parse_tag_kwargs
+from courses.utils import can_retry, get_passed_attempts
 
 register = Library()
 
@@ -16,36 +20,34 @@ def course_nav(context, user, course=None):
 @register.inclusion_tag("courses/search-form.html", takes_context=True)
 def course_search(context):
     return context
-    
 
-class ListCoursesNode(ListNode):
-    model = Course
+class CourseUserStatusNode(Node):
+    def __init__(self, course, user):
+        self.course = Variable(course)
+        self.user = Variable(user)
 
-@register.tag
-def list_courses(parser, token):
+    def render(self, context):
+        course = self.course.resolve(context)
+        user = self.user.resolve(context)
+        passed = get_passed_attempts(course, user)
+        retry, retry_time = can_retry(course, user)
+        context['has_passed'] = passed
+        context['can_retry'] = retry
+        context['retry_time_left'] = retry_time
+        return ''
+        
+def course_user_status(parser, token):
     """
-    Example:
-
-    {% list_courses as courses_list [user=user limit=3 tags=bloop bleep q=searchterm] %}
-    {% for course in courses %}
-        {{ course.something }}
-    {% endfor %}
+    Link to a user's options for a course.
+    Determines if the user can retry/take a course or has already passsed.
+    This will inject has_passed, retry_time_left and can_retry into the context.
+    example: {% course_user_status course user %}
     """
-    args, kwargs = [], {}
-    bits = token.split_contents()
-    context_var = bits[2]
+    try:
+        # split_contents() knows not to split quoted strings.
+        tag_name, course, user = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError("%r tag requires exactly two arguments" % token.contents.split()[0])
+    return CourseUserStatusNode(course, user)
 
-    if len(bits) < 3:
-        message = "'%s' tag requires more than 2" % bits[0]
-        raise TemplateSyntaxError(message)
-
-    if bits[1] != "as":
-        message = "'%s' second argument must be 'as'" % bits[0]
-        raise TemplateSyntaxError(message)
-
-    kwargs = parse_tag_kwargs(bits)
-
-    if 'order' not in kwargs:
-        kwargs['order'] = '-create_dt'
-
-    return ListCoursesNode(context_var, *args, **kwargs)
+register.tag('course_user_status', course_user_status)
