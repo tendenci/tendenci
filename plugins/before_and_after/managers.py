c35bb27865231c7f23bdd8b3a1c43faadf751522
jmoswalt <jmoswalt@gmail.com>
2011-12-01 16:45:59 -0600

    Pass on the BeforeAndAfter Manager.

--- plugins/before_and_after/managers.py
+++ plugins/before_and_after/managers.py
@@ -5,31 +5,5 @@ class BeforeAndAfterManager(TendenciBaseManager):
     """
     Model Manager
     """
-    
-    def search(self, query=None, *args, **kwargs):
-        """
-        Search the Django Haystack search index
-        Returns a SearchQuerySet object
-        """
-        
-        sqs = kwargs.get('sqs', SearchQuerySet())
-        
-        # filter out the big parts first
-        sqs = sqs.models(self.model)
-        
-        # user information
-        user = kwargs.get('user') or AnonymousUser()
-        user = self._impersonation(user)
-        self.user = user
+    pass
 
-        # if the status_detail is something like "published"
-        # then you can specify the kwargs to override
-        status_detail = kwargs.get('status_detail', 'active')
-
-        if query:
-            sqs = sqs.auto_query(sqs.query.clean(query))
-        
-        if self.user:
-            sqs = self._permissions_sqs(sqs, user, status_detail)
-        
-        return sqs
