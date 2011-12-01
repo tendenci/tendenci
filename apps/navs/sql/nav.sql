febb7529f3c4986ecb6d28bb7c6a5bf87260060e
jmoswalt <jmoswalt@gmail.com>
2011-12-01 16:23:58 -0600

    Removed SQL and updated initial nav data as a south data migration.

--- apps/navs/sql/nav.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `navs_nav` (`id`, `allow_anonymous_view`, `allow_user_view`, `allow_member_view`, `allow_anonymous_edit`, `allow_user_edit`, `allow_member_edit`, `create_dt`, `update_dt`, `creator_id`, `creator_username`, `owner_id`, `owner_username`, `status`, `status_detail`, `title`, `description`, `megamenu`)
-VALUES
-    (1, 1, 0, 0, 0, 0, 0, '2011-11-28 17:12:59', '2011-11-28 17:12:59', 1, 'admin', 1, 'admin', 1, 'active', 'Main Navigation', 'The main navigation that is initially created.', 0);
