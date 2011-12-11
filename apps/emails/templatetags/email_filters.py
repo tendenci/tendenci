d500b5118425e022aa2e97e73c0a0c56f4a66fc6
Sam Lee <sam@bixly.com>
2011-12-12 07:48:38 +0800

    auto mark safe for relative_to_absolute filter

--- apps/emails/templatetags/email_filters.py
+++ apps/emails/templatetags/email_filters.py
@@ -1,6 +1,7 @@
 from django import template
 
 from site_settings.utils import get_setting
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -10,12 +11,15 @@ print GLOBAL_SITE_URL
 
 def relative_to_absolute_urls(value):
     """
-    Converts all relative urls to absolute urls
+    Converts all relative urls to absolute urls.
+    Automatically marks the strings as safe.
+    e.g.
+    {{ event.description|relative_to_absolute_urls }}
     """
     value = value.replace('src="/', 'src="%s/' % GLOBAL_SITE_URL)
     value = value.replace("src='/", "src='%s/" % GLOBAL_SITE_URL)
     value = value.replace('href="/', 'href="%s/' % GLOBAL_SITE_URL)
     value = value.replace("href='/", "<href='%s/" % GLOBAL_SITE_URL)
-    return value
+    return mark_safe(value)
     
 register.filter('relative_to_absolute_urls', relative_to_absolute_urls)
