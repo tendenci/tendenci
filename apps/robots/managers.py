2414ee8ecc366a2ca956f9105743252a382d183f
Eloy Zuniga Jr <ezuniga@schipul.com>
2011-12-27 12:57:43 -0600

    Ignoring unicode decode exceptions within user agents
    Ignoring unicode decoding exceptions when testing user agents.  If the character cannot be decoded then we simply ignore the character.  Remove the character that cannot be converted.

--- apps/robots/managers.py
+++ apps/robots/managers.py
@@ -9,7 +9,11 @@ class RobotManager(Manager):
         if not robots:
             cache_all_robots()
             robots = cache.get(CACHE_PRE_KEY + '.all', [])
-        
+
+        # UnicodeDecodeError: 'ascii' codec can't decode byte 0xf3
+        # http://stackoverflow.com/questions/2392732/sqlite-python-unicode-and-non-utf-data
+        user_agent = unicode(user_agent, errors='ignore')
+
         for robot in robots:
             if robot.name.lower() in user_agent.lower():
                 return robot
