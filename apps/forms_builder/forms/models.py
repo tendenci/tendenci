d939a57931ee9b90ccc7b5bf44d213071c3e3ee9
jmoswalt <jmoswalt@gmail.com>
2011-12-29 14:41:44 -0600

    Updates to custom forms to not require intro and response
    These are no longer required since we have completion URL.

--- apps/forms_builder/forms/models.py
+++ apps/forms_builder/forms/models.py
@@ -48,8 +48,8 @@ class Form(TendenciBaseModel):
 
     title = models.CharField(_("Title"), max_length=50)
     slug = models.SlugField(max_length=100, unique=True)
-    intro = models.TextField(_("Intro"), max_length=2000)
-    response = models.TextField(_("Confirmation Text"), max_length=2000)
+    intro = models.TextField(_("Intro"), max_length=2000, blank=True)
+    response = models.TextField(_("Confirmation Text"), max_length=2000, blank=True)
     email_text = models.TextField(_("Email Text to Submitter"), default='', blank=True, help_text=
         _("If Send email is checked, this is the text that will be sent in an email to the person submitting the form."), max_length=2000)
 #    status = models.IntegerField(_("Status"), choices=STATUS_CHOICES, 
