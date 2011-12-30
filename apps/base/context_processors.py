188fd6c845706acaf559dccc0cb4497015f9786c
Eloy Zuniga Jr <ezuniga@schipul.com>
2011-12-30 12:17:28 -0600

    Made new context variable called "TODAY."
    Added new context variable called "TODAY" holds todays date according to server.  Also learned about timezones today.  Thanks David.

--- apps/base/context_processors.py
+++ apps/base/context_processors.py
@@ -1,7 +1,11 @@
 from django.conf import settings
+from datetime import datetime
 
 def static_url(request):
     return {'STATIC_URL': settings.STATIC_URL}
 
 def index_update_note(request):
     return {'INDEX_UPDATE_NOTE': settings.INDEX_UPDATE_NOTE}
+
+def today(request):
+    return {'TODAY': datetime.today()}
