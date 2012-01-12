d72087c96791becafb6686a6a997c216c716ecb0
Sam Lee <sam@bixly.com>
2012-01-13 05:42:17 +0800

    app_registry update for articles and news

--- apps/news/app_registry.py
+++ apps/news/app_registry.py
@@ -9,6 +9,22 @@ class NewsRegistry(CoreRegistry):
     author_email = 'programmers@schipul.com'
     description = 'Create news to let your vistors keep current'
     icon = '/site_media/static/images/icons/news-color-64x64.png'
+    
+    #Event log
+    base_id = '305000'
+    base_color = 'FF0033'
+    add_id = '305100'
+    add_color = 'FF0033'
+    edit_id = '305200'
+    edit_color = 'FF0033'
+    delete_id = '305300'
+    delete_color = 'FF0033'
+    search_id = '305400'
+    search_color = 'FF0033'
+    view_id = '305500'
+    view_color = '8C0000'
+    print_view_id = '305501'
+    print_view_color = '8C0000'
 
     url = {
         'add': lazy_reverse('news.add'),
