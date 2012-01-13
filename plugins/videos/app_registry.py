b9d7d87bcd8f66cfdec8924a3abe4d298378db92
Sam Lee <sam@bixly.com>
2012-01-14 06:00:47 +0800

    events log colors in registry for plugins

--- plugins/videos/app_registry.py
+++ plugins/videos/app_registry.py
@@ -8,5 +8,16 @@ class VideoRegistry(PluginRegistry):
     author = 'Schipul - The Web Marketing Company'
     author_email = 'programmers@schipul.com'
     description = 'Add video and display them in a grid format'
+    
+    event_logs = {
+        'video':{
+            'base':('1200000','993355'),
+            'add':('1200100','119933'),
+            'edit':('1200200','EEDD55'),
+            'delete':('1200300','AA2222'),
+            'search':('1200400','CC55EE'),
+            'view':('1200500','55AACC'),
+        }
+    }
 
 site.register(Video, VideoRegistry)
