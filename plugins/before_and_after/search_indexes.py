a400fcffe19c62b92a5da601ae52775a705fa016
jmoswalt <jmoswalt@gmail.com>
2012-01-13 14:33:10 -0600

    Added Drag-drop reordering for Before and After.
    Updates were pushed out manually.

--- plugins/before_and_after/search_indexes.py
+++ plugins/before_and_after/search_indexes.py
@@ -9,7 +9,8 @@ class BeforeAndAfterIndex(TendenciBaseSearchIndex):
     title = indexes.CharField(model_attr='title')
     category = indexes.CharField()
     subcategory = indexes.CharField(null=True)
-        
+    ordering = indexes.IntegerField(model_attr='ordering')
+
     def prepare_category(self, obj):
         return obj.category.pk
         
