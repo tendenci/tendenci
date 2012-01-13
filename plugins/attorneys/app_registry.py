4d81ff92b286d7ebb15319dd5f3220d9ecec101c
Sam Lee <sam@bixly.com>
2012-01-14 06:00:47 +0800

    events log colors in registry for plugins

--- plugins/attorneys/app_registry.py
+++ plugins/attorneys/app_registry.py
@@ -8,5 +8,17 @@ class AttorneyRegistry(PluginRegistry):
     author = 'Schipul - The Web Marketing Company'
     author_email = 'programmers@schipul.com'
     description = 'Attorneys plugin for showing a list of Attorneys.'
+    
+    event_logs = {
+        'attorney':{
+            'base':('490000','773300'),
+            'add':('491000','119933'),
+            'edit':('492000','EEDD55'),
+            'delete':('493000','AA2222'),
+            'search':('494000','CC55EE'),
+            'detail':('495000','55AACC'),
+            'index':('496000','886655'),
+        }
+    }
 
 site.register(Attorney, AttorneyRegistry)
