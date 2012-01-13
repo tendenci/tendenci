c3563cf3615718e8209a36e2d5718c0d38c110e7
Sam Lee <sam@bixly.com>
2012-01-14 02:07:28 +0800

    app registries up to corp_memberships done.

--- apps/help_files/app_registry.py
+++ apps/help_files/app_registry.py
@@ -8,6 +8,25 @@ class HelpFileRegistry(CoreRegistry):
     author = 'Schipul - The Web Marketing Company'
     author_email = 'programmers@schipul.com'
     description = 'Create help files, tutorials and more!'
+    
+    event_logs = {
+        'help_file':{
+            'base':('1000000','D11300'),
+            'add':('1000100','D52500'),
+            'edit':('1000200','DA3800'),
+            'delete':('1000300','DF4A00'),
+            'search':('1000400','E35D00'),
+            'view':('1000500','E86F00'),
+        },
+        'help_file_topic':{
+            'base':('1001000','A20900'),
+            'add':('1001100','AC1300'),
+            'edit':('1001200','B51C00'),
+            'delete':('1001300','B51C00'),
+            'search':('1001400','C72E00'),
+            'view':('1001500','D13800'),
+        }
+    }
 
     url = {
         'search': lazy_reverse('help_files.search'),
