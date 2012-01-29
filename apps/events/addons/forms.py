fcc688069a5fe1359ea7a3c45287bd5f8cadb4fa
Sam Lee <sam@bixly.com>
2012-01-29 10:01:34 +0800

    RegAddon creation and incorporation to total registration price.

--- apps/events/addons/forms.py
+++ apps/events/addons/forms.py
@@ -36,7 +36,7 @@ class RegAddonForm(forms.Form):
         
         # dynamically create a field for all the possible options
         for option in AddonOption.objects.filter(addon__in=self.addons):
-            field_name = '%s_%s'%(option.addon.pk, option.title.replace(' ', ''))
+            field_name = option.field_name()
             choices = [(op, op) for op in option.choice_list()]
             self.fields[field_name] = forms.ChoiceField(
                 choices=choices, label=_(option.title), required=False)
@@ -48,3 +48,33 @@ class RegAddonForm(forms.Form):
         addon = self.cleaned_data['addon']
         if addon not in self.valid_addons:
             raise forms.ValidationError(_('Addon is invalid for current set of registrants'))
+        return addon
+            
+    def clean(self):
+        """Validate the option fields for the selected addon only"""
+        data = self.cleaned_data
+        addon = data['addon']
+        for option in addon.options.all():
+            try:
+                data[option.field_name()]
+            except KeyError:
+                raise forms.ValidationError(_('%s is a required option for %s' % (option.title, addon.title)))
+        return data
+    
+    def save(self, registration):
+        if self.is_valid():
+            data = self.cleaned_data
+            addon = data['addon']
+            regaddon = RegAddon.objects.create(
+                registration=registration,
+                addon=addon,
+                amount=addon.price,
+            )
+            for option in addon.options.all():
+                RegAddonOption.objects.create(
+                    selected_option = data[option.field_name()],
+                    option = option,
+                    regaddon = regaddon,
+                )
+            return regaddon
+        return None
