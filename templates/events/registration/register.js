4868d486d9097f15f1c898620412de19365c7337
Sam Lee <sam@bixly.com>
2012-02-01 04:46:08 +0800

    generalization of summary table

--- templates/events/registration/register.js
+++ templates/events/registration/register.js
@@ -90,6 +90,43 @@ function getPricingList(){
     });
 };
 
+function updateSummaryEntry(prefix, idx, price){
+    // update the summary entry in the summary table
+    // insert it into the table if it does not exist yet
+    ver = entry_id = '#summary-'+prefix+'-'+idx
+    var summary_table = $('#summary-price');
+    var entry = $(entry_id);
+    if(entry.length>0){
+        entry.find('.price').html(price);
+    }else{
+        var row = $('#summary-item-hidden').clone(true);
+        row.attr('id', 'summary-'+prefix+'-'+idx);
+        row.find('.item-price').html(price);
+        row.find('.item-label').html(prefix + " #" + idx);
+        summary_table.append(row);
+    }
+    updateSummaryTotal();
+}
+
+function removeSummaryEntry(prefix, idx){
+    var entry = $('#summary-'+prefix+'-'+idx);
+    entry.remove();
+    updateSummaryTotal();
+}
+
+function updateSummaryTotal(){
+    // update the total
+    var summary_table = $('#summary-price');
+    var items = $('#summary-price>tr');
+    var total_amount = 0;
+    for(i=0;i<items.length;i++){
+        item_amount = parseFloat($(items[i]).find('.item-price').html());
+        total_amount = total_amount + item_amount;
+    }
+    $('#total-amount').html(total_amount.toFixed(2));
+    $('#final-amount').html(total_amount.toFixed(2));
+}
+
 $(document).ready(function(){
     // REGISTRANT CONTROLS
     default_pricings = $('#pricing-choices').html();
