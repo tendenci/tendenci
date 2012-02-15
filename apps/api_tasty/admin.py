8ca1ff76c4c4a3d6bf6c22adc23619cf0992dea5
Sam Lee <sam@bixly.com>
2012-02-15 17:23:34 +0800

    validation and api field updates. An update to the required tasty pie specified in requirements.txt is needed.

--- apps/api_tasty/admin.py
+++ apps/api_tasty/admin.py
@@ -8,4 +8,5 @@ class ApiKeyAdmin(admin.ModelAdmin):
     list_display = ['user', 'key', 'created']
     form = ApiKeyForm
 
-admin.site.register(ApiKey, ApiKeyAdmin)
+# already registered in a more recent version of tasty pie
+#admin.site.register(ApiKey, ApiKeyAdmin)
