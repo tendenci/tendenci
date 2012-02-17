4c461592eb958c775690e2fc77fe79a811e77896
Sam Lee <sam@bixly.com>
2012-02-18 05:47:26 +0800

    validator that injects creator and owner when they are not specified in the bundle

--- apps/api_tasty/validation.py
+++ apps/api_tasty/validation.py
@@ -1,36 +1,39 @@
+from django.contrib.auth.models import User
 from tastypie.validation import CleanedDataFormValidation
 
-class ModelFormValidation(CleanedDataFormValidation):
+class TendenciValidation(CleanedDataFormValidation):
+    """Validation that makes use of a given Form.
+    The Form is assumed to be a ModelForm of a TendenciBaseModel subclass
     """
-    A validation class that uses a Django ``Form`` to validate the data.
-
-    This class **ALTERS** data sent by the user!!!
-
-    This class requires a ``form_class`` argument, which should be a Django
-    ``Form`` (or ``ModelForm``, though ``save`` will never be called) class.
-    This form will be used to validate the data in ``bundle.data``.
-    """
+    
     def is_valid(self, bundle, request=None):
+        """Validate the bundle with the given form.
+        Creator and Owner fields and defaulted to be the user of 
+        the developerkey if they were not specified.
         """
-        Checks ``bundle.data``to ensure it is valid & replaces it with the
-        cleaned results.
-
-        If the form is valid, an empty list (all valid) will be returned. If
-        not, a list of errors will be returned.
-        """
+        
         data = bundle.data
         # Ensure we get a bound Form, regardless of the state of the bundle.
         if data is None:
             data = {}
-
+        
+        if request:
+            user = User.objects.get(username=request.GET.get('username'))
+            if not data.get('creator', None):
+                data['creator'] = user
+                data['creator_username'] = user.username
+            if not data.get('owner', None):
+                data['owner'] = user
+                data['owner_username'] = user.username
+        
         form = self.form_class(data, instance=bundle.obj, request=request)
-
+        
         if form.is_valid():
             # We're different here & relying on having a reference to the same
             # bundle the rest of the process is using.
             bundle.data = form.cleaned_data
             return {}
-
+        
         # The data is invalid. Let's collect all the error messages & return
         # them.
         return form.errors
