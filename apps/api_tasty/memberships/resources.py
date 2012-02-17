f6d109ab2f70ec09fb520fee5c2d8366d2d558d7
Sam Lee <sam@bixly.com>
2012-02-18 00:33:45 +0800

    membership list filtering, resources for membership type and apps

--- apps/api_tasty/memberships/resources.py
+++ apps/api_tasty/memberships/resources.py
@@ -1,12 +1,61 @@
 from tastypie import fields
 from tastypie.validation import CleanedDataFormValidation
 from api_tasty.resources import TendenciResource
-from memberships.models import Membership
+from api_tasty.users.resources import UserResource
+from memberships.models import Membership, MembershipType, App
+
+class MembershipTypeResource(TendenciResource):
+    class Meta(TendenciResource.Meta):
+        queryset = MembershipType.objects.all()
+        resource_name = 'membership_type'
+        list_allowed_methods = ['get',]
+        detail_allowed_methods = ['get',]
+        
+class AppResource(TendenciResource):
+    class Meta(TendenciResource.Meta):
+        queryset = App.objects.all()
+        resource_name = 'app'
+        list_allowed_methods = ['get',]
+        detail_allowed_methods = ['get',]
 
 class MembershipResource(TendenciResource):
+    """Membership API
+    list: /api_tasty/v1/membership/?format=json&username=sam&api_key=6f21b5cad4841d7ba76e6d76d5b9332dddf109bf
+    """
+    
+    user = fields.ForeignKey(UserResource, 'user')
+    membership_type = fields.ForeignKey(MembershipTypeResource, 'membership_type')
+    app = fields.ForeignKey(AppResource, 'app', null=True)
+    
     class Meta(TendenciResource.Meta):
         queryset = Membership.objects.all()
         resource_name = 'membership'
         list_allowed_methods = ['get']
         detail_allowed_methods = ['get', 'post', 'put', 'delete']
         
+    def get_object_list(self, request):
+        """Membership list filtered by GET params
+        """
+        # initialize all filters:
+        mem_id = request.GET.get('mem_id', None)
+        mem_type = request.GET.get('mem_type', None)
+        mem_app = request.GET.get('mem_app', None)
+        corp_mem_id = request.GET.get('mem_corp_mem_id', None)
+        username = request.GET.get('mem_username', None)
+        userid = request.GET.get('mem_userid', None)
+        
+        mems = self._meta.queryset._clone()
+        if mem_id:
+            mems = mems.filter(pk=mem_id)
+        if mem_type:
+            mems = mems.filter(membership_type__pk=mt)
+        if mem_app:
+            mems = mems.filter(ma__pk=ma)
+        if corp_mem_id:
+            mems = mems.filter(corporate_membership_id=corp_mem_id)
+        if username:
+            mems = mems.filter(user__username=username)
+        if userid:
+            mems = mems.filter(user__pk=userid)
+        
+        return mems
--- apps/api_tasty/urls.py
+++ apps/api_tasty/urls.py
@@ -6,14 +6,21 @@ from api_tasty.users.resources import UserResource
 from api_tasty.profiles.resources import ProfileResource
 from api_tasty.discounts.resources import DiscountResource
 from api_tasty.entities.resources import EntityResource
-from api_tasty.memberships.resources import MembershipResource
+from api_tasty.memberships.resources import (MembershipResource, 
+    MembershipTypeResource, AppResource)
 
 api = SafeApi(api_name='v1')
+# user profiles
 api.register(UserResource())
 api.register(EntityResource())
 api.register(ProfileResource())
+# memberships
 api.register(MembershipResource())
+api.register(MembershipTypeResource())
+api.register(AppResource())
+# settings
 api.register(SettingResource())
+# discounts
 api.register(DiscountResource())
 
 urlpatterns = patterns('',
