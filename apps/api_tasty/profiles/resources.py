d2cdb0c81ab96d6a5f67c2013315a03077d39676
Sam Lee <sam@bixly.com>
2012-02-20 07:02:29 +0800

    updates to profile api

--- apps/api_tasty/profiles/resources.py
+++ apps/api_tasty/profiles/resources.py
@@ -8,6 +8,18 @@ from api_tasty.entities.resources import EntityResource
 from api_tasty.profiles.forms import ProfileForm
 
 class ProfileResource(TendenciResource):
+    """Profile API
+    list:
+    *example: http://0.0.0.0:8000/api_tasty/v1/profile/?format=json&username=sam&api_key=6f21b5cad4841d7ba76e6d76d5b9332dddf109bf
+    create:
+    *example: curl -H "Content-Type: application/json" -X POST --data @data.json "http://0.0.0.0:8000/api_tasty/v1/profile/?format=json&username=sam&api_key=6f21b5cad4841d7ba76e6d76d5b9332dddf109bf"
+    update:
+    curl -H "Content-Type: application/json" -X PUT --data @data.json "http://0.0.0.0:8000/api_tasty/v1/profile/12/?format=json&username=sam&api_key=6f21b5cad4841d7ba76e6d76d5b9332dddf109bf"
+    *update options: same sa create's options
+    delete:
+    *example: curl -H "Content-Type: application/json" -X DELETE "http://0.0.0.0:8000/api_tasty/v1/profile/12/?format=json&username=sam&api_key=6f21b5cad4841d7ba76e6d76d5b9332dddf109bf"
+    """
+    
     user = fields.ForeignKey(UserResource, 'user')
     entity = fields.ForeignKey(EntityResource, 'entity', null=True)
     
