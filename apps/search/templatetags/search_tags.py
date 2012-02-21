3d48fedc09b719bda697e9d9242b8198ae92dc4f
Jenny Qian <jqian@schipul.com>
2012-02-21 15:41:11 -0600

    Changed var_name from member to membership and corporate_member to corporate_membership in SearchResultNode, otherwise, global search is broken on memberships and corporate memberships

--- apps/search/templatetags/search_tags.py
+++ apps/search/templatetags/search_tags.py
@@ -25,6 +25,10 @@ class SearchResultNode(IncludeNode):
             if var_name == 'user':
                 # special case for users and profiles
                 var_name = 'profile'
+            if var_name == 'member':
+                var_name = 'membership'
+            if var_name == 'corporate_member':
+                var_name = 'corporate_membership'
 
             if var_name == 'photo_set':
                 #special case since Image and PhotoSet share the same app.
