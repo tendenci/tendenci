b26dca0aac8ce6eb6ff2cda38dcf5f23e9feaab0
jmoswalt <jmoswalt@gmail.com>
2012-02-29 09:45:57 -0600

    Updated Contacts app to no longer pull from search index.

--- apps/contacts/urls.py
+++ apps/contacts/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls.defaults import patterns, url
-#from contacts.feeds import LatestEntriesFeed
 
 urlpatterns = patterns('contacts',                  
-    url(r'^$', 'views.index', name="contact"),
-    url(r'^(?P<id>\d+)/$', 'views.index', name="contact"),
+    url(r'^$', 'views.list', name="contacts"),
+    url(r'^(?P<id>\d+)/$', 'views.details', name="contact"),
     url(r'^search/$', 'views.search', name="contact.search"),
     url(r'^print-view/(?P<id>\d+)/$', 'views.print_view', name="contact.print_view"),
     url(r'^delete/(?P<id>\d+)/$', 'views.delete', name="contact.delete"),
-#    url(r'^feed/$', LatestEntriesFeed(), name='contact.feed'),
 )
