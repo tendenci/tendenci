cb531ab8116555cb00f83cbef16897fc0bf38387
jmoswalt <jmoswalt@gmail.com>
2012-02-29 18:12:46 -0600

    Locations moved off of search index for list pages.

--- apps/locations/urls.py
+++ apps/locations/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls.defaults import patterns, url
+from locations.feeds import LatestEntriesFeed
+from site_settings.utils import get_setting
+
+urlpath = get_setting('module', 'locations', 'url')
+if not urlpath:
+    urlpath = "locations"
 
 urlpatterns = patterns('',                  
-    url(r'^$', 'locations.views.index', name="locations"),
-    url(r'^(?P<id>\d+)/$', 'locations.views.index', name="location"),
-    url(r'^search/$', 'locations.views.search', name="location.search"),
-    url(r'^nearest/$', 'locations.views.nearest', name="location.nearest"),
-    url(r'^print-view/(?P<id>\d+)/$', 'locations.views.print_view', name="location.print_view"),
-    url(r'^add/$', 'locations.views.add', name="location.add"),
-    url(r'^edit/(?P<id>\d+)/$', 'locations.views.edit', name="location.edit"),
-    url(r'^delete/(?P<id>\d+)/$', 'locations.views.delete', name="location.delete"),
+    url(r'^%s/$' % urlpath, 'locations.views.list', name="locations"),
+    url(r'^%s/(?P<id>\d+)/$' % urlpath, 'locations.views.index', name="location"),
+    url(r'^%s/search/$' % urlpath, 'locations.views.search', name="location.search"),
+    url(r'^%s/nearest/$' % urlpath, 'locations.views.nearest', name="location.nearest"),
+    url(r'^%s/print-view/(?P<id>\d+)/$' % urlpath, 'locations.views.print_view', name="location.print_view"),
+    url(r'^%s/add/$' % urlpath, 'locations.views.add', name="location.add"),
+    url(r'^%s/edit/(?P<id>\d+)/$' % urlpath, 'locations.views.edit', name="location.edit"),
+    url(r'^%s/feed/$' % urlpath, LatestEntriesFeed(), name='locations.feed'),
+    url(r'^%s/delete/(?P<id>\d+)/$' % urlpath, 'locations.views.delete', name="location.delete"),
 )
