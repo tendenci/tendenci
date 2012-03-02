d6a49ea0eeb7dd38a5d616e7b9761a5fd0a21151
jmoswalt <jmoswalt@gmail.com>
2012-03-02 15:23:39 -0600

    Removed a print. Optimized UnindexedItem creation to use get_or_create.

--- apps/search/signals.py
+++ apps/search/signals.py
@@ -4,9 +4,4 @@ from search.models import UnindexedItem
 def save_unindexed_item(sender, **kwargs):
     instance = kwargs['instance']
     content_type = ContentType.objects.get_for_model(instance)
-
-    unindexed_item = UnindexedItem()
-    unindexed_item.content_type = content_type
-    unindexed_item.object_id = instance.id
-
-    unindexed_item.save()
+    UnindexedItem.objects.get_or_create(content_type=content_type, object_id=instance.id)
