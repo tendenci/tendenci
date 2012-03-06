4504e8c4d4d381caeab9cbfaf1ba8a1b8b07d89a
Sam Lee <sam@bixly.com>
2012-03-07 00:53:49 +0800

    logregistry for accounting, entities, invoices, make_payments, payments

---
+++ apps/make_payments/app_registry.py
@@ -0,0 +1,19 @@
+from registry import site
+from registry.base import LogRegistry, lazy_reverse
+from make_payments.models import MakePayment
+
+class MakePaymentRegistry(LogRegistry):
+    event_logs = {
+        'make_payment':{
+            'base':('670000','66CC00'), # base
+            'add':('671000','66CC00'), # add
+            'edit':('672000','66CC33'), # edit
+            'delete':('673000','66CC66'), # delete
+            'search':('674000','66FF00'), # search 
+            'view':('675000','66FF33'), # view 
+        },
+    }
+
+site.register(MakePayment, MakePaymentRegistry)
+
+
