fc4623602427260795d3d767fb586f6410b7faf8
Eloy Zuniga Jr <eloyz.email@gmail.com>
2012-03-06 17:03:44 -0600

    Updating files, stories, and user groups, to use search and search_redirect views.

--- apps/stories/urls.py
+++ apps/stories/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls.defaults import patterns, url
 from stories.feeds import LatestEntriesFeed
 
 urlpatterns = patterns('',                  
-    url(r'^$', 'stories.views.list', name="stories"),
+    url(r'^$', 'stories.views.search', name="stories"),
     url(r'^(?P<id>\d+)/$', 'stories.views.details', name="story"),
     url(r'^print/(?P<id>\d+)/$', 'stories.views.print_details', name="story.print_details"),
-    url(r'^search/$', 'stories.views.search', name="story.search"),
+    url(r'^search/$', 'stories.views.search_redirect', name="story.search"),
     url(r'^add/$', 'stories.views.add', name="story.add"),
     url(r'^edit/(?P<id>\d+)/$', 'stories.views.edit', name="story.edit"),
     url(r'^upload/(?P<id>\d+)/$', 'stories.views.upload', name="story.upload"),
