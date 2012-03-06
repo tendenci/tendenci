6d38c480c4dcd9b0ba9dcc77ff222282195282ba
jmoswalt <jmoswalt@gmail.com>
2012-03-05 18:45:11 -0600

    Fix image URL for edit link in django admin.

--- apps/help_files/admin.py
+++ apps/help_files/admin.py
@@ -43,7 +43,7 @@ class HelpFileAdmin(admin.ModelAdmin):
     edit_link.short_description = 'edit'
 
     def view_on_site(self, obj):
-        link_icon = '%s/images/icons/external_16x16.png' % settings.STATIC_URL
+        link_icon = '%simages/icons/external_16x16.png' % settings.STATIC_URL
         link = '<a href="%s" title="%s"><img src="%s" /></a>' % (
             reverse('help_file.details', args=[obj.slug]),
             obj.question,
