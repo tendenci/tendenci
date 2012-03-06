26cb92e07ed24d2b46e5bd1da71bb3a6f27328a0
Eloy Zuniga Jr <eloyz.email@gmail.com>
2012-03-06 10:43:35 -0600

    Moving text to one line of code within app_registry.

--- apps/articles/app_registry.py
+++ apps/articles/app_registry.py
@@ -7,8 +7,7 @@ class ArticleRegistry(CoreRegistry):
     version = '1.0'
     author = 'Schipul - The Web Marketing Company'
     author_email = 'programmers@schipul.com'
-    description = 'Create articles to display basic content ' \
-                  'throughout the site'
+    description = 'Create articles to display basic content throughout the site'
     icon = '/site_media/static/images/icons/articles-color-64x64.png'
     
     event_logs = {
