6f05451957975f10656da5e2a6cfcbc65a8b77fb
Jenny Qian <jqian@schipul.com>
2012-03-07 11:11:45 -0600

    Removed search indexes dependency for attorneys when search index setting is not on

--- plugins/attorneys/urls.py
+++ plugins/attorneys/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('attorneys.views',
-    url(r'^attorneys/$', 'index', name="attorneys"),
+    url(r'^attorneys/$', 'search', name="attorneys"),
     url(r'^attorneys/search/$', 'search', name="attorneys.search"),
     url(r'^attorneys/(?P<slug>[\w\-]+)/vcard/$', 'vcard', name="attorneys.vcard"),
     url(r'^attorneys/(?P<slug>[\w\-]+)/$', 'detail', name="attorneys.detail"),
