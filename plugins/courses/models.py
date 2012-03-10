ead5de1548622df4d4ceafa3a27c1616e819cc1c
Sam Lee <sam@bixly.com>
2012-03-10 16:21:42 +0800

    changes to deadline

--- plugins/courses/models.py
+++ plugins/courses/models.py
@@ -18,23 +18,13 @@ class Course(TendenciBaseModel):
     You can think of this as test since there will be several questions
     linked to it.
     """
-    now = datetime.now()
-    dd_year = now.year + 1
-    dd_month = now.month
-    dd_day = now.day
-    
-    if dd_month == 2 and dd_day == 29:
-        # check if next year is a leap year
-        if not calendar.isleap(dd_year):
-            dd_month = 3
-            dd_day = 1
     
     title = models.CharField(_(u'Title'), max_length=200)
     content = models.TextField(_(u'Content'))
     retries = models.IntegerField(_(u'Retries'), help_text=u'Number of retries allowed (0, means unlimited)', default=0)
     retry_interval = models.IntegerField(_(u'Retry Interval'), help_text=u'Number of hours before another retry', default=0)
     passing_score = models.DecimalField(_(u'Passing Score'), help_text=u'out of 100%', max_digits=5, decimal_places=2)
-    deadline = models.DateTimeField(_(u'Deadline'), default=datetime(year=dd_year, month=dd_month, day=dd_day))
+    deadline = models.DateTimeField(_(u'Deadline'), null=True)
     close_after_deadline = models.BooleanField(_(u'Close After Deadline'), default=False)
     tags = TagField(blank=True, help_text='Tag 1, Tag 2, ...')
     
@@ -62,10 +52,12 @@ class Course(TendenciBaseModel):
         return total_points
     
     def is_closed(self):
-        return (self.close_after_deadline and datetime.now() > self.deadline)
+        return (self.close_after_deadline and self.deadline_has_passed())
         
     def deadline_has_passed(self):
-        return (datetime.now() > self.deadline)
+        if self.deadline:
+            return (datetime.now() > self.deadline)
+        return False
 
 class Question(models.Model):
     """Represents a single question for a course.
