7429f9dc51855997dc5a25549828f1d455f78f01
Eloy Zuniga Jr <ezuniga@schipul.com>
2012-03-15 15:14:47 -0500

    Ending the notification email URL in a forward slash, this allows for queries to easily be passed after the official URL.

--- apps/notification/urls.py
+++ apps/notification/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns('',
     url(r'^(\d+)/$', single, name="notification_notice"),
     url(r'^feed/$', feed_for_user, name="notification_feed_for_user"),
     url(r'^mark_all_seen/$', mark_all_seen, name="notification_mark_all_seen"),
-    url(r'^(?P<guid>[\d\w-]+)?$', email, name="notification_email"),
+    url(r'^(?P<guid>[\d\w-]+)?/$', email, name="notification_email"),
 )
