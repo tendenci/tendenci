096fe5afb7ced2f2f96d1497399e5163ce5cef24
Sam Lee <sam@bixly.com>
2012-03-16 00:28:31 +0800

    notification hash view, resend admin action

--- apps/notification/admin.py
+++ apps/notification/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.core.urlresolvers import reverse
+from django.conf import settings
+
 from notification.models import (NoticeType, NoticeSetting, Notice,
     ObservedItem, NoticeEmail)
     
@@ -11,9 +14,28 @@ class NoticeSettingAdmin(admin.ModelAdmin):
 class NoticeAdmin(admin.ModelAdmin):
     list_display = ('message', 'user', 'notice_type', 'added', 'unseen', 'archived')
 
+class NoticeEmailAdmin(admin.ModelAdmin):
+    list_display = ('title', 'emails', 'view_on_site')
+    actions = ['resend']
+    
+    def view_on_site(self, obj):
+        link_icon = '%simages/icons/external_16x16.png' % settings.STATIC_URL
+        link = '<a href="%s" title="%s"><img src="%s" /></a>' % (
+            reverse('notification_email', args=[obj.guid]),
+            obj,
+            link_icon,
+        )
+        return link
+    view_on_site.allow_tags = True
+    view_on_site.short_description = 'view'
+    
+    def resend(self, request, queryset):
+        for q in queryset:
+            q.resend()
+    resend.short_description = "Resend the selected emails"
 
 admin.site.register(NoticeType, NoticeTypeAdmin)
 admin.site.register(NoticeSetting, NoticeSettingAdmin)
 admin.site.register(Notice, NoticeAdmin)
 admin.site.register(ObservedItem)
-admin.site.register(NoticeEmail)
+admin.site.register(NoticeEmail, NoticeEmailAdmin)
