5410608182ac0f78c0375b07f8afbea23f561d6d
jmoswalt <jmoswalt@gmail.com>
2012-03-20 15:08:40 -0500

    Update jobs signals to be in their own file to avoid auto discover errors with haystack.
    Replicates the way the signals are created in files app.

---
+++ apps/jobs/signals.py
@@ -0,0 +1,6 @@
+def init_signals():
+    from django.db.models.signals import post_save
+    from jobs.models import Job
+    from contributions.signals import save_contribution
+
+    post_save.connect(save_contribution, sender=Job, weak=False)
