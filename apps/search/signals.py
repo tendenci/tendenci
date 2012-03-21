211c45dea73ad7cd1b22c6a14502359ff7bc37e2
jmoswalt <jmoswalt@gmail.com>
2012-03-21 12:23:38 -0500

    changed unindexed item signal to try/except since get_or_create is failing in some cases.

--- apps/search/signals.py
+++ apps/search/signals.py
@@ -4,4 +4,9 @@ from search.models import UnindexedItem
 def save_unindexed_item(sender, **kwargs):
     instance = kwargs['instance']
     content_type = ContentType.objects.get_for_model(instance)
-    UnindexedItem.objects.get_or_create(content_type=content_type, object_id=instance.id)
+    # get_or_create fails in rare cases, throwing MultipleObjectsReturned errors
+    # switched to try/except to prevent errors
+    try:
+        UnindexedItem.objects.get(content_type=content_type, object_id=instance.id)
+    except:
+        UnindexedItem.objects.create(content_type=content_type, object_id=instance.id)
