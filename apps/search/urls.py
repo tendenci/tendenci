1d1225e81762ed71f7626b173866d99c678be044
Eloy Zuniga Jr <ezuniga@schipul.com>
2012-03-26 17:23:51 -0500

    Removing "is_admin" and "is_developer" import from perms middleware.  Moving the haystack auto discover method from apps/search/urls.py to apps/search/__init__.py

--- apps/search/urls.py
+++ apps/search/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 from search.views import SearchView
 
-import haystack
-haystack.autodiscover()
-
 urlpatterns = patterns('search.views',
     url(r'^$', SearchView(), name='haystack_search'),
     url(r'^open-search/$', 'open_search', name='open_search')
