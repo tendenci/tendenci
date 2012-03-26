0c200b197f1338bf4210fa3aec7f25ae90ffa174
Eloy Zuniga Jr <ezuniga@schipul.com>
2012-03-26 17:10:23 -0500

    Revert "Moving save and delete hooks from the CustomSearchIndex.  Experimenting."
    This reverts commit 1555907ad5308d56202af1996480668f6272c2f4.
    
    Conflicts:
    
    	apps/events/search_indexes.py
    	apps/invoices/search_indexes.py
    	apps/perms/indexes.py
    	apps/recurring_payments/search_indexes.py

--- apps/invoices/search_indexes.py
+++ apps/invoices/search_indexes.py
@@ -2,10 +2,10 @@ from django.db.models import signals
 
 from haystack import indexes
 from haystack import site
-
 from invoices.models import Invoice
+from search.indexes import CustomSearchIndex
 
-class InvoiceIndex(indexes.SearchIndex):
+class InvoiceIndex(CustomSearchIndex):
     text = indexes.CharField(document=True, use_template=True)
     object_type = indexes.CharField(model_attr='object_type', null=True)
     object_id = indexes.IntegerField(model_attr='object_id', null=True)
@@ -42,21 +42,5 @@ class InvoiceIndex(indexes.SearchIndex):
         if myobj:
             return myobj._meta.verbose_name
         return obj.object_type
-
-    def _setup_save(self, model):
-        from search.signals import save_unindexed_item
-        signals.post_save.connect(save_unindexed_item, sender=model, weak=False)
-        
-    def _teardown_save(self, model):
-        from search.signals import save_unindexed_item
-        signals.post_save.disconnect(save_unindexed_item, sender=model)
-
-    def _setup_delete(self, obj):
-        from search.signals import save_unindexed_item
-        signals.post_delete.connect(self.remove_object, sender=obj)
-
-    def _teardown_delete(self, obj):
-        from search.signals import save_unindexed_item
-        signals.post_delete.disconnect(self.remove_object, sender=obj)
 
 site.register(Invoice, InvoiceIndex)
--- apps/recurring_payments/search_indexes.py
+++ apps/recurring_payments/search_indexes.py
@@ -1,10 +1,10 @@
 from django.db.models import signals
 from haystack import indexes
 from haystack import site
-
 from recurring_payments.models import RecurringPayment
+from search.indexes import CustomSearchIndex
 
-class RecurringPaymentIndex(indexes.SearchIndex):
+class RecurringPaymentIndex(CustomSearchIndex):
     text = indexes.CharField(document=True, use_template=True)
     user = indexes.CharField(model_attr='user', faceted=True)
     user_object = indexes.CharField(model_attr='user', faceted=True)
@@ -13,7 +13,7 @@ class RecurringPaymentIndex(indexes.SearchIndex):
 
     def get_updated_field(self):
         return 'update_dt'
-
+    
     def prepare_user_object(self, obj):
         return obj.user.username
 
@@ -24,21 +24,6 @@ class RecurringPaymentIndex(indexes.SearchIndex):
         
     def index_queryset(self):
         return RecurringPayment.objects.all().order_by('user')
-
-    def _setup_save(self, model):
-        from search.signals import save_unindexed_item
-        signals.post_save.connect(save_unindexed_item, sender=model, weak=False)
-        
-    def _teardown_save(self, model):
-        from search.signals import save_unindexed_item
-        signals.post_save.disconnect(save_unindexed_item, sender=model)
-
-    def _setup_delete(self, obj):
-        from search.signals import save_unindexed_item
-        signals.post_delete.connect(self.remove_object, sender=obj)
-
-    def _teardown_delete(self, obj):
-        from search.signals import save_unindexed_item
-        signals.post_delete.disconnect(self.remove_object, sender=obj)
+    
 
 site.register(RecurringPayment, RecurringPaymentIndex)
