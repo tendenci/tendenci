5153e553646297bd0d37fa4ad073062ce9b69eab
jmoswalt <jmoswalt@gmail.com>
2012-03-29 10:24:34 -0500

    Tab over render_to_403 to prevent 403's on all exceptions.

--- apps/base/middleware.py
+++ apps/base/middleware.py
@@ -5,9 +5,9 @@ from django.core.exceptions import PermissionDenied
 from base.http import Http403, render_to_403
 
 class Http403Middleware(object):
-	def process_exception(self, request, exception):
-		from django.contrib.auth.views import redirect_to_login
-		if isinstance(exception, Http403):
-			if request.user.is_anonymous():
-				return redirect_to_login(request.path)
-		return render_to_403(context_instance=RequestContext(request))
+    def process_exception(self, request, exception):
+        from django.contrib.auth.views import redirect_to_login
+        if isinstance(exception, Http403):
+            if request.user.is_anonymous():
+                return redirect_to_login(request.path)
+            return render_to_403(context_instance=RequestContext(request))
