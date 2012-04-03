a41633ef0251164b04fa2917d9c067104ff6b122
jmoswalt <jmoswalt@gmail.com>
2012-04-03 10:23:55 -0700

    Adding a sample local settings file.

---
+++ local_settings_sample.py
@@ -0,0 +1,38 @@
+# Rename this to local_settings.py and uncomment/edit as necessary.
+
+
+# DEBUG
+
+# DEBUG = True
+# TEMPLATE_DEBUG = DEBUG
+
+
+# ADMINS
+
+# ADMINS = (
+#     ('You', 'you@yoursite.com'),
+# )
+
+
+# MySQL database setup
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': '',
+#         'USER': '',
+#         'PASSWORD': '',
+#         'HOST': '',
+#         'PORT': '',
+#     },
+# }
+
+
+# Root folder name for the project
+
+# ROOT_URLCONF = 'tendenci.urls'
+
+
+# Haystack search index using Solr
+# HAYSTACK_SEARCH_ENGINE = 'solr'
+# HAYSTACK_SOLR_URL = 'http://localhost:8080/solr/'
