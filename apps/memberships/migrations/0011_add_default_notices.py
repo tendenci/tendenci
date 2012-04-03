2be8e28e8aabfea6eb0b8b2ca4acc9d46f85222b
Jenny Qian <jqian@schipul.com>
2012-04-03 14:36:45 -0500

    Splitting sql in membership migrations 0007 and 0011 to make sqlite happy

--- apps/memberships/migrations/0011_add_default_notices.py
+++ apps/memberships/migrations/0011_add_default_notices.py
@@ -26,8 +26,9 @@ class Migration(DataMigration):
                     
             with open(sql_file, 'r') as f:
                 sql = f.read()
-                
-                cursor.execute(sql)
+                sql_stats = sql.split(';')
+                for sql_stat in sql_stats:
+                    cursor.execute(sql_stat)
                 transaction.commit_unless_managed()
 
 
