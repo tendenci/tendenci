fa2ab30c101bfd6bc2da7fef3140108dc2cfdff3
jmoswalt <jmoswalt@gmail.com>
2012-05-02 10:27:40 -0500

    New management command to enable plugins.

---
+++ apps/pluginmanager/management/commands/enable_plugin.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+
+    def handle(self, plugin_name, **options):
+        """
+        Add a plugin and activate it.
+        """
+        from pluginmanager.models import PluginApp
+        
+        # strip out the 'plugins.' if it is in there
+        plugin_name = plugin_name.replace("plugins.","")
+
+        try:
+            __import__("plugins.%s" % plugin_name)
+            try:
+                plugin = PluginApp.objects.get(
+                        title=plugin_name,
+                        package=".".join(["plugins",plugin_name]),
+                    )
+                plugin.is_enabled = True
+                plugin.is_installed = True
+                plugin.save()
+                print "The plugin %s existed. It has been enabled." % plugin_name
+            except:
+                plugin = PluginApp.objects.create(
+                        title=plugin_name,
+                        package=".".join(["plugins",plugin_name]),
+                        description=plugin_name,
+                        is_installed=True,
+                        is_enabled=True
+                    )
+                print "New plugin %s installed and enabled." % plugin_name
+        except ImportError:
+            print "No plugin %s found." % plugin_name
