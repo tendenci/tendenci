639fac497250eff04c345a04c7720da8d67656d4
Eloy Zuniga Jr <ezuniga@schipul.com>
2012-05-17 16:50:19 -0500

    When a new user is created via the membership import we set the interactive/active flag based on your membership import options located on the first page of the membership import.  We also attempt to create a username is one is not provided.  We use your first name, last name, and email address.

--- apps/memberships/importer/tasks.py
+++ apps/memberships/importer/tasks.py
@@ -24,6 +24,8 @@ class ImportMembershipsTask(Task):
 
         app = memport.app
         key = memport.key
+        interactive = memport.interactive
+
         file_path = os.path.join(settings.MEDIA_ROOT, memport.get_file().file.name)
 
         #get parsed membership dicts
@@ -36,9 +38,7 @@ class ImportMembershipsTask(Task):
 
         for m in mems:
             if not m['skipped']:
-                # get membership type.
-                # this should not throw DNE errors
-                # otherwise it should have been marked skipped.
+                # membership type exists; we have tested; can add more tests here
                 membership_type = MembershipType.objects.get(name=m['membershiptype'])
 
                 # initialize dates
@@ -62,9 +62,22 @@ class ImportMembershipsTask(Task):
                 elif payment_method in cash:
                     payment_method_id = 3
 
-                # get or create user
-                user = get_user(username = m['username']) or \
-                    User.objects.create_user(m['username'], m['email'])
+                user = get_user(username=m['username'])
+
+                if not user:
+                    if not m['username']:
+                        if m['firstname'] or m['lastname']:
+                            m['username'] = spawn_username(
+                                m['firstname'],
+                                m['lastname'],
+                            )
+                        elif m['email']:
+                            m['username'] = spawn_username(
+                                m['email']
+                            )
+                    if m['username'] and m['email']:
+                        user = User.objects.create_user(m['username'], m['email'])
+                        user.is_active = bool(interactive)
 
                 # get or create profile
                 try:
@@ -75,46 +88,45 @@ class ImportMembershipsTask(Task):
                     profile = Profile.objects.create_profile(user)
 
                 if memport.override:
-                    user.first_name = m.get('firstname') or user.first_name
-                    user.last_name = m.get('lastname') or user.last_name
+                    user.first_name = m.get('firstname', '') or user.first_name
+                    user.last_name = m.get('lastname', '') or user.last_name
                     user.email = m['email'] or user.email
-                    profile.company = m.get('company') or profile.company
-                    profile.position_title = m.get('positiontitle') or profile.position_title
-                    profile.address = m.get('mailingaddress') or profile.address
-                    profile.address2 = m.get('address2') or profile.address2
-                    profile.city = m.get('city') or profile.city
-                    profile.state = m.get('state') or profile.state
-                    profile.zipcode = m.get('zipcode') or profile.zipcode
-                    profile.county = m.get('county') or profile.county
-                    profile.address_type = m.get('addresstype') or profile.address_type
-                    profile.work_phone = m.get('workphone') or profile.work_phone
-                    profile.home_phone = m.get('homephone') or profile.home_phone
-                    profile.mobile_phone = m.get('mobilephone') or profile.mobile_phone
+                    profile.company = m.get('company', '') or profile.company
+                    profile.position_title = m.get('positiontitle', '') or profile.position_title
+                    profile.address = m.get('mailingaddress', '') or profile.address
+                    profile.address2 = m.get('address2', '') or profile.address2
+                    profile.city = m.get('city', '') or profile.city
+                    profile.state = m.get('state', '') or profile.state
+                    profile.zipcode = m.get('zipcode', '') or profile.zipcode
+                    profile.county = m.get('county', '') or profile.county
+                    profile.address_type = m.get('addresstype', '') or profile.address_type
+                    profile.work_phone = m.get('workphone', '') or profile.work_phone
+                    profile.home_phone = m.get('homephone', '') or profile.home_phone
+                    profile.mobile_phone = m.get('mobilephone', '') or profile.mobile_phone
                     profile.email = user.email
-                    profile.email2 = m.get('email2') or profile.email2
-                    profile.url = m.get('website') or profile.url
-                    if m.get('dob'):
-                        profile.dob = dt_parse(m.get('dob')) or datetime.now()
+                    profile.email2 = m.get('email2', '') or profile.email2
+                    profile.url = m.get('website', '') or profile.url
+                    profile.dob = dt_parse(m.get('dob', ''))
                 else:
                     user.first_name = user.first_name or m['firstname']
                     user.last_name = user.last_name or m['lastname']
                     user.email = user.email or m['email']
-                    profile.company = profile.company or m.get('company')
-                    profile.position_title = profile.position_title or m.get('positiontitle')
-                    profile.address = profile.address or m.get('mailingaddress')
-                    profile.address2 = profile.address2 or m.get('address2')
-                    profile.city = profile.city or m.get('city')
-                    profile.state = profile.state or m.get('state')
-                    profile.zipcode = profile.zipcode or m.get('zipcode')
-                    profile.country = profile.county or m.get('county')
-                    profile.address_type = profile.address_type or m.get('addresstype')
-                    profile.work_phone = profile.work_phone or m.get('workphone')
-                    profile.home_phone = profile.home_phone or m.get('homephone')
-                    profile.mobile_phone = profile.mobile_phone or m.get('mobilephone')
-                    profile.email = user.email or m.get('email')
-                    profile.email2 = profile.email2 or m.get('email2')
-                    profile.url = profile.url or m.get('website')
-                    profile.dob = profile.dob or dt_parse(m.get('dob')) or datetime.now()
+                    profile.company = profile.company or m.get('company', '')
+                    profile.position_title = profile.position_title or m.get('positiontitle', '')
+                    profile.address = profile.address or m.get('mailingaddress', '')
+                    profile.address2 = profile.address2 or m.get('address2', '')
+                    profile.city = profile.city or m.get('city', '')
+                    profile.state = profile.state or m.get('state', '')
+                    profile.zipcode = profile.zipcode or m.get('zipcode', '')
+                    profile.country = profile.county or m.get('county', '')
+                    profile.address_type = profile.address_type or m.get('addresstype', '')
+                    profile.work_phone = profile.work_phone or m.get('workphone', '')
+                    profile.home_phone = profile.home_phone or m.get('homephone', '')
+                    profile.mobile_phone = profile.mobile_phone or m.get('mobilephone', '')
+                    profile.email = user.email or m.get('email', '')
+                    profile.email2 = profile.email2 or m.get('email2', '')
+                    profile.url = profile.url or m.get('website', '')
+                    profile.dob = profile.dob or dt_parse(m.get('dob', ''))
 
                 user.save()
                 profile.save()
@@ -126,7 +138,8 @@ class ImportMembershipsTask(Task):
                 memberships = Membership.objects.filter(
                     user=user,
                     membership_type=membership_type,
-                )
+                )  # oldest on top
+
                 if memberships:
                     membership = memberships[0]
                 else:
@@ -143,9 +156,9 @@ class ImportMembershipsTask(Task):
                     membership.status = m.get('status') or True
                     membership.status_detail = m.get('statusdetail') or 'Active'
                     membership.expire_dt = expire_dt
-                # save membership
-                membership.save()
-                
+
+                membership.save()  # update_dt changes
+
                 # bind corporate membership with membership if it exists
                 corp_memb_name = m.get('corpmembershipname')
                 if corp_memb_name:
@@ -156,40 +169,44 @@ class ImportMembershipsTask(Task):
                         membership.save()
                     except CorporateMembership.DoesNotExist:
                         pass
-                
-                # create entry
-                entry = AppEntry.objects.create(
-                    app = app,
-                    user = membership.user,
-                    entry_time = datetime.now(),
-                    membership = membership,  # pk required here
-                    is_renewal = membership.renewal,
-                    is_approved = True,
-                    decision_dt = membership.subscribe_dt,
-                    judge = membership.creator,
-                    creator=membership.creator,
-                    creator_username=membership.creator_username,
-                    owner=membership.owner,
-                    owner_username=membership.owner_username,
-                    allow_anonymous_view=False,
-                )
 
-                # create entry fields
-                for key, value in fields.items():
 
-                    app_fields = AppField.objects.filter(app=app, field_name=key)
-                    if app_fields and m.get(key):
+                entry = membership.get_entry()
+                entry_created = False
 
-                        try:
-                            value = unicode(m.get(unicode(key)))
-                        except (UnicodeDecodeError) as e:
-                            value = ''
+                if not entry:
+                    entry_created = True
+                    entry = AppEntry.objects.create(
+                        app = app,
+                        user = membership.user,
+                        entry_time = datetime.now(),
+                        membership = membership,  # pk required here
+                        is_renewal = membership.renewal,
+                        is_approved = True,
+                        decision_dt = membership.subscribe_dt,
+                        judge = membership.creator,
+                        creator=membership.creator,
+                        creator_username=membership.creator_username,
+                        owner=membership.owner,
+                        owner_username=membership.owner_username,
+                        allow_anonymous_view=False,
+                    )
 
-                        AppFieldEntry.objects.create(
-                            entry=entry,
-                            field=app_fields[0],
-                            value=value,
-                        )
+                if entry_created:  # create entry fields
+                    for key, value in fields.items():
+                        app_fields = AppField.objects.filter(app=app, field_name=key)
+                        if app_fields and m.get(key):
+
+                            try:
+                                value = unicode(m.get(unicode(key)))
+                            except (UnicodeDecodeError) as e:
+                                value = ''
+
+                            AppFieldEntry.objects.create(
+                                entry=entry,
+                                field=app_fields[0],
+                                value=value,
+                            )
 
                 # update membership number
                 if not membership.member_number:
