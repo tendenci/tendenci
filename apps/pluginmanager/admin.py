19275e4a816fcfa66ccc6d98e68ad7da31894a29
Sam Lee <sam@bixly.com>
2012-06-02 06:31:15 +0800

    package cleaner for pluginmanager admin

--- apps/pluginmanager/admin.py
+++ apps/pluginmanager/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from models import PluginApp
+from pluginmanager.models import PluginApp
+from pluginmanager.forms import PluginAppForm
 
 class PluginAppAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'package', 'is_installed', 'is_enabled']
     list_filter = ['is_installed', 'is_enabled']
-
+    form = PluginAppForm
 
 admin.site.register(PluginApp, PluginAppAdmin)
