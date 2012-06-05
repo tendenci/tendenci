284564e8dc64060e3b183cbdaa631e60c49e8ca0
Eloy Zuniga Jr <eloyz.email@gmail.com>
2012-06-05 10:26:49 -0500

    Removing unused code from lots/models.py

--- plugins/lots/models.py
+++ plugins/lots/models.py
@@ -1,31 +1,32 @@
 from django.db import models
-from django.contrib.contenttypes.models import ContentType
 from django.utils.translation import ugettext_lazy as _
 from tagging.fields import TagField
 from perms.models import TendenciBaseModel
-from files.models import file_directory, File
+from files.models import File
 from lots.managers import LotManager, MapManager
+
 
 class Map(File):
     """This is an image where users will place lot coordinates on
-    """    
+    """
     objects = MapManager()
-    
+
     class Meta:
-        permissions = (("view_map","Can view map"),)
-        
+        permissions = (("view_map", "Can view map"),)
+
     def __unicode__(self):
         return self.get_name()
-        
+
     @property
     def content_type(self):
         return 'lots'
-    
+
     def height_for(self, new_width):
         iwidth, iheight = self.image_dimensions()
-        new_height = iheight*new_width/iwidth
+        new_height = iheight * new_width / iwidth
         return new_height
-    
+
+
 class Lot(TendenciBaseModel):
     """A Lot is a set of coordinates.
     Coordinates are integer 2-tuples that describe a point on an uploaded image.
@@ -36,16 +37,17 @@ class Lot(TendenciBaseModel):
     name = models.CharField(_(u'Name'), max_length=200,)
     description = models.TextField(_('Description'), blank=True)
     objects = LotManager()
-    
+
     class Meta:
-        permissions = (("view_lot","Can view lot"),)
-        
+        permissions = (("view_lot", "Can view lot"),)
+
     def __unicode__(self):
         return self.name
-    
+
     @models.permalink
     def get_absolute_url(self):
         return ("lots.detail", [self.pk])
+
 
 class Line(models.Model):
     """Line coordinates for a lot.
