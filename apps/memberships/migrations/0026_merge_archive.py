c43921be5dff10c07cdec56f60427bba28096992
Eloy Zuniga Jr <ezuniga@schipul.com>
2012-06-18 12:47:32 -0500

    Memberships: set status-detail to archive

--- apps/memberships/migrations/0026_merge_archive.py
+++ apps/memberships/migrations/0026_merge_archive.py
@@ -15,7 +15,7 @@ class Migration(DataMigration):
         Membership = orm['memberships.Membership']
         MembershipArchive = orm['memberships.MembershipArchive']
 
-        MembershipArchive.objects.update(status_detail='expired')
+        MembershipArchive.objects.update(status_detail='archived')
         membership_archives = MembershipArchive.objects.all()
 
         for membership_archive in membership_archives:
