660b087e75ac623a0f3fa9b43ba23c1d3293612b
Eloy Zuniga Jr <ezuniga@schipul.com>
2012-07-05 10:50:16 -0500

    Tests: Made new No-DB Test Runner

---
+++ apps/base/test.py
@@ -0,0 +1,19 @@
+from django.test.simple import DjangoTestSuiteRunner
+
+
+class NoDbTestRunner(DjangoTestSuiteRunner):
+    """
+    A test runner to test withouth database creation
+    """
+
+    def setup_databases(self, **kwargs):
+        """
+        Override the database creation defined in parent class
+        """
+        pass
+
+    def teardown_databases(self, old_config, **kwargs):
+        """
+        Override the database teardown defined in parent class
+        """
+        pass
