f0470cb0c85a2cc2df8f09a3fb0cc25a616f54bf
jmoswalt <jmoswalt@gmail.com>
2012-08-02 16:33:36 -0500

    Adding Stock Static URL to load stock images.

--- tendenci/core/base/context_processors.py
+++ tendenci/core/base/context_processors.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 
 def static_url(request):
-    return {'STATIC_URL': settings.STATIC_URL, 'LOCAL_STATIC_URL': settings.LOCAL_STATIC_URL, 'TINYMCE_JS_URL': settings.TINYMCE_JS_URL}
+    return {'STATIC_URL': settings.STATIC_URL, 'LOCAL_STATIC_URL': settings.LOCAL_STATIC_URL, 'STOCK_STATIC_URL': settings.STOCK_STATIC_URL, 'TINYMCE_JS_URL': settings.TINYMCE_JS_URL}
 
 def index_update_note(request):
     return {'INDEX_UPDATE_NOTE': settings.INDEX_UPDATE_NOTE}
