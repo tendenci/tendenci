6646989926a17022f0b04488748cde1b3218ae40
Jenny Qian <jqian@schipul.com>
2012-08-15 08:34:03 -0500

    Revised the list_tables management command.

--- tendenci/core/base/management/commands/list_tables.py
+++ tendenci/core/base/management/commands/list_tables.py
@@ -1,4 +1,3 @@
-import os
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from django.db.models import get_models, get_app
@@ -40,35 +39,57 @@ class Command(BaseCommand):
             if app:
                 for model in get_models(app, include_auto_created=True):
                     models_d[model._meta.db_table] = model
-                    
-        def sort_table_list(table_list, rel_table, table):
-            i = table_list.index(rel_table)
-            try:
-                j = table_list.index(table)
-                if j<i:
-                    del table_list[i]
-                    table_list.insert(j, rel_table)
-            except:
-                table_list.append(table)
+                    tables_list.append(model._meta.db_table)
             
-                
-        for table in models_d.keys():
-            if table not in tables_list:
-                tables_list.append(table)
-                
-            for field in models_d[table]._meta.fields:
-                if isinstance(field, ForeignKey) \
-                    or isinstance(field, OneToOneField):
-                    # get the related table
-                    rel_table = field.rel.to._meta.db_table
-                    if rel_table not in tables_list:
-                        tables_list.append(rel_table)
-                    sort_table_list(tables_list, rel_table, table)
-                    
+
         tables_list.remove('mig_help_files_helpfile_t4_to_t5')
+        # django 1.4 doesn't have auth_message table
+        if 'auth_message' in tables_list:
+            tables_list.remove('auth_message')
+
+                
+        related_tables = {}
+        # get a list of related tables for each table
+        for table in tables_list:
+            related_tables[table] = [field.rel.to._meta.db_table \
+                                    for field in models_d[table]._meta.fields \
+                                    if isinstance(field, (ForeignKey, OneToOneField))]
+            
+        sorted_list = []
+        for table in tables_list:
+            if not related_tables[table]:
+                sorted_list.append(table)
+                
+        n = 100
+        # avoid getting into the infinite loop - just in case
+        #while related_tables:
+        while n > 1:
+            # remove tables from related_tables if already in the sorted_list
+            for key in related_tables.keys():
+                for rel_table in related_tables[key]:
+                    if rel_table in sorted_list:
+                        related_tables[key].remove(rel_table)
+                if not related_tables[key]:
+                    del related_tables[key]
+                    
+            # add to the sorted_list if there is no related_tables for this table
+            for table in tables_list:
+                # if the related_tables is gone
+                if table not in sorted_list and (not related_tables.has_key(table)):
+                    sorted_list.append(table)
+            
+            # continue until all the related tables are gone
+            if not related_tables:
+                break 
+             
+            n = n - 1       
+            
+        if related_tables:
+            print "ERROR: Sorting not completed."
+
         
         # copy the list to your conf.yml file.           
-        print '-', '\n- '.join(tables_list)
+        print '-', '\n- '.join(sorted_list)
 
                                 
                         
