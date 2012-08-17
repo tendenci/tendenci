8e14e3b47f369ec56248c444182a396475d9d06e
Eloy Zuniga Jr <ezuniga@schipul.com>
2012-08-17 16:02:56 -0500

    Profiles: Created management command called refresh_profile_member_numbers.py

---
+++ tendenci/addons/memberships/management/commands/refresh_profile_member_numbers.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """
+    Populate the member IDs (or member numbers) to profiles.
+    """
+    def handle(self, *args, **options):
+        from profiles.models import Profile
+        verbosity = int(options['verbosity'])
+
+        for profile in Profile.objects.all():
+            member_number = profile.refresh_member_number()
+
+            if verbosity:
+                print 'profile(%d) has member number %s' % \
+                    (profile.pk, member_number)
