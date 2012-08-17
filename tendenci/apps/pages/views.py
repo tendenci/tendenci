085547662eb5af1263a07fdb677f65c40099d2c7
Alex <ragsdale.a@gmail.com>
2012-08-17 13:23:05 -0500

    Copying over changes from pages views 8/9.

--- tendenci/apps/pages/views.py
+++ tendenci/apps/pages/views.py
@@ -15,7 +15,7 @@ from tendenci.core.base.utils import check_template
 from tendenci.core.event_logs.models import EventLog
 from tendenci.core.meta.models import Meta as MetaTags
 from tendenci.core.meta.forms import MetaForm
-from tendenci.core.perms.utils import update_perms_and_save, get_notice_recipients, has_perm,  has_view_perm, get_query_filters
+from tendenci.core.perms.utils import update_perms_and_save, get_notice_recipients, has_perm,  has_view_perm, admin_required, get_query_filters
 from tendenci.core.perms.decorators import admin_required
 from tendenci.core.categories.forms import CategoryForm
 from tendenci.core.categories.models import Category
@@ -33,81 +33,80 @@ except:
     notification = None
 
 def index(request, slug=None, template_name="pages/view.html"):
-    if not slug: return HttpResponseRedirect(reverse('page.search'))
-    page = get_object_or_404(Page, slug=slug)
-    
-    # non-admin can not view the non-active content
-    # status=0 has been taken care of in the has_perm function
-    if (page.status_detail).lower() <> 'active' and (not request.user.profile.is_superuser):
+    """
+    Return active page or deleted page
+    """
+
+    if not slug and not id:
+        return HttpResponseRedirect(reverse('page.search'))
+
+    if id:
+        page = get_object_or_404(Page, pk=id)
+    if slug:
+
+        try:
+            page = get_object_or_404(
+                Page, slug=slug, status_detail='active'
+            )
+        except Page.MultipleObjectsReturned:
+            pages = Page.objects.filter(
+                slug=slug, status_detail='active'
+            ).order_by('-pk')
+
+            for page in pages:
+                print page.pk, page
+
+            page = pages[0]
+
+    if not has_view_perm(request.user, 'pages.view_page', page):
         raise Http403
 
     if not page.template or not check_template(page.template):
         page.template = "pages/base.html"
 
-    if has_perm(request.user,'pages.view_page',page):
-        log_defaults = {
-            'event_id' : 585000,
-            'event_data': '%s (%d) viewed by %s' % (page._meta.object_name, page.pk, request.user),
-            'description': '%s viewed' % page._meta.object_name,
-            'user': request.user,
-            'request': request,
-            'instance': page,
-        }
-        EventLog.objects.log(**log_defaults)        
-        
-        return render_to_response(template_name, {'page': page}, 
-            context_instance=RequestContext(request))
-    else:
-        raise Http403
+    EventLog.objects.log(instance=page)
+
+    return render_to_response(template_name, {'page': page},
+        context_instance=RequestContext(request))
 
 def search(request, template_name="pages/search.html"):
+    """
+    Search pages.
+    """
     query = request.GET.get('q', None)
+
     if get_setting('site', 'global', 'searchindex') and query:
-        pages = Page.objects.search(query, user=request.user)
+        pages = Page.objects.search(query, user=request.user).filter(
+            status=True, status_detail='active')
     else:
         filters = get_query_filters(request.user, 'pages.view_page')
-        pages = Page.objects.filter(filters).distinct()
+        pages = Page.objects.active().filter(filters).distinct()
         if request.user.is_authenticated():
-            pages = pages.select_related() 
+            pages = pages.select_related()
+
     pages = pages.order_by('-create_dt')
 
-    log_defaults = {
-        'event_id' : 584000,
-        'event_data': '%s searched by %s' % ('Page', request.user),
-        'description': '%s searched' % 'Page',
-        'user': request.user,
-        'request': request,
-        'source': 'pages'
-    }
-    EventLog.objects.log(**log_defaults)
-    
-    return render_to_response(template_name, {'pages':pages}, 
+    EventLog.objects.log()
+
+    return render_to_response(template_name, {'pages': pages},
         context_instance=RequestContext(request))
 
 def print_view(request, slug, template_name="pages/print-view.html"):
-    
-    page = get_object_or_404(Page, slug=slug)
+    page = get_object_or_404(Page, slug=slug, status_detail='active')
 
-    if has_perm(request.user,'pages.view_page',page):
-        log_defaults = {
-            'event_id' : 585001,
-            'event_data': '%s (%d) viewed by %s' % (page._meta.object_name, page.pk, request.user),
-            'description': '%s viewed - print view' % page._meta.object_name,
-            'user': request.user,
-            'request': request,
-            'instance': page,
-        }
-        EventLog.objects.log(**log_defaults)
-        return render_to_response(template_name, {'page': page}, 
-            context_instance=RequestContext(request))
-    else:
+    if not has_perm(request.user, 'pages.view_page', page):
         raise Http403
+
+    EventLog.objects.log(instance=page)
+
+    return render_to_response(template_name, {'page': page},
+        context_instance=RequestContext(request))
 
 @login_required
 def edit(request, id, form_class=PageForm, meta_form_class=MetaForm, category_form_class=CategoryForm, template_name="pages/edit.html"):
         
     page = get_object_or_404(Page, pk=id)
-    
+        
     if not has_perm(request.user,'pages.change_page',page):
         raise Http403
     
@@ -171,19 +170,11 @@ def edit(request, id, form_class=PageForm, meta_form_class=MetaForm, category_fo
                     
             #save relationships
             page.save()
-            
-            log_defaults = {
-                'event_id' : 582000,
-                'event_data': '%s (%d) edited by %s' % (page._meta.object_name, page.pk, request.user),
-                'description': '%s edited' % page._meta.object_name,
-                'user': request.user,
-                'request': request,
-                'instance': page,
-            }
-            EventLog.objects.log(**log_defaults)               
+
+            EventLog.objects.log(instance=page)
 
             messages.add_message(request, messages.SUCCESS, 'Successfully updated %s' % page)
-            
+
             if not request.user.profile.is_superuser:
                 # send notification to administrators
                 recipients = get_notice_recipients('module', 'pages', 'pagerecipients')
@@ -200,7 +191,7 @@ def edit(request, id, form_class=PageForm, meta_form_class=MetaForm, category_fo
         form = form_class(instance=page, user=request.user)
         metaform = meta_form_class(instance=page.meta, prefix='meta')
         categoryform = category_form_class(content_type, initial=initial_category_form_data, prefix='category')
-    
+
     return render_to_response(template_name,
         {
             'page': page, 
@@ -211,12 +202,69 @@ def edit(request, id, form_class=PageForm, meta_form_class=MetaForm, category_fo
         context_instance=RequestContext(request))
 
 @login_required
+def change_version(request, id, form_class=ChangeVersionForm, template_name="pages/change-version.html"):
+
+    page = get_object_or_404(Page, pk=id)
+
+    if not has_perm(request.user, 'pages.change_page', page):
+        raise Http403
+
+    EventLog.objects.log(instance=page)
+
+    if request.method == "POST":
+        form = form_class(data=request.POST, page=page)
+        if form.is_valid():
+            page = form.save()
+            return HttpResponseRedirect(reverse('page', args=[page.slug]))
+    else:
+        form = form_class(page=page)
+
+    return render_to_response(template_name,
+        {'page': page, 'form': form},
+        context_instance=RequestContext(request))
+
+
+@admin_required
+@login_required
+def inactive_pages(request, template_name="pages/inactive-pages.html"):
+    """
+    Return inactive pages
+    """
+
+    EventLog.objects.log()
+
+    # Get list of pages without active version
+    # This includes deleted, archive, and pending status.
+    active_pages = Page.objects.active()
+    active_guid = []
+    for page in active_pages:
+        active_guid.append(page.guid)
+    inactive_pages = Page.objects.exclude(guid__in=active_guid).order_by('-create_dt')
+
+    # Only include the latest version
+    inactive_guid = []
+    pages = []
+    for page in inactive_pages:
+        if not page.guid in inactive_guid:
+            inactive_guid.append(page.guid)
+            pages.append(page)
+
+    return render_to_response(template_name,
+        {'pages': pages}, context_instance=RequestContext(request))
+
+
+@login_required
 def edit_meta(request, id, form_class=MetaForm, template_name="pages/edit-meta.html"):
+    """
+    Return page that allows you to edit meta-html information.
+    """
 
     # check permission
     page = get_object_or_404(Page, pk=id)
-    if not has_perm(request.user,'pages.change_page',page):
+    if not has_perm(request.user, 'pages.change_page', page):
         raise Http403
+
+    EventLog.objects.log(instance=page)
 
     defaults = {
         'title': page.get_title(),
@@ -229,17 +277,18 @@ def edit_meta(request, id, form_class=MetaForm, template_name="pages/edit-meta.h
     if request.method == "POST":
         form = form_class(request.POST, instance=page.meta)
         if form.is_valid():
-            page.meta = form.save() # save meta
-            page.save() # save relationship
+            page.meta = form.save()  # save meta
+            page.save()  # save relationship
 
             messages.add_message(request, messages.SUCCESS, 'Successfully updated meta for %s' % page)
-            
+
             return HttpResponseRedirect(reverse('page', args=[page.slug]))
     else:
         form = form_class(instance=page.meta)
 
-    return render_to_response(template_name, {'page': page, 'form':form}, 
+    return render_to_response(template_name, {'page': page, 'form': form},
         context_instance=RequestContext(request))
+
 
 @login_required
 def add(request, form_class=PageForm, meta_form_class=MetaForm, category_form_class=CategoryForm, template_name="pages/add.html"):
@@ -301,16 +350,8 @@ def add(request, form_class=PageForm, meta_form_class=MetaForm, category_form_cl
             #save relationships
             page.save()
 
-            log_defaults = {
-                'event_id' : 581000,
-                'event_data': '%s (%d) added by %s' % (page._meta.object_name, page.pk, request.user),
-                'description': '%s added' % page._meta.object_name,
-                'user': request.user,
-                'request': request,
-                'instance': page,
-            }
-            EventLog.objects.log(**log_defaults)
-            
+            EventLog.objects.log()
+
             messages.add_message(request, messages.SUCCESS, 'Successfully added %s' % page)
             
             if not request.user.profile.is_superuser:
@@ -323,8 +364,9 @@ def add(request, form_class=PageForm, meta_form_class=MetaForm, category_form_cl
                             'request': request,
                         }
                         notification.send_emails(recipients,'page_added', extra_context)
-                
-            return HttpResponseRedirect(reverse('page', args=[page.slug]))
+            if page.status and page.status_detail == 'active':    
+                return HttpResponseRedirect(reverse('page', args=[page.slug]))
+            return HttpResponseRedirect(reverse('page.search'))
     else:
         initial_category_form_data = {
             'app_label': 'pages',
@@ -342,51 +384,47 @@ def add(request, form_class=PageForm, meta_form_class=MetaForm, category_form_cl
             },
             context_instance=RequestContext(request))
 
+
 @login_required
 def delete(request, id, template_name="pages/delete.html"):
     page = get_object_or_404(Page, pk=id)
 
-    if has_perm(request.user,'pages.delete_page'):   
-        if request.method == "POST":
-            log_defaults = {
-                'event_id' : 583000,
-                'event_data': '%s (%d) deleted by %s' % (page._meta.object_name, page.pk, request.user),
-                'description': '%s deleted' % page._meta.object_name,
-                'user': request.user,
-                'request': request,
-                'instance': page,
-            }
-            EventLog.objects.log(**log_defaults)
-            messages.add_message(request, messages.SUCCESS, 'Successfully deleted %s' % page)
-            
-            # send notification to administrators
-            recipients = get_notice_recipients('module', 'pages', 'pagerecipients')
-            if recipients:
-                if notification:
-                    extra_context = {
-                        'object': page,
-                        'request': request,
-                    }
-                    notification.send_emails(recipients,'page_deleted', extra_context)
-            
-            page.delete()
-            return HttpResponseRedirect(reverse('page.search'))
-    
-        return render_to_response(template_name, {'page': page}, 
-            context_instance=RequestContext(request))
-    else:
+    if not has_perm(request.user, 'pages.delete_page'):
         raise Http403
+
+    if request.method == "POST":
+        EventLog.objects.log(instance=page)
+        messages.add_message(request, messages.SUCCESS, 'Successfully deleted %s' % page)
+
+        # send notification to administrators
+        recipients = get_notice_recipients('module', 'pages', 'pagerecipients')
+        if recipients:
+            if notification:
+                extra_context = {
+                    'object': page,
+                    'request': request,
+                }
+                notification.send_emails(recipients, 'page_deleted', extra_context)
+
+        # Soft delete
+        page.status = False
+        page.status_detail = 'inactive'
+        page.save()
+        return HttpResponseRedirect(reverse('page.search'))
+
+    return render_to_response(template_name, {'page': page},
+        context_instance=RequestContext(request))
+
 
 @login_required
 def export(request, template_name="pages/export.html"):
     """Export Pages"""
-    
+
     if not request.user.is_superuser:
         raise Http403
-    
+
     if request.method == 'POST':
-        # initilize initial values
-        file_name = "pages.csv"
+
         fields = [
             'guid',
             'title',
@@ -402,8 +440,9 @@ def export(request, template_name="pages/export.html"):
             'meta',
             'categories',
         ]
+
         export_id = run_export_task('pages', 'page', fields)
         return redirect('export.status', export_id)
-        
+
     return render_to_response(template_name, {
     }, context_instance=RequestContext(request))
