072729ba8840e43059e937872a2af1d004f4e356
jmoswalt <jmoswalt@gmail.com>
2012-08-20 10:55:25 -0500

    Update the path to touch_settings.

--- tendenci/apps/pluginmanager/management/commands/touch_settings.py
+++ tendenci/apps/pluginmanager/management/commands/touch_settings.py
@@ -1,13 +1,14 @@
-import os, sys
+import os
 from django.core.management.base import BaseCommand
 from django.db.transaction import commit_on_success
+
 
 class Command(BaseCommand):
 
     @commit_on_success
     def handle(self, *args, **kwargs):
         from django.conf import settings
-        wsgi = os.path.join(settings.PROJECT_ROOT, 'tendenci-site', 'wsgi.py')
-        settings_path = os.path.join(settings.PROJECT_ROOT, 'tendenci-site', 'settings.py')
-        os.system('touch '+wsgi)
-        os.system('touch '+settings_path)
+        wsgi = os.path.join(settings.PROJECT_ROOT, 'conf', 'wsgi.py')
+        settings_path = os.path.join(settings.PROJECT_ROOT, 'conf', 'settings.py')
+        os.system('touch ' + wsgi)
+        os.system('touch ' + settings_path)
