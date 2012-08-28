faadfd843b9c01e75cb2da83ce4d7c8291ea710d
Alex <aragsdale@schipul.com>
2012-08-28 11:37:07 -0500

    Events: command to populate creator IDs from registrations to invoices.

---
+++ tendenci/addons/events/management/commands/populate_users_to_invoices.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """
+    Populate invoices with creator IDs from registrations.
+    """
+    def handle(self, *args, **options):
+        from tendenci.addons.events.models import Registration
+        from tendenci.apps.invoices.models import Invoice
+
+        registrations = Registration.objects.filter(creator__isnull=False)
+        
+        for registration in registrations:
+            related_invoice = Invoice.objects.filter(id=registration.invoice_id)
+            for invoice in related_invoice:
+                invoice.creator_id = registration.creator_id
+                invoice.save()
+                print 'Invoice %s updated for Registraion %s' % (invoice.id, registration.id)
