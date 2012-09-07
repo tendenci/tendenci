a7f5e69bc432f07836e9581f7afd26ab730b7a5f
jmoswalt <jmoswalt@gmail.com>
2012-09-07 13:53:05 -0500

    Pluginmanager deprecation comment.

--- tendenci/apps/pluginmanager/__init__.py
+++ tendenci/apps/pluginmanager/__init__.py
@@ -1,2 +1,5 @@
+# This module is deprecated as of 2012-09-07.
+# It is no longer being imported or used.
+
 from tendenci.apps.pluginmanager.settings import plugin_apps
 from tendenci.apps.pluginmanager.urls import get_url_patterns
