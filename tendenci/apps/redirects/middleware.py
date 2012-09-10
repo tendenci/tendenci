ecb277fdfc14a94b9ba56086b04695fc028f091f
Alex <aragsdale@schipul.com>
2012-09-10 15:20:54 -0500

    Redirects: updated reference to dynamic_urls in middleware.

--- tendenci/apps/redirects/middleware.py
+++ tendenci/apps/redirects/middleware.py
@@ -8,16 +8,13 @@ class RedirectMiddleware(object):
         
         path = request.get_full_path()
         try:
-            redirect, args, kwargs = resolve(path, urlconf='redirects.dynamic_urls')
+            redirect, args, kwargs = resolve(path, urlconf='tendenci.apps.redirects.dynamic_urls')
             args = [value for value in kwargs.values()]
-            #print redirect, args, kwargs
             to_url = kwargs.pop('url')
             for key in kwargs.keys():
                 to_url = to_url.replace("(%s)"%key, kwargs[key])
-            #print to_url
             args[0] = to_url
             return redirect(request, *args)
         except Exception, e:
             # No redirect was found. Return the response.
-            #print e
             return response
