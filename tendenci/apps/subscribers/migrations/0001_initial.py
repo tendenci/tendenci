f97aa7870a46854482bc4fd6924e19bd9fc0139e
dharol <dharol@gmail.com>
2012-09-14 09:46:55 -0500

    Reset migrations on apps/subscribers

--- tendenci/apps/subscribers/migrations/0001_initial.py
+++ tendenci/apps/subscribers/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(SchemaMigration):
         db.create_table('subscribers_groupsubscription', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('group', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['user_groups.Group'])),
-            ('subscriber', self.gf('django.db.models.fields.related.ForeignKey')(related_name='subscriptions', to=orm['forms.FormEntry'])),
+            ('subscriber', self.gf('django.db.models.fields.related.ForeignKey')(related_name='subscriptions', null=True, to=orm['forms.FormEntry'])),
             ('create_dt', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
             ('update_dt', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
         ))
@@ -21,14 +21,32 @@ class Migration(SchemaMigration):
         # Adding unique constraint on 'GroupSubscription', fields ['group', 'subscriber']
         db.create_unique('subscribers_groupsubscription', ['group_id', 'subscriber_id'])
 
+        # Adding model 'SubscriberData'
+        db.create_table('subscribers_subscriberdata', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('subscription', self.gf('django.db.models.fields.related.ForeignKey')(related_name='data', to=orm['subscribers.GroupSubscription'])),
+            ('field_label', self.gf('django.db.models.fields.CharField')(max_length=2000)),
+            ('value', self.gf('django.db.models.fields.CharField')(max_length=2000)),
+        ))
+        db.send_create_signal('subscribers', ['SubscriberData'])
+
+        # Adding unique constraint on 'SubscriberData', fields ['subscription', 'field_label']
+        db.create_unique('subscribers_subscriberdata', ['subscription_id', 'field_label'])
+
 
     def backwards(self, orm):
         
+        # Removing unique constraint on 'SubscriberData', fields ['subscription', 'field_label']
+        db.delete_unique('subscribers_subscriberdata', ['subscription_id', 'field_label'])
+
         # Removing unique constraint on 'GroupSubscription', fields ['group', 'subscriber']
         db.delete_unique('subscribers_groupsubscription', ['group_id', 'subscriber_id'])
 
         # Deleting model 'GroupSubscription'
         db.delete_table('subscribers_groupsubscription')
+
+        # Deleting model 'SubscriberData'
+        db.delete_table('subscribers_subscriberdata')
 
 
     models = {
@@ -47,7 +65,7 @@ class Migration(SchemaMigration):
         },
         'auth.user': {
             'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2012, 9, 14, 9, 46, 8, 911266)'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
@@ -55,7 +73,7 @@ class Migration(SchemaMigration):
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2012, 9, 14, 9, 46, 8, 911138)'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
             'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
@@ -108,22 +126,24 @@ class Migration(SchemaMigration):
             'allow_user_view': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'completion_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
             'create_dt': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'creator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'form_creator'", 'to': "orm['auth.User']"}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'forms_form_creator'", 'to': "orm['auth.User']"}),
             'creator_username': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'custom_payment': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'email_copies': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
             'email_from': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'email_text': ('django.db.models.fields.TextField', [], {'default': "''", 'max_length': '2000', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'intro': ('django.db.models.fields.TextField', [], {'max_length': '2000'}),
-            'owner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'form_owner'", 'to': "orm['auth.User']"}),
+            'intro': ('django.db.models.fields.TextField', [], {'max_length': '2000', 'blank': 'True'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'forms_form_owner'", 'to': "orm['auth.User']"}),
             'owner_username': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'response': ('django.db.models.fields.TextField', [], {'max_length': '2000'}),
+            'payment_methods': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['payments.PaymentMethod']", 'symmetrical': 'False', 'blank': 'True'}),
+            'response': ('django.db.models.fields.TextField', [], {'max_length': '2000', 'blank': 'True'}),
             'send_email': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '100', 'db_index': 'True'}),
             'status': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'status_detail': ('django.db.models.fields.CharField', [], {'default': "'active'", 'max_length': '50'}),
             'subject_template': ('django.db.models.fields.CharField', [], {'default': "'[title] - [first name]  [last name] - [phone]'", 'max_length': '200', 'null': 'True', 'blank': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'update_dt': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
         },
         'forms.formentry': {
@@ -131,15 +151,49 @@ class Migration(SchemaMigration):
             'entry_path': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '200', 'blank': 'True'}),
             'entry_time': ('django.db.models.fields.DateTimeField', [], {}),
             'form': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'entries'", 'to': "orm['forms.Form']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'payment_method': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['payments.PaymentMethod']", 'null': 'True'}),
+            'pricing': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['forms.Pricing']", 'null': 'True'})
+        },
+        'forms.pricing': {
+            'Meta': {'object_name': 'Pricing'},
+            'form': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['forms.Form']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'price': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '2', 'blank': 'True'})
+        },
+        'payments.paymentmethod': {
+            'Meta': {'object_name': 'PaymentMethod'},
+            'admin_only': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'human_name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_online': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'machine_name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'perms.objectpermission': {
+            'Meta': {'object_name': 'ObjectPermission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'create_dt': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'group': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['user_groups.Group']", 'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'object_id': ('django.db.models.fields.IntegerField', [], {}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True'})
         },
         'subscribers.groupsubscription': {
             'Meta': {'unique_together': "(('group', 'subscriber'),)", 'object_name': 'GroupSubscription'},
             'create_dt': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'group': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['user_groups.Group']"}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'subscriber': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'subscriptions'", 'to': "orm['forms.FormEntry']"}),
+            'subscriber': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'subscriptions'", 'null': 'True', 'to': "orm['forms.FormEntry']"}),
             'update_dt': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        'subscribers.subscriberdata': {
+            'Meta': {'unique_together': "(('subscription', 'field_label'),)", 'object_name': 'SubscriberData'},
+            'field_label': ('django.db.models.fields.CharField', [], {'max_length': '2000'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'subscription': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'data'", 'to': "orm['subscribers.GroupSubscription']"}),
+            'value': ('django.db.models.fields.CharField', [], {'max_length': '2000'})
         },
         'user_groups.group': {
             'Meta': {'object_name': 'Group'},
@@ -155,7 +209,7 @@ class Migration(SchemaMigration):
             'auto_respond_priority': ('django.db.models.fields.FloatField', [], {'default': '0', 'blank': 'True'}),
             'auto_respond_template': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
             'create_dt': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'creator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'group_creator'", 'to': "orm['auth.User']"}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'user_groups_group_creator'", 'to': "orm['auth.User']"}),
             'creator_username': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
             'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             'email_recipient': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
@@ -166,7 +220,7 @@ class Migration(SchemaMigration):
             'members': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.User']", 'through': "orm['user_groups.GroupMembership']", 'symmetrical': 'False'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
             'notes': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'owner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'group_owner'", 'to': "orm['auth.User']"}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'user_groups_group_owner'", 'to': "orm['auth.User']"}),
             'owner_username': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
             'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'group_permissions'", 'blank': 'True', 'to': "orm['auth.Permission']"}),
             'show_as_option': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
