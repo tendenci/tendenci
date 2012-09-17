a8b6c3f0d48548c9c928d9f4d687ffbab81b8788
dharol <dharol@gmail.com>
2012-09-17 14:31:53 -0500

    Added admin to addons/Jobs

--- tendenci/addons/jobs/admin.py
+++ tendenci/addons/jobs/admin.py
@@ -1,4 +1,40 @@
 from django.contrib import admin
-from tendenci.addons.jobs.models import Job
 
-# admin.site.register(Job)
+from tendenci.core.perms.admin import TendenciBaseModelAdmin
+from tendenci.addons.jobs.models import Job
+from tendenci.addons.jobs.forms import JobForm
+
+class JobAdmin(TendenciBaseModelAdmin):
+    list_display = ['title', 'post_dt', 'owner_link', 'admin_perms', 'admin_status']
+    list_filter = ['status_detail', 'owner_username']
+    prepopulated_fields = {'slug': ['title']}
+    search_fields = ['title', 'description']
+    fieldsets = (
+        ('Job Information', {
+            'fields': ('title',
+                'slug',
+                'description',
+                'skills',
+                'location',
+                'contact_email',
+                'contact_website',
+                'tags',
+                'post_dt',
+                )
+        }),
+        ('Permissions', {'fields': ('allow_anonymous_view',)}),
+        ('Advanced Permissions', {'classes': ('collapse',), 'fields': (
+            'user_perms',
+            'member_perms',
+            'group_perms',
+            )}),
+        ('Publishing Status', {'fields': (
+            'syndicate',
+            'status',
+            'status_detail',
+            )}),
+    )
+    form = JobForm
+    ordering = ['-update_dt']
+
+admin.site.register(Job, JobAdmin)
