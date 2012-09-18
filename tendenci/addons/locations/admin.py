2d7fa61aca7802d6980b63704379da0f110eef18
dharol <dharol@gmail.com>
2012-09-18 12:14:41 -0500

    Added admin.py to addons/locations

--- tendenci/addons/locations/admin.py
+++ tendenci/addons/locations/admin.py
@@ -1,4 +1,39 @@
 from django.contrib import admin
-from tendenci.addons.locations.models import Location
 
-# admin.site.register(Location)
+from tendenci.core.perms.admin import TendenciBaseModelAdmin
+from tendenci.addons.locations.models import Location
+from tendenci.addons.locations.forms import LocationForm
+
+class LocationAdmin(TendenciBaseModelAdmin):
+    list_display = ['location_name', 'owner_link', 'admin_perms', 'admin_status']
+    list_filter = ['status_detail', 'owner_username']
+    prepopulated_fields = { }#'slug': ['title']}
+    search_fields = ['title', 'description']
+    fieldsets = (
+        ('Job Information', {
+            'fields': ('location_name',
+#                       'slug',
+                       'description',
+                       'city',
+                       'zipcode',
+                       'contact',
+                       'email',
+                       'phone',
+                       'website',
+                )
+        }),
+        ('Permissions', {'fields': ('allow_anonymous_view',)}),
+        ('Advanced Permissions', {'classes': ('collapse',), 'fields': (
+            'user_perms',
+            'member_perms',
+            'group_perms',
+            )}),
+        ('Status', {'fields': (
+            'status',
+            'status_detail',
+            )}),
+        )
+    form = LocationForm
+    ordering = ['-update_dt']
+
+admin.site.register(Location, LocationAdmin)
