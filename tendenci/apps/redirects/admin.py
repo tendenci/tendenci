e2790f19ace99b929a4076f0494442d20531a8c7
dharol <dharol@gmail.com>
2012-09-18 16:44:52 -0500

    cleaned admin.py of redirects

--- tendenci/apps/redirects/admin.py
+++ tendenci/apps/redirects/admin.py
@@ -5,34 +5,7 @@ from tendenci.apps.redirects.models import Redirect
 from tendenci.apps.redirects.forms import RedirectForm
 
 class RedirectAdmin(TendenciBaseModelAdmin):
-    list_display = [ ]#'title', 'post_dt', 'owner_link', 'admin_perms', 'admin_status']
-    list_filter = [] #'status_detail', 'owner_username']
-    prepopulated_fields = {}#'slug': ['title']}
-#    search_fields = ['title', 'description']
-#    fieldsets = (
-#        ('Redirect Information', {
-#            'fields': ('title',
-#                       'slug',
-#                       'description',
-#                       'skills',
-#                       'location',
-#                       'contact_email',
-#                       'contact_website',
-#                       'tags',
-#                       'post_dt',
-#                )
-#        }),
-#        ('Permissions', {'fields': ('allow_anonymous_view',)}),
-#        ('Advanced Permissions', {'classes': ('collapse',), 'fields': (
-#            'user_perms',
-#            'member_perms',
-#            'group_perms',
-#            )}),
-#        ('Status', {'fields': (
-#            'status',
-#            'status_detail',
-#            )}),
-#        )
+    list_display = [ ]
     form = RedirectForm
     ordering = ['-update_dt']
 
