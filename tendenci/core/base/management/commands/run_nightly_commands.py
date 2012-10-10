30a2ec15a069ee05b4d7ade64f7ec0157ceb8fe4
jmoswalt <jmoswalt@gmail.com>
2012-10-10 12:11:02 -0500

    Add new management command to be run nightly that is a collection of other commands.

---
+++ tendenci/core/base/management/commands/run_nightly_commands.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+
+class Command(BaseCommand):
+    """
+    Run a series of commands that should be run on a
+    nightly (daily) basis.
+    """
+    def handle(self, *args, **options):
+        call_command('collect_metrics')
+        call_command('expire_jobs')
+        call_command('expire_resumes')
+        call_command('expire_stories')
+        call_command('send_event_reminders')
+        call_command('send_membership_notices')
+        call_command('remove_expired_memberships')
