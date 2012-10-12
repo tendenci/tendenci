ed1477b10488ee675c569a61d43132eb76de8429
Jenny Qian <jqian@schipul.com>
2012-10-12 15:07:02 -0500

    Added the option src_url.

--- tendenci/core/files/management/commands/scan_and_retrieve_files.py
+++ tendenci/core/files/management/commands/scan_and_retrieve_files.py
@@ -1,5 +1,6 @@
 import re
-from django.core.management.base import BaseCommand
+from optparse import make_option
+from django.core.management.base import BaseCommand, CommandError
 
 
 class Command(BaseCommand):
@@ -7,7 +8,18 @@ class Command(BaseCommand):
     Scan and retrieve media files (images, pdf..) from T4 to heroku.
 
     Scan content for Articles, News, Pages, Jobs and Events.
+
+    Example:
+        ./manage.py scan_and_retriev_files articles pages --src_url=http://YourOldSite.com
     """
+    option_list = BaseCommand.option_list + (
+        make_option('--src_url',
+            action='store',
+            dest='src_url',
+            default=None,
+            help='src_url from which the files being downloaded'),
+        )
+
     def handle(self, *apps, **kwargs):
         from tendenci.core.site_settings.utils import get_setting
         from tendenci.core.files.utils import AppRetrieveFiles
@@ -18,8 +30,15 @@ class Command(BaseCommand):
             apps_to_scan = ['articles', 'news', 'pages', 'jobs', 'events']
             #apps_to_scan = ['articles']
 
-        t4_src_url = 'http://www.spegcs.org'
         site_url = get_setting('site', 'global', 'siteurl')
+        #src_url = 'http://www.spegcs.org'
+        src_url = kwargs['src_url']
+        if not src_url:
+            raise CommandError('--src_url is required')
+
+        if src_url == site_url:
+            raise CommandError('src_url cannot be the same as the site_url')
+
         # if site_url == t4_src_url, throws an error
 
         exts = '|'.join(['jpg', 'jpeg', 'gif', 'tif',
@@ -30,7 +49,7 @@ class Command(BaseCommand):
 
         params = {
                   'site_url': site_url,
-                  'src_url': t4_src_url,
+                  'src_url': src_url,
                   'p': p,
                   }
         retriever = AppRetrieveFiles(**params)
