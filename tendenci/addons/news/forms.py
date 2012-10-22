9b8ab2e79ec70f50a52ea7cb01dcb0292eb73b5f
Alex <aragsdale@schipul.com>
2012-10-22 17:37:52 -0500

    News: add thumbnail image field, update forms, views, save method and add and edit templates accordingly.

--- tendenci/addons/news/forms.py
+++ tendenci/addons/news/forms.py
@@ -1,3 +1,5 @@
+import imghdr
+from os.path import splitext, basename
 from datetime import datetime
 
 from django import forms
@@ -7,6 +9,13 @@ from tendenci.addons.news.models import News
 from tendenci.core.perms.forms import TendenciBaseForm
 from tinymce.widgets import TinyMCE
 from tendenci.core.base.fields import SplitDateTimeField
+
+ALLOWED_LOGO_EXT = (
+    '.jpg',
+    '.jpeg',
+    '.gif',
+    '.png'
+)
 
 class NewsForm(TendenciBaseForm):
     body = forms.CharField(required=False,
@@ -19,14 +28,18 @@ class NewsForm(TendenciBaseForm):
 
     status_detail = forms.ChoiceField(
         choices=(('active','Active'),('inactive','Inactive'), ('pending','Pending'),))
-           
+
+    photo_upload = forms.FileField(label=_('Thumbnail Image'), required=False, help_text=_('The thumbnail image can be used on your homepage or sidebar if it is setup in your theme. It will not display on the news page.'))
+
     class Meta:
         model = News
+
         fields = (
         'headline',
         'slug',
         'summary',
         'body',
+        'photo_upload',
         'source',
         'website',
         'release_dt',
@@ -52,6 +65,7 @@ class NewsForm(TendenciBaseForm):
                                  'summary',
                                  'body',
                                  'tags',
+                                 'photo_upload',
                                  'source', 
                                  'website',
                                  'release_dt',
@@ -82,7 +96,23 @@ class NewsForm(TendenciBaseForm):
                                  'status_detail'], 
                       'classes': ['admin-only'],
                     })]     
-         
+ 
+    def clean_photo_upload(self):
+        photo_upload = self.cleaned_data['photo_upload']
+        if photo_upload:
+            extension = splitext(photo_upload.name)[1]
+            
+            # check the extension
+            if extension.lower() not in ALLOWED_LOGO_EXT:
+                raise forms.ValidationError('The photo must be of jpg, gif, or png image type.')
+            
+            # check the image header
+            image_type = '.%s' % imghdr.what('', photo_upload.read())
+            if image_type not in ALLOWED_LOGO_EXT:
+                raise forms.ValidationError('The photo is an invalid image. Try uploading another photo.')
+
+        return photo_upload
+    
     def __init__(self, *args, **kwargs): 
         super(NewsForm, self).__init__(*args, **kwargs)
         if self.instance.pk:
@@ -90,12 +120,10 @@ class NewsForm(TendenciBaseForm):
         else:
             self.fields['body'].widget.mce_attrs['app_instance_id'] = 0
 
-        #        if not self.user.profile.is_superuser:
+        #if not self.user.profile.is_superuser:
         if self.user and not self.user.profile.is_superuser:
             if 'status' in self.fields:
                 self.fields.pop('status')
             if 'status_detail' in self.fields:
                 self.fields.pop('status_detail')
-
-        
         
