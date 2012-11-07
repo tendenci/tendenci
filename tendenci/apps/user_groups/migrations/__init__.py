3bccec562a279d7cef386dc424fdab576ab9a047
jmoswalt <jmoswalt@gmail.com>
2012-11-07 14:57:10 -0600

    Updating signals to only run on migration of their specific apps.

--- tendenci/apps/user_groups/migrations/__init__.py
+++ tendenci/apps/user_groups/migrations/__init__.py
@@ -3,11 +3,12 @@ from south.signals import post_migrate
 from tendenci.apps.user_groups.models import Group
 from tendenci.core.site_settings.utils import get_setting
 
-def create_default_group(sender, **kwargs):
+
+def create_default_group(sender, app, **kwargs):
     """
     Auto-create a default group if no groups exist.
     """
-    if not Group.objects.all().exists():
+    if app == "user_groups" and not Group.objects.all().exists():
         site_name = get_setting("site", "global", "sitedisplayname")
         group = Group()
         if site_name:
@@ -18,7 +19,7 @@ def create_default_group(sender, **kwargs):
         group.allow_self_add = False
         group.allow_self_remove = False
         group.description = "Initial group auto-generated on site creation."
-        
+
         group.save()
 
 post_migrate.connect(create_default_group)
