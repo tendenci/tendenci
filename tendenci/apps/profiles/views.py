fc8d650fc3ebde176d59664616bde691cfc8827d
jmoswalt <jmoswalt@gmail.com>
2012-11-08 10:43:57 -0600

    Adding missing import for HttpResponse to profiles views.

--- tendenci/apps/profiles/views.py
+++ tendenci/apps/profiles/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, get_object_or_404, redirect, Http404
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.db.models import Count, Q, get_app
 from django.contrib.admin.views.decorators import staff_member_required
