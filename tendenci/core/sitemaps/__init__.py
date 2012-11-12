f7ecaeb984144b7b5ce2552d6a964ee781df9e36
jmoswalt <jmoswalt@gmail.com>
2012-11-12 16:31:03 -0600

    Adding caching to the urls in the site map to prevent timeouts in generating the sitemap.xml document

--- tendenci/core/sitemaps/__init__.py
+++ tendenci/core/sitemaps/__init__.py
@@ -1,20 +1,47 @@
 from django.contrib.sitemaps import Sitemap
+from django.contrib.sites.models import Site
+from django.core.exceptions import ImproperlyConfigured
+
 
 class TendenciSitemap(Sitemap):
-    pass
 
+    def __get(self, name, obj, default=None):
+        try:
+            attr = getattr(self, name)
+        except AttributeError:
+            return default
+        if callable(attr):
+            return attr(obj)
+        return attr
 
-#        
-# Stories removed from sitemap because they are not end pieces of content.
-# They are used in rotators and the end pages are not meant to be viewed. - JMO       
-#
-# class StorySitemap(Sitemap):
-#     changefreq = "weekly"
-#     priority = 0.2
-#     
-#     def items(self):
-#         sqs = Story.objects.search().order_by('-create_dt')
-#         return [sq.object for sq in sqs]
-#                                         
-#     def lastmod(self, obj):
-#         return obj.create_dt
+    def get_urls(self, page=1, site=None, protocol=None):
+        # Determine protocol
+        if self.protocol is not None:
+            protocol = self.protocol
+        if protocol is None:
+            protocol = 'http'
+
+        # Determine domain
+        if site is None:
+            if Site._meta.installed:
+                try:
+                    site = Site.objects.get_current()
+                except Site.DoesNotExist:
+                    pass
+            if site is None:
+                raise ImproperlyConfigured("To use sitemaps, either enable the sites framework or pass a Site/RequestSite object in your view.")
+        domain = site.domain
+
+        urls = []
+        for item in self.paginator.page(page).object_list:
+            loc = "%s://%s%s" % (protocol, domain, self.__get('location', item))
+            priority = self.__get('priority', item, None)
+            url_info = {
+                #'item':       item,
+                'location':   loc,
+                'lastmod':    self.__get('lastmod', item, None),
+                'changefreq': self.__get('changefreq', item, None),
+                'priority':   str(priority is not None and priority or ''),
+            }
+            urls.append(url_info)
+        return urls
--- tendenci/core/sitemaps/views.py
+++ tendenci/core/sitemaps/views.py
@@ -4,8 +4,13 @@ Created on 28-02-2011
 @author: hpolloni
 '''
 
+from django.contrib.sites.models import get_current_site
+from django.core.paginator import EmptyPage, PageNotAnInteger
+from django.http import Http404
+from django.template.response import TemplateResponse
+
 from django.conf import settings
-from django.contrib.sitemaps.views import sitemap
+from django.core.cache import cache
 from tendenci.core.sitemaps import TendenciSitemap
 
 
@@ -15,19 +20,52 @@ def get_all_sitemaps():
         _try_import(app + '.feeds')
     return TendenciSitemap.__subclasses__()
 
+
 def _try_import(module):
     try:
         __import__(module)
     except ImportError, e:
         pass
 
+
 def create_sitemap(request):
     sitemap_classes = get_all_sitemaps()
-    #print "Found %d sitemap classes" % len(sitemap_classes)
-    #for cls in sitemap_classes:
-    #    print cls.__name__
     sitemaps = dict([(cls.__name__, cls) for cls in sitemap_classes])
-    #print str(sitemaps)
     return sitemap(request, sitemaps)
 
 
+def sitemap(request, sitemaps, section=None,
+            template_name='sitemap.xml', mimetype='application/xml'):
+    req_protocol = 'https' if request.is_secure() else 'http'
+    req_site = get_current_site(request)
+
+    if section is not None:
+        if section not in sitemaps:
+            raise Http404("No sitemap available for section: %r" % section)
+        maps = [sitemaps[section]]
+    else:
+        maps = sitemaps.values()
+    page = request.GET.get("p", 1)
+
+    urls = []
+    for site in maps:
+        try:
+            if callable(site):
+                site = site()
+                site_key = site.__class__.__name__
+            else:
+                site_key = site.__name__
+            # Cache the sitemap urls
+            sitemap_cache_key = '.'.join([settings.SITE_CACHE_KEY, 'sitemap_cache', site_key])
+            site_urls = cache.get(sitemap_cache_key)
+            if not site_urls:
+                site_urls = site.get_urls(page=page, site=req_site,
+                                          protocol=req_protocol)
+                cache.set(sitemap_cache_key, list(site_urls), 86400)
+            urls.extend(site_urls)
+        except EmptyPage:
+            raise Http404("Page %s empty" % page)
+        except PageNotAnInteger:
+            raise Http404("No page '%s'" % page)
+    return TemplateResponse(request, template_name, {'urlset': urls},
+                            content_type=mimetype)
