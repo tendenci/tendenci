80407095e2bb2f1907ce2cf9d71c3c11a2799236
Alex <aragsdale@schipul.com>
2012-11-20 15:59:10 -0600

    Help text for priority field.

--- tendenci/addons/events/models.py
+++ tendenci/addons/events/models.py
@@ -880,7 +880,7 @@ class Event(TendenciBaseModel):
         help_text=_('Photo that represents this event.'), null=True, blank=True)
     group = models.ForeignKey(Group, null=True, on_delete=models.SET_NULL, default=get_default_group)
     tags = TagField(blank=True)
-    priority = models.BooleanField(default=False)
+    priority = models.BooleanField(default=False, help_text=_("Priority events will show up at the top of the events list. They will be featured with a star icon on the monthly calendar."))
 
     # additional permissions
     display_event_registrants = models.BooleanField(_('Display Attendees'), default=False)
