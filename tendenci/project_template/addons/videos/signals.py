167da2652d1fe50c540736c3cfcdb577f2bc5b59
jmoswalt <jmoswalt@gmail.com>
2012-12-05 09:44:34 -0600

    Remove videos from the project_template.

--- tendenci/project_template/addons/videos/signals.py
+++
@@ -1,6 +0,0 @@
-def init_signals():
-    from django.db.models.signals import post_save
-    from addons.videos.models import Video
-    from tendenci.apps.contributions.signals import save_contribution
-
-    post_save.connect(save_contribution, sender=Video, weak=False)
