501746e8c0efbed3b717a1aa2e3a3bfdb556f27a
Eloy Zuniga Jr <eloyz.email@gmail.com>
2012-12-20 15:41:46 -0600

    Updated the profile model to insert blank user defined data by default.

--- tendenci/apps/profiles/models.py
+++ tendenci/apps/profiles/models.py
@@ -65,11 +65,11 @@ class Profile(Person):
     agreed_to_tos = models.BooleanField(_('agrees to tos'), default=False)
     original_username = models.CharField(max_length=50)
 
-    ud1 = models.TextField(blank=True)
-    ud2 = models.TextField(blank=True)
-    ud3 = models.TextField(blank=True)
-    ud4 = models.TextField(blank=True)
-    ud5 = models.TextField(blank=True)
+    ud1 = models.TextField(blank=True, default=u'')
+    ud2 = models.TextField(blank=True, default=u'')
+    ud3 = models.TextField(blank=True, default=u'')
+    ud4 = models.TextField(blank=True, default=u'')
+    ud5 = models.TextField(blank=True, default=u'')
 
     objects = ProfileManager()
     actives = ProfileActiveManager()
