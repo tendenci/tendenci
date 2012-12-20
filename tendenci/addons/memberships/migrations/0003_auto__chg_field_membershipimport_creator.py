056e26c621e39446e8caf1aa6aac5af5be811ab3
jmoswalt <jmoswalt@gmail.com>
2012-12-20 16:45:22 -0600

    Reverting file to a previous state.

--- tendenci/addons/memberships/migrations/0003_auto__chg_field_membershipimport_creator.py
+++ tendenci/addons/memberships/migrations/0003_auto__chg_field_membershipimport_creator.py
@@ -7,13 +7,13 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-
+        
         # Changing field 'MembershipImport.creator'
         db.alter_column('memberships_membershipimport', 'creator_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True))
 
 
     def backwards(self, orm):
-
+        
         # Changing field 'MembershipImport.creator'
         db.alter_column('memberships_membershipimport', 'creator_id', self.gf('django.db.models.fields.related.ForeignKey')(default=1, to=orm['auth.User']))
 
