13124c04cd1853a905315b7def90cb0425644400
Jenny Qian <jqian@schipul.com>
2013-01-02 15:54:01 -0600

    Two fixtures, default_entities.json and default_groups.json, for setting up default entities and groups.
    The fixtures are loaded by their migration signals

--- tendenci/apps/user_groups/migrations/__init__.py
+++ tendenci/apps/user_groups/migrations/__init__.py
@@ -1,5 +1,6 @@
 from south.signals import post_migrate
 from django.db import connection
+from django.core.management import call_command
 
 from tendenci.core.site_settings.models import Setting
 from tendenci.apps.user_groups.models import Group
@@ -11,22 +12,25 @@ def create_default_group(sender, app, **kwargs):
     Auto-create a default group with id=1 if none exist.
     """
     if app == "user_groups":
-        if not Group.objects.filter(pk=1):
-            site_name = "Default"
-            table_exists = Setting._meta.db_table in \
-                connection.introspection.table_names()
-            if table_exists and get_setting("site", "global", "sitedisplayname"):
-                site_name = get_setting("site", "global", "sitedisplayname")
-
-            group = Group()
-            group.name = site_name
-            group.label = site_name
-            group.show_as_option = False
-            group.allow_self_add = False
-            group.allow_self_remove = False
-            group.description = "Initial group auto-generated on site creation."
-            group.id = 1
-
-            group.save()
+        if not Group.objects.all():
+            call_command("loaddata", "default_groups.json")
+        else:
+            if not Group.objects.filter(pk=1):
+                site_name = "Default"
+                table_exists = Setting._meta.db_table in \
+                    connection.introspection.table_names()
+                if table_exists and get_setting("site", "global", "sitedisplayname"):
+                    site_name = get_setting("site", "global", "sitedisplayname")
+    
+                group = Group()
+                group.name = site_name
+                group.label = site_name
+                group.show_as_option = False
+                group.allow_self_add = False
+                group.allow_self_remove = False
+                group.description = "Initial group auto-generated on site creation."
+                group.id = 1
+    
+                group.save()
 
 post_migrate.connect(create_default_group)
