49532ce377704e24d8dd952f0411788e7846e620
jmoswalt <jmoswalt@gmail.com>
2013-01-09 12:48:55 -0600

    Updating stripe version to prevent requests warning.

--- setup.py
+++ setup.py
@@ -187,7 +187,7 @@ setup(
         "python-dateutil==1.5",
         "pdfminer==20110515",
         "slate==0.3",
-        "stripe==1.7.2",
+        "stripe==1.7.7",
         "pycrypto==2.6",
         "boto==2.5.2",
         "django-timezones==0.2",
