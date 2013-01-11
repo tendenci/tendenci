7226ec75d45f2d27df8f651a8c334c090df28964
jmoswalt <jmoswalt@gmail.com>
2013-01-11 09:32:05 -0600

    Squashed commit of the following: Updated forms export.
    commit cd9ee4b5bfcac7ee8414c2c4a427fa0894bd07e9
    Author: Alex <aragsdale@schipul.com>
    Date:   Thu Jan 10 16:07:26 2013 -0600
    
        Remove the old fronted form entries export URL.
    
    commit c8f948fb85e28113de49d906f282cbc9c5c2f651
    Author: Alex <aragsdale@schipul.com>
    Date:   Thu Jan 10 15:57:36 2013 -0600
    
        Spell permanently correctly.
    
    commit 6fe0baa21146c1eec6cfc01b01e3cb82d3650da6
    Author: Alex <aragsdale@schipul.com>
    Date:   Thu Jan 10 15:57:11 2013 -0600
    
        Link entries export without files to the admin.
    
    commit 439ee2c7d0150a475f332595151aeba35284715b
    Author: John Eddie Ayson <je@bixly.com>
    Date:   Sat Dec 15 17:44:39 2012 +0800
    
        updated form entries export to be a celery task
    
    commit b977ee432abd4495cc57f8bc7d090d25ee8ed09d
    Author: John Eddie Ayson <je@bixly.com>
    Date:   Fri Dec 14 18:20:18 2012 +0800
    
        fixed zip file bug, added a separate link for export with files

--- tendenci/apps/forms_builder/forms/tasks.py
+++ tendenci/apps/forms_builder/forms/tasks.py
@@ -1,11 +1,26 @@
 import os
+import csv
+import zipfile
+from os.path import join
+from os import unlink
+from time import time
+from tempfile import NamedTemporaryFile
+
+from django.conf import settings
 from django.db.models import Avg, Max, Min, Count
 from django.db.models.fields.related import ManyToManyField, ForeignKey
 from django.contrib.contenttypes import generic
+from django.http import HttpResponse
+from django.utils.encoding import smart_str
+from django.template.defaultfilters import yesno
+from django.core.files.storage import default_storage
 from celery.task import Task
 from celery.registry import tasks
+
 from tendenci.core.exports.utils import full_model_to_dict, render_csv
 from tendenci.apps.forms_builder.forms.models import Form
+
+
 
 class FormsExportTask(Task):
     """Export Task for Celery
@@ -113,3 +128,72 @@ class FormsExportTask(Task):
         for i in range(0, max_pricings):
             fields = fields + ["pricing %s %s" % (i, f) for f in pricing_fields]
         return render_csv(file_name, fields, data_row_list)
+
+
+class FormEntriesExportTask(Task):
+
+    def run(self, form_instance, entries, include_files, **kwargs):
+
+        response = HttpResponse(mimetype='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=export_entries_%d.csv' % time()
+        headers = []
+        has_files = form_instance.has_files() and include_files
+
+        # if the object hase file store the csv elsewhere
+        # so that we can zip the files
+        if has_files:
+            temp_csv = NamedTemporaryFile(mode='w', delete=False)
+            temp_zip = NamedTemporaryFile(mode='wb', delete=False)
+            writer = csv.writer(temp_csv, delimiter=',')
+            zip = zipfile.ZipFile(temp_zip, 'w', compression=zipfile.ZIP_DEFLATED)
+        else:
+            writer = csv.writer(response, delimiter=',')
+
+        # get the header for headers for the csv
+        headers.append('submitted on')
+        for field in entries[0].fields.all():
+            headers.append(smart_str(field.field.label))
+        writer.writerow(headers)
+
+        # write out the values
+        for entry in entries:
+            values = []
+            values.append(entry.entry_time)
+            for field in entry.fields.all():
+                if has_files and field.field.field_type == 'FileField':
+                    archive_name = join('files',field.value)
+                    if hasattr(settings, 'USE_S3_STORAGE') and settings.USE_S3_STORAGE:
+                        file_path = field.value
+                        try:
+                            # TODO: for large files, we may need to copy down
+                            # the files before adding them to the zip file.
+                            zip.writestr(archive_name, default_storage.open(file_path).read())
+                        except IOError:
+                            pass
+                    else:
+                        file_path = join(settings.MEDIA_ROOT,field.value)
+                        zip.write(file_path, archive_name, zipfile.ZIP_DEFLATED)
+
+                if field.field.field_type == 'BooleanField':
+                    values.append(yesno(smart_str(field.value)))
+                else:
+                    values.append(smart_str(field.value))
+            writer.writerow(values)
+
+        # add the csv file to the zip, close it, and set the response
+        if has_files:
+            # add the csv file and close it all out
+            temp_csv.close()
+            zip.write(temp_csv.name, 'entries.csv', zipfile.ZIP_DEFLATED)
+            zip.close()
+            temp_zip.close()
+
+            # set the response for the zip files
+            response = HttpResponse(file(temp_zip.name).read(), content_type='application/zip')
+            response['Content-Disposition'] = 'attachment; filename=export_entries_%d.zip' % time()
+
+            # remove the temporary files
+            unlink(temp_zip.name)
+            unlink(temp_csv.name)
+
+        return response
