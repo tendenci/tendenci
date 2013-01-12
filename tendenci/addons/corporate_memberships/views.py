4a92c64f19fb6eca8ef513146aee01e90047fada
Jenny Qian <jqian@schipul.com>
2013-01-11 20:23:25 -0600

    Display join_dt and expiration_dt on corp view for editor.

--- tendenci/addons/corporate_memberships/views.py
+++ tendenci/addons/corporate_memberships/views.py
@@ -432,6 +432,14 @@ def corpmembership_view(request, id,
     app_fields = list(app_fields.order_by('order'))
 
     if can_edit:
+        app_field = CorpMembershipAppField(label='Join Date',
+                                            field_name='join_dt',
+                                            required=True)
+        app_fields.append(app_field)
+        app_field = CorpMembershipAppField(label='Expiration Date',
+                                            field_name='expiration_dt',
+                                            required=True)
+        app_fields.append(app_field)
         app_field = CorpMembershipAppField(label='Representatives',
                                     field_type='section_break',
                                     admin_only=False)
