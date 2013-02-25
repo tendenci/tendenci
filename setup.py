5a553e2e226d1068ae6539d62a93ac63a1cfe4b6
jmoswalt <jmoswalt@gmail.com>
2013-02-25 12:22:26 -0600

    Add new command to update the tendenci project files
    Includes non-local files in conf and deploy.py

--- setup.py
+++ setup.py
@@ -142,6 +142,7 @@ setup(
     entry_points="""
             [console_scripts]
             create-tendenci-project=tendenci.bin.create_tendenci_project:create_project
+            update-tendenci-project=tendenci.bin.update_tendenci_project:update_project
         """,
     include_package_data=True,
     dependency_links=[
