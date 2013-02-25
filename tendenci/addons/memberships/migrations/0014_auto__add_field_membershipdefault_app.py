4bed8b2b34a5d578e7715ff96ef1138813fe3720
Jenny Qian <jqian@schipul.com>
2013-02-25 14:47:10 -0600

    Resolve migration conflits

--- tendenci/addons/memberships/migrations/0014_auto__add_field_membershipdefault_app.py
+++ tendenci/addons/memberships/migrations/0014_auto__add_field_membershipdefault_app.py
@@ -34,7 +34,7 @@ class Migration(SchemaMigration):
         },
         'auth.user': {
             'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 2, 20, 11, 30, 26, 784459)'}),
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 2, 25, 14, 43, 52, 742041)'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
@@ -42,7 +42,7 @@ class Migration(SchemaMigration):
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 2, 20, 11, 30, 26, 784352)'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 2, 25, 14, 43, 52, 741921)'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
             'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
@@ -646,6 +646,16 @@ class Migration(SchemaMigration):
             'subject': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             'system_generated': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'update_dt': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        'memberships.noticedefaultlogrecord': {
+            'Meta': {'object_name': 'NoticeDefaultLogRecord'},
+            'action_taken': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'action_taken_dt': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'create_dt': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'guid': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'membership': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'default_log_records'", 'to': "orm['memberships.MembershipDefault']"}),
+            'notice_log': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'default_log_records'", 'to': "orm['memberships.NoticeLog']"})
         },
         'memberships.noticelog': {
             'Meta': {'object_name': 'NoticeLog'},
