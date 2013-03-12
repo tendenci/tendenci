add2492660bc970789eeff9748840d44c2350478
Jenny Qian <jqian@schipul.com>
2013-03-12 11:56:00 -0500

    Combine two migrations 0020 and 0021 to one.

--- tendenci/addons/memberships/migrations/0020_auto__add_field_membershipimportdata_error__add_field_membershipimport.py
+++ tendenci/addons/memberships/migrations/0020_auto__add_field_membershipimportdata_error__add_field_membershipimport.py
@@ -8,6 +8,9 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         
+        # Adding field 'MembershipImportData.error'
+        db.add_column('memberships_membershipimportdata', 'error', self.gf('django.db.models.fields.CharField')(default='', max_length=500), keep_default=False)
+
         # Adding field 'MembershipImport.recap_file'
         db.add_column('memberships_membershipimport', 'recap_file', self.gf('django.db.models.fields.files.FileField')(max_length=260, null=True), keep_default=False)
 
@@ -17,6 +20,9 @@ class Migration(SchemaMigration):
 
     def backwards(self, orm):
         
+        # Deleting field 'MembershipImportData.error'
+        db.delete_column('memberships_membershipimportdata', 'error')
+
         # Deleting field 'MembershipImport.recap_file'
         db.delete_column('memberships_membershipimport', 'recap_file')
 
@@ -40,7 +46,7 @@ class Migration(SchemaMigration):
         },
         'auth.user': {
             'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 3, 11, 16, 10, 52, 648380)'}),
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 3, 12, 11, 50, 8, 605050)'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
@@ -48,7 +54,7 @@ class Migration(SchemaMigration):
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 3, 11, 16, 10, 52, 648227)'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 3, 12, 11, 50, 8, 604943)'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
             'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
