0fbbcaa358b8f768a602ddd3637fe37c2f41b15e
Eloy Zuniga Jr <eloyz.email@gmail.com>
2013-03-27 11:54:27 -0500

    Removing spawn_username function [duplicate] inside of membership utilities.

--- tendenci/addons/memberships/utils.py
+++ tendenci/addons/memberships/utils.py
@@ -705,37 +705,6 @@ def get_notice_token_help_text(notice=None):
     return help_text
 
 
-def spawn_username(*args):
-    """
-    Join arguments to create username [string].
-    Find similiar usernames; auto-increment newest username.
-    Return new username [string].
-    """
-    if not args:
-        raise Exception('spawn_username() requires atleast 1 argument; 0 were given')
-
-    max_length = 8
-
-    un = ' '.join(args)             # concat args into one string
-    un = re.sub('\s+', '_', un)       # replace spaces w/ underscores
-    un = re.sub('[^\w.-]+', '', un)   # remove non-word-characters
-    un = un.strip('_.- ')           # strip funny-characters from sides
-    un = un[:max_length].lower()    # keep max length and lowercase username
-
-    others = []  # find similiar usernames
-    for u in User.objects.filter(username__startswith=un):
-        if u.username.replace(un, '0').isdigit():
-            others.append(int(u.username.replace(un, '0')))
-
-    if others and 0 in others:
-        # the appended digit will compromise the username length
-        # there would have to be more than 99,999 duplicate usernames
-        # to kill the database username max field length
-        un = '%s%s' % (un, str(max(others) + 1))
-
-    return un.lower()
-
-
 def get_user(**kwargs):
     """
     Returns first user that matches filters.
