6af9acdc16f80b3989a3951d8a7cd4ae3eb32020
Jenny Qian <jqian@schipul.com>
2013-04-18 13:54:36 -0500

    Update ordering for notice tokens in help_text.

--- tendenci/addons/memberships/utils.py
+++ tendenci/addons/memberships/utils.py
@@ -662,7 +662,7 @@ def get_notice_token_help_text(notice=None):
                                         display=True,
                                         ).exclude(
                                         field_name=''
-                                        ).order_by('order')
+                                        ).order_by('position')
             help_text += "<ul>"
             for field in fields:
                 help_text += '<li>{{ %s }} - (for %s)</li>' % (
