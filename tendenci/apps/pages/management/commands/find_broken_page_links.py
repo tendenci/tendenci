c816608f33c23a80488929146d1baaedef579193
Eloy Zuniga Jr <eloyz.email@gmail.com>
2013-05-31 11:50:04 -0500

    Broken page link finder now returns link instead of URL
    More updates to the broken page link finder.  Is capable of returning broken images and returns broken links as opposed to just the broken URL's.
    
    Some of the extra features added have been commented out for now.  Will be available after more testing, maybe in a separate management command.

--- tendenci/apps/pages/management/commands/find_broken_page_links.py
+++ tendenci/apps/pages/management/commands/find_broken_page_links.py
@@ -1,6 +1,11 @@
 import os
 import re
 import requests
+from urlparse import urlparse
+from requests.exceptions import (
+    MissingSchema, InvalidSchema, InvalidURL, ConnectionError)
+from BeautifulSoup import BeautifulSoup
+from requests.packages.urllib3.exceptions import LocationParseError
 from django.core.management.base import BaseCommand
 
 
@@ -9,30 +14,75 @@ class Command(BaseCommand):
     Finds broken anchor links on all pages
     and prints page slug along with list of broken links
     """
+    COMMON_EXCEPTIONS = (
+        MissingSchema,
+        InvalidSchema,
+        InvalidURL,
+        ConnectionError,
+        LocationParseError)
+
+    def get_broken_links(self, links):
+        """
+        Returns a list of broken links
+        """
+        broken_links = []
+        for link in links:
+
+            url = link.get('href')
+
+            try:
+                if url.startswith('/'):
+                    url = self.SITE_URL + url
+
+                r = requests.head(url, timeout=10)
+                if not r.ok:
+                    broken_links.append(link)
+
+            except self.COMMON_EXCEPTIONS:
+                pass
+
+        return broken_links
+
+    def get_broken_images(self, images):
+        """
+        Returns a list of broken links
+        """
+        broken_images = []
+        for image in images:
+
+            src = image.get('src')
+
+            try:
+                if src.startswith('/'):
+                    src = self.SITE_URL + src
+
+                r = requests.head(src, timeout=10)
+                if not r.ok:
+                    broken_images.append(image)
+
+            except self.COMMON_EXCEPTIONS:
+                pass
+
+        return broken_images
+
     def handle(self, *args, **options):
         from tendenci.apps.pages.models import Page
         from tendenci.core.site_settings.utils import get_setting
 
-        site_url = get_setting('site', 'global', 'siteurl')
+        self.SITE_URL = get_setting('site', 'global', 'siteurl')
 
         for page in Page.objects.all():
 
-            urls = re.findall(r'href=[\'"]?([^\'" >]+)', page.content)
+            soup = BeautifulSoup(page.content)
 
-            broken_urls = []
-            for url in urls:
+            links = soup.findAll('a')
+            # images = soup.findAll('img')
 
-                try:
-                    if url.startswith('/'):
-                        url = site_url + url
+            broken_links = self.get_broken_links(links)
+            # broken_images = self.get_broken_images(images)
 
-                    r = requests.head(url)
-                    if not r.ok:
-                        broken_urls.append(url)
+            # print page.slug,
+            # print 'images', '%s/%s' % (len(images), len(broken_images)),
+            # print 'links', '%s/%s' % (len(links), len(broken_links)), broken_links
 
-                except requests.exceptions.MissingSchema:
-                    pass
-                except requests.exceptions.InvalidSchema:
-                    pass
-
-            print page.slug, broken_urls
+            print page.slug, broken_links
