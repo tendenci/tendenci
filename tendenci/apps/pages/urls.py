066aa491cd1ec0a79a1a7e45029330b548def202
jmoswalt <jmoswalt@gmail.com>
2013-06-13 16:16:19 -0500

    #374: Customize URL Setting for all Modules
    Conflicts:
    	tendenci/addons/events/urls.py

--- tendenci/apps/pages/urls.py
+++ tendenci/apps/pages/urls.py
@@ -1,19 +1,24 @@
 from django.conf.urls.defaults import patterns, url
 from tendenci.apps.pages.feeds import LatestEntriesFeed
+from tendenci.core.site_settings.utils import get_setting
+
+urlpath = get_setting('module', 'pages', 'url')
+if not urlpath:
+    urlpath = "pages"
 
 urlpatterns = patterns('tendenci.apps.pages.views',
-    url(r'^$', 'index', name="pages"),
-    url(r'^search/$', 'search', name="page.search"),
-    url(r'^print-view/(?P<slug>[\w\-\/]+)/$', 'print_view', name="page.print_view"),
-    url(r'^export/$', 'export', name="page.export"),
-    url(r'^preview/$', 'preview', name="page.preview"),
-    url(r'^preview/(?P<id>\d+)/$', 'preview', name="page.preview"),
-    url(r'^add/$', 'add', name="page.add"),
-    url(r'^edit/(?P<id>\d+)/$', 'edit', name="page.edit"),
-    url(r'^versions/(?P<hash>[\w\-]+)/$', 'index', name="page.version"),
-    url(r'^inactive/(?P<id>\d+)/$', 'index', name="page.inactive"),
-    url(r'^edit/meta/(?P<id>\d+)/$', 'edit_meta', name="page.edit.meta"),
-    url(r'^delete/(?P<id>\d+)/$', 'delete', name="page.delete"),
-    url(r'^feed/$', LatestEntriesFeed(), name='page.feed'),
-    url(r'^header_image/(?P<id>\d+)/$', 'display_header_image', name="page.header_image"),
+    url(r'^%s/$' % urlpath, 'index', name="pages"),
+    url(r'^%s/search/$' % urlpath, 'search', name="page.search"),
+    url(r'^%s/print-view/(?P<slug>[\w\-\/]+)/$' % urlpath, 'print_view', name="page.print_view"),
+    url(r'^%s/export/$' % urlpath, 'export', name="page.export"),
+    url(r'^%s/preview/$' % urlpath, 'preview', name="page.preview"),
+    url(r'^%s/preview/(?P<id>\d+)/$' % urlpath, 'preview', name="page.preview"),
+    url(r'^%s/add/$' % urlpath, 'add', name="page.add"),
+    url(r'^%s/edit/(?P<id>\d+)/$' % urlpath, 'edit', name="page.edit"),
+    url(r'^%s/versions/(?P<hash>[\w\-]+)/$' % urlpath, 'index', name="page.version"),
+    url(r'^%s/inactive/(?P<id>\d+)/$' % urlpath, 'index', name="page.inactive"),
+    url(r'^%s/edit/meta/(?P<id>\d+)/$' % urlpath, 'edit_meta', name="page.edit.meta"),
+    url(r'^%s/delete/(?P<id>\d+)/$' % urlpath, 'delete', name="page.delete"),
+    url(r'^%s/feed/$' % urlpath, LatestEntriesFeed(), name='page.feed'),
+    url(r'^%s/header_image/(?P<id>\d+)/$' % urlpath, 'display_header_image', name="page.header_image"),
 )
