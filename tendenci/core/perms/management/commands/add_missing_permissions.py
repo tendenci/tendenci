dfec01da2c4afc3e32c7c25cfbe531ad7adf79bf
Eloy Zuniga Jr <eloyz.email@gmail.com>
2013-06-19 17:14:28 -0500

    "Verbosity" in command add_missing_permissions

--- tendenci/core/perms/management/commands/add_missing_permissions.py
+++ tendenci/core/perms/management/commands/add_missing_permissions.py
@@ -7,7 +7,9 @@ class Command(BaseCommand):
     """
     def handle(self, *args, **options):
         from django.contrib.auth.management import create_permissions
-        from django.db.models import get_apps
+        from django.db.models import get_apps, get_models
+
+        verbosity = int(options['verbosity'])
 
         for app in get_apps():
-           create_permissions(app, None, 2)
+            create_permissions(app, None, verbosity)
