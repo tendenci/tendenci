cc9622b597b5e3f31f15c23a5f11be9898faafc1
kgayle <kgayle@schipul.com>
2013-06-25 12:37:29 -0500

    changed model from Membership to MembershipDefault in management command

--- tendenci/addons/memberships/management/commands/memberships_set_status_detail.py
+++ tendenci/addons/memberships/management/commands/memberships_set_status_detail.py
@@ -7,19 +7,19 @@ class Command(BaseCommand):
     """
     def handle(self, *args, **options):
         from datetime import datetime
-        from tendenci.addons.memberships.models import Membership
+        from tendenci.addons.memberships.models import MembershipDefault
         verbosity = int(options['verbosity'])
 
         # memberships will remain "expired" because of their dt
-        expired = Membership.objects.filter(expire_dt__lt=datetime.now(), status_detail='expired'
+        expired = MembershipDefault.objects.filter(expire_dt__lt=datetime.now(), status_detail='expired'
             ).update(status_detail='active')
 
         # memberships will be set to inactive; setting them to 'expired' was a workaround
-        inactive = Membership.objects.filter(expire_dt__gte=datetime.now(), status_detail='expired'
+        inactive = MembershipDefault.objects.filter(expire_dt__gte=datetime.now(), status_detail='expired'
             ).update(status_detail='inactive')
         
         # memberships will be set to active because of the expire_dt
-        active = Membership.objects.filter(expire_dt__gt=datetime.now(), status_detail='expired'
+        active = MembershipDefault.objects.filter(expire_dt__gt=datetime.now(), status_detail='expired'
         ).update(status_detail='active')
 
 
