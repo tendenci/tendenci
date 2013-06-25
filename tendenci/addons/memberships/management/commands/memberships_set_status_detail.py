4ef751f4437d2537ae1209b278422e2c528cab2f
kgayle <kgayle@schipul.com>
2013-06-25 13:50:22 -0400

    updating memberships_set_status_detail.py
    removed setting memberships to inactive, updated the method to set expired membership to status_detail expired.

--- tendenci/addons/memberships/management/commands/memberships_set_status_detail.py
+++ tendenci/addons/memberships/management/commands/memberships_set_status_detail.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     """
-    Resets membership to expired or inactive.
+    Resets membership to expired or active.
     """
     def handle(self, *args, **options):
         from datetime import datetime
@@ -11,12 +11,9 @@ class Command(BaseCommand):
         verbosity = int(options['verbosity'])
 
         # memberships will remain "expired" because of their dt
-        expired = MembershipDefault.objects.filter(expire_dt__lt=datetime.now(), status_detail='expired'
-            ).update(status_detail='active')
+        expired = MembershipDefault.objects.filter(expire_dt__lt=datetime.now(), status_detail='active'
+            ).update(status_detail='expired')
 
-        # memberships will be set to inactive; setting them to 'expired' was a workaround
-        inactive = MembershipDefault.objects.filter(expire_dt__gte=datetime.now(), status_detail='expired'
-            ).update(status_detail='inactive')
         
         # memberships will be set to active because of the expire_dt
         active = MembershipDefault.objects.filter(expire_dt__gt=datetime.now(), status_detail='expired'
@@ -24,4 +21,4 @@ class Command(BaseCommand):
 
 
         if verbosity:
-            print 'Success!', '%s set to expired and %s set to inactive and %s set to active.' % (expired, inactive, active)
+            print 'Success!', '%s set to expired and %s set to active.' % (expired, active)
