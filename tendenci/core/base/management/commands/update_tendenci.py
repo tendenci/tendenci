c16ac5497c06d1303f2338fb11859ef9dfeb9781
Eloy Zuniga Jr <eloyz.email@gmail.com>
2013-09-11 13:00:42 -0500

    #372: fixes to tendenci update

--- tendenci/core/base/management/commands/update_tendenci.py
+++ tendenci/core/base/management/commands/update_tendenci.py
@@ -8,8 +8,11 @@ class Command(BaseCommand):
     Update tendenci via pip and restarts the server
     """
     def handle(self, *args, **kwargs):
-        print "Updating tendenci"
+        print "Updating tendenci package"
         os.system('pip install tendenci --upgrade')
+
+        print "Updating tendenci site"
+        os.system('python deploy.py')
 
         print "Restarting Server"
         # This depends on the server used, please update accordingly
