ad9a141c83a2c92a2195400690cae028dc0d277e
jmoswalt <jmoswalt@gmail.com>
2013-09-12 09:33:44 -0500

    Version 5.1.168 - Wrapping base 0001 migration in try in case table already exists

--- tendenci/core/base/migrations/0001_initial.py
+++ tendenci/core/base/migrations/0001_initial.py
@@ -3,17 +3,21 @@ import datetime
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
+from django.db.utils import DatabaseError
 
 
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding model 'UpdateTracker'
-        db.create_table('base_updatetracker', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('is_updating', self.gf('django.db.models.fields.BooleanField')(default=False)),
-        ))
-        db.send_create_signal('base', ['UpdateTracker'])
+        try:
+            db.create_table('base_updatetracker', (
+                ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+                ('is_updating', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ))
+            db.send_create_signal('base', ['UpdateTracker'])
+        except DatabaseError:
+            print "Database table already exists. Marking migration as complete."
 
 
     def backwards(self, orm):
