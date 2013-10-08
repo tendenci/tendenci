93017bfed28ac7b8f94fdf3082897b860c512e66
Eloy Zuniga Jr <eloyz.email@gmail.com>
2013-10-08 13:13:46 -0500

    Removing "import include" from invoices/urls.py

--- tendenci/apps/invoices/urls.py
+++ tendenci/apps/invoices/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url, include
+from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('tendenci.apps.invoices.views',  
     url(r'^$',                                'search', name="invoices"),
