8df02b8e5f3c31b713cd1a3c9e09af793d59c033
Jenny Qian <jqian@schipul.com>
2013-11-22 14:47:25 -0600

    added depends_on to the memberships and files migrations - without those dependencies, the migration was broken on new dbs

--- tendenci/addons/memberships/migrations/0001_initial.py
+++ tendenci/addons/memberships/migrations/0001_initial.py
@@ -5,6 +5,10 @@ from south.v2 import SchemaMigration
 from django.db import models
 
 class Migration(SchemaMigration):
+    depends_on = (
+        ('invoices', '0001_initial'),
+        ('payments', '0001_initial'),
+    )
 
     def forwards(self, orm):
         
