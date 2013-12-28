527070eb79da3e4cc9c462548c82f637773a14f1
Jenny Qian <jqian@schipul.com>
2013-12-28 13:16:35 -0600

    update deploy.py to prevent the commands from running out of the order
    unlike os.system, subprocess.check_call will wait for command to complete

--- tendenci/project_template/deploy.py
+++ tendenci/project_template/deploy.py
@@ -1,9 +1,11 @@
-import os
+import subprocess
 
-os.system('python manage.py syncdb --noinput')
-os.system('python manage.py migrate --noinput')
-os.system('python manage.py update_settings')
-os.system('python manage.py copy_themes_to_s3')
-os.system('python manage.py clear_theme_cache')
-os.system('python manage.py collectstatic --noinput')
-os.system('python manage.py s3_replace_static_urls')
+subprocess.check_call(['python', 'manage.py',  'syncdb', '--noinput'])
+subprocess.check_call(['python', 'manage.py',  'migrate', '--noinput'])
+subprocess.check_call(['python', 'manage.py',  'update_settings'])
+subprocess.check_call(['python', 'manage.py',  'copy_themes_to_s3'])
+subprocess.check_call(['python', 'manage.py',  'clear_theme_cache'])
+subprocess.check_call(['python', 'manage.py',  'collectstatic', '--noinput'])
+subprocess.check_call(['python', 'manage.py',  's3_replace_static_urls'])
+subprocess.check_call(['python', 'manage.py',  'populate_default_entity'])
+subprocess.check_call(['python', 'manage.py',  'populate_entity_and_auth_group_columns'])
