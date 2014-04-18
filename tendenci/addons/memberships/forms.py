464415aa2985351ea73f579e3037a3acfc288bf0
Jayjay <jayjay@bixly.com>
2014-04-19 03:59:55 +0800

    #890 - Fix in Memberships DemographicsForm initialization

--- tendenci/addons/memberships/forms.py
+++ tendenci/addons/memberships/forms.py
@@ -842,7 +842,7 @@ class DemographicsForm(forms.ModelForm):
                             self.fields[field_name].initial = file_instance.file
 
                     else:
-                        self.fields[field_name].initial = getattr(demographics, field_name)
+                        self.fields[field_name].initial = getattr(self.demographics, field_name)
 
     def save(self, commit=True, *args, **kwargs):
         pks ={}
@@ -2283,7 +2283,7 @@ class MembershipDefaultForm(TendenciBaseForm):
                         self.fields[field_name].initial = file_instance.file
 
                 else:
-                    self.fields[field_name].initial = getattr(self.instance.demographics, field_name)
+                    self.fields[field_name].initial = getattr(demographics, field_name)
         # end demographic
 
     def clean(self):
