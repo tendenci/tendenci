a6055352e03e5320c45b53081ffea0f0ee6496da
Jayjay <jayjay@bixly.com>
2014-05-20 04:50:30 +0800

    #969 - Color-coded editor for CSS in Theme Editor

--- tendenci/apps/theme_editor/views.py
+++ tendenci/apps/theme_editor/views.py
@@ -73,6 +73,11 @@ def edit_file(request, form_class=FileForm, template_name="theme_editor/index.ht
     # get the current file name
     current_file = os.path.basename(default_file)
 
+    # get file ext
+    name = current_file.split('/')[-1]
+    ext = name.split('.')[-1]
+    stylesheets = ['css', 'less']
+
     # get the present working directory
     # and make sure they cannot list root
     pwd = os.path.dirname(default_file)
@@ -146,6 +151,8 @@ def edit_file(request, form_class=FileForm, template_name="theme_editor/index.ht
         'is_file': is_file,
         'is_dir': is_dir,
         'all_files_folders': all_files_folders,
+        'ext' : ext,
+        'stylesheets' : stylesheets
     }, context_instance=RequestContext(request))
 
 
