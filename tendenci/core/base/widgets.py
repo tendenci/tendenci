642b588be817cba4d3362ea7cb06668583fca52e
Max <maxc@bixly.com>
2014-08-04 16:28:19 +0800

    #1162 - remove placeholder of EmailVerification field

--- tendenci/core/base/widgets.py
+++ tendenci/core/base/widgets.py
@@ -66,12 +66,10 @@ class EmailVerificationWidget(MultiWidget):
         email0_attrs['class'] = email_class_0
         email0_attrs['maxlength'] = 75
         email0_attrs['title'] = _('Email')
-        email0_attrs['placeholder'] = _('Email')
         email1_attrs = attrs.copy()
         email1_attrs['class'] = email_class_1
         email1_attrs['maxlength'] = 75
         email1_attrs['title'] = _('Confirm Email')
-        email1_attrs['placeholder'] = _('Confirm Email')
 
         widgets = (TextInput(attrs=email0_attrs),
                    TextInput(attrs=email1_attrs))
