8eb2c0ddc9175305a595d4ff87172bdd4ecec3ba
Jenny Qian <jqian@tendenci.com>
2014-08-14 09:22:16 -0500

    set default USE_I18N to True per Ed

--- tendenci/settings.py
+++ tendenci/settings.py
@@ -47,7 +47,7 @@ SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-USE_I18N = False
+USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
