b99e5c9716b70fea99043dd331292e222320a7a3
Max <maxc@bixly.com>
2014-11-15 10:30:24 +0800

    added app navigation for contacts'

--- tendenci/apps/contacts/templatetags/contact_tags.py
+++ tendenci/apps/contacts/templatetags/contact_tags.py
@@ -2,6 +2,7 @@ from django.template import Library
 
 register = Library()
 
+
 @register.inclusion_tag("contacts/options.html", takes_context=True)
 def contact_options(context, user, contact):
     context.update({
@@ -9,6 +10,7 @@ def contact_options(context, user, contact):
         "user": user
     })
     return context
+
 
 @register.inclusion_tag("contacts/nav.html", takes_context=True)
 def contact_nav(context, user, contact=None):
@@ -18,6 +20,16 @@ def contact_nav(context, user, contact=None):
     })
     return context
 
+
 @register.inclusion_tag("contacts/search-form.html", takes_context=True)
 def contact_search(context):
+    return context
+
+
+@register.inclusion_tag("contacts/top_nav_items.html", takes_context=True)
+def contact_current_app(context, user, contact=None):
+    context.update({
+        "app_object": contact,
+        "user": user
+    })
     return context
