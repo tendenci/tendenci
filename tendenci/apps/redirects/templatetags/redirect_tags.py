a193e849f25a1f786caeb64295e69c76974dfceb
Max <maxc@bixly.com>
2014-11-17 09:37:01 +0800

    added app navigation for redirects module

--- tendenci/apps/redirects/templatetags/redirect_tags.py
+++ tendenci/apps/redirects/templatetags/redirect_tags.py
@@ -2,6 +2,7 @@ from django.template import Library
 
 register = Library()
 
+
 @register.inclusion_tag("redirects/options.html", takes_context=True)
 def redirect_options(context, user, redirect):
     context.update({
@@ -10,6 +11,7 @@ def redirect_options(context, user, redirect):
     })
     return context
 
+
 @register.inclusion_tag("redirects/nav.html", takes_context=True)
 def redirect_nav(context, user, redirect=None):
     context.update({
@@ -17,6 +19,16 @@ def redirect_nav(context, user, redirect=None):
         "user": user
     })
     return context
+
+
+@register.inclusion_tag("redirects/top_nav_items.html", takes_context=True)
+def redirect_current_app(context, user, redirect=None):
+    context.update({
+        "app_object" : redirect,
+        "user": user
+    })
+    return context
+
 
 @register.inclusion_tag("redirects/search-form.html", takes_context=True)
 def redirect_search(context):
