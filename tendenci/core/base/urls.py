8d45309ed36a63f3b5846b515d12f4f1e8815ad6
Max <maxc@bixly.com>
2015-01-05 16:12:18 +0800

    #1301 - Hide view as link and replaced with settings list; Added full app list link on admin and page for full app list in alphabetical order; added fix to upload avatar issues

--- tendenci/core/base/urls.py
+++ tendenci/core/base/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls.defaults import patterns, url
+from django.contrib.auth.decorators import login_required
+
+from .views import AppsListView
 
 urlpatterns = patterns('tendenci.core.base.views',
     url(r'^image-preview/(?P<app_label>\w+)/(?P<model>\w+)/(?P<id>\d+)/(?P<size>(\d+|\d+x\d+))/$', 'image_preview', name="image_preview"),
@@ -10,7 +13,7 @@ urlpatterns = patterns('tendenci.core.base.views',
     url(r'^timeout-test/$', 'timeout_test', name="timeout_test"),
 
     url(r'^checklist/$', 'checklist', name="completion_checklist"),
-    
+
     url(r'^setlang/$', 'set_language', name="base.set_language"),
 
     url(r'^update-tendenci/$', 'update_tendenci', name="update_tendenci"),
@@ -20,4 +23,6 @@ urlpatterns = patterns('tendenci.core.base.views',
     url(r'addon/upload/preview/(?P<sid>\d+)$', 'addon_upload_preview', name="addon.upload.preview"),
     url(r'addon/upload/status/(?P<sid>\d+)$', 'addon_upload_process', name="addon.upload.process"),
     url(r'addon/upload/check/(?P<sid>\d+)$', 'addon_upload_check', name="addon.upload.check"),
+
+    url(r'apps-list/$', login_required(AppsListView.as_view()), name="apps.list"),
 )
