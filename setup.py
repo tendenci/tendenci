4f1b7e64bc749407df624df0035c0b61a8e4be56
Jenny Qian <jqian@tendenci.com>
2015-01-16 11:10:35 -0600

    Update django version from 1.6 to 1.6.10

--- setup.py
+++ setup.py
@@ -147,7 +147,7 @@ setup(
             update-tendenci-project=tendenci.bin.update_tendenci_project:update_project
         """,
     install_requires=[
-        "Django==1.6",
+        "Django==1.6.10",
         "pisa",
         "Reportlab==2.5",
         "html5lib",
