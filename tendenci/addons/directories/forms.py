4f2ad298555f1a392461eb09db76b91183a53223
Jenny Qian <jqian@tendenci.com>
2015-02-10 17:49:45 -0600

    trim extra white spaces from directory headline

--- tendenci/addons/directories/forms.py
+++ tendenci/addons/directories/forms.py
@@ -249,6 +249,12 @@ class DirectoryForm(TendenciBaseForm):
                 logo = None
 
         return logo
+    
+    def clean_headline(self):
+        """
+        remove extra leading and trailing white spaces
+        """
+        return self.cleaned_data.get('headline', '').strip()
 
     def __init__(self, *args, **kwargs):
         super(DirectoryForm, self).__init__(*args, **kwargs)
