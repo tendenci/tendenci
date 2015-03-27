b026dfd4e9b22f9e1511b86948ecef9c65897533
Jenny Qian <jqian@tendenci.com>
2015-03-27 10:28:54 -0500

    move save_contribution signals to models to avoid ImportError:  ImportError tendenci.apps.contributions: cannot import name create_salesforce_contact

--- tendenci/apps/contributions/models.py
+++ tendenci/apps/contributions/models.py
@@ -2,9 +2,14 @@ import uuid
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import AnonymousUser
+from django.db.models.signals import post_save
 
 from tendenci.apps.contributions.managers import ContributionManager
 from tendenci.apps.perms.models import TendenciBaseModel
+from tendenci.apps.pages.models import Page
+from tendenci.apps.stories.models import Story
+from tendenci.apps.files.models import File
 
 class Contribution(TendenciBaseModel):
     guid = models.CharField(max_length=40)
@@ -34,3 +39,46 @@ class Contribution(TendenciBaseModel):
 
     def __unicode__(self):
         return self.title
+    
+    
+def save_contribution(sender, **kwargs):
+    instance = kwargs['instance']
+
+    # TODO: Possibly allow anonymous contributions
+    # to be added to the system
+    if isinstance(instance.owner, AnonymousUser):
+        return None
+    if isinstance(instance.creator, AnonymousUser):
+        return None
+    if not instance.owner or not instance.creator:
+        return None
+
+    content_type = ContentType.objects.get_for_model(instance)
+
+    try:  # record exists
+        contrib = Contribution.objects.get(
+            content_type=content_type,
+            object_id=instance.pk
+            )
+    except:  # create record
+        contrib = Contribution()
+        contrib.content_type = content_type
+        contrib.object_id = instance.id
+        contrib.creator = instance.owner
+
+        title_attrs = (
+            'title',
+            'headline',
+            'name',
+        )
+        for attr in title_attrs:
+            if hasattr(instance, attr):
+                contrib.title = getattr(instance, attr, '')
+                break
+
+        contrib.owner = instance.owner
+        contrib.save()
+
+post_save.connect(save_contribution, sender=Page, weak=False)
+post_save.connect(save_contribution, sender=Story, weak=False)
+post_save.connect(save_contribution, sender=File, weak=False)
