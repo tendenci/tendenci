42fbd5f913725526dd755f49c0b1ebce13c86dfb
Jayjay <jayjay@bixly.com>
2015-03-30 19:04:12 +0800

    All views now have a link to data export. Started data export view

--- tendenci/apps/explorer_extensions/views.py
+++ tendenci/apps/explorer_extensions/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import get_object_or_404, render_to_response, render, redirect
 from django.template import RequestContext
+from django.db.models import Q
 
 from explorer import app_settings
 from explorer.views import change_permission
+from tendenci.apps.explorer_extensions.models import DatabaseDumpFile
 
 
 def get_app_permissions(request):
@@ -13,5 +15,14 @@ def get_app_permissions(request):
 def export_page(request):
     ctx = {}
     ctx.update(get_app_permissions(request))
+
+    # get all active DB Dump Files
+    # if current existing DB Dump Files are less than the limit, enable form submission
+    db_objs = DatabaseDumpFile.objects.filter(~Q(status='expired'))
+
+    ctx['objects'] = db_objs
+    if len(db_objs) < 3:
+        ctx['enable_form'] = True
+
     return render_to_response("explorer/export_page.html", ctx,
                                 context_instance=RequestContext(request))
