1fdaff4120cabfcf0e73c733bf2652e021da4f96
Max <maxc@bixly.com>
2015-04-09 10:49:11 +0800

    #1473 WIP initial changes for entities module fix

--- tendenci/apps/entities/models.py
+++ tendenci/apps/entities/models.py
@@ -47,6 +47,10 @@ class Entity(models.Model):
         verbose_name_plural = _("entities")
         ordering = ("entity_name",)
 
+    @models.permalink
+    def get_absolute_url(self):
+        return ("entity", [self.pk])
+
     def __unicode__(self):
         return self.entity_name
 
