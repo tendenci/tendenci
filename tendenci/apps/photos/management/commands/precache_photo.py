6bb8e42dccd9768f852940cddfd6037970311ac0
Jenny Qian <jqian@tendenci.com>
2015-06-17 12:14:19 -0500

    add photo_id argument to precache_photo

--- tendenci/apps/photos/management/commands/precache_photo.py
+++ tendenci/apps/photos/management/commands/precache_photo.py
@@ -3,13 +3,17 @@ from django.core.management.base import BaseCommand, CommandError
 class Command(BaseCommand):
     help = ('Caches the photo for the size on the batch edit page and the photoset view page.')
 
-    def handle(self, *args, **options):
+    def add_arguments(self, parser):
+        parser.add_argument('photo_id', type=int)
+        
+
+    def handle(self, photo_id, **options):
         from tendenci.apps.photos.utils.caching import cache_photo_size
 
         cache_kwargs_list = []
-        cache_kwargs_list.append({"id": args[0], "size": "422x700", "constrain": True})
-        cache_kwargs_list.append({"id": args[0], "size": "102x78", "crop": True})
-        cache_kwargs_list.append({"id": args[0], "size": "640x640", "constrain": True})
+        cache_kwargs_list.append({"id": photo_id, "size": "422x700", "constrain": True})
+        cache_kwargs_list.append({"id": photo_id, "size": "102x78", "crop": True})
+        cache_kwargs_list.append({"id": photo_id, "size": "640x640", "constrain": True})
 
         for cache_kwargs in cache_kwargs_list:
             cache_photo_size(**cache_kwargs)
