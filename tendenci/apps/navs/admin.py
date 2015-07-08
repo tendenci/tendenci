be6a6a7ea1e0f198484640fc5a2263707ddd2f0b
Jenny Qian <jqian@tendenci.com>
2015-07-08 17:02:44 -0500

    allow queryset to be cached in choices for foreignkey page

--- tendenci/apps/navs/admin.py
+++ tendenci/apps/navs/admin.py
@@ -15,6 +15,12 @@ class ItemAdmin(admin.TabularInline):
     ordering = ("position",)
     template = 'admin/navs/edit_inline/tabular.html'
 
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        formfield = super(ItemAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+        if db_field.name == 'page':
+            formfield.choices = formfield.choices
+        return formfield
+
 
 class NavAdmin(TendenciBaseModelAdmin):
     inlines = (ItemAdmin,)
