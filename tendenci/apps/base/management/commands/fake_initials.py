704d088501cbd6856630f9d9f429850389d6519d
Jenny Qian <jqian@tendenci.com>
2015-08-11 18:26:31 -0500

    command fake_initials for migrating db from t6 to t7

---
+++ tendenci/apps/base/management/commands/fake_initials.py
@@ -0,0 +1,103 @@
+import traceback
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+
+class Command(BaseCommand):
+    """
+    This is the first command you should run when migrating db from t6 to t7.
+    It fakes all initials.
+    The --fake-initial option of the migrate command doesn't work well, therefore,
+    it cannot replace this command.
+    """
+    def handle(self, *args, **options):
+        try:
+            call_command('migrate', 'contenttypes', '0001', '--fake')
+        except:
+            pass
+        call_command('migrate', 'contenttypes')
+        call_command('migrate', 'auth', '0001', '--fake')
+        call_command('migrate', 'auth')
+
+        apps = ('admin',
+                'user_groups',
+                'entities',
+                'accountings',
+                'announcements',
+                'articles',
+                'avatar',
+                'base',
+                'boxes',
+                'campaign_monitor',
+                'captcha',
+                'careers',
+                'case_studies',
+                'categories',
+                'committees',
+                'contacts',
+                'contributions',
+                'corporate_memberships',
+                'dashboard',
+                'directories',
+                'discounts',
+                'donations',
+                'educations',
+                'email_blocks',
+                'emails',
+                'event_logs',
+                'events',
+                'explorer',
+                'explorer_extensions',
+                'exports',
+                'files',
+                'forms',
+                'handler404',
+                'help_files',
+                'ics',
+                'imports',
+                'industries',
+                'invoices',
+                'jobs',
+                'locations',
+                'make_payments',
+                'memberships',
+                'meta',
+                'metrics',
+                'navs',
+                'news',
+                'newsletters',
+                'notifications',
+                'pages',
+                'payments',
+                'perms',
+                'photos',
+                'profiles',
+                'recurring_payments',
+                'redirects',
+                'regions',
+                'registration',
+                'reports',
+                'resumes',
+                'robots',
+                'search',
+                'sessions',
+                'site_settings',
+                'sites',
+                'social_auth',
+                'speakers',
+                'staff',
+                'stories',
+                'studygroups',
+                'tagging',
+                'tendenci_guide',
+                'testimonials',
+                'theme_editor',
+                'versions',
+                'videos',
+                'wp_exporter',
+                'wp_importer',
+                )
+        for a in apps:
+            try:
+                call_command('migrate', a, '0001', '--fake')
+            except:
+                print traceback.format_exc()
