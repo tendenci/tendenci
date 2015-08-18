13d196d63c855ead7f82eb4a3993cd3879dcc07a
Ed Schipul <eschipul@tendenci.com>
2015-08-18 18:12:45 -0500

    flushing out redirect admin
    redirect admin page start

--- tendenci/apps/redirects/admin.py
+++ tendenci/apps/redirects/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
+from django.template.defaultfilters import striptags, truncatewords
+from django.utils.translation import ugettext_lazy as _
 
 from tendenci.apps.perms.admin import TendenciBaseModelAdmin
 from tendenci.apps.redirects.models import Redirect
 from tendenci.apps.redirects.forms import RedirectForm
 
 class RedirectAdmin(TendenciBaseModelAdmin):
-    list_display = [ ]
+    list_display = ['from_url','to_url', 'status']
+    list_filter = ['from_url', 'status', ]
+    search_fields = ['from_url', 'to_url', 'status']
     form = RedirectForm
     ordering = ['-update_dt']
 
