a318324384d2c6f6523c02c7e1ab4372c69df774
Jenny Qian <jqian@tendenci.com>
2015-11-18 10:50:43 -0600

    add poll_question to the topic admin form

--- tendenci/apps/forums/admin.py
+++ tendenci/apps/forums/admin.py
@@ -83,7 +83,7 @@ class TopicAdmin(admin.ModelAdmin):
     search_fields = ['name']
     fieldsets = (
         (None, {
-                'fields': ('forum', 'name', 'user', ('created', 'updated'), 'poll_type',)
+                'fields': ('forum', 'name', 'user', ('created', 'updated'), 'poll_type', 'poll_question',)
                 }
          ),
         (_('Additional options'), {
