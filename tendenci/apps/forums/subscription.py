e8fde82a4d681887e50f5b33bff6156684131394
Jenny Qian <jqian@tendenci.com>
2016-02-02 16:51:00 -0600

    notify forum moderators when a new topic is created

--- tendenci/apps/forums/subscription.py
+++ tendenci/apps/forums/subscription.py
@@ -20,48 +20,59 @@ else:
     from django.core.mail import send_mass_mail
 
 
+def get_email_message(user, **kwargs):
+    try:
+        validate_email(user.email)
+    except:
+        # Invalid email
+        return
+
+    if user.email == '%s@example.com' % getattr(user, compat.get_username_field()):
+        return
+
+    lang = util.get_pybb_profile(user).language or settings.LANGUAGE_CODE
+    translation.activate(lang)
+
+    message = render_to_string('pybb/mail_templates/subscription_email_body.html',
+                               kwargs)
+    return message
+
 def notify_topic_subscribers(post):
     topic = post.topic
-    if post != topic.head:
-        old_lang = translation.get_language()
+    old_lang = translation.get_language()
 
-        # Define constants for templates rendering
-        delete_url = reverse('pybb:delete_subscription', args=[post.topic.id])
-        current_site = Site.objects.get_current()
-        site_url = get_setting('site', 'global', 'siteurl')
-        from_email = settings.DEFAULT_FROM_EMAIL
+    # Define constants for templates rendering
+    context_vars = {'delete_url': reverse('pybb:delete_subscription', args=[post.topic.id]),
+                    'site_url': get_setting('site', 'global', 'siteurl'),
+                    'is_new_topic': post == topic.head,
+                    'post': post,
+                    'post_url': reverse('pybb:post', args=[post.id]),
+                    'forum_url': reverse('pybb:forum', args=[topic.forum.id]),
+                    }
+    from_email = settings.DEFAULT_FROM_EMAIL
 
-        subject = render_to_string('pybb/mail_templates/subscription_email_subject.html',
-                                   {'site': current_site,
-                                    'site_url': site_url,
-                                    'post': post})
-        # Email subject *must not* contain newlines
-        subject = ''.join(subject.splitlines())
-
-        mails = tuple()
-        for user in topic.subscribers.exclude(pk=post.user.pk):
-            try:
-                validate_email(user.email)
-            except:
-                # Invalid email
-                continue
-
-            if user.email == '%s@example.com' % getattr(user, compat.get_username_field()):
-                continue
-
-            lang = util.get_pybb_profile(user).language or settings.LANGUAGE_CODE
-            translation.activate(lang)
-
-            message = render_to_string('pybb/mail_templates/subscription_email_body.html',
-                                       {'site': current_site,
-                                        'site_url': site_url,
-                                        'post': post,
-                                        'delete_url': delete_url,
-                                        'user': user})
+    mails = tuple()
+    if post == topic.head:
+        subject_template = 'pybb/mail_templates/moderator_email_subject.html'
+        users = topic.forum.moderators.all()
+    else:
+        subject_template = 'pybb/mail_templates/subscription_email_subject.html'
+        users = topic.subscribers.exclude(pk=post.user.pk)
+        
+    subject = render_to_string(subject_template,
+                                   {'site_url': context_vars['site_url'],
+                                    'post': post,
+                                    'topic': topic})
+    # Email subject *must not* contain newlines
+    subject = ''.join(subject.splitlines())
+    
+    for user in users:
+        message = get_email_message(user, **context_vars)
+        if message:
             mails += ((subject, message, from_email, [user.email]),)
 
-        # Send mails
-        send_mass_mail(mails, fail_silently=True)
+    # Send mails
+    send_mass_mail(mails, fail_silently=True)
 
-        # Reactivate previous language
-        translation.activate(old_lang)
+    # Reactivate previous language
+    translation.activate(old_lang)
