5507c65114660dbeb570fe7f388d70eba46b9746
Jenny Qian <jqian@tendenci.com>
2016-03-08 15:03:05 -0600

    update country fields from 50 to 255 chars for invoices

---
+++ tendenci/apps/invoices/migrations/0002_auto_20160308_1501.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('invoices', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='invoice',
+            name='bill_to_country',
+            field=models.CharField(max_length=255, null=True, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='invoice',
+            name='ship_to_country',
+            field=models.CharField(max_length=255, blank=True),
+        ),
+    ]
