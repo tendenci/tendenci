9d6b72045ced263df208e781e7a906be666bdbf3
Jenny Qian <jqian@tendenci.com>
2016-06-13 10:43:30 -0500

    add directory pricing to admin backend

--- tendenci/apps/directories/admin.py
+++ tendenci/apps/directories/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from tendenci.apps.directories.models import Directory
+from tendenci.apps.directories.models import DirectoryPricing
+from tendenci.apps.directories.forms import DirectoryPricingForm
 
 
 class DirectoryAdmin(admin.ModelAdmin):
     list_display = ['headline', 'create_dt']
 
 # admin.site.register(Directory, DirectoryAdmin)
+
+class DirectoryPricingAdmin(admin.ModelAdmin):
+    list_display = ['duration', 'regular_price', 'premium_price',
+                    'regular_price_member', 'premium_price_member', 'status']
+    fieldsets = (
+        (None, {'fields': ('duration', 'regular_price', 'premium_price',
+                           'regular_price_member', 'premium_price_member', 'status',)}),
+    )
+    form = DirectoryPricingForm
+
+admin.site.register(DirectoryPricing, DirectoryPricingAdmin)
