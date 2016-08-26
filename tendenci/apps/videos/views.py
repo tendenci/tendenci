f2d4ab18e87b52684996a6aab5bd0798d56a7c26
Jenny Qian <jqian@tendenci.com>
2016-08-26 12:33:45 -0500

    fix 'Q' not defined error on video search

--- tendenci/apps/videos/views.py
+++ tendenci/apps/videos/views.py
@@ -1,5 +1,6 @@
 from django.template.context import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
+from django.db.models import Q
 
 from tendenci.apps.base.http import Http403
 from tendenci.apps.site_settings.utils import get_setting
