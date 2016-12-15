b13a7055fcf1c359bd8ddc68168ba153287c9d3f
Jenny Qian <jqian@tendenci.com>
2016-12-15 11:15:23 -0600

    resolve migration conflict after merged

--- tendenci/apps/memberships/migrations/0007_membershipdefault_auto_renew.py
+++ tendenci/apps/memberships/migrations/0007_membershipdefault_auto_renew.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('memberships', '0004_auto_20160520_1740'),
+        ('memberships', '0006_membershipset_donation_amount'),
     ]
 
     operations = [
