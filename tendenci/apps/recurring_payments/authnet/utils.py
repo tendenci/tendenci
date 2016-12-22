302ebb9196bc34ce2a5917cfad25ea89974dd4f1
Jenny Qian <jqian@tendenci.com>
2016-12-22 16:26:55 -0600

    make card code required

--- tendenci/apps/recurring_payments/authnet/utils.py
+++ tendenci/apps/recurring_payments/authnet/utils.py
@@ -139,9 +139,12 @@ def get_token(rp, CIMCustomerProfile, CIMHostedProfilePage, iframe_communicator_
             return token, gateway_error
 
     hosted_profile_page = CIMHostedProfilePage(rp.customer_profile_id)
+    hosted_profile_validation_mode = 'liveMode'
 
     d = {'hosted_profile_settings':
          {'hosted_profile_heading_bg_color': '#e0e0e0',     # the bg color of sections can be customized
+          'hosted_profile_card_code_required': 'true',
+          'hosted_profile_validation_mode': hosted_profile_validation_mode,
          'hosted_profile_iFrame_communicator_url': '%s' % iframe_communicator_url}}
     success, response_d = hosted_profile_page.get(**d)
     #print success, response_d
