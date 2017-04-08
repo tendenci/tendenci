tendenci/tendenci

commit fdf24238c8952ee24967bbeb44f2600f7f17d939

Fix username label and help text, and password label
Do not override the configured username label and help text with
hard-coded values.  Also use the configured password label, and use a
translated string for the confirm_password label.

tendenci/apps/memberships/forms.py
@@ -588,6 +588,7 @@ class UserForm(FormControlWidgetMixin, forms.ModelForm):
             passwd = app_field_objs.filter(field_name='password')[0]
             self.fields['password'] = forms.CharField(
                 initial=u'',
+                label=passwd.label,
                 widget=forms.PasswordInput,
                 required=False,
                 help_text=passwd.help_text
@@ -596,18 +597,20 @@ class UserForm(FormControlWidgetMixin, forms.ModelForm):
 
             self.fields['confirm_password'] = forms.CharField(
                 initial=u'',
+                label=_("Confirm password"),
                 widget=forms.PasswordInput,
                 required=False,
             )
             self.fields['confirm_password'].widget.attrs.update({'size': 28})
 
         if 'username' in self_fields_keys:
+            username = app_field_objs.filter(field_name='username')[0]
             self.fields['username'] = forms.RegexField(regex=r'^[\w.@+-]+$',
                                 required=False,
                                 max_length=30,
                                 widget=forms.TextInput,
-                                label=_(u'Username'),
-                                help_text = _("Allowed characters are letters, digits, at sign (@), period (.), plus sign (+), dash (-), and underscore (_)."),
+                                label=username.label,
+                                help_text=username.label,
                                 error_messages = {
                                     'invalid' : _("Allowed characters are letters, digits, at sign (@), period (.), plus sign (+), dash (-), and underscore (_).")
                                 })

commit 2a35e15d1b48d84e4380adaa9a20acfab4b5cdf6

Use the proper field for the username help text
Commit fdf24238 accidentally used the wrong field for the help text

tendenci/apps/memberships/forms.py
@@ -610,7 +610,7 @@ class UserForm(FormControlWidgetMixin, forms.ModelForm):
                                 max_length=30,
                                 widget=forms.TextInput,
                                 label=username.label,
-                                help_text=username.label,
+                                help_text=username.help_text,
                                 error_messages = {
                                     'invalid' : _("Allowed characters are letters, digits, at sign (@), period (.), plus sign (+), dash (-), and underscore (_).")
                                 })