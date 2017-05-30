3658caa74829fb14487f0af89e40298f8c66fbce
Jenny Qian <jqian@tendenci.com>
2017-05-30 10:34:34 -0500

    validate email2

--- tendenci/apps/newsletters/management/commands/send_newsletter.py
+++ tendenci/apps/newsletters/management/commands/send_newsletter.py
@@ -103,12 +103,13 @@ class Command(BaseCommand):
                     reply_to=email.reply_to,
                     recipient=recipient.member.email
                     )
+            print "Sending to %s" % recipient.member.email
             email_to_send.send(connection=connection)
             counter += 1
             print "Newsletter sent to %s" % recipient.member.email
 
             if newsletter.send_to_email2 and hasattr(recipient.member, 'profile') \
-                and recipient.member.profile.email2:
+                and validate_email(recipient.member.profile.email2):
                 email_to_send.recipient = recipient.member.profile.email2
                 email_to_send.send(connection=connection)
                 counter += 1
