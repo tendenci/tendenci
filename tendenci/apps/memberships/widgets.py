dc6c852a86ae2a333e98e15afaf6d4f861d09b2c
Jenny Qian <jqian@tendenci.com>
2017-08-31 12:19:04 -0500

    remove extra </div>

--- tendenci/apps/memberships/widgets.py
+++ tendenci/apps/memberships/widgets.py
@@ -375,7 +375,7 @@ class DonationOptionAmountWidget(forms.MultiWidget):
         # donation_option
         OPTION_CHOICE = (
                           ("default", "%s%s" % (currency_symbol, self.default_amount)),
-                          ("custom", mark_safe('%s %s</div>' % \
+                          ("custom", mark_safe('%s %s' % \
                                           (currency_symbol, rendered_donation_amount))),)
         donation_option_widget = self.pos_d['donation_option'][1]
         donation_option_widget.choices=OPTION_CHOICE
