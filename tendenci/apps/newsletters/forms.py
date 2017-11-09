23af2c3e942aeb4907d743c8ea52da9bad73dbcb
Jenny Qian <jqian@tendenci.com>
2017-11-09 16:38:08 -0600

    change the order of the fields "member_only" and "group"

--- tendenci/apps/newsletters/forms.py
+++ tendenci/apps/newsletters/forms.py
@@ -168,7 +168,7 @@ class MarketingStepThreeForm(forms.ModelForm):
 class MarketingStepFourForm(forms.ModelForm):
     class Meta:
         model = Newsletter
-        fields = ('subject', 'send_to_email2', 'sla', 'group', 'member_only')
+        fields = ('subject', 'send_to_email2', 'sla', 'member_only', 'group')
 
     def __init__(self, *args, **kwargs):
         super(MarketingStepFourForm, self).__init__(*args, **kwargs)
