tendenci/tendenci

commit abc6e935c9eee87b7101825f6e7ebff2a182628b

Fix flake8 warnings about import/class/variable redefinitions

tendenci/apps/memberships/utils.py
@@ -691,7 +691,7 @@ def get_over_time_stats():
     """
     today = date.today()
     year = datetime(day=1, month=1, year=today.year)
-    times = [
+    time_ranges = [
         ("Last Month", months_back(1)),
         ("Last 3 Months", months_back(3)),
         ("Last 6 Months", months_back(6)),
@@ -701,8 +701,8 @@ def get_over_time_stats():
     ]
 
     stats = []
-    for time in times:
-        start_dt = time[1]
+    for time_range in time_rangess:
+        start_dt = time_range[1]
         d = {}
         active_mems = MembershipDefault.objects.filter(
                         status=True,
@@ -711,7 +711,7 @@ def get_over_time_stats():
                      ).distinct('user__id', 'membership_type__id')
         d['new'] = active_mems.filter(renewal=False).count()
         d['renewing'] = active_mems.filter(renewal=True).count()
-        d['time'] = time[0]
+        d['time'] = time_range[0]
         d['start_dt'] = start_dt
         stats.append(d)
 

tendenci/apps/base/utils.py
@@ -329,12 +329,12 @@ def generate_meta_keywords(value):
         while search_end < value_length:
             s = one_word_pattern.search(value, search_end)
             if s:
-                word = s.group(1)
+                one_word = s.group(1)
                 # remove the : from the word
-                if word[-1] == ':':
-                    word = word[:-1]
+                if one_word[-1] == ':':
+                    one_word = one_word[:-1]
 
-                one_words.append(word)
+                one_words.append(one_word)
                 search_end = s.end()
             else: break
 

tendenci/apps/events/utils.py
@@ -10,7 +10,6 @@ from dateutil.rrule import rrule, DAILY, WEEKLY, MONTHLY, YEARLY
 from decimal import Decimal
 
 from django.contrib.auth.models import User
-from django.core.exceptions import AppRegistryNotReady
 from django.core.files.storage import default_storage
 from django.core.urlresolvers import reverse
 from django.db import connection
@@ -28,18 +27,6 @@ from tendenci.apps.events.models import (Event, Place, Speaker, Organizer,
     Registration, RegistrationConfiguration, Registrant, RegConfPricing,
     CustomRegForm, Addon, AddonOption, CustomRegField, Type,
     TypeColorSet, RecurringEvent)
-try: from tendenci.apps.events.forms import (FormForCustomRegForm,
-    EMAIL_AVAILABLE_TOKENS)
-# This module is imported by search_indexes.py during Django initialization
-# before translations are available.  However, tendenci.apps.events.forms
-# imports tendenci.apps.base.forms, which iterates through
-# django_countries.countries, which causes the django_countries module to
-# attempt to translate the country names.  search_indexes.py does not require
-# any of the functionality provided by tendenci.apps.events.forms, so simply
-# ignore the exception that is thrown when this happens.
-# tendenci.apps.events.forms will be imported again later when this module is
-# imported by other modules after Django initialization.
-except AppRegistryNotReady: pass
 from tendenci.apps.discounts.models import Discount, DiscountUse
 from tendenci.apps.discounts.utils import assign_discount
 from tendenci.apps.site_settings.utils import get_setting

tendenci/apps/meta/utils.py
@@ -55,12 +55,12 @@ def generate_meta_keywords(value):
         while search_end < value_length:
             s = one_word_pattern.search(value, search_end)
             if s:
-                word = s.group(1)
+                one_word = s.group(1)
                 # remove the : from the word
-                if word[-1] == ':':
-                    word = word[:-1]
+                if one_word[-1] == ':':
+                    one_word = one_word[:-1]
 
-                one_words.append(word)
+                one_words.append(one_word)
                 search_end = s.end()
             else: break
 

tendenci/apps/recurring_payments/utils.py
@@ -353,7 +353,6 @@ def api_rp_setup(data):
         username
         result_code
     """
-    from decimal import Decimal
     from tendenci.apps.base.utils import validate_email
     import dateutil.parser as dparser
     from tendenci.apps.imports.utils import get_unique_username
@@ -553,7 +552,6 @@ def api_add_rp(data):
                       'trial_period_end_dt',
                       'trial_amount',
                       )
-    from decimal import Decimal
     from tendenci.apps.base.utils import validate_email
     import dateutil.parser as dparser
     from tendenci.apps.imports.utils import get_unique_username

tendenci/apps/wp_exporter/utils.py
@@ -244,7 +244,6 @@ def encode_news(xml, offset=0):
 def encode_jobs(xml, offset=0):
     try:
         from tendenci.apps.jobs.models import Job
-        from tendenci.apps.jobs.models import Job
         jobs = Job.objects.filter(status=True)
         ct = ContentType.objects.get_for_model(Job)
         for job in jobs:

.flake8
@@ -1,7 +1,7 @@
 [flake8]
 ignore =
-  # Unused/undefined/redefined variables
-  F402,F811,F812,F821,F841,
+  # Unused/undefined variables
+  F821,F841,
   # Import issues
   E401,E402,F401,
   # Unnecessary lambdas

tendenci/apps/base/management/commands/remove_npo_defaults.py
@@ -7,7 +7,6 @@ from django.contrib.contenttypes.models import ContentType
 from django.core.management import call_command
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
-from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import FieldError
 
 

commit 83f8d02d55aae66c430c9b5b8110d222df582902

Fix a typo introduced in abc6e935

tendenci/apps/memberships/utils.py
@@ -701,7 +701,7 @@ def get_over_time_stats():
     ]
 
     stats = []
-    for time_range in time_rangess:
+    for time_range in time_ranges:
         start_dt = time_range[1]
         d = {}
         active_mems = MembershipDefault.objects.filter(