6984b5298101c045168666d368bdfbc7a568ece4
Jenny Qian <jqian@tendenci.com>
2017-11-28 16:12:52 -0600

    update delimiter

--- tendenci/apps/recurring_payments/authnet/utils.py
+++ tendenci/apps/recurring_payments/authnet/utils.py
@@ -67,7 +67,8 @@ def direct_response_dict(direct_response_str):
     response_dict = {}
     max_length = len(direct_response_fields)
     # the x_delim_char we specified is || (in cim.py)
-    direct_response_list = direct_response_str.split('||')
+    # the actual delimiter in the response_code is comma now
+    direct_response_list = direct_response_str.split(',')
     for i, value in enumerate(direct_response_list):
         if i < max_length:
             response_dict[direct_response_fields[i]] = value
