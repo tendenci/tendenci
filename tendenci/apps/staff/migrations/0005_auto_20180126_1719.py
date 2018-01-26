a7284258fbb476062750b83905f62b080d4409b4
Jenny Qian <jqian@tendenci.com>
2018-01-26 17:41:13 -0600

    slug field should be unique

---
+++ tendenci/apps/staff/migrations/0005_auto_20180126_1719.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+def make_slug_unique(apps, schema_editor):
+    """
+        The slug field should be unique
+    """
+    Staff = apps.get_model('staff', 'Staff')
+    for staff in Staff.objects.all():
+        if Staff.objects.filter(slug=staff.slug).exclude(id=staff.id).exists():
+            staff.slug = '{0}-{1}'.format(staff.slug[:(75-len(str(staff.id))-1)], staff.id)
+            staff.save()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('staff', '0004_auto_20170309_1510'),
+    ]
+
+    operations = [
+        migrations.RunPython(make_slug_unique),
+    ]
---
+++ tendenci/apps/staff/migrations/0006_auto_20180126_1732.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('staff', '0005_auto_20180126_1719'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='staff',
+            name='slug',
+            field=models.SlugField(unique=True, max_length=75),
+        ),
+    ]
