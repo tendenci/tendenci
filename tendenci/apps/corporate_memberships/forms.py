tendenci/tendenci

commit b6fc7b738aa2e4fdb8cd6076745c6bc475d1891e

report for active corp memberships
help desk ticket 2591

tendenci/apps/corporate_memberships/forms.py
@@ -797,6 +797,35 @@ class CorpMembershipSearchForm(FormControlWidgetMixin, forms.Form):
         self.fields['search_criteria'].choices = search_choices
 
 
+class ReportByTypeForm(FormControlWidgetMixin, forms.Form):
+    DAYS_CHOICES = (
+                 ('0', 'ALL'),
+                 ('30', _('Last 30 days')),
+                 ('60', _('Last 60 days')),
+                 ('90', _('Last 90 days')),
+                 ('180', _('Last 180 days')),
+                 ('365', _('Last 365 days')),
+                 ('1826', _('Last 5 years')),
+                 )
+    days = forms.ChoiceField(label=_('Join Date'),
+                             required=False,
+                            choices=DAYS_CHOICES,)
+    corp_membership_type = forms.ChoiceField(label=_('Type'),
+                            required=False,
+                            choices= [(0, 'ALL')] + 
+                            [(t.id, t.name) for t in CorporateMembershipType.objects.filter(
+                                status=True,
+                                status_detail='active'
+                                ).order_by('name')])
+    
+    def __init__(self, *args, **kwargs):
+        super(ReportByTypeForm, self).__init__(*args, **kwargs)
+    
+        self.fields['days'].widget.attrs.update({'onchange': 'this.form.submit();'})
+        self.fields['corp_membership_type'].widget.attrs.update({'onchange': 'this.form.submit();'})
+    
+
+
 class CorpMembershipUploadForm(forms.ModelForm):
     KEY_CHOICES = (
         ('company_name', _('Company Name')),

tendenci/apps/corporate_memberships/models.py
@@ -348,6 +348,10 @@ class CorpProfile(TendenciBaseModel):
             return u''
 
         return reverse('file', args=[self.logo.pk])
+    
+    def get_member_rep(self):
+        [rep] = self.reps.filter(is_member_rep=True)[:1] or [None]
+        return rep
 
 
 class CorpMembership(TendenciBaseModel):

tendenci/apps/corporate_memberships/templates/corporate_memberships/reports/report_by_type.html
@@ -1,0 +1,120 @@
+{% extends "reports/base.html" %}
+{% load bootstrap_pagination_tags %}
+{% load i18n %}
+
+{% block extra_head %}
+{{ block.super }}
+<style>
+    .sort-arrows{
+        white-space: nowrap;
+    }
+</style>
+{% endblock %}
+
+{% block content %}
+
+<div class="page-header">
+  <h1>{% trans "Active Corporate Memberships Report" %}</h1>
+</div>
+
+<form class="search-form form-inline pull-right" action="" method="get">
+<div class="row"><label class="control-label">{% trans 'Filter by:' %}</label>
+  <div class="form-group">
+    <label for="id_days" class="control-label">{{ form.days.label }}</label>
+    {{ form.days }}
+  </div>
+  <div class="form-group">
+    <label for="id_days" class="control-label">{{ form.corp_membership_type.label }}</label>
+    {{ form.corp_membership_type }}
+  </div>
+</div>
+</form>
+
+<p><em>{{ corp_mems|length }} {% trans "Corporate Memberships listed" %}</em></p>
+{% autopaginate corp_mems 50 %}
+
+
+<p><a class="btn btn-default" href="{% url "reports-active-corp-mems-by-type" %}?output=csv{% if request.GET.days %}&days={{ request.GET.days }}{% endif %}">
+  {% trans 'Download CSV File' %}</a></p>
+
+<div>
+  <table class="table table-tendenci-reports">
+    <tr>
+      {% if active %}
+      <th></th>
+      <th>
+        {% trans "Company Name" %}&nbsp;
+        <span class="sort-arrows">
+        <a href="?sort=corp_profile" ><i class="fa fa-arrow-up"></i></a>&nbsp;
+        <a href="?sort=-corp_profile" ><i class="fa fa-arrow-down"></i></a>
+        </span>
+      </th>
+      <th>
+        {% trans "Parent Entity" %}&nbsp;
+        <span class="sort-arrows">
+        <a href="?sort=parent_entity" ><i class="fa fa-arrow-up"></i></a>&nbsp;
+        <a href="?sort=-parent_entity" ><i class="fa fa-arrow-down"></i></a>
+        </span>
+      </th>
+      <th>
+        {% trans "Type" %}&nbsp;
+        <span class="sort-arrows">
+        <a href="?sort=corporate_membership_type" ><i class="fa fa-arrow-up"></i></a>&nbsp;
+        <a href="?sort=-corporate_membership_type" ><i class="fa fa-arrow-down"></i></a>
+        </span>
+      </th>
+      <th>
+        {% trans "Join" %}&nbsp;
+        <span class="sort-arrows">
+        <a href="?sort=join_dt" ><i class="fa fa-arrow-up"></i></a>&nbsp;
+        <a href="?sort=-join_dt" ><i class="fa fa-arrow-down"></i></a>
+        </span>
+      </th>
+      <th>
+        {% trans "Member Rep Name" %}
+      </th>
+      <th>
+        {% trans "Member Rep Email" %}
+      </th>
+      <th>
+        {% trans "Roster" %}
+      </th>
+      <th>
+        {% trans "Invoice" %}
+      </th>
+      {% endif %}
+    </tr>
+    {% for corp_mem in corp_mems %}
+    {% with corp_mem.corp_profile as corp_profile  %}
+    <tr>
+      <td>{{ forloop.counter }}</td>
+      <td><a href="{% url 'corpmembership.view_profile' corp_profile.id %}">{{ corp_profile.name }}</a></td>
+      <td>{{ corp_profile.parent_entity.entity_name }}</td>
+      <td>{{ corp_mem.corporate_membership_type.name }}</td>
+      <td>{{ corp_mem.join_dt|date:"N j, Y" }}</td>
+      {% with corp_profile.get_member_rep as member_rep  %}
+      {% if member_rep %}
+      <td><a href="{% url 'profile' member_rep.user.username %}">{% if member_rep.user.get_fullname %}{{ member_rep.user.get_fullname }}{% else %}{{ member_rep.user.username }}{% endif %}</a></td>
+      <td>{{ member_rep.user.email }}</td>
+      {% else %}
+      <td></td>
+      <td></td>
+      {% endif %}
+      {% endwith %}
+      <td><a href="{% url 'corpmembership.roster_search' %}?cm_id={{ corp_mem.id }}" target="_blank">{% trans "View Roster" %}</a></td>
+      <td>
+          {% if corp_mem.invoice %}
+          <a href="{% url "invoice.view" corp_mem.invoice.pk %}">
+            {{ corp_mem.invoice.pk }}</a>
+          {% endif %}
+      </td>
+    </tr>
+    {% endwith %}
+    {% endfor %}
+   </table>
+</div>
+
+{% paginate %}
+<br /><br />
+
+{% endblock content %}

tendenci/apps/corporate_memberships/urls.py
@@ -107,6 +107,7 @@ urlpatterns = patterns('tendenci.apps.corporate_memberships.views',
 
 
     # reports
+    url(r"^%s/reports/active_members/$" % urlpath, "report_active_corp_members_by_type", name="reports-active-corp-mems-by-type"),
     url(r"^%s/reports/corp_mems_over_time/$" % urlpath, "new_over_time_report", name="reports-corp-mems-over-time"),
     url(r"^%s/reports/corp_mems_summary/$" % urlpath, "corp_mems_summary", name="reports-corp-mems-summary"),
     url(r"^%s/reports/free_passes_list/$" % urlpath, "free_passes_list", name="corp_memb.reports.free_passes_list"),

tendenci/apps/corporate_memberships/views.py
@@ -1,6 +1,6 @@
 import os
 import math
-from datetime import datetime, date, time
+from datetime import datetime, date, time, timedelta
 import csv
 import operator
 from hashlib import md5
@@ -63,6 +63,7 @@ from tendenci.apps.corporate_memberships.forms import (
                                          CorpMembershipRepForm,
                                          CreatorForm,
                                          CorpApproveForm,
+                                         ReportByTypeForm
                                          )
 from tendenci.apps.corporate_memberships.utils import (
                                          get_corporate_membership_type_choices,
@@ -85,6 +86,7 @@ from tendenci.apps.perms.decorators import superuser_required
 from tendenci.apps.base.utils import send_email_notification
 from tendenci.apps.profiles.models import Profile
 from tendenci.apps.site_settings.utils import get_setting
+
 
 @is_enabled('corporate_memberships')
 @staff_member_required
@@ -1837,3 +1839,96 @@ def corp_mems_summary(request, template_name='reports/corp_mems_summary.html'):
         'summary':summary,
         'total':total,
         }, context_instance=RequestContext(request))
+
+
+@is_enabled('corporate_memberships')
+@staff_member_required
+def report_active_corp_members_by_type(request,
+    template='corporate_memberships/reports/report_by_type.html'):
+    corp_mems = CorpMembership.objects.filter(
+                            status=True,
+                            status_detail='active').order_by('corp_profile__name')
+    form = ReportByTypeForm(request.GET)
+    if form.is_valid():
+        days = int(form.cleaned_data.get('days') or 0)
+        corp_membership_type = int(form.cleaned_data.get('corp_membership_type') or 0)
+    else:
+        days = 0
+        corp_membership_type = 0
+
+    if days:
+        compare_dt = datetime.now() - timedelta(days=days)
+        corp_mems = corp_mems.filter(join_dt__gte=compare_dt)
+    if corp_membership_type:
+        corp_mems = corp_mems.filter(corporate_membership_type=corp_membership_type)
+
+    allowed_sort_fields = {'corp_profile': 'corp_profile__name',
+                            'corporate_membership_type': 'corporate_membership_type__name',
+                            'parent_entity': 'corp_profile__parent_entity__entity_name',
+                            'join_dt': 'join_dt',}
+    
+    sort = request.GET.get('sort', 'corp_profile__name')
+    decending = False
+    if sort[0] == '-':
+        sort = sort[1:]
+        decending = True
+    
+    if sort not in allowed_sort_fields:
+        sort = 'corp_profile'
+     
+    if decending:
+        order_by_field = '-' + allowed_sort_fields[sort]
+    else:
+        order_by_field =  allowed_sort_fields[sort]
+    corp_mems = corp_mems.order_by(order_by_field)
+    
+    EventLog.objects.log()
+    
+    # process csv download
+    ouput = request.GET.get('output', '')
+    if ouput == 'csv':
+
+        table_header = [
+            'company name',
+            'parent entity',
+            'type',
+            'join',
+            'member rep name',
+            'member rep email',
+            'invoice',]
+
+        table_data = []
+        for corp_mem in corp_mems:
+            corp_profile = corp_mem.corp_profile
+            invoice_pk = u''
+            if corp_mem.invoice:
+                invoice_pk = u'%i' % corp_mem.invoice.pk
+            member_rep = corp_profile.get_member_rep()
+            if member_rep:
+                member_rep_name = member_rep.user.get_full_name()
+                member_rep_email = member_rep.user.email
+            else:
+                member_rep_name = ''
+                member_rep_email = ''
+            table_data.append([
+                corp_profile.name,
+                corp_profile.parent_entity.entity_name,
+                corp_mem.corporate_membership_type.name,
+                corp_mem.join_dt,
+                member_rep_name,
+                member_rep_email,
+                invoice_pk,
+            ])
+
+        return render_csv(
+            'active-corporate-memberships.csv',
+            table_header,
+            table_data,
+        )
+
+    return render(request, template,
+                  {'corp_mems': corp_mems,
+                   'active': True,
+                   'days': days,
+                   'form': form,
+                   })

commit 199999b2427369deb402c8f03861099a80290eed

Fix import errors caused by database lookup in Form class variable
Introduced in b6fc7b738a

tendenci/apps/corporate_memberships/forms.py
@@ -813,17 +813,18 @@ class ReportByTypeForm(FormControlWidgetMixin, forms.Form):
                             choices=DAYS_CHOICES,)
     corp_membership_type = forms.ChoiceField(label=_('Type'),
                             required=False,
-                            choices= [(0, 'ALL')] + 
-                            [(t.id, t.name) for t in CorporateMembershipType.objects.filter(
-                                status=True,
-                                status_detail='active'
-                                ).order_by('name')])
-    
+                            choices=[(0, 'ALL')])
+
     def __init__(self, *args, **kwargs):
         super(ReportByTypeForm, self).__init__(*args, **kwargs)
     
         self.fields['days'].widget.attrs.update({'onchange': 'this.form.submit();'})
         self.fields['corp_membership_type'].widget.attrs.update({'onchange': 'this.form.submit();'})
+        self.fields['corp_membership_type'].choices = ([(0, 'ALL')] +
+            [(t.id, t.name) for t in CorporateMembershipType.objects.filter(
+             status=True,
+             status_detail='active'
+             ).order_by('name')])
 
 
 class ReportByStatusForm(FormControlWidgetMixin, forms.Form):