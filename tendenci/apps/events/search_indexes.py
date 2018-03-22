tendenci/tendenci

commit 6e6cdb27b75424b92b2c5ed14350c1d4e0fb2bf9

Replace deprecated strip_entities()

tendenci/apps/events/search_indexes.py
@@ -1,13 +1,10 @@
 from haystack import indexes
 
-
-from django.utils.html import strip_tags, strip_entities
-
 from tendenci.apps.events.models import Event, Registrant
-from tendenci.apps.events.utils import count_event_spots_taken
 from tendenci.apps.perms.indexes import TendenciBaseSearchIndex
 from tendenci.apps.perms.object_perms import ObjectPermission
 from tendenci.apps.search.indexes import CustomSearchIndex
+from tendenci.apps.base.utils import strip_html, count_event_spots_taken
 
 
 class EventIndex(TendenciBaseSearchIndex, indexes.Indexable):
@@ -39,10 +36,7 @@ class EventIndex(TendenciBaseSearchIndex, indexes.Indexable):
         return Event
 
     def prepare_description(self, obj):
-        description = obj.description
-        description = strip_tags(description)
-        description = strip_entities(description)
-        return description
+        return strip_html(obj.description)
 
     def prepare_hour(self, obj):
         return int(obj.start_dt.hour)

tendenci/apps/articles/search_indexes.py
@@ -1,12 +1,10 @@
 from datetime import datetime
 from haystack import indexes
 
-
-from django.utils.html import strip_tags, strip_entities
-
 from tendenci.apps.articles.models import Article
 from tendenci.apps.perms.indexes import TendenciBaseSearchIndex
 from tendenci.apps.categories.models import Category
+from tendenci.apps.base.utils import strip_html
 
 
 class ArticleIndex(TendenciBaseSearchIndex, indexes.Indexable):
@@ -32,10 +30,7 @@ class ArticleIndex(TendenciBaseSearchIndex, indexes.Indexable):
         return 'update_dt'
 
     def prepare_body(self, obj):
-        body = obj.body
-        body = strip_tags(body)
-        body = strip_entities(body)
-        return body
+        return strip_html(obj.body)
 
     def prepare_category(self, obj):
         category = Category.objects.get_for_object(obj, 'category')

tendenci/apps/boxes/search_indexes.py
@@ -1,10 +1,8 @@
 from haystack import indexes
 
-
-from django.utils.html import strip_tags, strip_entities
-
 from tendenci.apps.boxes.models import Box
 from tendenci.apps.perms.indexes import TendenciBaseSearchIndex
+from tendenci.apps.base.utils import strip_html
 
 
 class BoxIndex(TendenciBaseSearchIndex, indexes.Indexable):
@@ -19,10 +17,7 @@ class BoxIndex(TendenciBaseSearchIndex, indexes.Indexable):
         return Box
 
     def prepare_content(self, obj):
-        content = obj.content
-        content = strip_tags(content)
-        content = strip_entities(content)
-        return content
+        return strip_html(obj.content)
 
     def prepare_order(self, obj):
         return obj.update_dt

tendenci/apps/directories/search_indexes.py
@@ -1,12 +1,11 @@
 from datetime import datetime
 from haystack import indexes
 
-
-from django.utils.html import strip_tags, strip_entities
-
 from tendenci.apps.directories.models import Directory
 from tendenci.apps.perms.indexes import TendenciBaseSearchIndex
 from tendenci.apps.categories.models import Category
+from tendenci.apps.base.utils import strip_html
+
 
 class DirectoryIndex(TendenciBaseSearchIndex, indexes.Indexable):
     headline = indexes.CharField(model_attr='headline', faceted=True)
@@ -28,10 +27,7 @@ class DirectoryIndex(TendenciBaseSearchIndex, indexes.Indexable):
         return Directory
 
     def prepare_body(self, obj):
-        body = obj.body
-        body = strip_tags(body)
-        body = strip_entities(body)
-        return body
+        return strip_html(obj.body)
 
     def prepare_category(self, obj):
         category = Category.objects.get_for_object(obj, 'category')

tendenci/apps/forms_builder/forms/search_indexes.py
@@ -1,9 +1,9 @@
-from django.utils.html import strip_tags, strip_entities
-
 from haystack import indexes
 
 from tendenci.apps.forms_builder.forms.models import Form
 from tendenci.apps.perms.indexes import TendenciBaseSearchIndex
+from tendenci.apps.base.utils import strip_html
+
 
 class FormsIndex(TendenciBaseSearchIndex, indexes.Indexable):
     title = indexes.CharField(model_attr='title')
@@ -15,13 +15,7 @@ class FormsIndex(TendenciBaseSearchIndex, indexes.Indexable):
         return Form
 
     def prepare_intro(self, obj):
-        intro = obj.intro
-        intro = strip_tags(intro)
-        intro = strip_entities(intro)
-        return intro
+        return strip_html(obj.intro)
 
     def prepare_response(self, obj):
-        response = obj.response
-        response = strip_tags(response)
-        response = strip_entities(response)
-        return response
+        return strip_html(obj.response)

tendenci/apps/help_files/search_indexes.py
@@ -1,11 +1,10 @@
 from datetime import datetime
 from haystack import indexes
 
-
-from django.utils.html import strip_tags, strip_entities
-
 from tendenci.apps.help_files.models import HelpFile
 from tendenci.apps.perms.indexes import TendenciBaseSearchIndex
+from tendenci.apps.base.utils import strip_html
+
 
 class HelpFileIndex(TendenciBaseSearchIndex, indexes.Indexable):
     question = indexes.CharField(model_attr='question')
@@ -22,10 +21,7 @@ class HelpFileIndex(TendenciBaseSearchIndex, indexes.Indexable):
         return HelpFile
 
     def prepare_answer(self, obj):
-        answer = obj.answer
-        answer = strip_tags(answer)
-        answer = strip_entities(answer)
-        return answer
+        return strip_html(obj.answer)
 
     def prepare_topic(self, obj):
         return [topic.pk for topic in obj.topics.all()]

tendenci/apps/jobs/search_indexes.py
@@ -1,10 +1,9 @@
 from haystack import indexes
 
-
-from django.utils.html import strip_tags, strip_entities
-
 from tendenci.apps.jobs.models import Job
 from tendenci.apps.perms.indexes import TendenciBaseSearchIndex
+from tendenci.apps.base.utils import strip_html
+
 
 class JobIndex(TendenciBaseSearchIndex, indexes.Indexable):
     title = indexes.CharField(model_attr='title')
@@ -34,10 +33,7 @@ class JobIndex(TendenciBaseSearchIndex, indexes.Indexable):
         return Job
 
     def prepare_description(self, obj):
-        description = obj.description
-        description = strip_tags(description)
-        description = strip_entities(description)
-        return description
+        return strip_html(obj.description)
 
     def prepare_cate(self, obj):
         category = obj.cat

tendenci/apps/locations/search_indexes.py
@@ -1,10 +1,9 @@
 from haystack import indexes
 
-
-from django.utils.html import strip_tags, strip_entities
-
 from tendenci.apps.locations.models import Location
 from tendenci.apps.perms.indexes import TendenciBaseSearchIndex
+from tendenci.apps.base.utils import strip_html
+
 
 class LocationIndex(TendenciBaseSearchIndex, indexes.Indexable):
     description = indexes.CharField(model_attr='description')
@@ -15,7 +14,4 @@ class LocationIndex(TendenciBaseSearchIndex, indexes.Indexable):
         return Location
 
     def prepare_description(self, obj):
-        description = obj.description
-        description = strip_tags(description)
-        description = strip_entities(description)
-        return description
+        return strip_html(obj.description)

commit e0ac0d7056fb0eae98663acabab65a202f77ec82

Fix import bug introduced in 6e6cdb27

tendenci/apps/events/search_indexes.py
@@ -1,10 +1,11 @@
 from haystack import indexes
 
 from tendenci.apps.events.models import Event, Registrant
+from tendenci.apps.events.utils import count_event_spots_taken
 from tendenci.apps.perms.indexes import TendenciBaseSearchIndex
 from tendenci.apps.perms.object_perms import ObjectPermission
 from tendenci.apps.search.indexes import CustomSearchIndex
-from tendenci.apps.base.utils import strip_html, count_event_spots_taken
+from tendenci.apps.base.utils import strip_html
 
 
 class EventIndex(TendenciBaseSearchIndex, indexes.Indexable):