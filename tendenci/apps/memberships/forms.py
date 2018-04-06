tendenci/tendenci

commit bb1217a6b0592446f07f1a4fe89f9d7eec4c7baa

Remove unnecessary dict.keys() calls

tendenci/apps/memberships/forms.py
@@ -511,13 +511,12 @@ def get_field_size(app_field_obj):
 
 
 def assign_fields(form, app_field_objs):
-    form_field_keys = form.fields.keys()
     # a list of names of app fields
     field_names = [field.field_name for field in app_field_objs
                    if field.field_name != '' and
-                   field.field_name in form_field_keys]
+                   field.field_name in form.fields]
 
-    for name in form_field_keys:
+    for name in form.fields:
         if name not in field_names and name != 'discount_code':
             del form.fields[name]
     # update the field attrs - label, required...
@@ -626,7 +625,7 @@ class UserForm(FormControlWidgetMixin, forms.ModelForm):
                                     'invalid' : _("Allowed characters are letters, digits, at sign (@), period (.), plus sign (+), dash (-), and underscore (_).")
                                 })
 
-        self.field_names = [name for name in self.fields.keys()]
+        self.field_names = [name for name in self.fields]
         self.add_form_control_class()
 
     def clean(self):
@@ -790,7 +789,7 @@ class ProfileForm(FormControlWidgetMixin, forms.ModelForm):
         del self.fields['referral_source']
 
         assign_fields(self, app_field_objs)
-        self.field_names = [name for name in self.fields.keys()]
+        self.field_names = [name for name in self.fields]
 
         self.add_form_control_class()
 
@@ -849,8 +848,7 @@ class EducationForm(FormControlWidgetMixin, forms.Form):
             self.user = None
         if app_field_objs:
             assign_fields(self, app_field_objs)
-        self.field_names = [name for name in self.fields.keys()]
-        self.keys = self.fields.keys()
+        self.field_names = [name for name in self.fields]
 
         if self.user:
             education_list = self.user.educations.all().order_by('pk')[0:4]
@@ -858,16 +856,16 @@ class EducationForm(FormControlWidgetMixin, forms.Form):
                 cnt = 1
                 for education in education_list:
                     field_key = 'school%s' % cnt
-                    if field_key in self.keys:
+                    if field_key in self.fields:
                         self.fields[field_key].initial = education.school
                     field_key = 'major%s' % cnt
-                    if field_key in self.keys:
+                    if field_key in self.fields:
                         self.fields[field_key].initial = education.major
                     field_key = 'degree%s' % cnt
-                    if field_key in self.keys:
+                    if field_key in self.fields:
                         self.fields[field_key].initial = education.degree
                     field_key = 'graduation_dt%s' % cnt
-                    if field_key in self.keys:
+                    if field_key in self.fields:
                         self.fields[field_key].initial = education.graduation_year
                     cnt += 1
 
@@ -929,7 +927,7 @@ class DemographicsForm(FormControlWidgetMixin, forms.ModelForm):
         self.membership = kwargs.pop('membership', None)
         super(DemographicsForm, self).__init__(*args, **kwargs)
         assign_fields(self, app_field_objs)
-        self.field_names = [name for name in self.fields.keys()]
+        self.field_names = [name for name in self.fields]
         self.file_upload_fields = {}
         # change the default widget to TextInput instead of TextArea
         for key, field in self.fields.items():
@@ -970,7 +968,7 @@ class DemographicsForm(FormControlWidgetMixin, forms.ModelForm):
     def save(self, commit=True, *args, **kwargs):
         pks ={}
         if self.file_upload_fields:
-            for key in self.file_upload_fields.keys():
+            for key in self.file_upload_fields:
                 new_file = self.cleaned_data.get(key, None)
 
                 if self.request:
@@ -1054,15 +1052,15 @@ class MembershipDefault2Form(FormControlWidgetMixin, forms.ModelForm):
         self.is_renewal = kwargs.pop('is_renewal', False)
         self.renew_from_id = kwargs.pop('renew_from_id', None)
 
-        if 'join_under_corporate' in kwargs.keys():
+        if 'join_under_corporate' in kwargs:
             self.join_under_corporate = kwargs.pop('join_under_corporate')
         else:
             self.join_under_corporate = False
-        if 'corp_membership' in kwargs.keys():
+        if 'corp_membership' in kwargs:
             self.corp_membership = kwargs.pop('corp_membership')
         else:
             self.corp_membership = None
-        if 'authentication_method' in kwargs.keys():
+        if 'authentication_method' in kwargs:
             self.corp_app_authentication_method = kwargs.pop('authentication_method')
         else:
             self.corp_app_authentication_method = ''
@@ -1109,15 +1107,13 @@ class MembershipDefault2Form(FormControlWidgetMixin, forms.ModelForm):
             require_payment = self.membership_app.membership_types.filter(
                                     Q(price__gt=0) | Q(admin_fee__gt=0)).exists()
 
-        self_fields_keys = self.fields.keys()
-
-        if 'status_detail' in self_fields_keys:
+        if 'status_detail' in self.fields:
             self.fields['status_detail'].widget = forms.widgets.Select(
                         choices=self.STATUS_DETAIL_CHOICES)
-        if 'status' in self_fields_keys:
+        if 'status' in self.fields:
             self.fields['status'].widget = forms.widgets.Select(
                         choices=self.STATUS_CHOICES)
-        if 'groups' in self_fields_keys:
+        if 'groups' in self.fields:
             self.fields['groups'].widget = forms.widgets.CheckboxSelectMultiple()
             self.fields['groups'].queryset = Group.objects.filter(
                                                 allow_self_add=True,
@@ -1125,7 +1121,7 @@ class MembershipDefault2Form(FormControlWidgetMixin, forms.ModelForm):
                                                 status_detail='active')
             self.fields['groups'].help_text = ''
 
-        if 'corporate_membership_id' in self_fields_keys:
+        if 'corporate_membership_id' in self.fields:
             if self.join_under_corporate and self.corp_membership:
                 self.fields['corporate_membership_id'].widget = forms.widgets.Select(
                                         choices=((self.corp_membership.id, self.corp_membership),))
@@ -1137,7 +1133,7 @@ class MembershipDefault2Form(FormControlWidgetMixin, forms.ModelForm):
                                                 status_detail__in=['archive', 'inactive'])
 
         assign_fields(self, app_field_objs)
-        self.field_names = [name for name in self.fields.keys()]
+        self.field_names = [name for name in self.fields]
 
         if self.instance and self.instance.pk:
             self.fields['membership_type'].widget.attrs['disabled'] = 'disabled'

tendenci/apps/base/forms.py
@@ -31,7 +31,7 @@ class FormControlWidgetMixin(object):
     def add_form_control_class(self):
 
         # Add .'form-control' class to all field widgets
-        for field_name in self.fields.keys():
+        for field_name in self.fields:
 
             #print('%s: %s' % (field_name, self.fields[field_name].widget.__class__.__name__.lower()))
             non_form_control_widgets = [
@@ -42,7 +42,7 @@ class FormControlWidgetMixin(object):
                 widget_attrs = self.fields[field_name].widget.attrs
 
                 class_attr = 'form-control'
-                if 'class' in widget_attrs.keys():
+                if 'class' in widget_attrs:
                     class_attr = widget_attrs['class']
 
                     if 'form-control' not in class_attr:

tendenci/apps/corporate_memberships/forms.py
@@ -234,12 +234,11 @@ def get_field_size(app_field_obj):
 
 
 def assign_fields(form, app_field_objs, instance=None):
-    form_field_keys = form.fields.keys()
     # a list of names of app fields
     field_names = [field.field_name for field in app_field_objs
                    if field.field_name != '' and
-                   field.field_name in form_field_keys]
-    for name in form_field_keys:
+                   field.field_name in form.fields]
+    for name in form.fields:
         if name not in field_names:
             del form.fields[name]
     # update the field attrs - label, required...
@@ -464,7 +463,7 @@ class CorpProfileForm(CorpProfileBaseForm):
             if selected_parent_entities.exists():
                 f.queryset = self.corpmembership_app.parent_entities.all()
 
-        self.field_names = [name for name in self.fields.keys()]
+        self.field_names = [name for name in self.fields]
 
     def clean_secret_code(self):
         secret_code = self.cleaned_data['secret_code']
@@ -498,7 +497,7 @@ class CorpProfileForm(CorpProfileBaseForm):
         if not self.request_user.is_anonymous():
             self.instance.owner = self.request_user
             self.instance.owner_username = self.request_user.username
-        for field_key in self.fields.keys():
+        for field_key in self.fields:
             if self.fields[field_key].widget.needs_multipart_form:
                 value = self.cleaned_data[field_key]
                 if value and hasattr(value, 'name'):
@@ -600,17 +599,16 @@ class CorpMembershipForm(FormControlWidgetMixin, forms.ModelForm):
                         choices=get_payment_method_choices(
                                     self.request_user,
                                     self.corpmembership_app))
-        self_fields_keys = self.fields.keys()
-        if 'status_detail' in self_fields_keys:
+        if 'status_detail' in self.fields:
             self.fields['status_detail'].widget = forms.widgets.Select(
                         choices=self.STATUS_DETAIL_CHOICES)
-        if 'status' in self_fields_keys:
+        if 'status' in self.fields:
             self.fields['status'].widget = forms.widgets.Select(
                         choices=self.STATUS_CHOICES)
 
         assign_fields(self, app_field_objs, instance=self.instance)
         self.add_form_control_class()
-        self.field_names = [name for name in self.fields.keys()]
+        self.field_names = [name for name in self.fields]
 
     def save(self, **kwargs):
         super(CorpMembershipForm, self).save(commit=False)
@@ -867,7 +865,7 @@ class CorpMembershipUploadForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(CorpMembershipUploadForm, self).__init__(*args, **kwargs)
         self.fields['key'].initial = 'name'
-        for k in self.fields.keys():
+        for k in self.fields:
             if k in ['key', 'override']:
                 self.fields[k].widget.attrs['class'] = 'form-control'
 
@@ -907,7 +905,7 @@ class CreatorForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(CreatorForm, self).__init__(*args, **kwargs)
         self.fields['captcha'] = CustomCatpchaField(label=_('Type the code below'))
-        for k in self.fields.keys():
+        for k in self.fields:
             self.fields[k].widget.attrs['class'] = 'form-control'
 
 
@@ -1063,10 +1061,10 @@ class CSVForm(forms.Form):
             csv = csv_to_dict(file_path)
 
             # choices list
-            choices = csv[0].keys()
+            choices = csv[0]
 
             # make tuples; sort tuples (case-insensitive)
-            choice_tuples = [(c, c) for c in csv[0].keys()]
+            choice_tuples = [(c, c) for c in csv[0]]
 
             # insert blank option
             choice_tuples.insert(0, ('', ''))
@@ -1104,7 +1102,7 @@ class CSVForm(forms.Form):
             # corp_memb_field_names = [smart_str(field.name)
             # for field in CorporateMembership._meta.fields]
             for key, label in extra_fields:
-                if key not in self.fields.keys():
+                if key not in self.fields:
                     self.fields[key] = ChoiceField(**{
                                             'label': label,
                                             'choices': choice_tuples,

tendenci/apps/discounts/forms.py
@@ -73,7 +73,7 @@ class DiscountForm(TendenciBaseForm):
         MODELS_WITH_DISCOUNT = ['registrationconfiguration',
                                 'membershipset']
         content_types = ContentType.objects.filter(model__in=MODELS_WITH_DISCOUNT)
-        if 'apps' in self.fields.keys():
+        if 'apps' in self.fields:
             self.fields['apps'].choices = ((c.id, c.app_label) for c in content_types)
 
     def clean_discount_code(self):

tendenci/apps/emails/forms.py
@@ -34,7 +34,7 @@ class EmailForm(forms.ModelForm):
         else:
             self.fields['body'].widget.mce_attrs['app_instance_id'] = 0
         # add form-control class
-        for k in self.fields.keys():
+        for k in self.fields:
             self.fields[k].widget.attrs['class'] = 'form-control'
 
     def save(self, user=None, *args, **kwargs):

tendenci/apps/events/forms.py
@@ -301,7 +301,7 @@ class FormForCustomRegForm(forms.ModelForm):
             # make the fields in the subsequent forms as not required
             if not reg_conf.require_guests_info:
                 if self.form_index and self.form_index > 0:
-                    for key in self.fields.keys():
+                    for key in self.fields:
                         self.fields[key].required = False
             else:
                 # this attr is required for form validation
@@ -706,13 +706,13 @@ class EventForm(TendenciBaseForm):
 
         if self.instance.pk:
             self.fields['description'].widget.mce_attrs['app_instance_id'] = self.instance.pk
-            if 'private_slug' in self.fields.keys():
+            if 'private_slug' in self.fields:
                 self.fields['enable_private_slug'].help_text = self.instance.get_private_slug(absolute_url=True)
             self.fields['start_event_date'].initial = self.instance.start_dt.date()
             self.fields['end_event_date'].initial = self.instance.end_dt.date()
         else:
             # kwargs['instance'] always trumps initial
-            if 'private_slug' in self.fields.keys():
+            if 'private_slug' in self.fields:
                 self.fields['private_slug'].initial = self.instance.get_private_slug()
                 self.fields['enable_private_slug'].widget = forms.HiddenInput()
 
@@ -1687,7 +1687,7 @@ class RegistrantForm(forms.Form):
         # make the fields in the subsequent forms as not required
         if not reg_conf.require_guests_info:
             if self.form_index and self.form_index > 0:
-                for key in self.fields.keys():
+                for key in self.fields:
                     self.fields[key].required = False
             if not self.event.is_table:
                 self.empty_permitted = False

tendenci/apps/events/registration/forms.py
@@ -99,7 +99,7 @@ class RegistrantForm(forms.Form):
 
         # make the fields in the subsequent forms as not required
         if self.form_index and self.form_index > 0:
-            for key in self.fields.keys():
+            for key in self.fields:
                 self.fields[key].required = False
 
         # initialize pricing options and reg_set field

tendenci/apps/forums/forms.py
@@ -119,7 +119,7 @@ class PostForm(forms.ModelForm):
         self.smiles_prefix = defaults.PYBB_SMILES_PREFIX
 
         # add form-control class
-        for k in self.fields.keys():
+        for k in self.fields:
             self.fields[k].widget.attrs['class'] = 'form-control'
 
     def clean_body(self):

commit ee2fec20b5d4dc0f817ec23ed0b2c4fed7cdabe6

Fix mutation during iteration bug introduced by bb1217a6

tendenci/apps/memberships/forms.py
@@ -516,7 +516,7 @@ def assign_fields(form, app_field_objs):
                    if field.field_name != '' and
                    field.field_name in form.fields]
 
-    for name in form.fields:
+    for name in list(form.fields):
         if name not in field_names and name != 'discount_code':
             del form.fields[name]
     # update the field attrs - label, required...