7758234b5caa53cdc4a93f54f3a2f1524885aa88
Jenny Qian <jqian@tendenci.com>
2018-04-10 14:16:11 -0500

    fix articles export

--- tendenci/apps/base/utils.py
+++ tendenci/apps/base/utils.py
@@ -793,7 +793,7 @@ class UnicodeWriter:
         self.encoder = codecs.getincrementalencoder(encoding)()
 
     def writerow(self, row):
-        self.writer.writerow([s.encode("utf-8") for s in row])
+        self.writer.writerow(row)
         # Fetch UTF-8 output from the queue ...
         data = self.queue.getvalue()
         data = data.decode("utf-8")
