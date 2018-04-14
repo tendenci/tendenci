d63c06847486e1f253024060ce8a525c140120a0
Paul Donohue <git@PaulSD.com>
2018-04-14 12:00:00 -0400

    Switch from abandoned python-memcached to pylibmc
    See https://github.com/linsomniac/python-memcached/issues/95

--- tendenci/settings.py
+++ tendenci/settings.py
@@ -303,11 +303,15 @@ for theme in os.listdir(BUILTIN_THEMES_DIR):
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
         'LOCATION': '127.0.0.1:11211',
         'TIMEOUT': 60*60*24*30,  # 30 days
     }
 }
+try:
+    import pylibmc
+except ImportError:
+    CACHES['default']['BACKEND'] = 'django.core.cache.backends.dummy.DummyCache'
 
 LOGGING = {
     'version': 1,
