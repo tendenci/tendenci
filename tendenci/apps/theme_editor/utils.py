tendenci/tendenci

commit bdb2c92a3564f976e14ee3f0b17358cd5215072a

Replace {{ THEME_URL }} usage in theme_editor

tendenci/apps/theme_editor/utils.py
@@ -150,72 +150,73 @@ def get_all_files_list(root_dir, theme):
 
             # Hide hidden files
             if not f.startswith('.'):
-                subdir['contents'].append({'name': f, 'path': os.path.join(path[len(root_dir) + 1:], f), 'editable': editable})
+                path = os.path.join(path[len(root_dir) + 1:], f)
+                subdir['contents'].append({'name': f, 'path': path, 'url': '/themes/'+theme+'/'+path, 'editable': editable})
 
         subdir['contents'] = sorted(subdir['contents'], key=itemgetter('name'))
         subdir['contents'].append({'folder_path': path})
         parent = reduce(dict.get, folders[:-1], files_folders)
         parent[folders[-1]] = subdir
 
-    if settings.USE_S3_THEME:
-        s3_files_folders = {'contents': []}
-        theme_folder = "%s/%s" % (settings.THEME_S3_PATH, theme)
-        conn = boto.connect_s3(settings.AWS_ACCESS_KEY_ID,
-                               settings.AWS_SECRET_ACCESS_KEY)
-        bucket = conn.get_bucket(settings.AWS_STORAGE_BUCKET_NAME)
+    if not settings.USE_S3_THEME:
+        return files_folders
 
-        for item in bucket.list(prefix=theme_folder):
+    s3_files_folders = {'contents': []}
+    theme_folder = "%s/%s" % (settings.THEME_S3_PATH, theme)
+    conn = boto.connect_s3(settings.AWS_ACCESS_KEY_ID,
+                           settings.AWS_SECRET_ACCESS_KEY)
+    bucket = conn.get_bucket(settings.AWS_STORAGE_BUCKET_NAME)
 
-            editable = False
-            if os.path.splitext(item.name)[1] in ALLOWED_EXTENSIONS:
-                editable = True
+    for item in bucket.list(prefix=theme_folder):
 
-            file_path = item.name.replace(theme_folder, '').lstrip('/')
-            path_split = file_path.split('/')
-            splits = len(path_split)
+        editable = False
+        if os.path.splitext(item.name)[1] in ALLOWED_EXTENSIONS:
+            editable = True
 
-            if splits == 1:
-                s3_files_folders['contents'].append({
-                        'name': path_split[0],
-                        'path': file_path,
-                        'editable': editable})
-            elif splits == 2:
-                if not path_split[0] in s3_files_folders:
-                    s3_files_folders[path_split[0]] = {'contents': [{'folder_path': "/".join(path_split[:-1])}]}
+        file_path = item.name.replace(theme_folder, '').lstrip('/')
+        path_split = file_path.split('/')
+        splits = len(path_split)
 
-                s3_files_folders[path_split[0]]['contents'].append({
-                        'name': path_split[1],
-                        'path': file_path,
-                        'editable': editable})
-            elif splits == 3:
-                if not path_split[0] in s3_files_folders:
-                    s3_files_folders[path_split[0]] = {'contents': [{'folder_path': "/".join(path_split[:-1])}]}
+        if splits == 1:
+            s3_files_folders['contents'].append({
+                    'name': path_split[0],
+                    'path': file_path,
+                    'editable': editable})
+        elif splits == 2:
+            if not path_split[0] in s3_files_folders:
+                s3_files_folders[path_split[0]] = {'contents': [{'folder_path': "/".join(path_split[:-1])}]}
 
-                if not path_split[1] in s3_files_folders[path_split[0]]:
-                    s3_files_folders[path_split[0]][path_split[1]] = {'contents': [{'folder_path': "/".join(path_split[:-1])}]}
+            s3_files_folders[path_split[0]]['contents'].append({
+                    'name': path_split[1],
+                    'path': file_path,
+                    'editable': editable})
+        elif splits == 3:
+            if not path_split[0] in s3_files_folders:
+                s3_files_folders[path_split[0]] = {'contents': [{'folder_path': "/".join(path_split[:-1])}]}
 
-                s3_files_folders[path_split[0]][path_split[1]]['contents'].append({
-                        'name': path_split[2],
-                        'path': file_path,
-                        'editable': editable})
-            elif splits == 4:
-                if not path_split[0] in s3_files_folders:
-                    s3_files_folders[path_split[0]] = {'contents': [{'folder_path': "/".join(path_split[:-1])}]}
+            if not path_split[1] in s3_files_folders[path_split[0]]:
+                s3_files_folders[path_split[0]][path_split[1]] = {'contents': [{'folder_path': "/".join(path_split[:-1])}]}
 
-                if not path_split[1] in s3_files_folders[path_split[0]]:
-                    s3_files_folders[path_split[0]][path_split[1]] = {'contents': [{'folder_path': "/".join(path_split[:-1])}]}
+            s3_files_folders[path_split[0]][path_split[1]]['contents'].append({
+                    'name': path_split[2],
+                    'path': file_path,
+                    'editable': editable})
+        elif splits == 4:
+            if not path_split[0] in s3_files_folders:
+                s3_files_folders[path_split[0]] = {'contents': [{'folder_path': "/".join(path_split[:-1])}]}
 
-                if not path_split[2] in s3_files_folders[path_split[0]][path_split[1]]:
-                    s3_files_folders[path_split[0]][path_split[1]][path_split[2]] = {'contents': [{'folder_path': "/".join(path_split[:-1])}]}
+            if not path_split[1] in s3_files_folders[path_split[0]]:
+                s3_files_folders[path_split[0]][path_split[1]] = {'contents': [{'folder_path': "/".join(path_split[:-1])}]}
 
-                s3_files_folders[path_split[0]][path_split[1]][path_split[2]]['contents'].append({
-                        'name': path_split[3],
-                        'path': file_path,
-                        'editable': editable})
+            if not path_split[2] in s3_files_folders[path_split[0]][path_split[1]]:
+                s3_files_folders[path_split[0]][path_split[1]][path_split[2]] = {'contents': [{'folder_path': "/".join(path_split[:-1])}]}
 
-        return {theme: s3_files_folders}
+            s3_files_folders[path_split[0]][path_split[1]][path_split[2]]['contents'].append({
+                    'name': path_split[3],
+                    'path': file_path,
+                    'editable': editable})
 
-    return files_folders
+    return {theme: s3_files_folders}
 
 
 def get_file_content(root_dir, theme, filename):

tendenci/apps/theme_editor/templates/theme_editor/file_link.html
@@ -3,6 +3,6 @@
 {% if item.editable %}
     <a href="{% url "theme_editor.editor" %}?file={{ item.path }}&theme_edit={{ current_theme }}">{{ item.name }}</a>
 {% else %}
-    <span class="non-editable-file">{{ item.name|safe }}<span class="links"> <a href="{{ THEME_URL }}{{ item.path }}" target="_blank" title="{% trans 'View this file in a new window.' %}"><span class="glyph">K</span></a> <a href="{% url "theme_editor.delete" %}?file={{ item.path }}" class="delete" title="{% trans 'Delete this file.' %}"><span class="glyph">×</span></a></span></span>
+    <span class="non-editable-file">{{ item.name|safe }}<span class="links"> <a href="{{ item.url }}" target="_blank" title="{% trans 'View this file in a new window.' %}"><span class="glyph">K</span></a> <a href="{% url "theme_editor.delete" %}?file={{ item.path }}" class="delete" title="{% trans 'Delete this file.' %}"><span class="glyph">×</span></a></span></span>
 {% endif %}
 </li>

tendenci/apps/theme_editor/templates/theme_editor/folder_structure.html
@@ -15,7 +15,7 @@
                 {% if item.editable %}
                     <a href="{% url "theme_editor.editor" %}?file={{ item.path }}&theme_edit={{ current_theme }}">{{ item.name }}</a>
                 {% else %}
-                    <span class="non-editable-file">{{ item.name|safe }}<span class="links"> <a href="{{ THEME_URL }}{{ item.path }}" target="_blank" title="{% trans 'View this file in a new window.' %}"><span class="glyph">K</span></a> <a href="{% url "theme_editor.delete" %}?file={{ item.path }}" class="delete" title="{% trans 'Delete this file.' %}"><span class="glyph">×</span></a></span></span>
+                    <span class="non-editable-file">{{ item.name|safe }}<span class="links"> <a href="{{ item.url }}" target="_blank" title="{% trans 'View this file in a new window.' %}"><span class="glyph">K</span></a> <a href="{% url "theme_editor.delete" %}?file={{ item.path }}" class="delete" title="{% trans 'Delete this file.' %}"><span class="glyph">×</span></a></span></span>
                 {% endif %}
                 </li>
             {% endfor %}

tendenci/apps/theme_editor/templates/theme_editor/index.html
@@ -105,7 +105,7 @@
                                     {% if item.editable %}
                                         <a href="{% url "theme_editor.editor" %}?file={{ item.path }}&theme_edit={{ current_theme }}">{{ item.name }}</a>
                                     {% else %}
-                                        <span class="non-editable-file">{{ item.name|safe }}<span class="links"> <a href="{{ THEME_URL }}{{ item.path }}" target="_blank" title="{% trans 'View this file in a new window.' %}"><span class="glyph">K</span></a> <a href="{% url "theme_editor.delete" %}?file={{ item.path }}&theme_edit={{ current_theme }}" class="delete" title="{% trans 'Delete this file.' %}"><span class="glyph">×</span></a></span></span>
+                                        <span class="non-editable-file">{{ item.name|safe }}<span class="links"> <a href="{{ item.url }}" target="_blank" title="{% trans 'View this file in a new window.' %}"><span class="glyph">K</span></a> <a href="{% url "theme_editor.delete" %}?file={{ item.path }}&theme_edit={{ current_theme }}" class="delete" title="{% trans 'Delete this file.' %}"><span class="glyph">×</span></a></span></span>
                                     {% endif %}
                                     </li>
                                 {% endfor %}
@@ -154,7 +154,7 @@
                                         <li style="border-top: solid 1px #111;">{% trans "Non-Editable Files" %}</li>
                                     {% for file in non_editable_files %}
                                         <li {% if current_file == file %}class="current_file"{% endif %}>
-                                            <a href="{{ THEME_URL }}{{ pwd }}/{{ file }}" target="_blank">{{ file|safe }}</a>
+                                            <a href="{{ file.url }}" target="_blank">{{ file|safe }}</a>
                                         </li>
                                     {% endfor %}
                                     {% endif %}

tendenci/themes/tendenci2018/templates/theme_editor/index.html
@@ -59,7 +59,7 @@
                 <a href="{% url "theme_editor.editor" %}?file={{ item.path }}&theme_edit={{ current_theme }}">{{ item.name }}</a>
                 {% else %}
                 <span class="non-editable-file">{{ item.name|safe }}
-                  <span class="links"> <a href="{{ THEME_URL }}{{ item.path }}" target="_blank" title="{% trans 'View this file in a new window.' %}"><span class="glyph">K</span></a> <a href="{% url "theme_editor.delete" %}?file={{ item.path }}" class="delete" title="{% trans 'Delete this file.' %}"><span class="glyph">×</span></a></span></span>
+                  <span class="links"> <a href="{{ item.url }}" target="_blank" title="{% trans 'View this file in a new window.' %}"><span class="glyph">K</span></a> <a href="{% url "theme_editor.delete" %}?file={{ item.path }}" class="delete" title="{% trans 'Delete this file.' %}"><span class="glyph">×</span></a></span></span>
                 {% endif %}
               </li>
               {% endfor %}
@@ -108,7 +108,7 @@
               <li style="border-top: solid 1px #111;">{% trans "Non-Editable Files" %}</li>
               {% for file in non_editable_files %}
               <li {% if current_file == file %}class="current_file"{% endif %}>
-                <a href="{{ THEME_URL }}{{ pwd }}/{{ file }}" target="_blank">{{ file|safe }}</a>
+                <a href="{{ file.url }}" target="_blank">{{ file|safe }}</a>
               </li>
               {% endfor %}
               {% endif %}

commit 7d92c9ccbbe44672ae15eff68f2d5296f55fa334

Fix theme_editor issues introduced by bdb2c92a

tendenci/apps/theme_editor/utils.py
@@ -134,6 +134,7 @@ def get_all_files_list(root_dir, theme):
     """
     files_folders = {}
 
+    theme_base_url = '/themes/'+theme+'/'
     start = root_dir.rfind(os.sep) + 1
     for path, dirs, files in os.walk(root_dir):
         folders = path[start:].split(os.sep)
@@ -150,8 +151,9 @@ def get_all_files_list(root_dir, theme):
 
             # Hide hidden files
             if not f.startswith('.'):
-                path = os.path.join(path[len(root_dir) + 1:], f)
-                subdir['contents'].append({'name': f, 'path': path, 'url': '/themes/'+theme+'/'+path, 'editable': editable})
+                file_path = os.path.join(path[len(root_dir) + 1:], f)
+                file_url = theme_base_url+file_path
+                subdir['contents'].append({'name': f, 'path': file_path, 'url': file_url, 'editable': editable})
 
         subdir['contents'] = sorted(subdir['contents'], key=itemgetter('name'))
         subdir['contents'].append({'folder_path': path})
@@ -162,7 +164,8 @@ def get_all_files_list(root_dir, theme):
         return files_folders
 
     s3_files_folders = {'contents': []}
-    theme_folder = "%s/%s" % (settings.THEME_S3_PATH, theme)
+    theme_folder = settings.THEME_S3_PATH+'/'+theme
+    theme_base_url = settings.AWS_LOCATION+'/'+theme_folder+'/'
     conn = boto.connect_s3(settings.AWS_ACCESS_KEY_ID,
                            settings.AWS_SECRET_ACCESS_KEY)
     bucket = conn.get_bucket(settings.AWS_STORAGE_BUCKET_NAME)
@@ -174,6 +177,7 @@ def get_all_files_list(root_dir, theme):
             editable = True
 
         file_path = item.name.replace(theme_folder, '').lstrip('/')
+        file_url = theme_base_url+file_path
         path_split = file_path.split('/')
         splits = len(path_split)
 
@@ -181,6 +185,7 @@ def get_all_files_list(root_dir, theme):
             s3_files_folders['contents'].append({
                     'name': path_split[0],
                     'path': file_path,
+                    'url': file_url,
                     'editable': editable})
         elif splits == 2:
             if not path_split[0] in s3_files_folders:
@@ -189,6 +194,7 @@ def get_all_files_list(root_dir, theme):
             s3_files_folders[path_split[0]]['contents'].append({
                     'name': path_split[1],
                     'path': file_path,
+                    'url': file_url,
                     'editable': editable})
         elif splits == 3:
             if not path_split[0] in s3_files_folders:
@@ -200,6 +206,7 @@ def get_all_files_list(root_dir, theme):
             s3_files_folders[path_split[0]][path_split[1]]['contents'].append({
                     'name': path_split[2],
                     'path': file_path,
+                    'url': file_url,
                     'editable': editable})
         elif splits == 4:
             if not path_split[0] in s3_files_folders:
@@ -214,6 +221,7 @@ def get_all_files_list(root_dir, theme):
             s3_files_folders[path_split[0]][path_split[1]][path_split[2]]['contents'].append({
                     'name': path_split[3],
                     'path': file_path,
+                    'url': file_url,
                     'editable': editable})
 
     return {theme: s3_files_folders}