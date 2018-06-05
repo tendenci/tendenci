161a2a77f421a06e0b9b0547923dd970f4f6ca10
Jenny Qian <jqian@tendenci.com>
2018-06-05 12:52:01 -0500

    updated hidden_code_block

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,7 @@ import os
 
 sys.path.append(os.path.abspath("../../"))
 sys.path.insert(0, os.path.abspath('.'))
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tendenci.settings")
 
 
