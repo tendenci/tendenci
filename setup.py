c81c1996b789237442a499f546e011122af77120
Jenny Qian <jqian@tendenci.com>
2019-04-05 09:51:45 -0500

    include python 3.7 support

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Internet :: WWW/HTTP :: WSGI',
