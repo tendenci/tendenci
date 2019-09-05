71ed00c09c4337d81197138c8d7402d0fb116427
Jenny Qian <jqian@tendenci.com>
2019-09-05 15:59:16 -0500

    remove outdated and unused jquery-ui files

--- tendenci/themes/t7-base/static/js/admin_menu.js
+++ tendenci/themes/t7-base/static/js/admin_menu.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#admin-bar li.right.search a').mouseenter(function() {
+    $('#admin-bar li.right.search a').on("mouseenter", function() {
         $('#admin-bar li.right.search .sub').css({'display':'block','right':'20px','left':'auto'});
         var enterText = $(this).html();
         $(this).text('Close Search');
