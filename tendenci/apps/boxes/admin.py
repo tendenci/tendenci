23f5f5ef59d7f7bcff6700d1ed048beef0c89266
Jenny Qian <jqian@tendenci.com>
2019-12-04 09:57:34 -0600

    fix FieldError: Related Field got invalid lookup: icontains

--- tendenci/apps/boxes/admin.py
+++ tendenci/apps/boxes/admin.py
@@ -11,7 +11,7 @@ from tendenci.apps.theme.templatetags.static import static
 
 class BoxAdmin(TendenciBaseModelAdmin):
     list_display = ('edit_link', 'pk', 'title', 'group', 'tags', 'short_content', 'admin_perms', 'status', 'position')
-    search_fields = ('title', 'content', 'tags', 'group', )
+    search_fields = ('title', 'content', 'tags', 'group__name', )
     list_editable = ['title', 'tags', 'status', 'position', 'group']
     list_filter = ('group', )
     fieldsets = (
