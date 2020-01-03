60591d00c8c56461a671a64fb0790be1042880bf
Jenny Qian <jqian@tendenci.com>
2020-01-03 13:39:36 -0600

    fix bad escape error

--- tendenci/apps/forms_builder/forms/utils.py
+++ tendenci/apps/forms_builder/forms/utils.py
@@ -63,7 +63,7 @@ def generate_email_subject(form, form_entry):
                 p = re.compile(r'(-\s+)?\[%s\]' % label, re.IGNORECASE)
             else:
                 p = re.compile(r'\[%s\]' % label, re.IGNORECASE)
-            subject = p.sub(value, subject)
+            subject = p.sub(re.escape(value), subject)
 
         # title
         p = re.compile(r'\[title\]', re.IGNORECASE)
