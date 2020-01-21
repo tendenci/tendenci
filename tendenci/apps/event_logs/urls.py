8ff5b2b112c9843124295c5c23e5b1789d77ea1a
Jenny Qian <jqian@tendenci.com>
2020-01-21 11:57:58 -0600

    comment out the non-working historical event logs summary report
    The historical event logs summary report uses the "source" field. This field was probably carried from T4 and it does not store anything.

--- tendenci/apps/event_logs/urls.py
+++ tendenci/apps/event_logs/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="event_logs"),
-    url(r'^reports/summary-historical/$', views.event_summary_historical_report, name='reports-events-summary-historical'),
+    #url(r'^reports/summary-historical/$', views.event_summary_historical_report, name='reports-events-summary-historical'),
     url(r'^reports/summary/$', views.event_summary_report, name='reports-events-summary'),
-    url(r'^reports/summary/source/([^/]+)/$', views.event_source_summary_report, name='reports-events-source'),
+    #url(r'^reports/summary/source/([^/]+)/$', views.event_source_summary_report, name='reports-events-source'),
     url(r'^reports/summary/application/(?P<application>[^/]+)/$', views.event_application_summary_report, name='reports-events-application'),
     url(r'^(?P<id>\d+)/$', views.index, name="event_log"),
     url(r'^search/$', views.search, name="event_log.search"),
