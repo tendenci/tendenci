b89fd1abd4e8a0b6ee4ba4132b00ddbf5eff9fb5
Jenny Qian <jqian@tendenci.com>
2020-02-10 12:36:27 -0600

    fix UnserializableContentError for /pages/feed/

--- tendenci/apps/pages/feeds.py
+++ tendenci/apps/pages/feeds.py
@@ -1,10 +1,12 @@
+from django.utils.translation import ugettext_lazy as _
+
 from tendenci.apps.rss.feedsmanager import SubFeed
 from tendenci.apps.site_settings.utils import get_setting
 from tendenci.apps.perms.utils import PUBLIC_FILTER
 from tendenci.apps.sitemaps import TendenciSitemap
-
 from tendenci.apps.pages.models import Page
-from django.utils.translation import ugettext_lazy as _
+from tendenci.apps.base.decorators import strip_control_chars
+
 
 class LatestEntriesFeed(SubFeed):
     title =  _('%(dname)s Latest Pages' % {'dname':get_setting('site','global','sitedisplayname')})
@@ -15,15 +17,18 @@ class LatestEntriesFeed(SubFeed):
         items = Page.objects.filter(**PUBLIC_FILTER).filter(syndicate=True).order_by('-create_dt')[:20]
         return items
 
+    @strip_control_chars
     def item_title(self, item):
         return item.title
 
+    @strip_control_chars
     def item_description(self, item):
         return item.content
 
     def item_pubdate(self, item):
         return item.create_dt
 
+    @strip_control_chars
     def item_link(self, item):
         return item.get_absolute_url()
 
