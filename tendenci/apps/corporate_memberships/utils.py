c25fe459f15f7fe3d6387af7525ee443719435f5
Jenny Qian <jqian@tendenci.com>
2020-03-13 21:19:18 -0500

    avoid to fill the bill_to with admin's info
    help desk 5693

--- tendenci/apps/corporate_memberships/utils.py
+++ tendenci/apps/corporate_memberships/utils.py
@@ -211,7 +211,7 @@ def corp_memb_inv_add(user, corp_memb, app=None, **kwargs):
         inv.object_id = corp_memb.id
         inv.title = corp_memb.corp_profile.name
 
-        if not user.is_anonymous:
+        if not (user.is_anonymous or user.is_superuser):
             inv.bill_to = '%s %s' % (user.first_name, user.last_name)
             inv.bill_to_first_name = user.first_name
             inv.bill_to_last_name = user.last_name
@@ -226,7 +226,7 @@ def corp_memb_inv_add(user, corp_memb, app=None, **kwargs):
                 inv.bill_to_last_name = cmc.last_name
                 inv.bill_to_email = cmc.email
             else:
-                inv.bill_to = corp_memb.name
+                inv.bill_to = corp_profile.name
 
         inv.bill_to_company = corp_profile.name
         inv.bill_to_address = corp_profile.address
