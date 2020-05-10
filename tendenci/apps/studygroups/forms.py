1a26fb5cb88b6a89fc160b86a7db57c1ab9f6414
Jenny Qian <jqian@tendenci.com>
2020-05-10 15:55:39 -0500

    fix a ValueError at studygroups edit
    The empty_permitted and use_required_attribute arguments may not both be True

--- tendenci/apps/studygroups/forms.py
+++ tendenci/apps/studygroups/forms.py
@@ -164,6 +164,7 @@ class OfficerForm(forms.ModelForm):
         exclude = ('study_group',)
 
     def __init__(self, study_group, *args, **kwargs):
+        kwargs.update({'use_required_attribute': False})
         super(OfficerForm, self).__init__(*args, **kwargs)
         self.fields.keyOrder = ['position', 'user', 'phone']
         # Initialize user.  Label depends on nullability.
