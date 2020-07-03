ea2ceb387774f4275c25c0ccf9f0d1ade5b84d0c
Jenny Qian <jqian@tendenci.com>
2020-07-03 16:02:53 -0500

    fix the enabled zip code field not displaying in the event registrant edit

--- tendenci/apps/events/views.py
+++ tendenci/apps/events/views.py
@@ -2604,8 +2604,9 @@ def registration_edit(request, reg8n_id=0, hash='', template_name="events/reg8n/
         fields=('salutation', 'first_name', 'last_name', 'mail_name', 'email',
                     'position_title', 'company_name', 'phone', 'address', 'city',
                     'state', 'zip', 'country', 'meal_option', 'comments')
-        fields = [field_name for field_name in fields if get_setting(
-                            'module', 'events', 'regform_%s_visible' % field_name)]
+        fields = [field_name for field_name in fields 
+                     if ((get_setting('module', 'events', 'regform_%s_visible' % field_name) and field_name != 'zip')\
+                        or (field_name == 'zip' and get_setting('module', 'events', 'regform_zip_code_visible')))]
         # use modelformset_factory for regular registration form
         RegistrantFormSet = modelformset_factory(
             Registrant, extra=0,
