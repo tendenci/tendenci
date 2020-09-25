d070851a75207a294087be0e4f1818f8e91cc61f
evanspaeder <espaeder@foresightpa.com>
2020-09-25 18:47:21 -0400

    fix file validation step

--- tendenci/apps/corporate_memberships/forms.py
+++ tendenci/apps/corporate_memberships/forms.py
@@ -922,7 +922,7 @@ class CorpMembershipUploadForm(forms.ModelForm):
         if not key:
             raise forms.ValidationError(_('Please specify the key to identify duplicates'))
 
-        file_content = upload_file.read()
+        file_content = upload_file.read().decode('utf-8')
         upload_file.seek(0)
         header_line_index = file_content.find('\n')
         header_list = ((file_content[:header_line_index]
