3fd85a57043bdb83b42cfb750a2bef2d84496cf1
Jenny Qian <jqian@tendenci.com>
2020-10-20 14:34:02 -0500

    pass entry_id if needed

--- tendenci/apps/forms_builder/forms/views.py
+++ tendenci/apps/forms_builder/forms/views.py
@@ -565,7 +565,8 @@ def form_detail(request, slug, template="forms/form_detail.html"):
 
             # default redirect
             if form.completion_url:
-                return HttpResponseRedirect(form.completion_url.strip())
+                completion_url = form.completion_url.strip().replace('[entry_id]', str(entry.id))
+                return HttpResponseRedirect(completion_url)
             return redirect("form_sent", form.slug)
 
     # set form's template to forms/base.html if no template or template doesn't exist
