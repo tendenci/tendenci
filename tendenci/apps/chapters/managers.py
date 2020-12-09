0f6a180c785a24ae89e83ece8c80fe33bc50e702
Jenny Qian <jqian@tendenci.com>
2020-12-09 17:01:04 -0600

    chapters module

---
+++ tendenci/apps/chapters/managers.py
@@ -0,0 +1,14 @@
+from tendenci.apps.perms.managers import TendenciBaseManager
+
+class ChapterManager(TendenciBaseManager):
+    """
+    Model Manager
+    """
+    pass
+
+
+class PositionManager(TendenciBaseManager):
+    """
+    Model Manager
+    """
+    pass
