402f11635a701f3bbb79b1f781687e425540751a
Jenny Qian <jqian@tendenci.com>
2021-02-23 13:18:23 -0600

    fix js issue not being able to add officers to chapters and committees

--- tendenci/themes/t7-base/static/js/dynamic-formset.js
+++ tendenci/themes/t7-base/static/js/dynamic-formset.js
@@ -1,9 +1,22 @@
-function updateElementIndex(el, prefix, ndx) {
-    var id_regex = new RegExp('(' + prefix + '-\\d+)');
-    var replacement = prefix + '-' + ndx;
-    if ($(el).attr("for")) $(el).attr("for", $(el).attr("for").replace(id_regex, replacement));
+function findReplaceElementIndex(el, id_regex, replacement) {
+	if ($(el).attr("for")) $(el).attr("for", $(el).attr("for").replace(id_regex, replacement));
     if (el.id) el.id = el.id.replace(id_regex, replacement);
     if (el.name) el.name = el.name.replace(id_regex, replacement);
+}
+
+function updateElementIndex(el, prefix, ndx) {
+	//console.log(el, ndx);
+	//console.log(prefix);
+    var id_regex = new RegExp('(' + prefix + '-\\d+)');
+    var replacement = prefix + '-' + ndx;
+	findReplaceElementIndex(el, id_regex, replacement);
+	id_regex = new RegExp('(' + prefix + '_\\d+)');
+    replacement = prefix + '_' + ndx;
+    findReplaceElementIndex(el, id_regex, replacement);
+/*    if ($(el).attr("for")) $(el).attr("for", $(el).attr("for").replace(id_regex, replacement));
+    if (el.id) el.id = el.id.replace(id_regex, replacement);
+    if (el.name) el.name = el.name.replace(id_regex, replacement);*/
+    //console.log(el, ndx);
 }
 
 function addForm(btn, prefix) {
@@ -18,7 +31,22 @@ function addForm(btn, prefix) {
         deleteForm(this, prefix);
     });
     $('#id_' + prefix + '-TOTAL_FORMS').val(formCount + 1);
-    return false;
+    return row;
+}
+
+function addFormWithCloned(btn, prefix, cloned) {
+    var formCount = parseInt($('#id_' + prefix + '-TOTAL_FORMS').val());
+    var row = cloned.clone(true).get(0);
+    $(row).removeAttr('id').insertAfter($('.dynamic-form:last')).children('.hidden').removeClass('hidden');
+    $(row).children().not(':last').children().each(function() {
+        updateElementIndex(this, prefix, formCount);
+        $(this).val('');
+    });
+    $(row).find('.delete-row').on("click", function() {
+        deleteForm(this, prefix);
+    });
+    $('#id_' + prefix + '-TOTAL_FORMS').val(formCount + 1);
+    return row;
 }
 
 function deleteForm(btn, prefix) {
@@ -28,6 +56,7 @@ function deleteForm(btn, prefix) {
     for (var i=0, formCount=forms.length; i<formCount; i++) {
         $(forms.get(i)).children().not(':last').children().each(function() {
             updateElementIndex(this, prefix, i);
+            //updateElementIndex(this, 'id_' + prefix, i);
         });
     }
     return false;
