c014893333b39cefebd4b44a575065a4d5af2293
Jenny Qian <jqian@tendenci.com>
2021-03-03 17:20:42 -0600

    fix an AttributeError

--- tendenci/apps/newsletters/utils.py
+++ tendenci/apps/newsletters/utils.py
@@ -60,16 +60,11 @@ def get_newsletter_connection():
 def is_newsletter_relay_set():
     connection = settings.NEWSLETTER_EMAIL_BACKEND
     if connection == "django_ses.SESBackend":
-        if settings.AWS_SES_ACCESS_KEY_ID:
-            ses_acc_key = True
-        if settings.AWS_SES_SECRET_ACCESS_KEY:
-            ses_sec_key = True
-        if ses_acc_key and ses_sec_key:
+        if hasattr(settings, 'AWS_SES_ACCESS_KEY_ID') and hasattr(settings, 'AWS_SES_SECRET_ACCESS_KEY'):
             return all([settings.AWS_SES_ACCESS_KEY_ID,
-                        settings.AWS_SES_SECRET_ACCESS_KEY])
-        else:
-            return all([settings.AWS_ACCESS_KEY_ID,
-                        settings.AWS_SECRET_ACCESS_KEY])
+                    settings.AWS_SES_SECRET_ACCESS_KEY]) 
+        return all([settings.AWS_ACCESS_KEY_ID,
+                    settings.AWS_SECRET_ACCESS_KEY])
     else:
         return all([settings.NEWSLETTER_EMAIL_HOST,
                     settings.NEWSLETTER_EMAIL_HOST_USER,
