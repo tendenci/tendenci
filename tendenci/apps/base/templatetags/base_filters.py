e5045a8d45b4ecd249bea3aee1682d58f341de98
Jenny Qian <jqian@tendenci.com>
2021-03-31 13:07:33 -0500

    fix AttributeError

--- tendenci/apps/base/templatetags/base_filters.py
+++ tendenci/apps/base/templatetags/base_filters.py
@@ -427,7 +427,9 @@ def make_range(value):
 
 @register.filter
 def underscore_space(value):
-    return value.replace("_", " ")
+    if isinstance(value, str):
+        return value.replace("_", " ")
+    return value
 
 @register.filter
 def format_string(value, arg):
