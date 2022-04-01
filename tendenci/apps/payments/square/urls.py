975672c794873fb7a053ad5820b79769bcfb82b8
Aaron Oxenrider <aoxenrider@benjaminandbond.com>
2022-04-01 10:35:22 -0400

    use re_path for new django version

--- tendenci/apps/payments/square/urls.py
+++ tendenci/apps/payments/square/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    url(r'^payonline/(?P<payment_id>\d+)/(?P<guid>[\d\w-]+)/$', views.pay_online, name="square.payonline"),
-    url(r'^thankyou/(?P<payment_id>\d+)/(?P<guid>[\d\w-]+)/$', views.thank_you, name="square.thank_you"),
-    url(r'^update-card/(?P<rp_id>\d+)/$', views.update_card, name="square.update_card"),
-    url(r'^giftcard/balance/$', views.ajax_giftcard_balance, name='square.gc_balance'),
-    url(r'^card/charge/$', views.ajax_charge_card, name='square.card_charge'),
+    re_path(r'^payonline/(?P<payment_id>\d+)/(?P<guid>[\d\w-]+)/$', views.pay_online, name="square.payonline"),
+    re_path(r'^thankyou/(?P<payment_id>\d+)/(?P<guid>[\d\w-]+)/$', views.thank_you, name="square.thank_you"),
+    re_path(r'^update-card/(?P<rp_id>\d+)/$', views.update_card, name="square.update_card"),
+    re_path(r'^giftcard/balance/$', views.ajax_giftcard_balance, name='square.gc_balance'),
+    re_path(r'^card/charge/$', views.ajax_charge_card, name='square.card_charge'),
 ]
 
