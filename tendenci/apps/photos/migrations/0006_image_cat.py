b12d36f973ec87d7f04d98479860da44671cd781
Jenny Qian <jqian@tendenci.com>
2022-07-19 18:23:48 -0500

    resolve delete not working

--- tendenci/apps/photos/migrations/0006_image_cat.py
+++ tendenci/apps/photos/migrations/0006_image_cat.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='image',
             name='cat',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='cat_photos', to='photos.photocategory', verbose_name='Category'),
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='cat_photos', to='photos.photocategory', verbose_name='Category'),
         ),
     ]
