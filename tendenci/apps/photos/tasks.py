ad73b59befe5bc46854670b6705941f1bb19d5f3
Jenny Qian <jqian@tendenci.com>
2022-11-04 12:24:29 -0500

    move zip_files to the export directory
    ... so that they can be auto-cleaned up later

--- tendenci/apps/photos/tasks.py
+++ tendenci/apps/photos/tasks.py
@@ -11,14 +11,19 @@ class ZipPhotoSetTask(celery.Task):
         """
         Compile all photos of a photo set into a single zip file.
         """
+        #TODO: make it work with default_storage (such as s3)
+        zip_file_name = f"set_{photo_set.id}.zip"
+        zip_file_directory = 'export/zip_files'
+        zip_file_path = os.path.join(settings.MEDIA_ROOT, zip_file_directory)
+        zip_file_full_path = os.path.join(zip_file_path, zip_file_name)
 
         #create zip files directory if it doesn't already exist
         try:
-            os.makedirs(os.path.join(settings.MEDIA_ROOT, 'zip_files'))
+            os.makedirs(zip_file_path)
         except OSError:
             pass
 
-        zfile = zipfile.ZipFile(os.path.join(settings.MEDIA_ROOT, 'zip_files', "set_%s.zip" % photo_set.id), 'w')
+        zfile = zipfile.ZipFile(zip_file_full_path, 'w')
         for image in photo_set.image_set.all():
             try:
                 zfile.write(image.image.path, os.path.basename(image.image.name))
@@ -27,4 +32,4 @@ class ZipPhotoSetTask(celery.Task):
                 pass
         zfile.close
 
-        return os.path.join(settings.MEDIA_URL, 'zip_files', "set_%s.zip" % photo_set.id)
+        return os.path.join(settings.MEDIA_URL, zip_file_directory, zip_file_name)
