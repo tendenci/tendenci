19007b95b2d47049870b432c1f6e90fed48d0a5d
Jenny Qian <jqian@tendenci.com>
2023-04-07 18:09:26 -0500

    update pull_bluevolt_exams

--- tendenci/apps/trainings/management/commands/pull_bluevolt_exams.py
+++ tendenci/apps/trainings/management/commands/pull_bluevolt_exams.py
@@ -1,6 +1,11 @@
+import traceback
+from logging import getLogger
+from datetime import datetime, date, timedelta
+
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
 from django.conf import settings
+from django.urls import reverse
 import requests
 
 
@@ -18,24 +23,32 @@ class Command(BaseCommand):
             default=None,
             help='import_id - specify import id')
 
-    def handle(self, *args, **options):
-        from datetime import datetime, date, timedelta
+    def import_bv_exams(self, import_id=None):
+        import dateutil.parser as dparser
         from tendenci.apps.trainings.models import BluevoltExamImport, Course, Transcript, Exam, Certification
         if not hasattr(settings, 'BLUEVOLT_API_KEY'):
             print('Bluevolt API is not set up. Exiting...')
             return 
         api_key = settings.BLUEVOLT_API_KEY
         api_endpoint_base_url = settings.BLUEVOLT_API_ENDPOINT_BASE_URL
-        enrollment_url = api_endpoint_base_url + 'GetUserCourseEnrollment'
-        course_url = api_endpoint_base_url + 'GetCourse'
-        user_url = api_endpoint_base_url + 'GetUser'
+        #enrollment_url = api_endpoint_base_url + 'GetUserCourseEnrollment'
+        # Currently, we have to use both v2 and v3 APIs to get what we need
+        score_url = api_endpoint_base_url + '/v3/modules/scores'
+        module_url = api_endpoint_base_url + '/v2/GetModule'
+        course_url = api_endpoint_base_url + '/v2/GetCourse'
+        user_url = api_endpoint_base_url + '/v2/GetUser'
         messages = []
         num_inserted = 0
 
-        import_id = options.get('import_id', None)
         if not import_id:
             date_from = date.today() - timedelta(days=8)
             date_to = date.today()
+            bv_import = BluevoltExamImport(
+                                    date_from=date_from,
+                                    date_to=date_to,
+                                    status_detail='Running',
+                                    run_start_date = datetime.now())
+            bv_import.save()
         else:
             [bv_import] = BluevoltExamImport.objects.filter(id=import_id)[:1] or [None]
             if not bv_import:
@@ -54,84 +67,96 @@ class Command(BaseCommand):
         # date_to = date.today()
         # ----------------
         
-        # STEP 1: Get a list of enrollments
+        # STEP 1: Get a list of scores
         payload = {'apiKey': api_key ,
-                   'startDate': date_from.strftime('%Y-%m-%d') ,
-                   'endDate': date_to.strftime('%Y-%m-%d'),
-                   'enrollmentStatus': 'Complete'}
-        r = requests.get(enrollment_url, params=payload)
+                   'completionDateStart': date_from.strftime('%Y-%m-%d') ,
+                   'completionDateEnd': date_to.strftime('%Y-%m-%d')}
+        r = requests.get(score_url, params=payload)
         if r.status_code == 200:
-            enrollment_results = r.json()
+            scores_results = r.json()
             messages.append(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' - STARTED')
-            for enrollment_result in enrollment_results:
-                course_id = enrollment_result['CourseId']
-                user_id = enrollment_result['UserId']
-                score = enrollment_result['Score']
-                completion_date = enrollment_result['CompletionDate']
-                # default certification_track
-                [certification_track] = Certification.objects.filter(enable_diamond=True)[:1] or [None]
-                
-                # STEP 2: Get course detail to course code
-                #TODO: Store the maps for CourseId and UserId so that we
-                #    don't have to retrieve the same courses and users
-                course_payload = {'apiKey': api_key,
-                                  'courseId': course_id}
-                course_r = requests.get(course_url, params=course_payload)
-                if course_r.status_code == 200:
-                    course_result = course_r.json()
-                    course_code = course_result['ExternalCourseCode']
-                    # given course_code, find the course
-                    [course] = Course.objects.filter(course_code=course_code)[:1] or [None]
-                    if not course:
-                        msg = f'Course with course code {course_code} does not exist!'
-                        print(msg)
-                        messages.append(msg)
-                        continue
+            # default certification_track
+            [certification_track] = Certification.objects.filter(enable_diamond=True)[:1] or [None]
 
-                    # STEP 3: Get user info to find username
-                    user_payload = {'apiKey': api_key,
-                                    'userID': user_id}
-                    user_r = requests.get(user_url, params=user_payload)
-                    if user_r.status_code == 200:
-                        user_result = user_r.json()[0]
-                        username = user_result['UserName']
-                        # given a username, find the user
-                        [user] = User.objects.filter(username=username)[:1] or [None]
-                        if not user:
-                            msg = f'User with username {username} does not exist!'
+            for scores_result in scores_results['Collection']:
+                module_id = scores_result['ModuleId']
+                user_id = scores_result['UserId']
+                score = scores_result['Score']
+                #print('score=', score)
+                completion_date = scores_result['CompletionDate']
+                # get course by module_id
+                module_payload = {'apiKey': api_key,
+                                  'id': module_id}
+                module_r = requests.get(module_url, params=module_payload)
+                if module_r.status_code == 200:
+                    module_result = module_r.json()
+                
+                    course_id = module_result['CourseInstanceId']
+
+                    # STEP 2: Get course detail to course code
+                    #TODO: Store the maps for CourseId and UserId so that we
+                    #    don't have to retrieve the same courses and users
+                    course_payload = {'apiKey': api_key,
+                                      'courseId': course_id}
+                    course_r = requests.get(course_url, params=course_payload)
+                    if course_r.status_code == 200:
+                        course_result = course_r.json()
+                        course_code = course_result['ExternalCourseCode']
+                        # given course_code, find the course
+                        [course] = Course.objects.filter(course_code=course_code)[:1] or [None]
+                        if not course:
+                            msg = f'Course with course code {course_code} does not exist!'
                             print(msg)
                             messages.append(msg)
                             continue
-                        
-                        # STEP 3: Insert into transcripts if not already in there
-
-                        # check if already exists
-                        [transcript] = Transcript.objects.filter(course=course, user=user)[:1] or [None]
-                        if not transcript:
-                            transcript = Transcript(
-                                         user=user,
-                                         course=course,
-                                         school_category=course.school_category,
-                                         credits=course.credits,
-                                         certification_track=certification_track,
-                                         status='approved'
-                                        )
-                            transcript.save()
-                            num_inserted += 1
-                            print(transcript, '... added')
-                            messages.append(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + f' - Transaction for Customer "{user.get_full_name()}" and Course "{course.name}" added')
-                        else:
-                            messages.append(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + f' - DUBLICATE TRANSACTION: Transaction for Customer "{user.get_full_name()}" and Course "{course.name}" already exists')
-                        if score and not transcript.exam: # Question: if score is null, does it mean course has not been completed yet?
-                            score = '%.0f' % float(score)
-                            exam = Exam(user=user,
-                                        course=course,
-                                        grade=score)
-                            exam.save()
-                            transcript.exam = exam
-                            transcript.parent_id = exam.id
-                            transcript.location_type = 'online'
-                            transcript.save(assign_diamond_number=False)
+    
+                        # STEP 3: Get user info to find username
+                        user_payload = {'apiKey': api_key,
+                                        'userID': user_id}
+                        user_r = requests.get(user_url, params=user_payload)
+                        if user_r.status_code == 200:
+                            user_result = user_r.json()[0]
+                            username = user_result['UserName']
+                            # given a username, find the user
+                            [user] = User.objects.filter(username=username)[:1] or [None]
+                            if not user:
+                                msg = f'User with username {username} does not exist!'
+                                print(msg)
+                                messages.append(msg)
+                                continue
+                            
+                            # STEP 3: Insert into transcripts if not already in there
+                            #         and user has completed the course          
+    
+                            # check if already exists
+                            [transcript] = Transcript.objects.filter(course=course, user=user)[:1] or [None]
+                            if completion_date and not transcript:
+                                if score:
+                                    score = '%.0f' % float(score)
+                                else:
+                                    score = 0
+                                exam = Exam(user=user,
+                                            course=course,
+                                            grade=score)
+                                exam.date = dparser.parse(completion_date)
+                                exam.save()
+                                transcript = Transcript(
+                                             exam=exam,
+                                             parent_id=exam.id,
+                                             location_type='online',
+                                             user=user,
+                                             course=course,
+                                             school_category=course.school_category,
+                                             credits=course.credits,
+                                             certification_track=certification_track,
+                                             status='approved'
+                                            )
+                                transcript.save()
+                                num_inserted += 1
+                                print(transcript, f'{transcript.id}... added')
+                                #messages.append(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + f' - Transaction for Customer "{user.get_full_name()}" and Course "{course.name}" added')
+                            #else:
+                                #messages.append(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + f' - DUBLICATE TRANSACTION: Transaction for Customer "{user.get_full_name()}" and Course "{course.name}" already exists')
 
             end_dt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             if num_inserted == 1:
@@ -150,12 +175,20 @@ class Command(BaseCommand):
                 bv_import.save()
                        
         else:
-            print(f'ERROR: Got {r.status_code} from API')              
+            print(f'ERROR: Got {r.status_code} from API')
+        print('Done!')             
 
-                
-                
-                                  
-                
-                
+    def handle(self, *args, **options):
+        from tendenci.apps.site_settings.utils import get_setting
+        logger = getLogger('import_bv_exams')
+        import_id = options.get('import_id', None)
+        try:
+            self.import_bv_exams(import_id=import_id)
+        except:
+            print(traceback.format_exc())
+            url = get_setting('site', 'global', 'siteurl')
+            if import_id:
+                url += reverse('admin:trainings_bluevoltexamimport_change', args=[import_id])
             
+            logger.error(f'Error importing training exams from BV {url}...\n\n{traceback.format_exc()}')
         
