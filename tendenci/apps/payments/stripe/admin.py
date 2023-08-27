3f58654571ae6a0529ca4352a406d946d98f72c3
Jenny Qian <jqian@tendenci.com>
2023-08-27 15:49:37 -0500

    stripe connect: add action to continue onboarding

--- tendenci/apps/payments/stripe/admin.py
+++ tendenci/apps/payments/stripe/admin.py
@@ -13,7 +13,7 @@ class StripeAccountAdmin(TendenciBaseModelAdmin):
     list_display = ('stripe_user_id', 'account_name',
                     'show_entity', 
                     'email', 'default_currency', 'scope',
-                     'status_detail')
+                     'status_detail', 'action')
     list_filter = ('status_detail',)
     search_fields = ('account_name', 'email')
     readonly_fields=('stripe_user_id', 'scope')
@@ -37,6 +37,13 @@ class StripeAccountAdmin(TendenciBaseModelAdmin):
         return '-'
     show_entity.short_description = _('Entity')
     show_entity.admin_order_field = 'entity__entity_name'
+    
+    @mark_safe
+    def action(self, instance):
+        if instance and instance.status_detail == 'not completed':
+            url = reverse('stripe_connect.acct_onboarding_refresh', args=[instance.id])
+            return f'<a href="{url}">Continue Onboarding</a>'
+        return '-'
 
 
 class ChargeAdmin(admin.ModelAdmin):
