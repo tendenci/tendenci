ece60783345867cc8b34576511b2f66266181041
Shandy Simmons <shandys@bixly.com>
2023-09-07 19:13:39 -0500

    Cleanup Zoom client code

--- tendenci/themes/t7-base/templates/events/include/zoom.js
+++ tendenci/themes/t7-base/templates/events/include/zoom.js
@@ -3,16 +3,12 @@
 
 <script type="text/javascript">
     ZoomMtg.setZoomJSLib('https://source.zoom.us/2.15.2/lib', '/av')
-
     ZoomMtg.preLoadWasm()
     ZoomMtg.prepareWebSDK()
     // loads language files, also passes any error messages to the ui
     ZoomMtg.i18n.load('en-US')
     ZoomMtg.i18n.reload('en-US')
 
-
-    // const client = ZoomMtgEmbedded.createClient()
-    let meetingSDKElement = document.getElementById('meetingSDKElement')
     document.getElementById('zmmtg-root').style.display = 'block';
     let meetingConfig = JSON.parse('{{ event.zoom_meeting_config|safe }}');
 
