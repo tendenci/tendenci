af0cc537a209384623b0cae2b6093e7867926c90
Rob Hills <rhills@medimorphosis.com.au>
2024-10-08 09:11:17 +0800

    Update directory expiration date after directory edit (#1313)
    * Update views.py
    
    Update directory.expiration_dt after editing a directory to realign to any changes to start date or duration
    
    * Update views.py
    
    Check for directory.activation_dt and requested duration before updating expiration_dt

--- tendenci/apps/directories/views.py
+++ tendenci/apps/directories/views.py
@@ -314,6 +314,14 @@ def edit(request, id, form_class=DirectoryForm, template_name="directories/edit.
                 except IOError:
                     pass
                     #directory.logo = None
+            
+            # set the expiration date in case activation date and/or requested_duration have changed
+            if directory.activation_dt and directory.requested_duration:
+                directory.expiration_dt = directory.activation_dt + timedelta(days=directory.requested_duration)
+            else:
+                # If activation date or requested duration are empty, expiration date should also be empty
+                directory.expiration_dt = None
+
             # update all permissions and save the model
             directory = update_perms_and_save(request, form, directory)
             form.save_m2m()
