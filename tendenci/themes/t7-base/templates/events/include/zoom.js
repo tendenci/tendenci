225562cdfd879179c10f69f87617744ebe933cf6
Jenny Qian <jqian@tendenci.com>
2024-10-18 13:17:14 -0500

    upgrade zoom to the latest SDK

--- tendenci/themes/t7-base/templates/events/include/zoom.js
+++ tendenci/themes/t7-base/templates/events/include/zoom.js
@@ -2,12 +2,11 @@
 {% block extra_body %}
 
 <script type="text/javascript">
-    ZoomMtg.setZoomJSLib('https://source.zoom.us/2.15.2/lib', '/av')
+    ZoomMtg.setZoomJSLib('https://source.zoom.us/3.9.0/lib', '/av')
     ZoomMtg.preLoadWasm()
     ZoomMtg.prepareWebSDK()
     // loads language files, also passes any error messages to the ui
     ZoomMtg.i18n.load('en-US')
-    ZoomMtg.i18n.reload('en-US')
 
     document.getElementById('zmmtg-root').style.display = 'block';
     let meetingConfig = JSON.parse('{{ event.zoom_meeting_config|safe }}');
