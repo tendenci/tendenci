85ff8cbfc7f78eb11414e172df047ff0554853c0
Jenny Qian <jqian@tendenci.com>
2024-11-13 12:36:42 -0600

    forum notifications update

--- tendenci/apps/forums/subscription.py
+++ tendenci/apps/forums/subscription.py
@@ -1,11 +1,11 @@
-
-
 from django.conf import settings
 from django.urls import reverse
 from django.core.validators import validate_email
 from django.template.loader import render_to_string
 from django.utils import translation
 from django.contrib.sites.models import Site
+from django.core.mail.message import EmailMessage
+
 from tendenci.apps.site_settings.utils import get_setting
 from tendenci.apps.base.utils import add_tendenci_footer
 from tendenci.apps.emails.models import Email
@@ -40,6 +40,18 @@ def get_email_message(user, **kwargs):
     return message.strip()
 
 
+def get_email_headers(sender_display_user):
+    headers = {}
+    sender = get_setting('site', 'global', 'siteemailnoreplyaddress')
+    site_name = get_setting('site', 'global', 'sitedisplayname')
+    if sender_display_user:
+        sender_display = f'{sender_display_user.get_full_name()} via {site_name} Forums' 
+    else:
+        sender_display = site_name
+    headers['From'] = f'{sender_display} <{sender}>'
+    return headers
+
+
 def notify_forum_subscribers(topic):
     forum = topic.forum
     qs = ForumSubscription.objects.exclude(user=topic.user).filter(forum=topic.forum)
@@ -51,7 +63,10 @@ def notify_forum_subscribers(topic):
             'topic': topic,
             'site_url': get_setting('site', 'global', 'siteurl'),
         }
-        send_notification(users, 'forum_subscription_email', context)
+        
+        headers = get_email_headers(topic.user)
+        
+        send_notification(users, 'forum_subscription_email', headers, context)
     subscriptions = qs.filter(type=ForumSubscription.TYPE_SUBSCRIBE)
     if subscriptions.count():
         users = (s.user for s in subscriptions.select_related('user'))
@@ -67,10 +82,10 @@ def notify_topic_subscribers(post):
                     'site_url': get_setting('site', 'global', 'siteurl'),
                     'is_new_topic': post == topic.head,
                     'post': post,
+                    'topic': topic,
                     'post_url': reverse('pybb:post', args=[post.id]),
                     'forum_url': reverse('pybb:forum', args=[topic.forum.id]),
                     }
-    from_email = settings.DEFAULT_FROM_EMAIL
 
     mails = tuple()
     if post == topic.head:
@@ -92,22 +107,35 @@ def notify_topic_subscribers(post):
             continue
         message = get_email_message(user, **context_vars)
         if message:
-            message = add_tendenci_footer(message, content_type="text")
-            mails += ((subject, message, from_email, [user.email]),)
+            message = add_tendenci_footer(message, content_type="html")
+            mails += ((subject, message, [user.email]),)
+
+    headers = get_email_headers(post.user)
 
     # Send mails
-    send_mass_mail(mails, fail_silently=True)
+    send_mass_mail_html(mails, headers)
 
     # Reactivate previous language
     translation.activate(old_lang)
 
 
-def send_notification(users, template, context={}):
+def send_mass_mail_html(datatuple, headers, content_type='html'):
+    """
+    Given a datatuple of (subject, message, from_email, recipient_list), send
+    each message to each recipient list.
+    """
+    sender = get_setting('site', 'global', 'siteemailnoreplyaddress')
+    for subject, message, recipient in datatuple:
+        email = EmailMessage(subject, message, sender, recipient, headers=headers)
+        email.content_subtype = content_type
+        email.send(fail_silently=True)
+
+
+def send_notification(users, template, headers, context={}):
     if not 'site' in context:
         context['site'] = Site.objects.get_current()
     context['site_url'] = get_setting('site', 'global', 'siteurl')
     old_lang = translation.get_language()
-    from_email = settings.DEFAULT_FROM_EMAIL
 
     subject = render_to_string('pybb/mail_templates/%s_subject.html' % template, context)
     # Email subject *must not* contain newlines
@@ -131,10 +159,10 @@ def send_notification(users, template, context={}):
 
         context['user'] = user
         message = render_to_string('pybb/mail_templates/%s_body.html' % template, context)
-        mails += ((subject, message, from_email, [user.email]),)
+        mails += ((subject, message, [user.email]),)
 
     # Send mails
-    send_mass_mail(mails, fail_silently=True)
+    send_mass_mail_html(mails, headers)
 
     # Reactivate previous language
     translation.activate(old_lang)
