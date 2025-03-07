dbfd766846ad551b72dcb783578f3660030c7f4c
Jenny Qian <jqian@tendenci.com>
2025-03-07 16:17:35 -0600

    show renew link for admin
    ... to renew a membership that is out of renewal period

--- tendenci/apps/memberships/models.py
+++ tendenci/apps/memberships/models.py
@@ -1583,7 +1583,8 @@ class MembershipDefault(TendenciBaseModel):
             renew = {}
 
         if status == 'active':
-            actions.update(renew)
+            if self.can_renew():
+                actions.update(renew)
             actions.update({
                 # '?action=pend': u'Make Pending',
                 expire_link: u'Expire Membership'})
@@ -1598,6 +1599,7 @@ class MembershipDefault(TendenciBaseModel):
                 disapprove_link: u'Disapprove',
                 expire_link: u'Expire Membership'})
         elif status == 'expired':
+            actions.update(renew)
             actions.update({
                 approve_link: u'Approve Membership'})
 
