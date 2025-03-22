a70f1ec84983a7232f55fb3c1c251f9ffcf63be5
Rob Hills <rhills@medimorphosis.com.au>
2025-03-23 05:18:52 +1100

    Improved logging, reduced memory footprint, added to nightly commands

--- tendenci/apps/event_logs/management/commands/prune_event_log_records.py
+++ tendenci/apps/event_logs/management/commands/prune_event_log_records.py
@@ -13,13 +13,16 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         from tendenci.apps.event_logs.models import EventLog
-        import datetime
+        from datetime import datetime, timedelta
 
         days_to_keep_eventlog = get_setting('site', 'global', 'keep_event_log_for_days')
 
         if days_to_keep_eventlog > 0:
-            cutoff_date = datetime.datetime.now() - datetime.timedelta(days = days_to_keep_eventlog)
-            deleted_count = EventLog.objects.filter(create_dt__lte=cutoff_date).delete()
-            print("{} EventLog records were deleted as they were older than {}".format(deleted_count, cutoff_date))
+            cutoff_date = datetime.now() - timedelta(days = days_to_keep_eventlog)
+            deleted_count = EventLog.objects.filter(create_dt__lte=cutoff_date).delete()[0]
+            if deleted_count > 0:
+                print("{} EventLog records were deleted as they were older than {}".format(deleted_count, cutoff_date.strftime("%d %b %Y %H:%M:%S")))
+            else:
+                print("{} No EventLog records older than {} so none deleted".format(deleted_count, cutoff_date.strftime("%d %b %Y %H:%M:%S")))
         else:
-            print("No EventLog record pruning skipped")
+            print("'keep_event_log_for_days' setting is 0, EventLog record pruning skipped")
