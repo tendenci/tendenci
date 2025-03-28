0ebc673ededcee85a7f202ebc6db262103574ce7
Rob Hills <rhills@medimorphosis.com.au>
2025-03-28 23:41:31 +0800

    Update prune_event_log_records.py
    Moving the 'keep_event_log_for_days' setting from Site Settings to Django Settings file

--- tendenci/apps/event_logs/management/commands/prune_event_log_records.py
+++ tendenci/apps/event_logs/management/commands/prune_event_log_records.py
@@ -1,12 +1,12 @@
 from django.core.management.base import BaseCommand
-from tendenci.apps.site_settings.utils import get_setting
+from django.conf import settings
 
 
 class Command(BaseCommand):
     """
-    If the Global site setting 'keep_event_log_for_days' is not zero,
+    If the Django setting 'KEEP_EVENT_LOG_FOR_DAYS' is not zero,
     then delete all event_logs_eventlog records older than 
-    'keep_event_log_for_days' days
+    'KEEP_EVENT_LOG_FOR_DAYS' days
 
     Usage: python manage.py prune_event_log_records
     """
@@ -15,14 +15,12 @@ class Command(BaseCommand):
         from tendenci.apps.event_logs.models import EventLog
         from datetime import datetime, timedelta
 
-        days_to_keep_eventlog = get_setting('site', 'global', 'keep_event_log_for_days')
-
-        if days_to_keep_eventlog > 0:
-            cutoff_date = datetime.now() - timedelta(days = days_to_keep_eventlog)
+        if settings.KEEP_EVENT_LOG_FOR_DAYS > 0:
+            cutoff_date = datetime.now() - timedelta(days = settings.KEEP_EVENT_LOG_FOR_DAYS)
             deleted_count = EventLog.objects.filter(create_dt__lte=cutoff_date).delete()[0]
             if deleted_count > 0:
                 print("{} EventLog records were deleted as they were older than {}".format(deleted_count, cutoff_date.strftime("%d %b %Y %H:%M:%S")))
             else:
                 print("{} No EventLog records older than {} so none deleted".format(deleted_count, cutoff_date.strftime("%d %b %Y %H:%M:%S")))
         else:
-            print("'keep_event_log_for_days' setting is 0, EventLog record pruning skipped")
+            print("'KEEP_EVENT_LOG_FOR_DAYS' setting is 0, EventLog record pruning skipped")
