369f3e85e8db3658d7036f99e7b3f935efd96523
Jenny Qian <jqian@tendenci.com>
2026-09-07 20:44:18 -0500

    careers update (continued)

--- tendenci/apps/careers/admin.py
+++ tendenci/apps/careers/admin.py
@@ -4,13 +4,14 @@ from django.utils.translation import gettext_lazy as _
 from tendenci.apps.perms.admin import TendenciBaseModelAdmin
 from tendenci.apps.careers.models import Career
 from tendenci.apps.careers.forms import CareerForm
+from tendenci.apps.site_settings.utils import get_setting
 
 
 class CareerAdmin(TendenciBaseModelAdmin):
     list_display = ['user',
                     'company',
-                    'position_title',
-                    'position_type',
+                    'position_title_str',
+                    'position_type_str',
                     'start_dt',
                     'end_dt',
                     'admin_perms',
@@ -50,5 +51,14 @@ class CareerAdmin(TendenciBaseModelAdmin):
     class Media:
         pass
 
+    def position_title_str(self, instance):
+        return instance.position_title
+    position_title_str.short_description = get_setting('module', 'careers', 'positiontitlelabel') or _('Position Title')
+    position_title_str.admin_order_field = 'position_title'
+    
+    def position_type_str(self, instance):
+        return instance.position_type
+    position_type_str.short_description = get_setting('module', 'careers', 'positiontypelabel') or _('Position Type')
+    position_type_str.admin_order_field = 'position_type'
 
 admin.site.register(Career, CareerAdmin)
