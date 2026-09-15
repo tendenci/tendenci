95a5f0c5ca86834a6250c995b61da167b7f40257
Jenny Qian <jqian@tendenci.com>
2026-09-15 13:03:26 -0500

    fix a migrate warning
      "Your models in app(s): 'photos' have changes that are not yet reflected in a migration, and so won't be applied."

--- tendenci/apps/photos/migrations/0001_initial.py
+++ tendenci/apps/photos/migrations/0001_initial.py
@@ -4,6 +4,7 @@ import tagging.fields
 import tendenci.apps.photos.models
 import tendenci.apps.user_groups.utils
 from django.conf import settings
+import django.utils.timezone
 import django.db.models.deletion
 import tendenci.apps.base.fields
 
@@ -149,7 +150,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('object_id', models.PositiveIntegerField()),
-                ('created_at', models.DateTimeField(default=datetime.datetime.now, verbose_name='created_at')),
+                ('created_at', models.DateTimeField(default=django.utils.timezone.now, verbose_name='created_at')),
                 ('content_type', models.ForeignKey(to='contenttypes.ContentType', on_delete=django.db.models.deletion.CASCADE)),
                 ('photo', models.ForeignKey(to='photos.Image', on_delete=django.db.models.deletion.CASCADE)),
             ],
